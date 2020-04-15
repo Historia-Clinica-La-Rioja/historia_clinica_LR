629cbee630eec2fc30584dd930ce433693965d51
Lucas Bergottini <lucas.bergottini@gmail.com>
2020-04-15 10:36:08 -0300

    tg-53 agregada annotation transactional para hacer roll back en caso de que falle un modulo del save

--- back-end/hospital-api/src/main/java/net/pladema/person/controller/PersonController.java
+++ back-end/hospital-api/src/main/java/net/pladema/person/controller/PersonController.java
@@ -12,6 +12,7 @@ import net.pladema.person.service.PersonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +43,7 @@ public class PersonController {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<BMPersonDto> addPerson(
             @RequestBody APersonDto personDto) throws URISyntaxException {
 
