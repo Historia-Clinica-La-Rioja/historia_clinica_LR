f25260c52a89dd6e3d00da3e161cbcf88ef798e1
Julio Rivera Laguna <julioriveralaguna@gmail.com>
2020-04-17 13:52:44 -0300

    tg-90 busqueda de paciente - componente table material para resultados con coincidencias

--- front-end/webapp/src/app/modules/pacientes/component/partial-matches-table/partial-matches-table.component.ts
+++ front-end/webapp/src/app/modules/pacientes/component/partial-matches-table/partial-matches-table.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
-  selector: 'app-partial-matches-table',
-  templateUrl: './partial-matches-table.component.html',
-  styleUrls: ['./partial-matches-table.component.scss']
+	selector: 'app-partial-matches-table',
+	templateUrl: './partial-matches-table.component.html',
+	styleUrls: ['./partial-matches-table.component.scss']
 })
 export class PartialMatchesTableComponent implements OnInit {
 
-  constructor() { }
+	displayedColumns: string[] = ['ID', 'Nombres', 'Apellidos', 'Sexo', 'F. Nac', 'DNI', 'Estado', 'Coincidencia'];
+	dataSource = new MatTableDataSource([
+		{
+			id: '123',
+			person: {
+				name: 'Juan',
+				lastName: 'Perez',
+				gender: {
+					description: 'masculino'
+				},
+				birthDate: '01/01/2020',
+				identificationNumber: '12345678',
+			},
+			type: {
+				description: 'Validado'
+			},
+			ranking: '80%'
+		}
+	]);
 
-  ngOnInit(): void {
-  }
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
+
+	getPatient(patient): void {
+		//TODO display popup
+	}
 
 }
