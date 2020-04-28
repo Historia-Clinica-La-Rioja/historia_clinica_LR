ddbf28d1fd3c92400089e34993fde8c9e0ed3029
Julio Rivera Laguna <julioriveralaguna@gmail.com>
2020-04-28 10:28:51 -0300

    tg-66 perfil de paciente - estructura y redireccionamiento desde busqueda rapida

--- front-end/webapp/src/app/modules/api-rest/services/person.service.ts
+++ front-end/webapp/src/app/modules/api-rest/services/person.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from "rxjs";
 import { environment } from "@environments/environment";
 import { HttpClient } from "@angular/common/http";
-import { BasicPatientDto, PersonBasicDataResponseDto } from '@api-rest/api-model';
+import { BasicPatientDto, PersonalInformationDto, PersonBasicDataResponseDto } from '@api-rest/api-model';
 
 @Injectable({
 	providedIn: 'root'
@@ -15,6 +15,11 @@ export class PersonService {
 	getRenaperPersonData(params): Observable<PersonBasicDataResponseDto> {
 		let url = `${environment.apiBase}/renaper/searchPerson`;
 		return this.http.get<PersonBasicDataResponseDto>(url, { params: params });
+	}
+
+	getPersonalInformation<PersonalInformationDto>(personId): Observable<PersonalInformationDto> {
+		let url = `${environment.apiBase}/person/${personId}/personalInformation`;
+		return this.http.get<PersonalInformationDto>(url);
 	}
 
 }
--- front-end/webapp/src/app/modules/pacientes/routes/profile/profile.component.ts
+++ front-end/webapp/src/app/modules/pacientes/routes/profile/profile.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from "rxjs";
-import { PatientBasicData } from "../../../presentation/components/patient-card/patient-card.component";
-import { BasicPatientDto } from "@api-rest/api-model";
+import { BasicPatientDto, PersonalInformationDto } from "@api-rest/api-model";
 import { map } from "rxjs/operators";
 import { PatientService } from "@api-rest/services/patient.service";
 import { MapperService } from "../../../presentation/services/mapper.service";
 import { ActivatedRoute } from "@angular/router";
+import { PersonService } from "@api-rest/services/person.service";
+import { PatientBasicData } from 'src/app/modules/presentation/components/patient-card/patient-card.component';
 
 @Component({
 	selector: 'app-profile',
@@ -15,11 +16,13 @@ import { ActivatedRoute } from "@angular/router";
 export class ProfileComponent implements OnInit {
 
 	public patient$: Observable<PatientBasicData>;
+	public person: PersonalInformationDto;
 
 	constructor(
 		private patientService: PatientService,
 		private mapperService: MapperService,
-		private route: ActivatedRoute) {
+		private route: ActivatedRoute,
+		private personService: PersonService) {
 	}
 
 	ngOnInit(): void {
@@ -28,9 +31,12 @@ export class ProfileComponent implements OnInit {
 			(params) => {
 				let id = Number(params.get('id'));
 				this.patient$ = this.patientService.getPatientBasicData<BasicPatientDto>(id).pipe(
-					map(patient => this.mapperService.toPatientBasicData(patient))
+					map(patient => {
+						this.personService.getPersonalInformation<PersonalInformationDto>(patient.person.id).subscribe(
+							data => this.person = data);
+						return this.mapperService.toPatientBasicData(patient);
+					})
 				);
-
 			}
 		);
 	}
