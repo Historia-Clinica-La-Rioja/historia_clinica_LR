0843835123b1fe00b8c571b4ed02a76fded22fe4
Lucas Telesco <telescolucas19@gmail.com>
2020-05-08 14:52:42 -0300

    tg-372 Corrige color de fondo para Estado Permanente

--- front-end/webapp/src/app/modules/presentation/components/patient-type-logo/patient-type-logo.component.ts
+++ front-end/webapp/src/app/modules/presentation/components/patient-type-logo/patient-type-logo.component.ts
@@ -1,35 +1,39 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-patient-type-logo',
-  templateUrl: './patient-type-logo.component.html',
-  styleUrls: ['./patient-type-logo.component.scss']
+	selector: 'app-patient-type-logo',
+	templateUrl: './patient-type-logo.component.html',
+	styleUrls: ['./patient-type-logo.component.scss']
 })
 
 export class PatientTypeLogoComponent implements OnInit {
 
 
-  @Input() patientTypeData: PatientTypeData;
-  public color: string;
-  constructor() { }
+	@Input() patientTypeData: PatientTypeData;
+	public color: string;
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
-  ngOnChanges():void {
-	this.color = colorsById[this.patientTypeData?.id]?.color;
-  }
+	ngOnChanges(): void {
+		this.color = colorsById[this.patientTypeData?.id]?.color;
+	}
 }
 
-enum PatientTypeColors{
+enum PatientTypeColors {
 	ACTIVO = '#009B68',
-	TEMPORARIO = '#D34444'
+	TEMPORARIO = '#D34444',
+	PERMANENTE = '#1976d2'
 }
 
-const colorsById = { 2:{color: PatientTypeColors.ACTIVO},
-				     3:{color: PatientTypeColors.TEMPORARIO}};
+const colorsById = {
+	1: { color: PatientTypeColors.PERMANENTE },
+	2: { color: PatientTypeColors.ACTIVO },
+	3: { color: PatientTypeColors.TEMPORARIO },
+};
 
-export class PatientTypeData{
+export class PatientTypeData {
 	id: number;
 	description: string;
 }
