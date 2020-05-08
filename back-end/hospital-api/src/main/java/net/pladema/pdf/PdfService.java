a87f0d192ed6cdaaafa2c8b47a89398d4e174ba2
Ezequiel Trapani <etrapani04@gmail.com>
2020-05-08 14:28:57 -0300

    tg-381. [BE] Eliminar diagnostico de ingreso en pdf de nota de evolución

--- back-end/hospital-api/src/main/java/net/pladema/pdf/PdfService.java
+++ back-end/hospital-api/src/main/java/net/pladema/pdf/PdfService.java
@@ -4,7 +4,6 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
-import net.pladema.internation.controller.documents.anamnesis.AnamnesisController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.InputStreamResource;
@@ -15,17 +14,14 @@ import org.springframework.stereotype.Component;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Component
 public class PdfService {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AnamnesisController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(PdfService.class);
 
     public static final String OUTPUT = "Output -> {}";
 
@@ -43,29 +39,24 @@ public class PdfService {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
         String formattedDateTime = LocalDateTime.now().format(formatter);
         name = name + "_" + formattedDateTime;
-        File htmlFile = new File(name + ".html");
         File pdfFile = new File(name + ".pdf");
         ResponseEntity<InputStreamResource> response;
         try {
-            htmlFile.createNewFile();
-            FileOutputStream fos = new FileOutputStream(htmlFile);
-            fos.write(templateEngine.process(templateName, ctx).getBytes());
-            fos.close();
-            pdfFile.createNewFile();
-            FileOutputStream fosPDF = new FileOutputStream(name + ".pdf");
+            OutputStream file = new FileOutputStream(pdfFile);
             Document document = new Document();
-            PdfWriter writer = PdfWriter.getInstance(document, fosPDF);
+            PdfWriter writer = PdfWriter.getInstance(document, file);
             document.open();
-            XMLWorkerHelper.getInstance().parseXHtml(writer, document, new FileInputStream(htmlFile));
+            InputStream is = new ByteArrayInputStream(templateEngine.process(templateName, ctx).getBytes());
+            XMLWorkerHelper.getInstance().parseXHtml(writer, document, is);
             document.close();
-            htmlFile.delete();
+            file.close();
             InputStreamResource resource = new InputStreamResource(new FileInputStream(pdfFile));
+            resource.getDescription();
             response = ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + pdfFile.getName())
                     .contentType(MediaType.APPLICATION_PDF).contentLength(pdfFile.length()).body(resource);
             pdfFile.delete();
         } catch (Exception e){
-            htmlFile.delete();
             pdfFile.delete();
             throw e;
         }
