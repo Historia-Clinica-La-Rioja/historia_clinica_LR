ed8de166822154c095dddc9fb698f26accb8e659
Nicolino Ayerdi <nicolinoayerdi@gmail.com>
2020-05-11 14:50:21 -0300

    tg-395 quito obligatoriedad de campos en nota de evolucion

--- front-end/webapp/src/app/modules/internacion/components/nota-evolucion-form/nota-evolucion-form.component.ts
+++ front-end/webapp/src/app/modules/internacion/components/nota-evolucion-form/nota-evolucion-form.component.ts
@@ -43,24 +43,24 @@ export class NotaEvolucionFormComponent implements OnInit {
 
 		this.form = this.formBuilder.group({
 			anthropometricData: this.formBuilder.group({
-				bloodType: [null, Validators.required],
-				height: [null, Validators.required],
-				weight: [null, Validators.required],
+				bloodType: [null],
+				height: [null],
+				weight: [null],
 			}),
 			vitalSigns: this.formBuilder.group({
-				heartRate: [null, Validators.required],
-				respiratoryRate: [null, Validators.required],
-				temperature: [null, Validators.required],
-				bloodOxygenSaturation: [null, Validators.required],
-				systolicBloodPressure: [null, Validators.required],
-				diastolicBloodPressure: [null, Validators.required],
+				heartRate: [null],
+				respiratoryRate: [null],
+				temperature: [null],
+				bloodOxygenSaturation: [null],
+				systolicBloodPressure: [null],
+				diastolicBloodPressure: [null],
 			}),
 			observations: this.formBuilder.group ({
-				currentIllnessNote: [null, Validators.required],
-				physicalExamNote: [null, Validators.required],
-				studiesSummaryNote: [null, Validators.required],
-				evolutionNote: [null, Validators.required],
-				clinicalImpressionNote: [null, Validators.required],
+				currentIllnessNote: [null],
+				physicalExamNote: [null],
+				studiesSummaryNote: [null],
+				evolutionNote: [null],
+				clinicalImpressionNote: [null],
 				otherNote: [null]
 			})
 		});
@@ -91,30 +91,38 @@ export class NotaEvolucionFormComponent implements OnInit {
 	}
 
 	private buildEvolutionNoteDto(): EvolutionNoteDto {
+		const formValues = this.form.value;
 		return {
 			confirmed: true,
 			allergies: this.allergies,
-			anthropometricData: {
-				bloodType: {
-					id: this.form.value.anthropometricData.bloodType.id,
-					value: this.form.value.anthropometricData.bloodType.description
-				},
-				height: {id: null, value: this.form.value.anthropometricData.height},
-				weight: {id: null, value: this.form.value.anthropometricData.weight},
-				bmi: null,
+			anthropometricData: isNull(formValues.anthropometricData) ? undefined : {
+				bloodType: formValues.bloodType ? {
+					id: formValues.anthropometricData.bloodType.id,
+					value: formValues.anthropometricData.bloodType.description
+				} : undefined,
+				height: getValue(formValues.anthropometricData.height),
+				weight: getValue(formValues.anthropometricData.weight),
 			},
 			diagnosis: this.diagnosticos,
 			inmunizations: this.inmunizations,
-			notes: this.form.value.observations,
-			vitalSigns: {
-				bloodOxygenSaturation: {id: null, value: this.form.value.vitalSigns.bloodOxygenSaturation},
-				diastolicBloodPressure: {id: null, value: this.form.value.vitalSigns.diastolicBloodPressure},
-				heartRate: {id: null, value: this.form.value.vitalSigns.heartRate},
-				respiratoryRate: {id: null, value: this.form.value.vitalSigns.respiratoryRate},
-				systolicBloodPressure: {id: null, value: this.form.value.vitalSigns.systolicBloodPressure},
-				temperature: {id: null, value: this.form.value.vitalSigns.temperature},
+			notes: isNull(formValues.observations) ? undefined : formValues.observations,
+			vitalSigns: isNull(formValues.vitalSigns) ? undefined : {
+				bloodOxygenSaturation: getValue(formValues.vitalSigns.bloodOxygenSaturation),
+				diastolicBloodPressure: getValue(formValues.vitalSigns.diastolicBloodPressure),
+				heartRate: getValue(formValues.vitalSigns.heartRate),
+				respiratoryRate: getValue(formValues.vitalSigns.respiratoryRate),
+				systolicBloodPressure: getValue(formValues.vitalSigns.systolicBloodPressure),
+				temperature: getValue(formValues.vitalSigns.temperature)
 			}
 		};
+
+		function isNull(formGroupValues: any): boolean {
+			return Object.values(formGroupValues).every(el => el === null);
+		}
+
+		function getValue(controlValue: any) {
+			return controlValue ? { value: controlValue } : undefined;
+		}
 	}
 
 	back(): void {
