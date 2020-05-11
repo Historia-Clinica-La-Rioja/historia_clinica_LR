e1d5981b04a9e05d4f0811fb96f0b8a74e0b3d4c
Lucas Bergottini <lucas.bergottini@gmail.com>
2020-05-11 15:17:08 -0300

    tg-322 acomodado servicio externo llama a servicio correctamente

--- back-end/hospital-api/src/main/java/net/pladema/staff/controller/service/HealthcareProfessionalExternalServiceImpl.java
+++ back-end/hospital-api/src/main/java/net/pladema/staff/controller/service/HealthcareProfessionalExternalServiceImpl.java
@@ -1,9 +1,6 @@
 package net.pladema.staff.controller.service;
 
 import net.pladema.internation.controller.internment.dto.HealthCareProfessionalGroupDto;
-import net.pladema.internation.controller.internment.mapper.HealthCareProfessionalGroupMapper;
-import net.pladema.internation.repository.core.HealthcareProfessionalGroupRepository;
-import net.pladema.internation.repository.core.entity.HealthcareProfessionalGroup;
 import net.pladema.internation.service.internment.impl.InternmentEpisodeServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,22 +11,16 @@ public class HealthcareProfessionalExternalServiceImpl implements HealthcareProf
 
     private static final Logger LOG = LoggerFactory.getLogger(InternmentEpisodeServiceImpl.class);
 
-    private final HealthcareProfessionalGroupRepository healthcareProfessionalGroupRepository;
+    private final HealthcareProfessionalService healthcareProfessionalService;
 
-    private final HealthCareProfessionalGroupMapper healthCareProfessionalGroupMapper;
-
-    public HealthcareProfessionalExternalServiceImpl(HealthcareProfessionalGroupRepository healthcareProfessionalGroupRepository, HealthCareProfessionalGroupMapper healthCareProfessionalGroupMapper) {
-        this.healthcareProfessionalGroupRepository = healthcareProfessionalGroupRepository;
-        this.healthCareProfessionalGroupMapper = healthCareProfessionalGroupMapper;
+    public HealthcareProfessionalExternalServiceImpl(HealthcareProfessionalService healthcareProfessionalService){
+        this.healthcareProfessionalService = healthcareProfessionalService;
     }
 
     @Override
     public HealthCareProfessionalGroupDto addHealthcareProfessionalGroup(Integer internmentEpisodeId, Integer healthcareProfessionalId) {
         LOG.debug("Input parameters -> internmentEpisode {}, healthcareProfessionalId {}", internmentEpisodeId, healthcareProfessionalId);
-        HealthcareProfessionalGroup healthcareProfessionalGroupToSave = new HealthcareProfessionalGroup(internmentEpisodeId, healthcareProfessionalId);
-        healthcareProfessionalGroupToSave.setResponsible(true);
-        HealthcareProfessionalGroup saved = healthcareProfessionalGroupRepository.save(healthcareProfessionalGroupToSave);
-        HealthCareProfessionalGroupDto result = healthCareProfessionalGroupMapper.toHealthcareProfessionalGroupDto(saved);
+        HealthCareProfessionalGroupDto result = healthcareProfessionalService.addHealthcareProfessionalGroup(internmentEpisodeId, healthcareProfessionalId);
         LOG.debug("Output -> {}", result);
         return result;
     }
