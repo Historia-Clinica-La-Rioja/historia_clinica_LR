cc763280b16f9a32755912f4f901e991ea4b078b
Ezequiel Trapani <etrapani04@gmail.com>
2020-06-05 09:48:36 -0300

    tg-714. [P1] Error 400 cuando se quiere borrar un diagnóstico con epicrisis creada

--- front-end/webapp/src/app/modules/internacion/components/main-diagnosis-summary/main-diagnosis-summary.component.ts
+++ front-end/webapp/src/app/modules/internacion/components/main-diagnosis-summary/main-diagnosis-summary.component.ts
@@ -13,6 +13,7 @@ import { Router } from '@angular/router';
 export class MainDiagnosisSummaryComponent implements OnInit {
 
 	@Input() internmentEpisodeId: number;
+	@Input() editable: boolean = true;
 
 	mainDiagnosticosSummary = DIAGNOSTICO_PRINCIPAL;
 	mainDiagnosis$: Observable<HealthConditionDto>;
