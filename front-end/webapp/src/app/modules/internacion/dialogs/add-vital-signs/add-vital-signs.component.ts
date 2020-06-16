5e8bba41bdeec802c40f21da6dee4e065b0f9ac6
Nicolino Ayerdi <nicolinoayerdi@gmail.com>
2020-06-16 12:16:38 -0300

    tg-819 fix popup de signos vitales guarda con fecha y hora actual

--- front-end/webapp/src/app/modules/internacion/dialogs/add-vital-signs/add-vital-signs.component.ts
+++ front-end/webapp/src/app/modules/internacion/dialogs/add-vital-signs/add-vital-signs.component.ts
@@ -48,7 +48,7 @@ export class AddVitalSignsComponent implements OnInit {
 	}
 
 	setVitalSignEffectiveTime(newEffectiveTime: Moment, formField: string): void {
-		(<FormGroup>(<FormGroup>this.form.controls['vitalSigns']).controls[formField]).controls['effectiveTime'].setValue(newEffectiveTime);
+		(this.form.controls[formField] as FormGroup).controls['effectiveTime'].setValue(newEffectiveTime);
 	}
 
 	submit() {
