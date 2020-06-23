7617d2e7f3a185f81a8e104704287263b76ab711
Javier Dottori <javier.dottori@gmail.com>
2020-06-23 16:15:16 -0300

    tg-860 Mejora en Build Docker

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -5,9 +5,11 @@ BASEDIR=$(dirname "$0")"/.."
 # supone que ya se corrio build-pack
 rm ci/docker/nginx-jar/*.jar || true
 cp *.jar ci/docker/nginx-jar/
-rm -r ci/docker/nginx-jar/front-end-dist || true
+
+rm -r ci/docker/nginx-jar/front-end-dist 2> /dev/null || true
 cp -r front-end/webapp/dist/ ci/docker/nginx-jar/front-end-dist
-rm -r ci/docker/nginx-jar/backoffice-dist || true
+
+rm -r ci/docker/nginx-jar/backoffice-dist 2> /dev/null || true
 cp -r front-end/backoffice/build/ ci/docker/nginx-jar/backoffice-dist
 
 cd ci/docker/nginx-jar/
