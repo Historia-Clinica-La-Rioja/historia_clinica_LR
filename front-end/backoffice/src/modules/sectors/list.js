b21baf686cbcfceb92808b42905d7533d17db802
Luis Manterola <manterolaluis@gmail.com>
2020-06-24 16:51:25 -0300

    tg-729 filtrar instituciones en listado de sectores

--- front-end/backoffice/src/modules/sectors/list.js
+++ front-end/backoffice/src/modules/sectors/list.js
@@ -4,10 +4,22 @@ import {
     Datagrid,
     ReferenceField,
     TextField,
+    Filter,
+    ReferenceInput,
+    SelectInput
 } from 'react-admin';
 
+const SectorFilter = props =>(
+    <Filter {...props}>
+        <ReferenceInput source="institutionId" reference="institutions" alwaysOn allowEmpty={false}>
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
+
+
 const SectorList = props => (
-    <List {...props} hasCreate={false} >
+    <List {...props} hasCreate={false} filters={<SectorFilter />} >
         <Datagrid rowClick="show">
             <TextField source="description" />
             <ReferenceField source="institutionId" reference="institutions">
