6e181f27ae6b6a4b22ce5e9ffe4e641ce2898350
Luis Manterola <manterolaluis@gmail.com>
2020-07-02 17:23:24 -0300

    tg-951 [FE] Adaptar opciones en menú de backoffice

--- front-end/backoffice/src/App.js
+++ front-end/backoffice/src/App.js
@@ -31,40 +31,53 @@ const dataProvider = springbootRestProvider('/backoffice', {
 
 const App = () => {
     const oauth = appInfoProvider.getInfo().oauthConfig.enabled;
+
+    const resourcesAdminInstitucional = [
+        <Resource name="institutions" {...institutions} />,
+        <Resource name="sectors" {...sectors}/>,
+        <Resource name="clinicalspecialtysectors" {...clinicalspecialtysectors}/>,
+        <Resource name="rooms" {...rooms}/>,
+        <Resource name="beds" {...beds}/>,
+    ];
+
+    const resourcesAdminRoot = [
+        <Resource name="provinces" />,
+        <Resource name="cities" {...cities} />,
+        <Resource name="departments" {...departments} />,
+        <Resource name="addresses" {...addresses}/>,
+        <Resource name="clinicalspecialties" {...clinicalspecialties}/>,
+        <Resource name="bedcategories" />,
+        <Resource name="healthcareprofessionals" {...healthcareprofessionals}/>,
+        <Resource name="professionalspecialties" {...professionalspecialties}/>,
+        <Resource name="educationtypes"/>,
+        <Resource name="people" {...people}/>,
+        <Resource name="users" {...users}/>,
+        <Resource name="password-reset" {...passwordReset}/>,
+        <Resource name="roles" />,
+        <Resource name="internmentepisodes" />,
+        <Resource name="healthcareprofessionalspecialties" {...healthcareprofessionalspecialties}/>,
+    ];
+
     return <Admin title="Historia de salud integrada"
-    customRoutes={customRoutes}
-    dataProvider={dataProvider}
-    authProvider={authProvider}
-    i18nProvider={i18nProvider}
-    loginPage={oauth ? OauthLoginPage : undefined}
-    dashboard={Dashboard}
+                  customRoutes={customRoutes}
+                  dataProvider={dataProvider}
+                  authProvider={authProvider}
+                  i18nProvider={i18nProvider}
+                  loginPage={oauth ? OauthLoginPage : undefined}
+                  dashboard={Dashboard}
     >
         {
-        permissions => permissions.hasAnyAssignment(
-            {role: 'ROOT', institutionId: -1},
-            {role: 'ADMINISTRADOR', institutionId: -1}) ?
-        [
-            <Resource name="cities" {...cities} />,
-            <Resource name="departments" {...departments} />,
-            <Resource name="institutions" {...institutions} />,
-            <Resource name="provinces" />,
-            <Resource name="addresses" {...addresses}/>,
-            <Resource name="sectors" {...sectors}/>,
-            <Resource name="clinicalspecialties" {...clinicalspecialties}/>,
-            <Resource name="clinicalspecialtysectors" {...clinicalspecialtysectors}/>,
-            <Resource name="rooms" {...rooms}/>,
-            <Resource name="beds" {...beds}/>,
-            <Resource name="bedcategories" />,
-            <Resource name="healthcareprofessionals" {...healthcareprofessionals}/>,
-            <Resource name="professionalspecialties" {...professionalspecialties}/>,
-            <Resource name="healthcareprofessionalspecialties" {...healthcareprofessionalspecialties}/>,
-            <Resource name="educationtypes"/>,
-            <Resource name="people" {...people}/>,
-            <Resource name="users" {...users}/>,
-            <Resource name="password-reset" {...passwordReset}/>,
-            <Resource name="roles" />,
-            <Resource name="internmentepisodes" />,
-        ] : [<span />]
+            permissions => permissions.hasAnyAssignment(
+                {role: 'ROOT', institutionId: -1},
+                {role: 'ADMINISTRADOR', institutionId: -1}) ?
+                [
+                    resourcesAdminRoot,
+                    resourcesAdminInstitucional
+                ]
+                :
+                [
+                    resourcesAdminInstitucional
+                ]
         }
     </Admin>
 }
