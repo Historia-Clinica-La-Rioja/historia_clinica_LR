847bdfa35fe11df5f4fc2615eef3f5a8b761e9b3
Lucas Bergottini <lucas.bergottini@gmail.com>
2020-07-03 20:13:08 -0300

    fix al tg-961, correcion en typo a nombre de columna, agrega entidad asociada a la tabla creada en tal commit

---
+++ back-end/hospital-api/src/main/java/net/pladema/clinichistory/outpatient/repository/domain/OutpatientConsultation.java
@@ -0,0 +1,52 @@
+package net.pladema.clinichistory.outpatient.repository.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import net.pladema.clinichistory.hospitalization.repository.listener.InternationAuditableEntity;
+import net.pladema.clinichistory.hospitalization.repository.listener.InternationListener;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "outpatient_consultation")
+@EntityListeners(InternationListener.class)
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class OutpatientConsultation extends InternationAuditableEntity {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 2944833030892268840L;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "patient_id", nullable = false)
+    private Integer patientId;
+
+    @Column(name = "clinical_specialty_id", nullable = false)
+    private Integer clinicalSpecialtyId;
+
+    @Column(name = "institution_id", nullable = false)
+    private Integer institutionId;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "document_id", nullable = false)
+    private Long documentId;
+
+    @Column(name = "doctor_id", nullable = false)
+    private Integer doctorId;
+
+    @Column(name = "billable", nullable = false)
+    private Boolean billable;
+}
