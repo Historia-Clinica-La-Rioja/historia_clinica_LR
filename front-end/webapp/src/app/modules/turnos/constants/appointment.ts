dc92b452987b1e8dafdc6a7f57e211de043c1ed3
Julio Rivera Laguna <julioriveralaguna@gmail.com>
2020-07-23 15:53:58 -0300

    tg-1155 Duracion de turnos divisores de 60 minutos

--- front-end/webapp/src/app/modules/turnos/constants/appointment.ts
+++ front-end/webapp/src/app/modules/turnos/constants/appointment.ts
@@ -9,32 +9,8 @@ export const APPOINTMENT_DURATIONS = [
 		description: '20 minutos'
 	},
 	{
-		id: 25,
-		description: '25 minutos'
-	},
-	{
 		id: 30,
 		description: '30 minutos'
-	},
-	{
-		id: 35,
-		description: '35 minutos'
-	},
-	{
-		id: 40,
-		description: '40 minutos'
-	},
-	{
-		id: 45,
-		description: '45 minutos'
-	},
-	{
-		id: 50,
-		description: '50 minutos'
-	},
-	{
-		id: 55,
-		description: '55 minutos'
 	},
 	{
 		id: 60,
