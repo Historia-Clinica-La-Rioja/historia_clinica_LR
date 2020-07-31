63b1bb5bf6b99c2031936e1038e7645be15ef3f6
Gonzalo Murgolo <gonzalomurgolo@gmail.com>
2020-07-31 10:49:02 -0300

    tg-1273 [FE] Agregar servicios para obtener informacion de camas

--- front-end/webapp/src/app/modules/api-rest/services/bed.service.ts
+++ front-end/webapp/src/app/modules/api-rest/services/bed.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { environment } from "@environments/environment";
-import { BedDto, PatientBedRelocationDto } from "@api-rest/api-model";
+import { BedDto, PatientBedRelocationDto, BedSummaryDto, BedInfoDto } from "@api-rest/api-model";
 import { ContextService } from '@core/services/context.service';
 
 @Injectable({
@@ -16,6 +16,16 @@ export class BedService {
 	getAllBedsByCategory(clinicalSpecialtyId): Observable<BedDto[]> {
 		const url = `${environment.apiBase}/institution/${this.contextService.institutionId}/bed/clinicalspecialty/${clinicalSpecialtyId}`;
 		 return this.http.get<BedDto[]>(url);
+	}
+
+	getBedsSummary(): Observable<BedSummaryDto[]> {
+		const url = `${environment.apiBase}/institution/${this.contextService.institutionId}/bed/summary-list`;
+		 return this.http.get<BedSummaryDto[]>(url);
+	}
+
+	getBedInfo(bedId): Observable<BedInfoDto> {
+		const url = `${environment.apiBase}/institution/${this.contextService.institutionId}/bed/${bedId}/info`;
+		 return this.http.get<BedInfoDto>(url);
 	}
 
 	getLastPatientBedRelocation(internmentEpisodeId): Observable<PatientBedRelocationDto> {
