b16a4efb4da4e31728298c86667d308f2e99db56
Nicolino Ayerdi <nicolinoayerdi@gmail.com>
2020-08-03 14:52:42 -0300

    tg-1313 agrego url con idprofesional en turnos

--- front-end/webapp/src/app/modules/turnos/routes/home/home.component.ts
+++ front-end/webapp/src/app/modules/turnos/routes/home/home.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { map, startWith } from 'rxjs/operators';
 import { HealthcareProfessionalService } from '@api-rest/services/healthcare-professional.service';
-import { HealthcareProfessionalDto } from '@api-rest/api-model';
+import { HealthcareProfessionalDto, ProfessionalDto } from '@api-rest/api-model';
+import { Observable } from 'rxjs';
+import { NewAgendaService } from '../../services/new-agenda.service';
+import { MatDialog } from '@angular/material/dialog';
+import { Location } from '@angular/common';
 
 @Component({
 	selector: 'app-home',
@@ -14,13 +18,20 @@ export class HomeComponent implements OnInit {
 
 	form: FormGroup;
 	profesionales: HealthcareProfessionalDto[] = [];
-	profesionalesFiltered: HealthcareProfessionalDto[];
+	profesionalesFiltered$: Observable<HealthcareProfessionalDto[]>;
 	profesionalSelected: HealthcareProfessionalDto;
+	newAgendaService: NewAgendaService;
 
 	constructor(
 		private readonly router: Router,
 		private readonly formBuilder: FormBuilder,
-		private readonly healthCareProfessionalService: HealthcareProfessionalService) {
+		private readonly healthCareProfessionalService: HealthcareProfessionalService,
+		private readonly cdr: ChangeDetectorRef,
+		private readonly dialog: MatDialog,
+		private readonly location: Location,
+		private readonly route: ActivatedRoute,
+	) {
+		this.newAgendaService = new NewAgendaService(this.dialog, this.cdr);
 	}
 
 	ngOnInit(): void {
@@ -28,16 +39,45 @@ export class HomeComponent implements OnInit {
 			profesional: [null, Validators.required]
 		});
 
-		this.healthCareProfessionalService.getAllDoctors()
-			.subscribe(doctors => this.profesionales = doctors);
+		this.route.paramMap
+			.subscribe((params: ParamMap) => {
+				const idProfesional = Number(params.get('idProfesional'));
+				if (idProfesional) {
+					this.healthCareProfessionalService.getOne(idProfesional)
+						.pipe(map(toHealthcareProfessionalDto))
+						.subscribe(profesional => {
+							if (profesional) {
+								this.profesionalSelected = profesional;
+								this.form.controls.profesional.setValue(this.getFullNameLicence(profesional));
+							}
+						});
+				}
+			});
 
-		this.form.controls.profesional.valueChanges.pipe(
-			startWith(''),
-			map(value => this._filter(value))
-		).subscribe(filtered => {
-			this.profesionalesFiltered = filtered;
+		this.newAgendaService.setAppointmentDuration(60); // todo reemplazar por obtenido del BE
+
+		this.healthCareProfessionalService.getAllDoctors().subscribe(doctors => {
+			this.profesionales = doctors;
+
+			this.profesionalesFiltered$ = this.form.controls.profesional.valueChanges.pipe(
+				startWith(''),
+				map(value => this._filter(value))
+			);
 		});
+
+		function toHealthcareProfessionalDto(profesional: ProfessionalDto): HealthcareProfessionalDto {
+			return profesional ? {
+				id: profesional.id,
+				licenceNumber: profesional.licenceNumber,
+				person: {
+					birthDate: null,
+					firstName: profesional.firstName,
+					lastName: profesional.lastName
+				}
+			} : null;
+		}
 	}
+
 
 	search(): void {
 		delete this.profesionalSelected;
@@ -45,13 +85,13 @@ export class HomeComponent implements OnInit {
 			const result = this._getResult();
 			if (result) {
 				this.form.controls.profesional.setValue(this.getFullNameLicence(result));
-				// todo load calendar and add idProfessional to url
+				console.log('result', result);
+				this.location.replaceState(`${this.router.url}/profesional/${result.id}`);
+				// todo load calendar
 			} else {
 				// todo show error no encontrado
 			}
 		}
-
-
 	}
 
 	getFullName(profesional: HealthcareProfessionalDto): string {
@@ -62,16 +102,16 @@ export class HomeComponent implements OnInit {
 		return `${this.getFullName(profesional)} - ${profesional.licenceNumber}`;
 	}
 
+	select(value): void {
+		this.profesionalSelected = value;
+	}
+
 	private _getResult(): HealthcareProfessionalDto {
 		if (this.profesionalSelected) {
 			return this.profesionalSelected;
 		}
 		const results = this._filter(this.form.value.profesional);
 		return results.length === 1 ? results[0] : null;
-	}
-
-	select(value): void {
-		this.profesionalSelected = value;
 	}
 
 	private _filter(value: string): HealthcareProfessionalDto[] {
--- front-end/webapp/src/app/modules/turnos/turnos-routing.module.ts
+++ front-end/webapp/src/app/modules/turnos/turnos-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from "../turnos/routes/home/home.component";
-import { NewAgendaComponent } from "./routes/new-agenda/new-agenda.component";
-import { ERole } from "@api-rest/api-model";
-import { RoleGuard } from "@core/guards/RoleGuard";
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../turnos/routes/home/home.component';
+import { NewAgendaComponent } from './routes/new-agenda/new-agenda.component';
+import { ERole } from '@api-rest/api-model';
+import { RoleGuard } from '@core/guards/RoleGuard';
 
 import { mockRouters } from '@presentation/utils/mock-routers.utils';
 import { MOCKS_TURNOS } from './constants/mock-routers';
@@ -12,17 +12,19 @@ const routes: Routes = [
 	{
 		path: '',
 		children: [
-			{
-				path: '',
-				component: HomeComponent,
-				canActivate: [RoleGuard],
-				data: { allowedRoles: [ERole.ESPECIALISTA_MEDICO, ERole.PROFESIONAL_DE_SALUD, ERole.ADMINISTRATIVO] }
-			},
+			...['', 'profesional/:idProfesional'].map(path => (
+				{
+					path,
+					component: HomeComponent,
+					canActivate: [RoleGuard],
+					data: {allowedRoles: [ERole.ESPECIALISTA_MEDICO, ERole.PROFESIONAL_DE_SALUD, ERole.ADMINISTRATIVO]}
+				})
+			),
 			{
 				path: 'nueva-agenda',
 				component: NewAgendaComponent,
 				canActivate: [RoleGuard],
-				data: { allowedRoles: [ERole.ADMINISTRATIVO] }
+				data: {allowedRoles: [ERole.ADMINISTRATIVO]}
 			},
 			{
 				path: 'profesional/profesionalId/agenda/:agendaId/editar',
