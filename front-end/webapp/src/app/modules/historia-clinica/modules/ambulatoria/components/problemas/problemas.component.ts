ccc7c0e4482304e242b377980cf49ba278e03620
Lucas Telesco <telescolucas19@gmail.com>
2020-08-04 11:35:58 -0300

    tg-1297 [FE] Corrige formato de fecha yyy-mm-dd en Problemas - Ambulatoria

--- front-end/webapp/src/app/modules/historia-clinica/modules/ambulatoria/components/problemas/problemas.component.ts
+++ front-end/webapp/src/app/modules/historia-clinica/modules/ambulatoria/components/problemas/problemas.component.ts
@@ -3,6 +3,9 @@ import { PROBLEMAS_ACTIVOS, PROBLEMAS_CRONICOS, PROBLEMAS_RESUELTOS } from '../.
 import { HCEPersonalHistoryDto } from '@api-rest/api-model';
 import { HceGeneralStateService } from '@api-rest/services/hce-general-state.service';
 import { ActivatedRoute } from '@angular/router';
+import { DateFormat, momentFormat, momentParseDate } from '@core/utils/moment.utils';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'app-problemas',
@@ -14,9 +17,9 @@ export class ProblemasComponent implements OnInit {
 	public readonly cronicos = PROBLEMAS_CRONICOS;
 	public readonly activos = PROBLEMAS_ACTIVOS;
 	public readonly resueltos = PROBLEMAS_RESUELTOS;
-	public activeProblems: HCEPersonalHistoryDto[];
-	public solvedProblems: HCEPersonalHistoryDto[];
-	public chronicProblems: HCEPersonalHistoryDto[];
+	public activeProblems$: Observable<HCEPersonalHistoryDto[]>;
+	public solvedProblems$: Observable<HCEPersonalHistoryDto[]>;
+	public chronicProblems$: Observable<HCEPersonalHistoryDto[]>;
 	private patientId: number;
 
 
@@ -31,15 +34,29 @@ export class ProblemasComponent implements OnInit {
 	}
 
 	ngOnInit(): void {
-		this.hceGeneralStateService.getActiveProblems(this.patientId).subscribe(activeProblems => {
-			this.activeProblems = activeProblems
-		});
-		this.hceGeneralStateService.getSolvedProblems(this.patientId).subscribe(solvedProblems => {
-			this.solvedProblems = solvedProblems
-		});
-		this.hceGeneralStateService.getChronicConditions(this.patientId).subscribe(chronicProblems => {
-			this.chronicProblems = chronicProblems
-		});
+		this.activeProblems$ = this.hceGeneralStateService.getActiveProblems(this.patientId).pipe(
+			map((problemas: HCEPersonalHistoryDto[]) => {
+				return problemas.map((problema: HCEPersonalHistoryDto) => {
+					problema.startDate = momentFormat(momentParseDate(problema.startDate), DateFormat.VIEW_DATE);
+					return problema;
+				})
+			})
+		);
+		this.solvedProblems$ = this.hceGeneralStateService.getSolvedProblems(this.patientId).pipe(
+			map((problemas: HCEPersonalHistoryDto[]) => {
+				return problemas.map((problema: HCEPersonalHistoryDto) => {
+					problema.startDate = momentFormat(momentParseDate(problema.startDate), DateFormat.VIEW_DATE);
+					return problema;
+				})
+			})
+		);
+		this.chronicProblems$ = this.hceGeneralStateService.getChronicConditions(this.patientId).pipe(
+			map((problemas: HCEPersonalHistoryDto[]) => {
+				return problemas.map((problema: HCEPersonalHistoryDto) => {
+					problema.startDate = momentFormat(momentParseDate(problema.startDate), DateFormat.VIEW_DATE);
+					return problema;
+				})
+			})
+		);
 	}
-
 }
