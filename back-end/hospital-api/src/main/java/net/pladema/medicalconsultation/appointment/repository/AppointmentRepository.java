0374ed77366bf19ee2f8c39831ee0033001d6af6
Ezequiel Trapani <etrapani04@gmail.com>
2020-08-18 16:40:06 -0300

    tg-1468. [BE] Filtrar los turnos cancelados

--- back-end/hospital-api/src/main/java/net/pladema/medicalconsultation/appointment/repository/AppointmentRepository.java
+++ back-end/hospital-api/src/main/java/net/pladema/medicalconsultation/appointment/repository/AppointmentRepository.java
@@ -24,7 +24,8 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Intege
             "JOIN AppointmentAssn AS aa ON (a.id = aa.pk.appointmentId) " +
             "JOIN Diary d ON (d.id = aa.pk.diaryId )" +
             "JOIN DiaryOpeningHours  AS doh ON (doh.pk.diaryId = d.id AND doh.pk.openingHoursId = aa.pk.openingHoursId) " +
-            "WHERE aa.pk.diaryId IN (:diaryIds) AND d.deleteable.deleted = false ")
+            "WHERE aa.pk.diaryId IN (:diaryIds) AND d.deleteable.deleted = false " +
+            "AND NOT a.appointmentStateId = " + AppointmentState.CANCELLED_STR)
     List<AppointmentDiaryVo> getAppointmentsByDiaries(@Param("diaryIds") List<Integer> diaryIds);
 
     @Transactional(readOnly = true)
