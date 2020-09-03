7518966911ba9e9b27f9de53ec6556aba5d1f6e6
Gonzalo Murgolo <gonzalomurgolo@gmail.com>
2020-09-03 10:10:29 -0300

    tg-1673 [FE] ampliar busqueda snomed

--- front-end/webapp/src/app/modules/api-rest/services/snowstorm.service.ts
+++ front-end/webapp/src/app/modules/api-rest/services/snowstorm.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import { map } from 'rxjs/operators';
-import { SnomedDto } from '@api-rest/api-model';
+import { SnomedDto, SnomedResponseDto } from '@api-rest/api-model';
 import { Observable } from 'rxjs';
 const CONCEPTS = 'concepts';
 const PREFERRED_AND_ACEPTABLE_CONCEPTS = ['450828004'];
-const RESULTS_LIMIT = '20';
+export const SNOMED_RESULTS_LIMIT = '30';
 
 @Injectable({
 	providedIn: 'root'
@@ -16,24 +16,24 @@ export class SnowstormService {
 	private params = new HttpParams()
 		.set('termActive', 'true')
 		.set('preferredOrAcceptableIn', `${PREFERRED_AND_ACEPTABLE_CONCEPTS.join(",")}`)
-		.set('limit', RESULTS_LIMIT);
+		.set('limit', SNOMED_RESULTS_LIMIT);
 
-	private headers = new HttpHeaders().set('Accept-Language', 'es-AR;q=0.8,en-GB;q=0.6')
+	private readonly headers = new HttpHeaders().set('Accept-Language', 'es-AR;q=0.8,en-GB;q=0.6')
 
 	constructor(
-		private http: HttpClient
+		private readonly http: HttpClient
 	) { }
 
-	getSNOMEDConcepts(params: ConceptRequestParams): Observable<SnomedDto[]> {
+	getSNOMEDConcepts(params: ConceptRequestParams): Observable<SnomedResponseDto> {
 		Object.keys(params).forEach(
 			keyParam => this.params = this.params.set(`${keyParam}`, `${params[keyParam]}`)
 		);
-		let url = `${environment.snowstorm}/${CONCEPTS}`;
+		const url = `${environment.snowstorm}/${CONCEPTS}`;
 		return this.http.get<any>(url, {
 			headers: this.headers,
 			params: this.params
-		}).pipe(map(results => results.items.map(
-			(i: any): SnomedDto => {
+		}).pipe(map(results => {
+			const newItems = results.items.map((i: any): SnomedDto => {
 				return {
 					id: i.conceptId,
 					pt: i.pt.term,
@@ -41,8 +41,11 @@ export class SnowstormService {
 					parentFsn: '',
 					parentId: ''
 				};
-			}
-		)));
+			});
+			results.items = newItems;
+			return results;
+		}
+		));
 	}
 
 }
