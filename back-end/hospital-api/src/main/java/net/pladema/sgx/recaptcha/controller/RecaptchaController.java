29b3e57a5b89ec3329196d0748df0adc303fe84c
Julio <julioriveralaguna@gmail.com>
2020-09-21 12:44:11 -0300

    tg-1841 BE controller enable/disable recaptcha

---
+++ back-end/hospital-api/src/main/java/net/pladema/sgx/recaptcha/controller/RecaptchaController.java
@@ -0,0 +1,35 @@
+package net.pladema.sgx.recaptcha.controller;
+
+import io.swagger.annotations.Api;
+import net.pladema.sgx.recaptcha.service.ICaptchaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/recaptcha")
+@Api(value = "Recaptcha", tags = { "Recaptcha" })
+public class RecaptchaController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RecaptchaController.class);
+
+    public static final String OUTPUT = "Output -> {}";
+
+    private final ICaptchaService captchaService;
+
+    public RecaptchaController(ICaptchaService captchaService) {
+        this.captchaService = captchaService;
+    }
+
+    @GetMapping(value = "/is-enable")
+    public ResponseEntity<Boolean> isRecaptchaEnable() {
+        LOG.debug("{}", "Check for recaptcha enable");
+        boolean result = captchaService.isRecaptchaEnable();
+        LOG.debug(OUTPUT, result);
+        return ResponseEntity.ok().body(result);
+    }
+}
