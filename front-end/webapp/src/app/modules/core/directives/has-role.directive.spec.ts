5e33516fb6f71568013d8a4196914da4aeed6770
nayerdi <nicolinoayerdi@gmail.com>
2020-10-02 12:20:45 -0300

    tg-2135 [FE] elimina tests que rompen la creación del karma server

--- front-end/webapp/src/app/modules/core/directives/has-role.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HasRoleDirective } from './has-role.directive';
-
-describe('HasRoleDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HasRoleDirective();
-    expect(directive).toBeTruthy();
-  });
-});
