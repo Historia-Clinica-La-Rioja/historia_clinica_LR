e2ba93ccdac352b27bee7cd0735517dbf2d19241
Silvana Aguero <silaguero19@gmail.com>
2020-11-05 19:18:44 -0300

    tg-1961 Se aplican validaciones sobre los endpoints requeridos mediante el recurso Fhir OperationOutcome

---
+++ back-end/interoperability/src/main/java/net/pladema/hl7/supporting/exchange/restful/IResponseOperationOutcome.java
@@ -0,0 +1,39 @@
+//
+// Operation outcomes are sets of error, warning and information messages that provide detailed information about the outcome
+// of an attempted system operation. They are provided as a direct system response, or component of one, and provide information
+// about the outcome of the operation.
+//
+
+package net.pladema.hl7.supporting.exchange.restful;
+
+import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Bundle.BundleType;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.OperationOutcome;
+
+public interface IResponseOperationOutcome {
+
+    default void badRequest(String diagnostics) {
+        OperationOutcome exception = new OperationOutcome();
+        exception.addIssue()
+                .setSeverity(OperationOutcome.IssueSeverity.ERROR)
+                .setCode(OperationOutcome.IssueType.PROCESSING)
+                .setDiagnostics(diagnostics);
+        throw new InvalidRequestException("Invalid parameter", exception);
+    }
+
+    default Bundle.BundleEntryComponent notFound(String message){
+        OperationOutcome warning = new OperationOutcome();
+        warning.setId("warning");
+        warning.addIssue()
+                .setSeverity(OperationOutcome.IssueSeverity.WARNING)
+                .setCode(OperationOutcome.IssueType.NOTFOUND)
+                .setDetails(new CodeableConcept().addCoding(new Coding().setDisplay(message)));
+        return new Bundle.BundleEntryComponent()
+                .setResource(warning)
+                .setSearch(new Bundle.BundleEntrySearchComponent().setMode(Bundle.SearchEntryMode.OUTCOME));
+        //throw new ResourceNotFoundException(Patient.class, new IdType());
+    }
+}
---
+++ back-end/interoperability/src/main/java/net/pladema/hl7/supporting/exchange/restful/validator/DocumentReferenceValidation.java
@@ -0,0 +1,62 @@
+package net.pladema.hl7.supporting.exchange.restful.validator;
+
+import ca.uhn.fhir.rest.param.TokenParam;
+import net.pladema.hl7.supporting.exchange.documents.BundleResource;
+import net.pladema.hl7.supporting.exchange.documents.profile.FhirDocument;
+import net.pladema.hl7.supporting.terminology.coding.CodingSystem;
+import net.pladema.hl7.supporting.exchange.restful.IResponseOperationOutcome;
+import net.pladema.hl7.dataexchange.model.domain.BundleVo;
+import org.hl7.fhir.r4.model.Bundle;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+@Component
+public class DocumentReferenceValidation implements IResponseOperationOutcome {
+
+    private final FhirDocument fhirDocument;
+
+    public DocumentReferenceValidation(FhirDocument fhirDocument){
+        super();
+        this.fhirDocument=fhirDocument;
+    }
+
+    public Optional<Bundle> inputParameter(@NotNull TokenParam subject,
+                               @NotNull TokenParam custodian,
+                               @NotNull TokenParam type, String dominio){
+
+        //''subject' parameter must have the system domain with a local patient ID
+        if(!subject.getSystem().equals(dominio) && subject.isEmpty())
+            badRequest("The URI \""+subject.getSystem()+"\" is not a valid system identifier");
+
+        //'custodian' parameter has to be the system domain
+        if(!custodian.getSystem().equals(dominio))
+            badRequest("TokenParam[system="+subject.getSystem()+",value="+subject.getValue()+"] no esta federado para TokenParam[system=,value="+custodian.getValue());
+
+        //'type' parameter must have a loinc system
+        if(!type.getSystem().equals(CodingSystem.LOINC))
+            badRequest("type parameter must have a loinc system");
+        else {
+            if(type.isEmpty() && !fhirDocument.isSupported(type)) {
+                //return empty Bundle with status 200
+                return Optional.of(BundleResource.empty());
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Bundle> data(BundleVo data, String patientId){
+        //the patient does not exist, a warning is registered due to a possible federation error
+        if(!data.existsPatient()){
+            Bundle document = BundleResource.empty();
+            document.addEntry(notFound("There is no matching patient for MRN "+ patientId));
+            return Optional.of(document);
+        }
+        else if(!data.hasDocuments()){
+            //the patient has no registered documents
+            return Optional.of(BundleResource.empty());
+        }
+        return Optional.empty();
+    }
+}
