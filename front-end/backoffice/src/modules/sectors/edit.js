533bf87c0c88a0b86325edc85bd27175433b71bd
Luis Manterola <manterolaluis@gmail.com>
2020-11-19 15:10:06 -0300

    tg-2345 [Backoffice] Modificar edición de sector

--- front-end/backoffice/src/modules/sectors/edit.js
+++ front-end/backoffice/src/modules/sectors/edit.js
@@ -1,19 +1,104 @@
 import React from 'react';
 import {
-    TextInput,
-    Edit,
-    SimpleForm,
-    required,
-    ReferenceManyField,
     Datagrid,
+    DateField,
     DeleteButton,
-    TextField,
+    Edit,
+    EditButton,
+    FormDataConsumer,
     ReferenceField,
+    ReferenceInput,
+    ReferenceManyField,
+    required,
+    SelectInput,
+    SimpleForm,
+    TextField,
+    TextInput,
 } from 'react-admin';
 import CreateRelatedButton from '../components/CreateRelatedButton';
 import SectionTitle from '../components/SectionTitle';
 import SgxSelectInput from "../../sgxSelectInput/SgxSelectInput";
 import CustomToolbar from "../../modules/components/CustomToolbar";
+
+const INTERNACION = 2;
+
+const SectorType = (sourceId) => {
+    return (
+        <ReferenceInput
+            {...sourceId}
+            reference="sectortypes"
+            perPage={100}
+            sort={{ field: 'description', order: 'ASC' }}
+        >
+            <SelectInput optionText="description" optionValue="id" validate={[required()]} />
+        </ReferenceInput>);
+
+};
+
+
+const AgeGroups = ({ formData, ...rest }) => {
+    if (formData.sectorTypeId !== INTERNACION) return null;
+    return ( <ReferenceInput
+        {...rest}
+        reference="agegroups"
+        sort={{ field: 'description', order: 'ASC' }}
+        perPage={10}
+    >
+        <SelectInput optionText="description" optionValue="id" validate={[required()]} />
+    </ReferenceInput> );
+};
+
+
+const SectorOrganization = ({ formData, ...rest }) => {
+    if (formData.sectorTypeId !== INTERNACION) return null;
+    return (
+        <ReferenceInput
+            {...rest}
+            reference="sectororganizations"
+            perPage={100}
+            sort={{ field: 'description', order: 'ASC' }}
+        >
+            <SelectInput optionText="description" optionValue="id" validate={[required()]} />
+        </ReferenceInput>);
+};
+
+const CareType = ({ formData, ...rest }) => {
+    if (formData.sectorTypeId !== INTERNACION) return null;
+    return (
+        <ReferenceInput
+            {...rest}
+            reference="caretypes"
+            perPage={100}
+            sort={{ field: 'description', order: 'ASC' }}
+        >
+            <SelectInput optionText="description" optionValue="id" validate={[required()]} />
+        </ReferenceInput>);
+};
+
+const HospitalizationType = ({ formData, ...rest }) => {
+    if (formData.sectorTypeId !== INTERNACION) return null;
+    return (
+        <ReferenceInput
+            {...rest}
+            reference="hospitalizationtypes"
+            perPage={100}
+            sort={{ field: 'description', order: 'ASC' }}
+        >
+            <SelectInput optionText="description" optionValue="id" validate={[required()]} />
+        </ReferenceInput>);
+};
+
+const Sector = (sourceId) => {
+    return (
+        <ReferenceInput
+            {...sourceId}
+            reference="sectors"
+            perPage={100}
+            sort={{ field: 'description', order: 'ASC' }}
+        >
+            <SelectInput optionText="description" optionValue="id" />
+        </ReferenceInput>);
+};
 
 const SectorEdit = props => (
     <Edit {...props}>
@@ -21,6 +106,27 @@ const SectorEdit = props => (
             <TextInput source="description" validate={[required()]} />
 
             <SgxSelectInput source="institutionId" element="institutions" optionText="name" alwaysOn allowEmpty={false}/>
+
+            {/*Sector*/}
+            <Sector source="parentSectorId"/>
+            {/*Sector Type*/}
+            <SectorType source="sectorTypeId"/>
+            {/*Age Groups*/}
+            <FormDataConsumer>
+                {formDataProps => ( <AgeGroups {...formDataProps} source="ageGroupId"/>)}
+            </FormDataConsumer>
+            {/*Sector Organizations*/}
+            <FormDataConsumer>
+                {formDataProps => ( <SectorOrganization {...formDataProps} source="sectorOrganizationId"/>)}
+            </FormDataConsumer>
+            {/*Care Type*/}
+            <FormDataConsumer>
+                {formDataProps => ( <CareType {...formDataProps} source="careTypeId"/>)}
+            </FormDataConsumer>
+            {/*Hospitalization Type*/}
+            <FormDataConsumer>
+                {formDataProps => ( <HospitalizationType {...formDataProps} source="hospitalizationTypeId"/>)}
+            </FormDataConsumer>
 
             <SectionTitle label="resources.sectors.fields.clinicalspecialtysectors"/>
             <CreateRelatedButton
@@ -41,6 +147,27 @@ const SectorEdit = props => (
                         <TextField source="name" />
                     </ReferenceField>
                     <DeleteButton />
+                </Datagrid>
+            </ReferenceManyField>
+
+            <SectionTitle label="resources.clinicalspecialtysectors.fields.rooms"/>
+            <CreateRelatedButton
+                reference="rooms"
+                refFieldName="clinicalSpecialtySectorId"
+                label="resources.rooms.createRelated"
+            />
+            <ReferenceManyField
+                addLabel={false}
+                reference="rooms"
+                target="clinicalSpecialtySectorId"
+                sort={{ field: 'description', order: 'DESC' }}
+            >
+                <Datagrid rowClick="show">
+                    <TextField source="roomNumber" />
+                    <TextField source="description"/>
+                    <TextField source="type" />
+                    <DateField source="dischargeDate" />
+                    <EditButton />
                 </Datagrid>
             </ReferenceManyField>
         </SimpleForm>
