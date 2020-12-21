d01808d785a3a026aa2ad4e1b92dfdc36968d1bc
nayerdi <nicolinoayerdi@gmail.com>
2020-12-21 08:50:15 -0300

    tg-2575 invoca PatientPhotoService desde PatientCardComponent

--- front-end/webapp/src/app/modules/presentation/components/patient-card/patient-card.component.ts
+++ front-end/webapp/src/app/modules/presentation/components/patient-card/patient-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PersonPhotoDto } from '@api-rest/api-model';
-import { Observable, Observer } from 'rxjs';
+import { Observable } from 'rxjs';
+import { PatientPhotoService } from '@presentation/services/patient-photo.service';
 
 @Component({
 	selector: 'app-patient-card',
@@ -11,59 +12,20 @@ export class PatientCardComponent implements OnInit {
 
 	@Input() patient: PatientBasicData;
 	@Input() personPhoto: PersonPhotoDto;
-	decodedPhoto;
-	constructor() { }
+	decodedPhoto$: Observable<string>;
+
+	constructor(private readonly patientPhotoService: PatientPhotoService) { }
 
 	ngOnInit(): void {
-			if(this.personPhoto.imageData){
-				this.getBase64ImageFromURL(this.personPhoto.imageData).subscribe(base64data => {
-					// this is the image as dataUrl
-					this.decodedPhoto = 'data:image/jpg;base64,' + base64data;
-				});
-			}
+		if (this.personPhoto.imageData) {
+				this.decodedPhoto$ = this.patientPhotoService.decodePhoto(this.personPhoto.imageData);
+		}
 	}
 
 	public viewGenderAge() {
 		const gender = (this.patient?.gender) ? (this.patient.gender + ' · ') : '';
 		const age = (this.patient?.age) || (this.patient?.age === 0) ? (this.patient.age + ' años') : '';
 		return gender + age;
-	}
-
-
-
-	getBase64ImageFromURL(url: string) {
-		return Observable.create((observer: Observer<string>) => {
-			// create an image object
-			let img = new Image();
-			img.crossOrigin = 'Anonymous';
-			img.src = url;
-			if (!img.complete) {
-				// This will call another method that will create image from url
-				img.onload = () => {
-					observer.next(this.getBase64Image(img));
-					observer.complete();
-				};
-				img.onerror = (err) => {
-					observer.error(err);
-				};
-			} else {
-				observer.next(this.getBase64Image(img));
-				observer.complete();
-			}
-		});
-	}
-
-	getBase64Image(img: HTMLImageElement) {
-		// We create a HTML canvas object that will create a 2d image
-		var canvas = document.createElement("canvas");
-		canvas.width = img.width;
-		canvas.height = img.height;
-		var ctx = canvas.getContext("2d");
-		// This will draw image
-		ctx.drawImage(img, 0, 0);
-		// Convert the drawn image to Data URL
-		var dataURL = canvas.toDataURL("image/png");
-		return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 	}
 }
 
