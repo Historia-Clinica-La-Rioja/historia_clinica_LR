58f5f84f5a705cb2af8064a27b7c20af09e84d9a
jlardapide <joacolardapide@gmail.com>
2021-01-06 14:00:11 -0300

    tg-2744 [FE] Agrega boton para completar paciente de episodio de guardia

--- front-end/webapp/src/app/modules/guardia/routes/episode-details/episode-details.component.ts
+++ front-end/webapp/src/app/modules/guardia/routes/episode-details/episode-details.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { CompletePatientDto, PersonalInformationDto, PersonPhotoDto } from '@api-rest/api-model';
 import { EmergencyCareEpisodeService, ResponseEmergencyCareDto } from '@api-rest/services/emergency-care-episode.service';
 import { PatientMedicalCoverageService } from '@api-rest/services/patient-medical-coverage.service';
 import { PatientService } from '@api-rest/services/patient.service';
 import { PersonService } from '@api-rest/services/person.service';
+import { ConfirmDialogComponent } from '@core/dialogs/confirm-dialog/confirm-dialog.component';
 import { MapperService } from '@presentation/services/mapper.service';
 import { Observable } from 'rxjs';
+import { SearchPatientComponent } from 'src/app/modules/pacientes/component/search-patient/search-patient.component';
 
 @Component({
 	selector: 'app-episode-details',
@@ -28,9 +31,9 @@ export class EpisodeDetailsComponent implements OnInit {
 		private readonly mapperService: MapperService,
 		private readonly patientMedicalCoverageService: PatientMedicalCoverageService,
 		private readonly personService: PersonService,
-		//No se puede hacer un llamado y que traiga todo de una? EN un monton de lados debemos estar haciendo esto...
+		//No se puede hacer un llamado y que traiga todo de una? En un monton de lados debemos estar haciendo esto...
 		private readonly emergencyCareEpisodeService: EmergencyCareEpisodeService,
-
+		private readonly dialog: MatDialog,
 
 	) { }
 
@@ -51,7 +54,33 @@ export class EpisodeDetailsComponent implements OnInit {
 
 	}
 
-	private loadPatient(patientId): void {
+	searchPatient(): void {
+		const dialogRef = this.dialog.open(SearchPatientComponent);
+		dialogRef.afterClosed().subscribe(
+			patient => {
+				if (patient) {
+					const confirmRef = this.dialog.open(ConfirmDialogComponent, {
+						data: {
+							title: 'guardia.episode.search_patient.CONFIRM',
+							content: `¿Está seguro que desea asociar el paciente con ID ${patient.basicData.id} al episodio de guardia?`,
+							okButtonLabel: 'buttons.CONFIRM'
+						}
+					});
+
+					confirmRef.afterClosed().subscribe(confirmed => {
+						if (confirmed) {
+							//persistir el nuevo paciente
+							this.loadPatient(patient.basicData.id);
+							this.responseEmergencyCare.patientId = patient.basicData.id;
+						}
+					})
+				}
+			}
+		);
+	}
+
+	//TODO Crear un servicio que englobe estos llamados (tg-2735)
+	private loadPatient(patientId: number): void {
 		this.patientService.getPatientCompleteData<CompletePatientDto>(patientId)
 			.subscribe(completeData => {
 				this.patientBasicData = this.mapperService.toPatientBasicData(completeData);
