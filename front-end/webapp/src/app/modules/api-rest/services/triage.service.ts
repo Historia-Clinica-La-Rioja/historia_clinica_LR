b1e2406464438a621e6ee634db76a01066b8297c
nayerdi <nicolinoayerdi@gmail.com>
2021-01-08 17:31:50 -0300

    tg-2759 agrega metodo TriageService.getAll(episodeId) con datos mockeados

--- front-end/webapp/src/app/modules/api-rest/services/triage.service.ts
+++ front-end/webapp/src/app/modules/api-rest/services/triage.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { ContextService } from "@core/services/context.service";
-import { TriageAdministrativeDto } from "@api-rest/api-model";
-import { environment } from "@environments/environment";
-import { Observable } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { ContextService } from '@core/services/context.service';
+import { TriageAdministrativeDto } from '@api-rest/api-model';
+import { environment } from '@environments/environment';
+import { Observable, of } from 'rxjs';
 
 const URL_PREFIX = '/emergency-care/episodes';
 const URL_SUFIX = '/triage';
@@ -13,14 +13,115 @@ const URL_SUFIX = '/triage';
 })
 export class TriageService {
 
-	constructor(private http: HttpClient,
-	            private contextService: ContextService) {
-	}
+	constructor(
+		private http: HttpClient,
+		private contextService: ContextService
+	) { }
 
 	createAdministrative(episodeId: number, triage: TriageAdministrativeDto): Observable<number> {
 		let url = `${environment.apiBase}/institution/${this.contextService.institutionId +
 		URL_PREFIX}/${episodeId}${URL_SUFIX}`;
 		return this.http.post<number>(url, triage);
+	}
+
+	getAll(episodeId: number): Observable<any[]> {
+		return of ( [
+			{
+				id: 1,
+				creationDate: {
+					date: { day: 11, month: 1, year: 2021 },
+					time: { hours: 10, minutes: 30 }
+				},
+				category: {
+					id: 1,
+					name: 'Nivel I',
+					description: 'Resucitación',
+					color: {
+						name: 'Rojo',
+						code: '#FF0000'
+					}
+				},
+				professional: {
+					id: 1,
+					firstName: 'Lucas',
+					lastName: 'Bergottini'
+				},
+				doctorsOffice: {
+					id: 1,
+					description: 'Sala N'
+				}
+			},
+			{
+				id: 2,
+				creationDate: {
+					date: { day: 11, month: 1, year: 2021 },
+					time: { hours: 9, minutes: 30 }
+				},
+				category: {
+					id: 1,
+					name: 'Nivel I',
+					description: 'Resucitación',
+					color: {
+						name: 'Rojo',
+						code: '#FF0000'
+					}
+				},
+				professional: {
+					id: 1,
+					firstName: 'Julio',
+					lastName: 'Rivera'
+				},
+				doctorsOffice: {
+					id: 1,
+					description: 'Sala N'
+				},
+				vitalSigns: {
+					bloodOxygenSaturation: {
+						value: 9,
+						effectiveTime: {
+							date: {day: 21, month: 11, year: 2020},
+							time: {hours: 12, minutes: 0, seconds: 0}
+						}
+					},
+					diastolicBloodPressure: {
+						value: 12,
+						effectiveTime: {
+							date: {day: 21, month: 11, year: 2020},
+							time: {hours: 12, minutes: 0, seconds: 0}
+						}
+					},
+					heartRate: {
+						value: 18,
+						effectiveTime: {
+							date: {day: 21, month: 11, year: 2020},
+							time: {hours: 12, minutes: 0, seconds: 0}
+						}
+					},
+					respiratoryRate: {
+						value: 3,
+						effectiveTime: {
+							date: {day: 21, month: 11, year: 2020},
+							time: {hours: 12, minutes: 0, seconds: 0}
+						}
+					},
+					systolicBloodPressure: {
+						value: 1,
+						effectiveTime: {
+							date: {day: 21, month: 11, year: 2020},
+							time: {hours: 12, minutes: 0, seconds: 0}
+						}
+					},
+					temperature: {
+						value: 10,
+						effectiveTime: {
+							date: {day: 21, month: 11, year: 2020},
+							time: {hours: 12, minutes: 0, seconds: 0}
+						}
+					}
+				},
+				notes: 'El paciente muestra signos de mejora'
+			}
+		]);
 	}
 
 }
