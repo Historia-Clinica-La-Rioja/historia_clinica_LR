bd8b2099c63db19b6a98e435e4eb71713453fea0
tmedina <medinatomasariel@gmail.com>
2021-03-05 15:14:19 -0300

    tg-3262 [BE] Agregar creación de archivo de properties que posea la versión

--- back-end/hospital-api/src/main/java/net/pladema/sgx/version/service/impl/ApplicationVersionServiceImpl.java
+++ back-end/hospital-api/src/main/java/net/pladema/sgx/version/service/impl/ApplicationVersionServiceImpl.java
@@ -10,28 +10,60 @@ import org.springframework.stereotype.Service;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Service
 public class ApplicationVersionServiceImpl implements ApplicationVersionService {
 
-    private final Logger LOG = LoggerFactory.getLogger(ApplicationVersionServiceImpl.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ApplicationVersionServiceImpl.class);
+
+    private static final String NO_INPUT_PARAMETERS = "No input parameters";
+
+    private static final String OUTPUT = "Output -> {}";
 
     @Override
     public String getCurrentVersion() {
-        LOG.debug("No input parameters");
+        LOG.debug(NO_INPUT_PARAMETERS);
         try {
-            MavenXpp3Reader reader = new MavenXpp3Reader();
-            Model model = reader.read(new FileReader("./pom-parent.xml"));
-            String snapshot = (String) model.getProperties().get("revision");
-            String version = snapshot.replace("-SNAPSHOT","");
-            LOG.debug("Output -> {}", version);
+            String version = getVersionFromPomParentFile();
+            LOG.debug(OUTPUT, version);
             return version;
         }
-        catch(IOException | XmlPullParserException ex){
-            String result = "";
-            LOG.debug("Output -> {}", result);
-            return result;
+        catch(IOException | XmlPullParserException pomParentReadException){
+            try {
+                String version = getVersionFromPomPropertiesFile();
+                LOG.debug(OUTPUT, version);
+                return version;
+            }
+            catch(IOException pomPropertiesReadException){
+                String result = "";
+                LOG.debug(OUTPUT, result);
+                return result;
+            }
         }
+    }
+
+    private String getVersionFromPomParentFile() throws IOException, XmlPullParserException {
+        LOG.debug(NO_INPUT_PARAMETERS);
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = reader.read(new FileReader("./pom-parent.xml"));
+        String version = (String) model.getProperties().get("revision");
+        LOG.debug(OUTPUT, version);
+        return version;
+    }
+
+    private String getVersionFromPomPropertiesFile() throws IOException {
+        LOG.debug(NO_INPUT_PARAMETERS);
+        String version = "";
+        Properties properties = new Properties();
+        InputStream pomFile = getClass().getClassLoader().getResourceAsStream("classpath:/pom.properties");
+        if (pomFile != null) {
+            properties.load(pomFile);
+            version = properties.getProperty("version", "");
+        }
+        LOG.debug(OUTPUT, version);
+        return version;
     }
 
 }
