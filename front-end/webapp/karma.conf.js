903e7cee81f394a50717ad0f5ce69c30ef4475a7
Emmanuel Arias <eamanu@yaerobi.com>
2021-03-17 08:18:37 -0300

    Use Firefox launcher for tests

--- front-end/webapp/karma.conf.js
+++ front-end/webapp/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
 		frameworks: ['jasmine', '@angular-devkit/build-angular'],
 		plugins: [
 			require('karma-jasmine'),
-			require('karma-chrome-launcher'),
+			require('karma-firefox-launcher'),
 			require('karma-jasmine-html-reporter'),
 			require('karma-coverage-istanbul-reporter'),
 			require('@angular-devkit/build-angular/plugins/karma')
@@ -25,14 +25,8 @@ module.exports = function (config) {
 		colors: true,
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
-		browsers: ['Chrome', 'ChromeHeadlessCI'],
+		browsers: ['Firefox'],
 		singleRun: false,
 		restartOnFileChange: true,
-		customLaunchers: {
-			ChromeHeadlessCI: {
-				base: 'ChromeHeadless',
-				flags: ['--no-sandbox']
-			}
-		},
 	});
 };
