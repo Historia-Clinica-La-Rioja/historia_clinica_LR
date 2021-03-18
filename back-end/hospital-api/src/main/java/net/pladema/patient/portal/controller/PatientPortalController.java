f174bc1c63fcf2a46280b1e8b4ce1c4a362a2b06
Tano <lucas.bergottini@gmail.com>
2021-03-18 14:31:15 -0300

    tg-3357 [BE] Crear servicio para obtencion de foto de paciente

--- back-end/hospital-api/src/main/java/net/pladema/patient/portal/controller/PatientPortalController.java
+++ back-end/hospital-api/src/main/java/net/pladema/patient/portal/controller/PatientPortalController.java
@@ -15,6 +15,7 @@ import net.pladema.patient.portal.service.PatientPortalService;
 import net.pladema.patient.repository.entity.Patient;
 import net.pladema.patient.service.PatientService;
 import net.pladema.person.controller.dto.BasicDataPersonDto;
+import net.pladema.person.controller.dto.PersonPhotoDto;
 import net.pladema.person.controller.dto.PersonalInformationDto;
 import net.pladema.person.controller.service.PersonExternalService;
 import org.slf4j.Logger;
@@ -22,6 +23,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +39,8 @@ public class PatientPortalController {
 	private static final Logger LOG = LoggerFactory.getLogger(PatientPortalController.class);
 
 	private static final String LOGGING_OUTPUT = "Output -> {}";
+
+	public static final String PATIENT_INVALID = "patient.invalid";
 
 	private final HCEHealthConditionsExternalService hceHealthConditionsExternalService;
 
@@ -123,10 +127,20 @@ public class PatientPortalController {
 	public ResponseEntity<BasicPatientDto> getBasicDataPatient() {
 		Integer patientId = patientPortalService.getPatientId();
 		Patient patient = patientService.getPatient(patientId)
-				.orElseThrow(() -> new EntityNotFoundException("patient.invalid"));
+				.orElseThrow(() -> new EntityNotFoundException(PATIENT_INVALID));
 		BasicDataPersonDto personData = personExternalService.getBasicDataPerson(patient.getPersonId());
 		BasicPatientDto result = new BasicPatientDto(patient.getId(), personData, patient.getTypeId());
 		LOG.debug(LOGGING_OUTPUT, result);
 		return ResponseEntity.ok().body(result);
+	}
+
+	@GetMapping("/photo")
+	public ResponseEntity<PersonPhotoDto> getPatientPhoto() {
+		Integer patientId = patientPortalService.getPatientId();
+		Patient patient = patientService.getPatient(patientId)
+				.orElseThrow(() -> new EntityNotFoundException(PATIENT_INVALID));
+		PersonPhotoDto personPhotoDto = personExternalService.getPersonPhoto(patient.getPersonId());
+		LOG.debug(LOGGING_OUTPUT, personPhotoDto);
+		return ResponseEntity.ok().body(personPhotoDto);
 	}
 }
