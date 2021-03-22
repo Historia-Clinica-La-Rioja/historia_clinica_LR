72abab829a4e449d6f6b1261deff919c3dc8c139
tmedina <medinatomasariel@gmail.com>
2021-03-22 13:56:18 -0300

    tg-3369 [P2] Fix no se ve el nombre de usuario al ingresar al acceso de paciente

--- front-end/webapp/src/app/modules/portal-paciente/portal-paciente.component.ts
+++ front-end/webapp/src/app/modules/portal-paciente/portal-paciente.component.ts
@@ -3,6 +3,8 @@ import { MenuFooter } from '@presentation/components/main-layout/main-layout.com
 import { ROLES_USER_SIDEBAR_MENU, NO_ROLES_USER_SIDEBAR_MENU } from '../portal-paciente/constants/menu';
 import {RoleAssignment} from '@api-rest/api-model';
 import {LoggedUserService} from '../auth/services/logged-user.service';
+import {mapToFullName} from '@api-rest/mapper/user-person-dto.mapper';
+import {AccountService} from '@api-rest/services/account.service';
 
 @Component({
 	selector: 'app-portal-paciente',
@@ -16,6 +18,7 @@ export class PortalPacienteComponent implements OnInit {
 
 	constructor(
 		private readonly loggedUserService: LoggedUserService,
+		private readonly accountService: AccountService,
 	) {
 	}
 
@@ -27,6 +30,12 @@ export class PortalPacienteComponent implements OnInit {
 				this.menuItems = NO_ROLES_USER_SIDEBAR_MENU;
 			}
 		});
+		this.accountService.getInfo()
+			.subscribe( userInfo => {
+					this.menuFooterItems.user.userName = userInfo.email;
+					this.menuFooterItems.user.fullName = mapToFullName(userInfo.personDto);
+				}
+			);
 	}
 
 	private userHasAnyRole(roleAssignments: RoleAssignment[]): boolean {
