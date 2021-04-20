bb68aaa1d1445dfa3b32e716470acc33329f1e12
nayerdi <nicolinoayerdi@gmail.com>
2021-04-20 12:22:34 -0300

    tg-3520 conecta modulo odontology con backend

--- front-end/apps/projects/odontology/src/lib/odontology.module.ts
+++ front-end/apps/projects/odontology/src/lib/odontology.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { OdontologyComponent } from './routes/odontology.component';
 import { OdontologyRoutingModule } from './odontology-routing.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
 	declarations: [OdontologyComponent],
 	imports: [
-		OdontologyRoutingModule
+		OdontologyRoutingModule,
+		CommonModule
 	],
 	exports: [OdontologyComponent]
 })
--- front-end/apps/projects/odontology/src/lib/services/odontology.service.ts
+++ front-end/apps/projects/odontology/src/lib/services/odontology.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class OdontologyService {
 
-  constructor() { }
+	private readonly ODONTOLOGY_BASE = 'api/odontology';
+
+	constructor(
+		private http: HttpClient
+	) {
+	}
+
+	getInfo(): Observable<string> {
+		const url = `${this.ODONTOLOGY_BASE}/info`;
+		return this.http.get<string>(url, { responseType: 'text' as 'json' });
+	}
+
 }
