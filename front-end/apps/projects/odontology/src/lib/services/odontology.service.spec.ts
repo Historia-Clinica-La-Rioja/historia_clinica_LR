4ddec8499317f5242ddab96364b4df6f76f3f8b1
nayerdi <nicolinoayerdi@gmail.com>
2021-04-20 14:12:14 -0300

    tg-3482 fix test de OdontologyService

--- front-end/apps/projects/odontology/src/lib/services/odontology.service.spec.ts
+++ front-end/apps/projects/odontology/src/lib/services/odontology.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OdontologyService } from './odontology.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('OdontologyService', () => {
   let service: OdontologyService;
 
   beforeEach(() => {
-	TestBed.configureTestingModule({});
+	  TestBed.configureTestingModule({
+		  imports: [HttpClientModule]
+	  });
 	service = TestBed.inject(OdontologyService);
   });
 
