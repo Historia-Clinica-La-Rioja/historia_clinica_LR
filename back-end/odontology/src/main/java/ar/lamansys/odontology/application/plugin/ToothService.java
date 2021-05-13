8618c9dea51e8310fe3717c9d08d972fd5dad1ca
Martín Santillán Cooper <marsancoo@gmail.com>
2021-05-13 15:04:26 -0300

    tg-3756 Eliminar archivos no utilizados

--- back-end/odontology/src/main/java/ar/lamansys/odontology/application/plugin/ToothService.java
+++
@@ -1,9 +0,0 @@
-package ar.lamansys.odontology.application.plugin;
-
-import ar.lamansys.odontology.application.plugin.exception.ToothServiceException;
-import ar.lamansys.odontology.domain.TeethBo;
-
-public interface ToothService {
-
-    TeethBo getTeeth() throws ToothServiceException;
-}
