3cb9db9333b7c009519482a78755732c7ab5b9ce
jlardapide <joacolardapide@gmail.com>
2021-05-18 12:51:21 -0300

    tg-3705 [FE] Crea odontograma

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/odontologia/odontologia.module.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/odontologia/odontologia.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ToothComponent } from './components/tooth/tooth.component';
 import { FlexLayoutModule, FlexModule } from '@angular/flex-layout';
 import { AppMaterialModule } from '@material/app.material.module';
+import { OdontogramComponent } from './components/odontogram/odontogram.component';
 
 
 
 @NgModule({
-	declarations: [ToothComponent],
+	declarations: [ToothComponent, OdontogramComponent],
 	imports: [
 		CommonModule,
 		FlexModule,
@@ -15,7 +16,8 @@ import { AppMaterialModule } from '@material/app.material.module';
 		AppMaterialModule
 	],
 	exports: [
-		ToothComponent
+		ToothComponent,
+		OdontogramComponent
 	]
 })
 export class OdontologiaModule { }
