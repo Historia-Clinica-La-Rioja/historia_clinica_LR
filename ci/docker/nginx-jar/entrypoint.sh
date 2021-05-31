961534894f2769161bdd8a4d4f795f751d30598d
Silvana Ivonne Aguero <silaguero19@gmail.com>
2021-05-31 09:56:26 -0300

    Actualiza valores de IP segun nuevo rango de red para SGx

--- ci/docker/nginx-jar/entrypoint.sh
+++ ci/docker/nginx-jar/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 nginx
 if [ ! -z "$GLOWROOT_AGENT_ID" ]; then
     echo "agent.id=$GLOWROOT_PROJECT::$GLOWROOT_AGENT_ID" > /glowroot/glowroot.properties
-    echo "collector.address=http://192.168.7.79:8181" >> /glowroot/glowroot.properties
+    echo "collector.address=http://10.125.125.79:8181" >> /glowroot/glowroot.properties
 fi
 exec "$@"
