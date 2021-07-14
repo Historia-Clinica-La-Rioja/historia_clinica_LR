32777ff73e5c469124147c114dfa3e06dab99a4d
etrapani <etrapani04@gmail.com>
2021-07-14 15:48:24 -0300

    tg-4357. Update test para junit5

--- back-end/hospital-api/src/test/java/net/pladema/staff/service/HealthcareProfessionalServiceImplTest.java
+++ back-end/hospital-api/src/test/java/net/pladema/staff/service/HealthcareProfessionalServiceImplTest.java
@@ -1,69 +1,62 @@
 package net.pladema.staff.service;
 
-import net.pladema.clinichistory.hospitalization.repository.HealthcareProfessionalGroupRepository;
 import ar.lamansys.sgx.shared.exceptions.NotFoundException;
+import net.pladema.clinichistory.hospitalization.repository.HealthcareProfessionalGroupRepository;
 import net.pladema.staff.repository.HealthcareProfessionalRepository;
 import net.pladema.staff.repository.domain.HealthcareProfessionalVo;
 import net.pladema.staff.service.domain.HealthcareProfessionalBo;
-import org.assertj.core.api.Assertions;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@RunWith(SpringRunner.class)
-public class HealthcareProfessionalServiceImplTest {
+@ExtendWith(MockitoExtension.class)
+class HealthcareProfessionalServiceImplTest {
 
 	private HealthcareProfessionalServiceImpl healthcareProfessionalService;
 
-	@MockBean
+	@Mock
 	private HealthcareProfessionalGroupRepository healthcareProfessionalGroupRepository;
 
-	@MockBean
+	@Mock
 	private HealthcareProfessionalRepository healthcareProfessionalRepository;
 
-	@Before
+	@BeforeEach
 	public void setUp() {
 		healthcareProfessionalService = new HealthcareProfessionalServiceImpl(healthcareProfessionalGroupRepository, healthcareProfessionalRepository);
 	}
 
 	@Test
-	public void test_exist_professional() {
+	void existProfessional() {
 
 		when(healthcareProfessionalRepository.findProfessionalById(any()))
 				.thenReturn(Optional.of(new HealthcareProfessionalVo(1, "1234/5", "Juan", "Perez", "1234")));
 
 		HealthcareProfessionalBo resultService = healthcareProfessionalService.findProfessionalById(1);
 
-		Assertions.assertThat(resultService)
-				.isNotNull();
+		Assertions.assertNotNull(resultService);
 
-		Assertions.assertThat(resultService.getFirstName())
-				.isEqualTo("Juan");
-
-		Assertions.assertThat(resultService.getLastName())
-				.isEqualTo("Perez");
-
-		Assertions.assertThat(resultService.getIdentificationNumber())
-				.isEqualTo("1234");
-
-		Assertions.assertThat(resultService.getLicenceNumber())
-				.isEqualTo("1234/5");
+		Assertions.assertEquals("Juan", resultService.getFirstName());
+		Assertions.assertEquals("Perez", resultService.getLastName());
+		Assertions.assertEquals("1234", resultService.getIdentificationNumber());
+		Assertions.assertEquals("1234/5", resultService.getLicenceNumber());
 	}
 
 
-	@Test(expected = NotFoundException.class)
-	public void test_not_exist_professional() {
+	@Test
+	void notExistProfessional() {
 
 		when(healthcareProfessionalRepository.findProfessionalById(any()))
 				.thenReturn(Optional.empty());
 
-		healthcareProfessionalService.findProfessionalById(1);
+		Assertions.assertThrows(NotFoundException.class, () ->	healthcareProfessionalService.findProfessionalById(1));
+
 	}
 }
