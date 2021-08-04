77b4a39a0a514c4a16048aa45eafde5444e25be7
etrapani <etrapani04@gmail.com>
2021-08-04 14:30:41 -0300

    tg-4516. Bug ajuste endpoint de información de vacuna no tiene bien definido el pathvariable

--- back-end/immunization/src/main/java/ar/lamansys/immunization/infrastructure/input/rest/VaccinesController.java
+++ back-end/immunization/src/main/java/ar/lamansys/immunization/infrastructure/input/rest/VaccinesController.java
@@ -7,8 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +29,7 @@ public class VaccinesController {
 
     @GetMapping("/{vaccineSctid}")
     @ResponseBody
-    public VaccineInformationDto get(@RequestParam("vaccineSctid") String vaccineSctid) {
+    public VaccineInformationDto vaccineInformation(@PathVariable("vaccineSctid") String vaccineSctid) {
         var vaccine = fetchVaccineById.run(vaccineSctid);
         var result = vaccine != null ? new VaccineInformationDto(vaccine) : null;
         logger.debug("Output -> {}", result);
