0ea23df4fe6a2d5f1a59f6ffab0ba2e5b1932280
etrapani <etrapani04@gmail.com>
2021-08-30 13:39:54 -0300

    tg-4716. [P2] [FE] Cuando confirmo la aplicación de una vacuna no se oculta la pestaña de aplicar vacuna por turno finalizado

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/dialogs/agregar-vacunas/agregar-vacunas.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/dialogs/agregar-vacunas/agregar-vacunas.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, Input, OnInit} from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ClinicalSpecialtyDto, ImmunizationDto, ImmunizePatientDto } from '@api-rest/api-model';
