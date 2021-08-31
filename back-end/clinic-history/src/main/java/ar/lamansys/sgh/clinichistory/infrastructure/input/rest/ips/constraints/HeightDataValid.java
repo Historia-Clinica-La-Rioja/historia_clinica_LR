37729eaee80c2f286bf41ed4f41c1cb361fd1a84
Emmanuel Arias <eamanu@yaerobi.com>
2021-08-31 00:49:14 -0300

    fix unresolved merge

--- back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/infrastructure/input/rest/ips/constraints/HeightDataValid.java
+++ back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/infrastructure/input/rest/ips/constraints/HeightDataValid.java
@@ -1,10 +1,4 @@
-<<<<<<< HEAD:back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/infrastructure/input/rest/ips/constraints/HeightDataValid.java
-package net.pladema.clinichistory.ips.controller.constraints;
-
-import net.pladema.clinichistory.ips.controller.constraints.validator.HeightDataValidator;
-=======
 package ar.lamansys.sgh.clinichistory.infrastructure.input.rest.ips.constraints;
->>>>>>> ff8c5eff6b5c05942dbfa19e04814441f6ecd2f8:back-end/hospital-api/src/main/java/net/pladema/clinichistory/ips/controller/constraints/AnthropometricDataValid.java
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
