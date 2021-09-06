1e32280e5a5eba2ec2e22841417517741fe36914
Tomás Medina <medinatomasariel@gmail.com>
2021-09-06 13:04:01 -0300

    tg-4788 [BE] Agregar campos necesarios a los PDFs de consulta odontológica

--- back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/infrastructure/output/repository/document/generateFile/AuditableContextBuilder.java
+++ back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/infrastructure/output/repository/document/generateFile/AuditableContextBuilder.java
@@ -1,6 +1,7 @@
 package ar.lamansys.sgh.clinichistory.infrastructure.output.repository.document.generateFile;
 
 import ar.lamansys.sgh.clinichistory.domain.document.IDocumentBo;
+import ar.lamansys.sgh.clinichistory.domain.ips.DentalActionBo;
 import ar.lamansys.sgh.clinichistory.domain.ips.ImmunizationBo;
 import ar.lamansys.sgh.clinichistory.infrastructure.input.rest.ips.dto.SnomedDto;
 import ar.lamansys.sgh.clinichistory.infrastructure.input.rest.ips.mapper.VitalSignMapper;
@@ -72,6 +73,8 @@ public class AuditableContextBuilder {
 		contextMap.put("personalHistories", document.getPersonalHistories());
 		contextMap.put("familyHistories", document.getFamilyHistories());
 		contextMap.put("allergies", document.getAllergies());
+		contextMap.put("dentalDiagnostics", document.getDentalActions().stream().filter(DentalActionBo::isDiagnostic).collect(Collectors.toList()));
+		contextMap.put("dentalProcedures", document.getDentalActions().stream().filter(DentalActionBo::isProcedure).collect(Collectors.toList()));
 
 		var immunizations =  mapImmunizations(document.getImmunizations());
 		contextMap.put("billableImmunizations", immunizations.stream().filter(ImmunizationInfoDto::isBillable).collect(Collectors.toList()));
