8ea437b278938362200c797fe80c494a9f8fb987
Tomás Medina <medinatomasariel@gmail.com>
2021-09-28 12:31:07 -0300

    tg-4976 [BE] Agregar guardado de índices de cada diente en consulta odontológica

---
+++ back-end/odontology/src/main/java/ar/lamansys/odontology/infrastructure/repository/consultation/ToothIndicesRepository.java
@@ -0,0 +1,6 @@
+package ar.lamansys.odontology.infrastructure.repository.consultation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ToothIndicesRepository extends JpaRepository<ToothIndices, ToothIndicesPK>  {
+}
