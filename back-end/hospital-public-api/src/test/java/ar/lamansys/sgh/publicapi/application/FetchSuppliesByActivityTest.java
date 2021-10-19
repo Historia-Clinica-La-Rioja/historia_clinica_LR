810da98a6c51767c95147ba0ecd6a70c64c72b8d
unknown <lucastoloza22@gmail.com>
2021-10-19 10:42:50 -0300

    tg-5146 tests unitarios funcionalidad

---
+++ back-end/hospital-public-api/src/test/java/ar/lamansys/sgh/publicapi/application/FetchSuppliesByActivityTest.java
@@ -0,0 +1,44 @@
+package ar.lamansys.sgh.publicapi.application;
+
+import ar.lamansys.sgh.publicapi.application.fetchsuppliesbyactivity.FetchSuppliesByActivity;
+import ar.lamansys.sgh.publicapi.application.port.out.ActivityInfoStorage;
+import ar.lamansys.sgh.publicapi.domain.SnomedBo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class FetchSuppliesByActivityTest {
+
+    private FetchSuppliesByActivity fetchSuppliesByActivity;
+
+    @Mock
+    private ActivityInfoStorage activityInfoStorage;
+
+    @BeforeEach
+    void setup() {
+        fetchSuppliesByActivity = new FetchSuppliesByActivity(activityInfoStorage);
+    }
+
+    @Test
+    void procedureSuccess() {
+        String refsetCode = "";
+        String provinceCode = "";
+        Long activityId = 10L;
+
+        when(activityInfoStorage.getSuppliesByActivity(refsetCode, provinceCode, activityId)).thenReturn(
+                Collections.singletonList(new SnomedBo("1", "1"))
+        );
+
+        List<SnomedBo> result = fetchSuppliesByActivity.run(refsetCode, provinceCode, activityId);
+        Assertions.assertEquals(result.size(), 1);
+    }
+}
