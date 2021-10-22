5bacbea350f118fc9dd2c0d325eafdf2f4262d2a
etrapani <etrapani04@gmail.com>
2021-10-22 12:16:00 -0300

    tg-5213. [P1] Eliminar MINUS de conceptos relacionados con abortos

--- back-end/sgx-shared/src/main/java/ar/lamansys/sgx/shared/SharedAutoConfiguration.java
+++ back-end/sgx-shared/src/main/java/ar/lamansys/sgx/shared/SharedAutoConfiguration.java
@@ -4,9 +4,11 @@ package ar.lamansys.sgx.shared;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import javax.annotation.PostConstruct;
 
@@ -22,5 +24,17 @@ public class SharedAutoConfiguration {
     @PostConstruct
     public void doLog() {
         LOG.debug("{}", "SharedAutoConfiguration loaded");
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        filter.setIncludeClientInfo(true);
+        return filter;
     }
 }
