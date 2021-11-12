4e0f27290fc2e0113a3c9e3b5a56bb8d963e16fc
bernarditacha <bernarditachacon.bc@gmail.com>
2021-11-12 09:54:45 -0300

    tg-5376 [P2] Comportamiento incorrecto del filtro de líneas de cuidado

--- back-end/hospital-api/src/main/java/net/pladema/establishment/controller/BackofficeCareLineController.java
+++ back-end/hospital-api/src/main/java/net/pladema/establishment/controller/BackofficeCareLineController.java
@@ -3,7 +3,9 @@ package net.pladema.establishment.controller;
 import net.pladema.establishment.controller.constraints.validator.permissions.BackofficeCareLineValidator;
 import net.pladema.establishment.repository.CareLineRepository;
 import net.pladema.establishment.repository.entity.CareLine;
+import net.pladema.sgx.backoffice.repository.BackofficeRepository;
 import net.pladema.sgx.backoffice.rest.AbstractBackofficeController;
+import net.pladema.sgx.backoffice.rest.SingleAttributeBackofficeQueryAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class BackofficeCareLineController extends AbstractBackofficeController<CareLine, Integer> {
 
     public BackofficeCareLineController(CareLineRepository repository, BackofficeCareLineValidator backofficeCareLineValidator) {
-        super(repository, backofficeCareLineValidator);
+        super(new BackofficeRepository<>(
+                repository,
+                new SingleAttributeBackofficeQueryAdapter<>("description")
+        ), backofficeCareLineValidator);
     }
 
 }
