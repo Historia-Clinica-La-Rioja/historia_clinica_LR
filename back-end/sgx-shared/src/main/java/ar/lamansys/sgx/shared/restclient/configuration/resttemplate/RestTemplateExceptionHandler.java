34eeebb5e4aa234c22a518d833a855427c399243
etrapani <etrapani04@gmail.com>
2021-11-25 22:40:43 -0300

    tg-5494. [BE] Agregar body al RestTemplateApiException

--- back-end/sgx-shared/src/main/java/ar/lamansys/sgx/shared/restclient/configuration/resttemplate/RestTemplateExceptionHandler.java
+++ back-end/sgx-shared/src/main/java/ar/lamansys/sgx/shared/restclient/configuration/resttemplate/RestTemplateExceptionHandler.java
@@ -4,18 +4,9 @@ import ar.lamansys.sgx.shared.restclient.configuration.resttemplate.exception.Re
 import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.DefaultResponseErrorHandler;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.CharBuffer;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import org.springframework.web.client.HttpStatusCodeException;
 
 public class RestTemplateExceptionHandler extends DefaultResponseErrorHandler {
 
@@ -24,44 +15,12 @@ public class RestTemplateExceptionHandler extends DefaultResponseErrorHandler {
 	@SneakyThrows
 	@Override
 	public void handleError(ClientHttpResponse httpResponse) {
-		LOG.error("Error en Cliente: {}", httpResponse);
 		try {
-			String body = getErrorMessage(httpResponse);
+			super.handleError(httpResponse);
+		} catch (HttpStatusCodeException e) {
+			var body = e.getResponseBodyAsString();
 			LOG.error("Body error: {}", body);
-			throw new RestTemplateApiException(httpResponse.getStatusCode(), body);
-		} catch (RestTemplateApiException e){
-			throw e;
-		} catch (Exception e){
-			LOG.error("SnowstormApi error", e);
-			throw new RestTemplateApiException(HttpStatus.INTERNAL_SERVER_ERROR, "No se puede parsear el body");
-		}
-	}
-
-	protected String getErrorMessage(ClientHttpResponse httpResponse) throws IOException {
-		int rawStatusCode = httpResponse.getRawStatusCode();
-		String statusText = httpResponse.getStatusText();
-		byte[] responseBody = this.getResponseBody(httpResponse);
-		Charset charset = this.getCharset(httpResponse);
-		String preface = rawStatusCode + " " + statusText + ": ";
-		if (ObjectUtils.isEmpty(responseBody)) {
-			return preface + "[no body]";
-		} else {
-			charset = charset == null ? StandardCharsets.UTF_8 : charset;
-			int maxChars = 200;
-			if (responseBody.length < maxChars * 2) {
-				return preface + "[" + new String(responseBody, charset) + "]";
-			} else {
-				try {
-					Reader reader = new InputStreamReader(new ByteArrayInputStream(responseBody), charset);
-					CharBuffer buffer = CharBuffer.allocate(maxChars);
-					reader.read(buffer);
-					reader.close();
-					buffer.flip();
-					return preface + "[" + buffer.toString() + "... (" + responseBody.length + " bytes)]";
-				} catch (IOException var9) {
-					throw new IllegalStateException(var9);
-				}
-			}
+			throw new RestTemplateApiException(httpResponse.getStatusCode(), body, body);
 		}
 	}
 }
--- back-end/sgx-shared/src/main/java/ar/lamansys/sgx/shared/restclient/configuration/resttemplate/exception/RestTemplateApiException.java
+++ back-end/sgx-shared/src/main/java/ar/lamansys/sgx/shared/restclient/configuration/resttemplate/exception/RestTemplateApiException.java
@@ -1,5 +1,7 @@
 package ar.lamansys.sgx.shared.restclient.configuration.resttemplate.exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
@@ -9,8 +11,21 @@ public class RestTemplateApiException extends Exception {
 
     private HttpStatus statusCode;
 
-    public RestTemplateApiException(HttpStatus statusCode, String message) {
+    private String body;
+
+    public RestTemplateApiException(HttpStatus statusCode, String message, String body) {
         super(message);
         this.statusCode = statusCode;
+        this.body = body;
+    }
+
+    public <T> T mapErrorBody(Class<T> responseType) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(body, responseType);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
