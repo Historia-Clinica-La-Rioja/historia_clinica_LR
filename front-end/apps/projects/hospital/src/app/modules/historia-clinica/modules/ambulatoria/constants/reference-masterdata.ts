e4d649b6dd69eb719759d47269a19f1ac8d36441
jvaldez <joselinavaldez99@gmail.com>
2021-12-20 11:36:41 -0300

    tg-5757 [FE] Agrega archivo que define las constantes que utiliza el filtro

---
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/constants/reference-masterdata.ts
@@ -0,0 +1,6 @@
+//El enum 3 esta reservado para cuando se agregue la visualizacion de las contrarrefencias
+export enum REFERENCE_STATES {
+    WITHOUT_REFERENCES = 'Sin referencia',
+    WITH_REFERENCES = 'Con referencia solicitada',
+    ALL = 'Todos'
+}
