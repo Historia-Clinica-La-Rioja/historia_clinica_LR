b730dea06b33aa3295dad05cd71dc40eebd00717
bernarditacha <bernarditachacon.bc@gmail.com>
2021-12-22 14:33:14 -0300

    tg-5789 [P0] Se debe poder responder contrarreferencias desde cualquier institución

--- back-end/reference-counter-ref/src/main/java/ar/lamansys/refcounterref/application/getreference/exceptions/ReferenceExceptionEnum.java
+++ back-end/reference-counter-ref/src/main/java/ar/lamansys/refcounterref/application/getreference/exceptions/ReferenceExceptionEnum.java
@@ -2,6 +2,5 @@ package ar.lamansys.refcounterref.application.getreference.exceptions;
 
 public enum ReferenceExceptionEnum {
     NULL_PATIENT_ID,
-    NULL_INSTITUTION_ID,
     NULL_CLINICAL_SPECIALTY_ID,
 }
