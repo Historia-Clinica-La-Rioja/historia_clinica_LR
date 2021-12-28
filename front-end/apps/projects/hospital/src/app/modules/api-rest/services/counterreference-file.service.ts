6aaafe88d77a9fb6483f1286f0f0e64b9afc4aca
jvaldez <joselinavaldez99@gmail.com>
2021-12-28 14:55:38 -0300

    tg-5849 [FE] Agrega al servicio el endpoint para descargar los archivos de una contrarreferencia

--- front-end/apps/projects/hospital/src/app/modules/api-rest/services/counterreference-file.service.ts
+++ front-end/apps/projects/hospital/src/app/modules/api-rest/services/counterreference-file.service.ts
@@ -3,11 +3,16 @@ import { Injectable } from '@angular/core';
 import { ContextService } from '@core/services/context.service';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
+import { saveAs } from 'file-saver';
+
+
 
 @Injectable({
 	providedIn: 'root'
 })
 export class CounterreferenceFileService {
+
+	private URL_PREFIX = `${environment.apiBase}/institutions/${this.contextService.institutionId}/counterreference-file/`;
 
 	constructor(
 		private readonly http: HttpClient,
@@ -17,13 +22,20 @@ export class CounterreferenceFileService {
 	uploadCounterreferenceFiles(patientId: number, counterreferenceFile: File): Observable<number> {
 		const fileFormdata = new FormData();
 		fileFormdata.append('file', counterreferenceFile);
-		const url = `${environment.apiBase}/institutions/${this.contextService.institutionId}/counterreference-file/patient/${patientId}/uploadFile`;
+		const url = this.URL_PREFIX + `patient/${patientId}/uploadFile`;
 		return this.http.post<number>(url, fileFormdata);
 	}
 
 	deleteCounterreferenceFiles(filesIds: number[]): Observable<any> {
 		const params = { fileIds: filesIds };
-		const url = `${environment.apiBase}/institutions/${this.contextService.institutionId}/counterreference-file/delete`;
+		const url = this.URL_PREFIX + `delete`;
 		return this.http.delete(url, { params });
+	}
+
+	downloadCounterreferenceFiles(fileId: number, fileName: string): void {
+		const url = this.URL_PREFIX + `download/${fileId}`;
+		this.http.get(url,
+			{ responseType: 'blob' }
+		).subscribe(blob => saveAs(blob, fileName));
 	}
 }
