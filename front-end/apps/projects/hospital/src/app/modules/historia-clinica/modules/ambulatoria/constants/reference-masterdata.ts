759fde652ca8f96744a64b98c4a9a0cfef765b7e
jvaldez <joselinavaldez99@gmail.com>
2021-12-29 09:27:53 -0300

    tg-5769 [FE] Agrega la constante correspondiente para filtrar por contrarreferencia

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/constants/reference-masterdata.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/constants/reference-masterdata.ts
@@ -1,6 +1,6 @@
-//El enum 3 esta reservado para cuando se agregue la visualizacion de las contrarrefencias
 export enum REFERENCE_STATES {
     WITHOUT_REFERENCES = 'Sin referencia',
     WITH_REFERENCES = 'Con referencia solicitada',
+    WITH_COUNTERREFERENCE = 'Con contrarreferencia',
     ALL = 'Todos'
 }
