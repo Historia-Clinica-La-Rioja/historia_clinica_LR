74ac8452af8ce77c5de7b5810d6a2c671c77bc60
Tomás Medina <medinatomasariel@gmail.com>
2022-01-03 16:20:29 -0300

    tg-5752 [FE] Ocultar Login de hospitales cuando está activo el de Keycloak

--- front-end/apps/projects/hospital/src/app/modules/auth/routes/login/login.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/auth/routes/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from "@api-rest/services/auth.service";
 
 @Component({
 	selector: 'app-login',
@@ -7,5 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent {
 
+	isOAuthEnabled = false;
+
+	constructor(private readonly authService: AuthService) {
+		authService.getOauthConfig().subscribe(config => this.isOAuthEnabled = config.enabled);
+	}
 
 }
