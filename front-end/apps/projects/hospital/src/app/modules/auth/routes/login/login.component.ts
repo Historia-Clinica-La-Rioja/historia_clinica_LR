199fa9ad2b2ac3cb3a0b5ef413e7b4d6ecd12190
Tomás Medina <medinatomasariel@gmail.com>
2022-01-04 14:05:40 -0300

    tg-5891 [FE] Corregir estilos en componentes de login

--- front-end/apps/projects/hospital/src/app/modules/auth/routes/login/login.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/auth/routes/login/login.component.ts
@@ -10,8 +10,13 @@ export class LoginComponent {
 
 	isOAuthEnabled = false;
 
+	isLoading = true;
+
 	constructor(private readonly authService: AuthService) {
-		authService.getOauthConfig().subscribe(config => this.isOAuthEnabled = config.enabled);
+		authService.getOauthConfig().subscribe(config => {
+			this.isOAuthEnabled = config.enabled;
+			this.isLoading = false;
+		});
 	}
 
 }
