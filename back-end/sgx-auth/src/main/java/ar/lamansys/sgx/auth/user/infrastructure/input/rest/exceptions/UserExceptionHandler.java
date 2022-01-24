230c5b44e7f163c2dcb91849b1af39902b7d17f1
etrapani <etrapani04@gmail.com>
2022-01-24 09:20:24 -0300

    tg-6066. [BE] Agregar código de excepción al rest exception handler

--- back-end/sgx-auth/src/main/java/ar/lamansys/sgx/auth/user/infrastructure/input/rest/exceptions/UserExceptionHandler.java
+++ back-end/sgx-auth/src/main/java/ar/lamansys/sgx/auth/user/infrastructure/input/rest/exceptions/UserExceptionHandler.java
@@ -16,53 +16,50 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Locale;
-
 @Order(Ordered.HIGHEST_PRECEDENCE)
-@RestControllerAdvice(basePackages = {"ar.lamansys.sgx.auth",
-		"net.pladema.user.infrastructure.input.rest"})
+@RestControllerAdvice(basePackages = {"ar.lamansys.sgx.auth"})
 public class UserExceptionHandler {
 
 	private static final Logger LOG = LoggerFactory.getLogger(UserExceptionHandler.class);
 
 	@ResponseStatus(HttpStatus.PRECONDITION_FAILED)
 	@ExceptionHandler({ RegisterUserException.class })
-	protected ApiErrorMessageDto handleRegisterUserException(RegisterUserException ex, Locale locale) {
+	protected ApiErrorMessageDto handleRegisterUserException(RegisterUserException ex) {
 		LOG.debug("RegisterUserException exception -> {}", ex.getMessage());
-		return new ApiErrorMessageDto(null, ex.getMessage());
+		return new ApiErrorMessageDto(ex.getCode().name(), ex.getMessage());
 	}
 
 	@ResponseStatus(HttpStatus.PRECONDITION_FAILED)
 	@ExceptionHandler({ PasswordResetTokenStorageException.class })
-	protected ApiErrorMessageDto handlePasswordResetTokenStorageException(PasswordResetTokenStorageException ex, Locale locale) {
+	protected ApiErrorMessageDto handlePasswordResetTokenStorageException(PasswordResetTokenStorageException ex) {
 		LOG.debug("PasswordResetTokenStorageException exception -> {}", ex.getMessage());
-		return new ApiErrorMessageDto(null, ex.getMessage());
+		return new ApiErrorMessageDto(ex.getCode().name(), ex.getMessage());
 	}
 
 	@ResponseStatus(HttpStatus.PRECONDITION_FAILED)
 	@ExceptionHandler({ UserException.class })
-	protected ApiErrorMessageDto handleUserException(UserException ex, Locale locale) {
+	protected ApiErrorMessageDto handleUserException(UserException ex) {
 		LOG.debug("UserException exception -> {}", ex.getMessage());
-		return new ApiErrorMessageDto(null, ex.getMessage());
+		return new ApiErrorMessageDto(ex.getCode().name(), ex.getMessage());
 	}
 
 	@ResponseStatus(HttpStatus.PRECONDITION_FAILED)
 	@ExceptionHandler({ UserStorageException.class })
-	protected ApiErrorMessageDto handleUserStorageException(UserStorageException ex, Locale locale) {
+	protected ApiErrorMessageDto handleUserStorageException(UserStorageException ex) {
 		LOG.debug("UserStorageException exception -> {}", ex.getMessage());
-		return new ApiErrorMessageDto(null, ex.getMessage());
+		return new ApiErrorMessageDto(ex.getCode().name(), ex.getMessage());
 	}
 
 	@ResponseStatus(HttpStatus.PRECONDITION_FAILED)
 	@ExceptionHandler({ UserPasswordException.class })
-	protected ApiErrorMessageDto handleUserPasswordException(UserPasswordException ex, Locale locale) {
+	protected ApiErrorMessageDto handleUserPasswordException(UserPasswordException ex) {
 		LOG.debug("UserPasswordException exception -> {}", ex.getMessage());
-		return new ApiErrorMessageDto(null, ex.getMessage());
+		return new ApiErrorMessageDto(ex.getCode().name(), ex.getMessage());
 	}
 
 	@ResponseStatus(HttpStatus.PRECONDITION_FAILED)
 	@ExceptionHandler({ OAuthUserException.class })
-	protected ApiErrorMessageDto handleOAuthUserException(OAuthUserException ex, Locale locale) {
+	protected ApiErrorMessageDto handleOAuthUserException(OAuthUserException ex) {
 		LOG.debug("OAuthUserException exception -> {}", ex.getMessage());
 		return new ApiErrorMessageDto(ex.getCode().name(), ex.getMessage());
 	}
