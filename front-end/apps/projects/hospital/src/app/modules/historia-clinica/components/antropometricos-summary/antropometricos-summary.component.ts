a0efef30f84948da185ae4cf30c8c3ccd5915f0c
Lucas Benjamin Obyszuk <lucasobyszuk@gmail.com>
2022-01-25 09:54:04 -0300

    tg-6051. [FE] Mostrar IMC en la Información Antropométrica

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/antropometricos-summary/antropometricos-summary.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/antropometricos-summary/antropometricos-summary.component.ts
@@ -41,6 +41,10 @@ export class AntropometricosSummaryComponent implements OnInit {
 		this.anthropometricData$.subscribe(
 			(anthropometricData: AnthropometricDataDto) => {
 				if (anthropometricData) {
+					const BMI = this.calculateBMI(anthropometricData.height?.value, anthropometricData.weight?.value);
+					if (BMI) {
+						anthropometricData.bmi = { value: BMI };
+					}
 					this.details = [];
 					Object.keys(this.LABELS).forEach(
 						key => {
@@ -74,4 +78,21 @@ export class AntropometricosSummaryComponent implements OnInit {
 		}
 		);
 	}
+
+	private calculateBMI(height: string, weight: string): string {
+		if (!height || !weight) {
+			return undefined;
+		}
+
+		const heightNumber = parseInt(height, 10) / 100;
+		const weightNumber = parseInt(weight, 10);
+
+		if (heightNumber > 0 && weightNumber > 0) {
+			const result = Math.round((weightNumber / (heightNumber * heightNumber)) * 10) / 10;
+			return result.toString(10);
+		}
+
+		return undefined;
+	}
+
 }
