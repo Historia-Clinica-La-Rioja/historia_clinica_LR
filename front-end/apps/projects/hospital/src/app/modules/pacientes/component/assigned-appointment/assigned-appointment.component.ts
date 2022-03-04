2456cda781539ab71c99c50b24028b40e6e549cf
Lucas Benjamin Obyszuk <lucasobyszuk@gmail.com>
2022-03-04 09:46:56 -0300

    tg-6480. [FE] Acomodar formato de fecha y hora de los turnos para coincidir con el diseño

--- front-end/apps/projects/hospital/src/app/modules/pacientes/component/assigned-appointment/assigned-appointment.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/pacientes/component/assigned-appointment/assigned-appointment.component.ts
@@ -1,5 +1,8 @@
+import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { AssignedAppointmentDto } from '@api-rest/api-model';
+import { dateDtoToDate, timeDtoToDate } from '@api-rest/mapper/date-dto.mapper';
+import { DatePipeFormat } from '@core/utils/date.utils';
 
 @Component({
   selector: 'app-assigned-appointment',
@@ -10,6 +13,14 @@ export class AssignedAppointmentComponent {
 
   @Input() appointment: AssignedAppointmentDto;
 
-  constructor() { }
+  constructor(private readonly datePipe: DatePipe) { }
+
+  getViewDate(): string {
+    return this.datePipe.transform(dateDtoToDate(this.appointment.date), DatePipeFormat.FULL_DATE);
+  }
+
+  getViewTime(): string {
+    return this.datePipe.transform(timeDtoToDate(this.appointment.hour), DatePipeFormat.SHORT_TIME);
+  }
 
 }
