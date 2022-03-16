604f0c2c43ea56eb085807320a2fb10383830d9a
jvaldez <joselinavaldez99@gmail.com>
2022-03-16 14:33:49 -0300

    tg-6609 [FE] Agrega la visualización de los días

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/internment-indications-card/internment-indications-card.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/internment-indications-card/internment-indications-card.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { INTERNMENT_INDICATIONS } from "@historia-clinica/constants/summaries";
+import { getDay, getMonth, isTomorrow, isYesterday, isToday, differenceInCalendarDays } from "date-fns";
+import { MONTHS_OF_YEAR, DAYS_OF_WEEK } from "@historia-clinica/modules/ambulatoria/constants/internment-indications";
+import { BehaviorSubject } from "rxjs";
+import { InternmentEpisodeService } from "@api-rest/services/internment-episode.service";
 
 @Component({
 	selector: 'app-internment-indications-card',
@@ -9,9 +13,55 @@ import { INTERNMENT_INDICATIONS } from "@historia-clinica/constants/summaries";
 export class InternmentIndicationsCardComponent implements OnInit {
 
 	internmentIndication = INTERNMENT_INDICATIONS;
+	viewDay$: BehaviorSubject<ViewDate>;
+	isToday = true;
+	isYesterday = false;
+	isTomorrow = false;
+	actualDate: Date = new Date();
+	entryDate: Date;
+	currentViewIsEntryDate = false;
+	@Input() internmentEpisodeId: number;
 
-	constructor() { }
+	constructor(
+		private readonly internmentEpisode: InternmentEpisodeService,
+	) { }
 
-	ngOnInit(): void { }
+	ngOnInit(): void {
+		this.viewDay$ = new BehaviorSubject<ViewDate>({
+			nameDay: DAYS_OF_WEEK[getDay(this.actualDate)],
+			numberDay: this.actualDate.getDate(),
+			month: MONTHS_OF_YEAR[getMonth(this.actualDate)]
+		});
+		this.internmentEpisode.getInternmentEpisode(this.internmentEpisodeId).subscribe(
+			internmentEpisode => this.entryDate = new Date(internmentEpisode.entryDate)
+		);
+	}
 
+	viewAnotherDay(daysToMove: number) {
+		this.actualDate.setDate(this.actualDate.getDate() + daysToMove);
+		this.isToday = isToday(this.actualDate);
+		this.isTomorrow = isTomorrow(this.actualDate);
+		this.isYesterday = isYesterday(this.actualDate);
+		this.loadDay();
+	}
+
+	private loadDay() {
+		const differenceInDays = differenceInCalendarDays(this.actualDate, this.entryDate);
+		if (differenceInDays >= 0) {
+			this.currentViewIsEntryDate = false;
+			this.viewDay$.next({
+				nameDay: DAYS_OF_WEEK[getDay(this.actualDate)],
+				numberDay: this.actualDate.getDate(),
+				month: MONTHS_OF_YEAR[getMonth(this.actualDate)]
+			});
+		}
+		if (differenceInDays <= 0)
+			this.currentViewIsEntryDate = true;
+	}
+}
+
+interface ViewDate {
+	nameDay: string,
+	numberDay: number,
+	month: string,
 }
