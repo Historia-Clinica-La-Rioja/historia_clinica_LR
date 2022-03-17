52a201c34f1fd3102192f27579cc0cdcb92f01c8
Tano <lucas.bergottini@gmail.com>
2022-03-17 13:56:30 -0300

    tg-6503 [FE] Incorporar componente de factores de riesgo en el formulario de nuevo episodio

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/guardia/components/adult-gynecological-triage/adult-gynecological-triage.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/guardia/components/adult-gynecological-triage/adult-gynecological-triage.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { TriageAdultGynecologicalDto } from '@api-rest/api-model';
 import { getError, hasError } from '@core/utils/form.utils';
 import { RiskFactorsValue, RiskFactorsFormService } from '../../../../services/risk-factors-form.service';
+import { FactoresDeRiesgoNuevaConsultaService } from '../../../ambulatoria/services/factores-de-riesgo-nueva-consulta.service'
 import { GuardiaMapperService } from '../../services/guardia-mapper.service';
 
 @Component({
@@ -17,6 +18,7 @@ export class AdultGynecologicalTriageComponent implements OnInit {
 	@Input() disableConfirmButton: boolean;
 	@Output() confirm = new EventEmitter();
 	@Output() cancel = new EventEmitter();
+	
 
 	hasError = hasError;
 	getError = getError;
@@ -26,18 +28,22 @@ export class AdultGynecologicalTriageComponent implements OnInit {
 
 	adultGynecologicalForm: FormGroup;
 	riskFactorsForm: FormGroup;
+	factoresDeRiesgoNuevaConsultaService: FactoresDeRiesgoNuevaConsultaService;
+	
 
 	constructor(
 		private formBuilder: FormBuilder,
 		private guardiaMapperService: GuardiaMapperService,
 		public riskFactorsFormService: RiskFactorsFormService,
-	) { }
+	) { 
+		this.factoresDeRiesgoNuevaConsultaService = new FactoresDeRiesgoNuevaConsultaService(formBuilder);
+	}
 
 	ngOnInit(): void {
 		this.adultGynecologicalForm = this.formBuilder.group({
 			evaluation: [null]
 		});
-		this.riskFactorsForm = this.riskFactorsFormService.buildForm();
+		this.riskFactorsForm = this.factoresDeRiesgoNuevaConsultaService.getForm();
 	}
 
 	setTriageCategoryId(triageCategoryId: number): void {
