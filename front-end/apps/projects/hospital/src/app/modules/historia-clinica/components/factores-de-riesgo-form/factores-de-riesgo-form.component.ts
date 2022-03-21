c71bc2a718fc611d5c9585c137b79ff78d9b2a7f
Lucas Benjamin Obyszuk <lucasobyszuk@gmail.com>
2022-03-21 14:07:10 -0300

    tg-6654. [FE] Agregar errores debajo de los campos del formulario de carga de factores de riesgo

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/factores-de-riesgo-form/factores-de-riesgo-form.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/factores-de-riesgo-form/factores-de-riesgo-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { getError, hasError } from '@core/utils/form.utils';
 import { isNumberOrDot } from '@core/utils/pattern.utils';
 import { FactoresDeRiesgoFormService } from '../../services/factores-de-riesgo-form.service';
 
@@ -12,7 +13,10 @@ export class FactoresDeRiesgoFormComponent implements OnInit {
   @Input() factoresDeRiesgoFormService: FactoresDeRiesgoFormService;
   @Input() showPreloadData: boolean = false;
   @Input() showRecomendation: boolean = false;
+  @Input() showErrors: boolean = false;
   readonly isNumberOrDot = isNumberOrDot;
+  readonly hasError = hasError;
+  readonly getError = getError;
 
   constructor() { }
 
