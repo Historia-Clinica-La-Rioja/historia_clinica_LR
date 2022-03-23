a1f8392c8fb08976262486a886fd1531053c866d
Tomás Medina <medinatomasariel@gmail.com>
2022-03-23 15:33:16 -0300

    tg-6684 [Backoffice] Ocultar botón de creación de grupo de Snomed para usuario ROOT

--- front-end/backoffice/src/modules/snomedgroups/SnomedGroupList.js
+++ front-end/backoffice/src/modules/snomedgroups/SnomedGroupList.js
@@ -2,19 +2,25 @@ import {
     List,
     Datagrid,
     TextField,
+    usePermissions,
 } from 'react-admin';
 import SgxDateField from "../../dateComponents/sgxDateField";
+import { ADMINISTRADOR, ADMINISTRADOR_INSTITUCIONAL_BACKOFFICE } from "../roles";
 
-const SnomedGroupList = props => (
-    <List {...props} bulkActionButtons={false}>
-        <Datagrid rowClick="show">
-            <TextField source="id" />
-            <TextField source="description" />
-            <TextField source="customId" />
-            <SgxDateField source="lastUpdate" />
-        </Datagrid>
-    </List>
-);
+const SnomedGroupList = props => {
+    const { permissions } = usePermissions();
+    const userIsAdminOrInstitutionalAdmin = permissions?.roleAssignments?.filter(roleAssignment => (roleAssignment.role === ADMINISTRADOR.role) || (roleAssignment.role === ADMINISTRADOR_INSTITUCIONAL_BACKOFFICE.role)).length > 0;
+    return (
+        <List {...props} bulkActionButtons={false} hasCreate={userIsAdminOrInstitutionalAdmin}>
+            <Datagrid rowClick="show">
+                <TextField source="id" />
+                <TextField source="description" />
+                <TextField source="customId" />
+                <SgxDateField source="lastUpdate" />
+            </Datagrid>
+        </List>
+    );
+};
 
 export default SnomedGroupList;
 
