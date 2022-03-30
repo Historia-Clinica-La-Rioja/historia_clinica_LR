86a6a51dc20991a649fa29fdd8892144cbe3b4d4
FacundoBarreiros <facundobarreiros@gmail.com>
2022-03-30 11:05:53 -0300

    tg-6772 [FE] Conectar con el BE para agregar otras indicaciones y obtener tipo de indicaciones

--- front-end/apps/projects/hospital/src/app/modules/api-rest/services/internment-indication.service.ts
+++ front-end/apps/projects/hospital/src/app/modules/api-rest/services/internment-indication.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { ContextService } from "@core/services/context.service";
 import { Observable } from "rxjs";
 import { environment } from "@environments/environment";
-import { DietDto } from "@api-rest/api-model";
+import { DietDto, IndicationDto, OtherIndicationDto } from "@api-rest/api-model";
 
 @Injectable({
 	providedIn: 'root'
@@ -29,4 +29,18 @@ export class InternmentIndicationService {
 		const url = `${environment.apiBase}/institutions/${this.contextService.institutionId}/internments/${internmentEpisodeId}/diet`;
 		return this.http.post<DietDto>(url, indication);
 	}
+
+	addOtherIndication(otherIndication: OtherIndicationDto, internmentEpisodeId: number): Observable<OtherIndicationDto> {
+		const url = `${environment.apiBase}/institutions/${this.contextService.institutionId}/internments/${internmentEpisodeId}/other-indication`;
+		return this.http.post<OtherIndicationDto>(url, otherIndication);
+	}
+
+	getOtherIndicationTypes(): Observable<OtherIndicationTypeDto[]> {
+		const url = `${environment.apiBase}/internments/masterdata/other-indication-type`;
+		return this.http.get<OtherIndicationTypeDto[]>(url);
+	}
+}
+export interface OtherIndicationTypeDto {
+	id: number,
+	description: string
 }
