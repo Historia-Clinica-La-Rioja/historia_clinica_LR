835b0f6b948e36399e5edb99a23809e12db6e974
FacundoBarreiros <facundobarreiros@gmail.com>
2022-04-14 08:43:50 -0300

    tg-6851[FE] Crear componente para agregar fármaco

---
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/modules/indicacion/dialogs/pharmaco/pharmaco.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pharmaco',
+  templateUrl: './pharmaco.component.html',
+  styleUrls: ['./pharmaco.component.scss']
+})
+export class PharmacoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
