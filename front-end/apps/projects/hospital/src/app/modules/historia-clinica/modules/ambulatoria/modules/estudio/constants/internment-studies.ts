5df027886a7f7ae928d222a81546aa7a69576efe
Mateo Maly Varni <mateomv.mmv@gmail.com>
2022-05-06 12:23:31 -0300

    tg-7124 Añadir íconos y títulos para las categorías de estudio

---
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/modules/estudio/constants/internment-studies.ts
@@ -0,0 +1,41 @@
+import { Title } from "@presentation/components/indication/indication.component";
+
+export const IMAGES: Title = {
+	title: 'ambulatoria.paciente.studies.sections.IMAGES',
+	svgIcon: 'images',
+}
+
+export const LABORATORY: Title = {
+	title: 'ambulatoria.paciente.studies.sections.LABORATORY',
+	svgIcon: 'laboratory',
+}
+
+export const PATHOLOGIC_ANATOMY: Title = {
+	title: 'ambulatoria.paciente.studies.sections.PATHOLOGIC_ANATOMY',
+	svgIcon: 'pathologic_anatomy',
+}
+
+export const HEMOTHERAPY: Title = {
+	title: 'ambulatoria.paciente.studies.sections.HEMOTHERAPY',
+	svgIcon: 'hemotherapy',
+}
+
+export const SURGICAL_PROCEDURE: Title = {
+	title: 'ambulatoria.paciente.studies.sections.SURGICAL_PROCEDURE',
+	svgIcon: 'surgical_procedure',
+}
+
+export const OTHER_PROCEDURES_AND_PRACTICES: Title = {
+	title: 'ambulatoria.paciente.studies.sections.OTHER_PROCEDURES_AND_PRACTICES',
+	svgIcon: 'other_procedures_and_practices',
+}
+
+export const ADVICE: Title = {
+	title: 'ambulatoria.paciente.studies.sections.ADVICE',
+	svgIcon: 'advice',
+}
+
+export const EDUCATION: Title = {
+	title: 'ambulatoria.paciente.studies.sections.EDUCATION',
+	svgIcon: 'education',
+}
