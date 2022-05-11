ae6362c22bc06d391ab68eebbe093d3a1e6cc34e
Cristian Quevedo <cristianquevedocqr@gmail.com>
2022-05-11 09:45:52 -0300

    Se creo la clase QueryFactoryPR que crea la consulta de reporte epidemiologico 1 y 2

---
+++ back-end/hospital-api/src/main/java/net/pladema/programreports/repository/QueryFactoryPR.java
@@ -0,0 +1,36 @@
+package net.pladema.programreports.repository;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import java.util.List;
+
+@Repository
+public class QueryFactoryPR {
+
+	@PersistenceContext
+	private final EntityManager entityManager;
+
+	public QueryFactoryPR(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<ConsultationDetailEpiI> query(Integer institutionId){
+		Query query = entityManager.createNamedQuery("ProgramReports.ConsultationDetailEpiI");
+		query.setParameter("institutionId", institutionId);
+		List<ConsultationDetailEpiI> data = query.getResultList();
+		return data;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<ConsultationDetailEpiII> queryII(Integer institutionId){
+		Query query = entityManager.createNamedQuery("ProgramReports.ConsultationDetailEpiII");
+		query.setParameter("institutionId", institutionId);
+		List<ConsultationDetailEpiII> data = query.getResultList();
+		return data;
+	}
+}
