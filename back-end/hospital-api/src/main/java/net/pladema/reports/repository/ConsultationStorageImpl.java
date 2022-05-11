bb6dc4f305d2df02a5f950ead82d940dab1d63a7
tcepeda <tcepeda@pladema.net>
2022-05-11 11:38:43 -0300

    tg-7231 Crear query para obtener las consultas odontologicas que correspondan a un paciente en particular

--- back-end/hospital-api/src/main/java/net/pladema/reports/repository/ConsultationStorageImpl.java
+++ back-end/hospital-api/src/main/java/net/pladema/reports/repository/ConsultationStorageImpl.java
@@ -1,12 +1,13 @@
 package net.pladema.reports.repository;
 
-import org.springframework.stereotype.Service;
-
-import javax.persistence.EntityManager;
 import java.math.BigInteger;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.stereotype.Service;
 
 @Service
 public class ConsultationStorageImpl implements ConsultationStorage {
@@ -19,7 +20,9 @@ public class ConsultationStorageImpl implements ConsultationStorage {
 
     @Override
     public List<ConsultationsVo> fetchAllByPatientId(Integer patientId) {
-        String sqlString = "WITH t AS ("
+		List<ConsultationsVo> result = new ArrayList<>();
+
+        String outpatientSqlString = "WITH t AS ("
                 +"  SELECT oc.id, d.id as doc_id, oc.start_date AS start_date, oc.patient_id, "
                 +"  oc.clinical_specialty_id, oc.doctor_id "
                 +"  FROM {h-schema}outpatient_consultation AS oc "
@@ -41,22 +44,54 @@ public class ConsultationStorageImpl implements ConsultationStorage {
                 +"  JOIN {h-schema}person AS pe ON (pe.id = hp.person_id) "
 				+"  JOIN {h-schema}person_extended AS pee ON (pe.id = pee.person_id) "
                 +"  ORDER BY t.start_date DESC";
-        List<Object[]> queryResult = entityManager.createNativeQuery(sqlString)
+        List<Object[]> outpatientResult = entityManager.createNativeQuery(outpatientSqlString)
                 .setParameter("patientId", patientId)
                 .getResultList();
-        List<ConsultationsVo> result = new ArrayList<>();
-        queryResult.forEach(a ->
-                result.add(new ConsultationsVo(
-                        (Integer) a[0],
-                        a[1] != null ? ((BigInteger)a[1]).longValue() : null,
-                        a[2] != null ? ((Date)a[2]).toLocalDate() : null,
-                        (String) a[3],
-                        (String) a[4],
-                        (String) a[5],
-                        (String) a[6],
+
+		outpatientResult.forEach(a ->
+				result.add(new ConsultationsVo(
+						(Integer) a[0],
+						a[1] != null ? ((BigInteger)a[1]).longValue() : null,
+						a[2] != null ? ((Date)a[2]).toLocalDate() : null,
+						(String) a[3],
+						(String) a[4],
+						(String) a[5],
+						(String) a[6],
 						(String) a[7],
-                        (String) a[8]))
-        );
+						(String) a[8]))
+		);
+
+		String odontologySqlString = "WITH t AS ("
+				+"  SELECT oc.id, d.id as doc_id, oc.performed_date AS start_date, oc.patient_id, "
+				+"  oc.clinical_specialty_id, oc.doctor_id "
+				+"  FROM {h-schema}odontology_consultation AS oc "
+				+  "JOIN {h-schema}document AS d ON (d.source_id = oc.id AND d.source_type_id = 6)"
+				+"  WHERE oc.patient_id = :patientId "
+				+"  AND oc.billable = true) "
+				+"  SELECT t.id, t.doc_id, t.start_date, cs.name, pe.first_name, pe.middle_names, pe.last_name, pe.other_last_names, pee.name_self_determination "
+				+"  FROM t "
+				+"  LEFT JOIN {h-schema}clinical_specialty AS cs ON (cs.id = t.clinical_specialty_id) "
+				+"  JOIN {h-schema}healthcare_professional AS hp ON (hp.id = t.doctor_id) "
+				+"  JOIN {h-schema}person AS pe ON (pe.id = hp.person_id) "
+				+"  JOIN {h-schema}person_extended AS pee ON (pe.id = pee.person_id) "
+				+"  ORDER BY t.start_date DESC";
+		List<Object[]> odontologyResult = entityManager.createNativeQuery(odontologySqlString)
+				.setParameter("patientId", patientId)
+				.getResultList();
+
+		odontologyResult.forEach(a ->
+				result.add(new ConsultationsVo(
+						(Integer) a[0],
+						a[1] != null ? ((BigInteger)a[1]).longValue() : null,
+						a[2] != null ? ((Date)a[2]).toLocalDate() : null,
+						(String) a[3],
+						(String) a[4],
+						(String) a[5],
+						(String) a[6],
+						(String) a[7],
+						(String) a[8]))
+		);
+
         return result;
     }
 }
