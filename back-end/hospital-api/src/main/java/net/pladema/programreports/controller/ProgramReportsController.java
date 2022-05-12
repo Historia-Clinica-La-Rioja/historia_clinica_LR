61c44652cc5fded47bc5efd5501819fdd54ca803
Cristian Quevedo <cristianquevedocqr@gmail.com>
2022-05-12 09:45:27 -0300

    Se modifico la clase controlador ProgramReportsController agregando el endpoint correspondiente al reporte de recupero

--- back-end/hospital-api/src/main/java/net/pladema/programreports/controller/ProgramReportsController.java
+++ back-end/hospital-api/src/main/java/net/pladema/programreports/controller/ProgramReportsController.java
@@ -7,6 +7,8 @@ import net.pladema.programreports.service.ExcelServiceEpiI;
 
 import net.pladema.programreports.service.ExcelServiceEpiII;
 
+import net.pladema.programreports.service.ExcelServiceRecupero;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,14 +35,17 @@ public class ProgramReportsController {
 
 	private final ExcelServiceEpiII excelServiceEpiII;
 
+	private final ExcelServiceRecupero excelServiceRecupero;
+
 	private final QueryFactoryPR queryFactoryPR;
 
 	private final LocalDateMapper localDateMapper;
 
 
-	public ProgramReportsController(ExcelServiceEpiI excelServiceEpiI, ExcelServiceEpiII excelServiceEpiII, QueryFactoryPR queryFactoryPR, LocalDateMapper localDateMapper) {
+	public ProgramReportsController(ExcelServiceEpiI excelServiceEpiI, ExcelServiceEpiII excelServiceEpiII, ExcelServiceRecupero excelServiceRecupero, QueryFactoryPR queryFactoryPR, LocalDateMapper localDateMapper) {
 		this.excelServiceEpiI = excelServiceEpiI;
 		this.excelServiceEpiII = excelServiceEpiII;
+		this.excelServiceRecupero = excelServiceRecupero;
 		this.queryFactoryPR = queryFactoryPR;
 		this.localDateMapper = localDateMapper;
 	}
@@ -97,6 +102,42 @@ public class ProgramReportsController {
 		LocalDate endDate = localDateMapper.fromStringToLocalDate(toDate);
 
 		IWorkbook wb = this.excelServiceEpiII.buildExcelFromQuery(tittle, headers, this.queryFactoryPR.queryII(institutionId));
+
+		String filename = tittle + "." + wb.getExtension();
+		response.addHeader("Content-disposition", "attachment;filename=" + filename);
+		response.setContentType(wb.getContentType());
+
+		OutputStream out = response.getOutputStream();
+		wb.write(out);
+		out.close();
+		out.flush();
+		response.flushBuffer();
+	}
+
+	@GetMapping(value = "/{institutionId}/monthlyRecupero")
+	public @ResponseBody
+	void getMonthlyRecuperoExcelReport(
+			@PathVariable Integer institutionId,
+			@RequestParam(value = "fromDate", required = false) String fromDate,
+			@RequestParam(value = "toDate", required = false) String toDate,
+			@RequestParam(value = "clinicalSpecialtyId", required = false) Integer clinicalSpecialtyId,
+			@RequestParam(value = "doctorId", required = false) Integer doctorId,
+			HttpServletResponse response
+	) throws Exception {
+		LOG.debug("Se creará el excel {}", institutionId);
+		LOG.debug("Inputs parameters -> institutionId {}, fromDate {}, toDate{}", institutionId, fromDate, toDate);
+
+		String tittle = "Recupero";
+		String [] headers = new String[]{"Unidad Operativa", "Prestador", "DNI", "Fecha de atención", "Cons.N°", "DNI Paciente", "Nombre Paciente", "Sexo", "Genero",
+				"Nombre con el que se identifica", "Fecha de nacimiento", "Edad a fecha del turno", "Edad a Hoy", "Etnia", "Obra/s social/es", "Domicilio",
+				"Localidad", "Nivel de Instrucción", "Situación laboral", "Presión sistólica", "Presión diastólica", "Presión arterial media", "Temperatura",
+				"Frecuencia cardíaca", "Presión respiratoria", "Saturación de hemoglobina con oxígeno", "Altura", "Peso", "Indice de masa corporal", "Motivos",
+				"Procedimientos", "Problemas", "Medicación", "Evolución"};
+
+		LocalDate startDate = localDateMapper.fromStringToLocalDate(fromDate);
+		LocalDate endDate = localDateMapper.fromStringToLocalDate(toDate);
+
+		IWorkbook wb = this.excelServiceRecupero.buildExcelFromQuery(tittle, headers, this.queryFactoryPR.queryIII(institutionId));
 
 		String filename = tittle + "." + wb.getExtension();
 		response.addHeader("Content-disposition", "attachment;filename=" + filename);
