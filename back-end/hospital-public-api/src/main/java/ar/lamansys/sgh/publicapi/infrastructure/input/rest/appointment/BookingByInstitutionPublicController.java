8d2bf34f4829b1c4948b8aab8cd5e07ab3025d8b
Ezequiel Trapani <etrapani04@gmail.com>
2022-05-17 13:56:40 -0300

    tg-7299. [BE] Reordenar urls de API para separar api

---
+++ back-end/hospital-public-api/src/main/java/ar/lamansys/sgh/publicapi/infrastructure/input/rest/appointment/BookingByInstitutionPublicController.java
@@ -0,0 +1,68 @@
+package ar.lamansys.sgh.publicapi.infrastructure.input.rest.appointment;
+
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.lamansys.sgh.shared.infrastructure.input.service.appointment.SharedAppointmentPort;
+import ar.lamansys.sgh.shared.infrastructure.input.service.appointment.dto.PublicAppointmentListDto;
+import ar.lamansys.sgh.shared.infrastructure.input.service.booking.BookingDto;
+import ar.lamansys.sgh.shared.infrastructure.input.service.booking.SharedBookingPort;
+import ar.lamansys.sgx.shared.dates.configuration.LocalDateMapper;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/public-api/institution/{sisaCode}/appointment/booking")
+public class BookingByInstitutionPublicController {
+	private final SharedBookingPort bookAppointmentPort;
+	private final SharedAppointmentPort appointmentPort;
+	private final LocalDateMapper localDateMapper;
+
+	public BookingByInstitutionPublicController(SharedBookingPort bookAppointmentPort,
+												SharedAppointmentPort appointmentPort,
+												LocalDateMapper localDateMapper) {
+		this.bookAppointmentPort = bookAppointmentPort;
+		this.appointmentPort = appointmentPort;
+		this.localDateMapper = localDateMapper;
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public String bookPreappointment(@RequestBody BookingDto bookingDto) {
+		return bookAppointmentPort.makeBooking(bookingDto);
+	}
+
+
+	@PutMapping("/cancel")
+	public void cancelBooking(@RequestBody String uuid) {
+		bookAppointmentPort.cancelBooking(uuid);
+		log.debug("cancel booking {}", uuid);
+	}
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public Collection<PublicAppointmentListDto> getBookingList(
+			@PathVariable(name = "sisaCode") String sisaCode,
+			@RequestParam(name = "identificationNumber", required = false) String identificationNumber,
+			@RequestParam(name = "startDate", required = false) String startDateStr,
+			@RequestParam(name = "endDate", required = false) String endDateStr
+	) {
+		LocalDate startDate = localDateMapper.fromStringToLocalDate(startDateStr);
+		LocalDate endDate = localDateMapper.fromStringToLocalDate(endDateStr);
+		return appointmentPort.fetchAppointments(sisaCode, identificationNumber, List.of((short)6), startDate, endDate);
+	}
+
+}
--- back-end/hospital-public-api/src/main/java/ar/lamansys/sgh/publicapi/infrastructure/input/rest/appointment/BookingPublicController.java
+++ back-end/hospital-public-api/src/main/java/ar/lamansys/sgh/publicapi/infrastructure/input/rest/appointment/BookingPublicController.java
@@ -1,73 +1,29 @@
 package ar.lamansys.sgh.publicapi.infrastructure.input.rest.appointment;
 
 
-import java.time.LocalDate;
-import java.util.Collection;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import ar.lamansys.sgh.shared.infrastructure.input.service.appointment.SharedAppointmentPort;
-import ar.lamansys.sgh.shared.infrastructure.input.service.appointment.dto.PublicAppointmentListDto;
-import ar.lamansys.sgh.shared.infrastructure.input.service.booking.BookingDto;
 import ar.lamansys.sgh.shared.infrastructure.input.service.booking.BookingHealthInsuranceDto;
 import ar.lamansys.sgh.shared.infrastructure.input.service.booking.BookingInstitutionDto;
 import ar.lamansys.sgh.shared.infrastructure.input.service.booking.BookingSpecialtyDto;
 import ar.lamansys.sgh.shared.infrastructure.input.service.booking.PracticeDto;
 import ar.lamansys.sgh.shared.infrastructure.input.service.booking.SharedBookingPort;
-import ar.lamansys.sgx.shared.dates.configuration.LocalDateMapper;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/public-api/institution/{sisaCode}/appointment/booking")
+@RequestMapping("/public-api/appointment/booking")
 public class BookingPublicController {
 	private final SharedBookingPort bookAppointmentPort;
-	private final SharedAppointmentPort appointmentPort;
-	private final LocalDateMapper localDateMapper;
-
-	public BookingPublicController(SharedBookingPort bookAppointmentPort,
-								   SharedAppointmentPort appointmentPort,
-								   LocalDateMapper localDateMapper) {
+	public BookingPublicController(SharedBookingPort bookAppointmentPort) {
 		this.bookAppointmentPort = bookAppointmentPort;
-		this.appointmentPort = appointmentPort;
-		this.localDateMapper = localDateMapper;
-	}
-
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public String bookPreappointment(@RequestBody BookingDto bookingDto) {
-		return bookAppointmentPort.makeBooking(bookingDto);
-	}
-
-
-	@PutMapping("/cancel")
-	public void cancelBooking(@RequestBody String uuid) {
-		bookAppointmentPort.cancelBooking(uuid);
-		log.debug("cancel booking {}", uuid);
-	}
-
-	@GetMapping
-	@ResponseStatus(HttpStatus.OK)
-	public Collection<PublicAppointmentListDto> getBookingList(
-			@PathVariable(name = "sisaCode") String sisaCode,
-			@RequestParam(name = "identificationNumber", required = false) String identificationNumber,
-			@RequestParam(name = "startDate", required = false) String startDateStr,
-			@RequestParam(name = "endDate", required = false) String endDateStr
-	) {
-		LocalDate startDate = localDateMapper.fromStringToLocalDate(startDateStr);
-		LocalDate endDate = localDateMapper.fromStringToLocalDate(endDateStr);
-		return appointmentPort.fetchAppointments(sisaCode, identificationNumber, List.of((short)6), startDate, endDate);
 	}
 
 	@GetMapping("/institution")
@@ -82,7 +38,14 @@ public class BookingPublicController {
 		return ResponseEntity.ok(result);
 	}
 
-	@GetMapping("/specialty/{clinicalSpecialtyId}/practices/medicalCoverages/{medicalCoverageId}")
+	@GetMapping("/specialties")
+	public ResponseEntity<List<BookingSpecialtyDto>> getAllSpecialties() {
+		List<BookingSpecialtyDto> result = bookAppointmentPort.fetchSpecialties();
+		log.debug("Get all practices => {}", result);
+		return ResponseEntity.ok(result);
+	}
+
+	@GetMapping("/specialty/{clinicalSpecialtyId}/medicalCoverages/{medicalCoverageId}/practices")
 	public ResponseEntity<List<PracticeDto>> getAllPracticesBySpecialtyAndMedicalCoverage(
 			@PathVariable(name = "clinicalSpecialtyId") Integer clinicalSpecialtyId,
 			@PathVariable(name = "medicalCoverageId") Integer medicalCoverageId,
@@ -95,7 +58,7 @@ public class BookingPublicController {
 		return ResponseEntity.ok(result);
 	}
 
-	@GetMapping("/practices/professional/{healthcareProfessionalId}/medicalCoverages/{medicalCoverageId}/specialty/{clinicalSpecialtyId}")
+	@GetMapping("/professional/{healthcareProfessionalId}/medicalCoverages/{medicalCoverageId}/specialty/{clinicalSpecialtyId}/practices")
 	public ResponseEntity<List<PracticeDto>> getAllPracticesByProfessionalAndMedicalCoverage(
 			@PathVariable(name = "healthcareProfessionalId") Integer healthcareProfessionalId,
 			@PathVariable(name = "medicalCoverageId") Integer medicalCoverageId,
@@ -107,14 +70,7 @@ public class BookingPublicController {
 		return ResponseEntity.ok(result);
 	}
 
-	@GetMapping("/specialties")
-	public ResponseEntity<List<BookingSpecialtyDto>> getAllSpecialties() {
-		List<BookingSpecialtyDto> result = bookAppointmentPort.fetchSpecialties();
-		log.debug("Get all practices => {}", result);
-		return ResponseEntity.ok(result);
-	}
-
-	@GetMapping("/specialties/professional/{healthcareProfessionalId}")
+	@GetMapping("/professional/{healthcareProfessionalId}/specialties")
 	public ResponseEntity<List<BookingSpecialtyDto>> getSpecialtiesByProfessional(
 			@PathVariable(name = "healthcareProfessionalId") Integer healthcareProfessionalId
 	) {
