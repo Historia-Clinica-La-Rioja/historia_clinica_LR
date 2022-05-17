0223821387b0feae836f55f757bdbf64b5fbab27
veneremaia <venere.maia@gmail.com>
2022-05-17 11:00:51 -0300

    tg-7280 [P1] Endpoint get document no devuelve evaluacion clinica

--- back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/application/notes/NoteServiceImpl.java
+++ back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/application/notes/NoteServiceImpl.java
@@ -37,7 +37,7 @@ public class NoteServiceImpl implements NoteService {
     @Override
     public String getDescriptionById(Long noteId) {
         LOG.debug("Input parameters -> noteId {}", noteId);
-        if (noteId != null)
+        if (noteId == null)
             return null;
         Optional<Note> noteOptional = noteRepository.findById(noteId);
         String result = noteOptional.map(Note::getDescription).orElse(null);
