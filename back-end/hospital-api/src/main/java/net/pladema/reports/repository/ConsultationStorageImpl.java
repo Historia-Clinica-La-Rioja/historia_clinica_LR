2d4ebd4388a42e420dc1422e752a0a1cdf8f424a
tcepeda <tcepeda@pladema.net>
2022-05-30 17:14:38 -0300

    tg-7345 Crear query para obtener las consultas de enfermeria que correspondan a un paciente en particular

--- back-end/hospital-api/src/main/java/net/pladema/reports/repository/ConsultationStorageImpl.java
+++ back-end/hospital-api/src/main/java/net/pladema/reports/repository/ConsultationStorageImpl.java
@@ -92,6 +92,36 @@ public class ConsultationStorageImpl implements ConsultationStorage {
 						(String) a[8]))
 		);
 
-        return result;
+		String nursingSqlString = "WITH t AS ("
+				+"  SELECT nc.id, d.id as doc_id, nc.performed_date AS start_date, nc.patient_id, "
+				+"  nc.clinical_specialty_id, nc.doctor_id "
+				+"  FROM {h-schema}nursing_consultation AS nc "
+				+  "JOIN {h-schema}document AS d ON (d.source_id = nc.id AND d.source_type_id = 7)"
+				+"  WHERE nc.patient_id = :patientId) "
+				+"  SELECT t.id, t.doc_id, t.start_date, cs.name, pe.first_name, pe.middle_names, pe.last_name, pe.other_last_names, pee.name_self_determination "
+				+"  FROM t "
+				+"  LEFT JOIN {h-schema}clinical_specialty AS cs ON (cs.id = t.clinical_specialty_id) "
+				+"  JOIN {h-schema}healthcare_professional AS hp ON (hp.id = t.doctor_id) "
+				+"  JOIN {h-schema}person AS pe ON (pe.id = hp.person_id) "
+				+"  JOIN {h-schema}person_extended AS pee ON (pe.id = pee.person_id) "
+				+"  ORDER BY t.start_date DESC";
+		List<Object[]> nursingResult = entityManager.createNativeQuery(nursingSqlString)
+				.setParameter("patientId", patientId)
+				.getResultList();
+
+		nursingResult.forEach(a ->
+				result.add(new ConsultationsVo(
+						(Integer) a[0],
+						a[1] != null ? ((BigInteger)a[1]).longValue() : null,
+						a[2] != null ? ((Date)a[2]).toLocalDate() : null,
+						(String) a[3],
+						(String) a[4],
+						(String) a[5],
+						(String) a[6],
+						(String) a[7],
+						(String) a[8]))
+		);
+
+		return result;
     }
 }
