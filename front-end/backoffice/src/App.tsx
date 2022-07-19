09be4e99e7c21c9dadbca5bb761e2a9d3dab3ced
Sebastian Perruolo <sebastianperruolo@gmail.com>
2022-07-19 14:31:17 -0300

    tg-7956 Quitar logout del backoffice

--- front-end/backoffice/src/App.tsx
+++ front-end/backoffice/src/App.tsx
@@ -14,6 +14,7 @@ import {
     Dashboard,
     Layout,
     LoginPage,
+    LogoutButton,
 } from './layout';
 
 import customRoutes from './layout/routes';
@@ -28,6 +29,7 @@ const App = () => (
         loginPage={LoginPage}
         dashboard={Dashboard}
         layout={Layout}
+        logoutButton={LogoutButton}
         disableTelemetry >
         {permissions => resources(permissions)}
     </Admin>
--- front-end/backoffice/src/layout/index.ts
+++ front-end/backoffice/src/layout/index.ts
@@ -2,9 +2,11 @@
 import Layout from './Layout';
 import LoginPage from './login/LoginPage';
 import Dashboard from './dashboard/Dashboard';
+import LogoutButton from './login/LogoutButton';
 
 export { 
     Dashboard,
     Layout,
     LoginPage,
+    LogoutButton,
 };
