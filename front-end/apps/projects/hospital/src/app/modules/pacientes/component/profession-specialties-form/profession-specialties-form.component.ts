928130a6e8aa6b9664447622be3b6600793fb721
FacundoBarreiros <facundobarreiros@gmail.com>
2022-07-20 12:19:41 -0300

    tg-7883 [FE] Agregar lógica  del componente sub formulario de profesion-especialidades

--- front-end/apps/projects/hospital/src/app/modules/pacientes/component/profession-specialties-form/profession-specialties-form.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/pacientes/component/profession-specialties-form/profession-specialties-form.component.ts
@@ -1,15 +1,174 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnDestroy } from '@angular/core';
+import { ControlValueAccessor, FormArray, FormBuilder, FormControl, FormGroup, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
+import { ClinicalSpecialtyDto, HealthcareProfessionalSpecialtyDto, ProfessionalProfessionsDto, ProfessionalSpecialtyDto } from '@api-rest/api-model';
+import { TypeaheadOption } from '@presentation/components/typeahead/typeahead.component';
+import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-profession-specialties-form',
 	templateUrl: './profession-specialties-form.component.html',
-	styleUrls: ['./profession-specialties-form.component.scss']
-})
-export class ProfessionSpecialtiesFormComponent implements OnInit {
+	styleUrls: ['./profession-specialties-form.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => ProfessionSpecialtiesFormComponent),
+			multi: true,
+		}
+	],
 
-	constructor() { }
+})
+
+export class ProfessionSpecialtiesFormComponent implements ControlValueAccessor, OnDestroy {
+
+	professionsTypeahead: TypeaheadOption<ProfessionalSpecialtyDto>[] = [];
+	specialtiesTypeahead: TypeaheadOption<ClinicalSpecialtyDto>[] = [];
+	initValueTypeaheadSpecialties: TypeaheadOption<ClinicalSpecialtyDto>[] = [];
+	initValueTypeaheadProfessions: TypeaheadOption<ProfessionalSpecialtyDto> = null;
+	onChangeSub: Subscription;
+	@Input() allProfessions: ProfessionalSpecialtyDto[] = [];
+	@Input() allSpecialties: ClinicalSpecialtyDto[] = [];
+	@Input() confirmationValidation: boolean = false;
+	@Input() professionalProfessionsDto?: ProfessionalProfessionsDto = null;
+	onTouched = () => { };
+
+	form = this.formBuilder.group({
+		id: null,
+		healthcareProfessionalId: null,
+		profession: new FormControl([]),
+		specialties: new FormArray([])
+	});
+
+	constructor(
+		private formBuilder: FormBuilder,
+	) { }
 
 	ngOnInit(): void {
+		this.professionsTypeahead = this.allProfessions.map(d => this.toProfessionDtoTypeahead(d));
+		this.specialtiesTypeahead = this.allSpecialties.map(d => this.toSpecialtyDtoTypeahead(d));
+		!!this.professionalProfessionsDto ? this.setPreviousSpeciality() : this.addSpecialties();
+
 	}
 
+	private setPreviousSpeciality(): void {
+		this.initValueTypeaheadProfessions = this.toProfessionDtoTypeahead(this.professionalProfessionsDto.profession);
+		this.form.controls.healthcareProfessionalId.setValue(this.professionalProfessionsDto.healthcareProfessionalId);
+		this.form.controls.id.setValue(this.professionalProfessionsDto.id);
+		this.professionalProfessionsDto.specialties.forEach((elem: HealthcareProfessionalSpecialtyDto) => {
+			this.addSpecialties(elem);
+			this.initValueTypeaheadSpecialties.push(this.toSpecialtyDtoTypeahead(elem.clinicalSpecialty));
+		});
+	}
+
+	setProfession($event: ProfessionalSpecialtyDto): void {
+		this.form.controls.profession.setValue($event);
+		if ($event === null) {
+			this.initValueTypeaheadSpecialties = [];
+			this.deleteSpecialties();
+			this.form.controls.healthcareProfessionalId.setValue(null);
+			this.form.controls.id.setValue(null);
+		}
+	}
+
+	private deleteSpecialties(): void {
+		const arraySpecialties = this.form.get('specialties') as FormArray;
+		arraySpecialties.clear();
+	}
+
+	setSpeciality($event: ClinicalSpecialtyDto, pointIndex: number): void {
+		const arraySpecialties = this.form.get('specialties') as FormArray;
+
+		arraySpecialties.at(pointIndex).setValue({
+			id: arraySpecialties.at(pointIndex).value.id,
+			professionalProfessionId: arraySpecialties.at(pointIndex).value.professionalProfessionId,
+			healthcareProfessionalId: arraySpecialties.at(pointIndex).value.healthcareProfessionalId,
+			clinicalSpecialty: {
+				id: $event ? $event.id : null,
+				specialty: $event ? $event.name : null
+			}
+		});
+
+		if ($event === null) {
+			this.initValueTypeaheadSpecialties.splice(pointIndex, 1);
+			if (arraySpecialties.length > 1) {
+				arraySpecialties.removeAt(pointIndex)
+			}
+		}
+
+	}
+
+	private toProfessionDtoTypeahead(professionDto: ProfessionalSpecialtyDto): TypeaheadOption<ProfessionalSpecialtyDto> {
+		return {
+			compareValue: professionDto.description,
+			value: professionDto
+		};
+	}
+
+	private toSpecialtyDtoTypeahead(speciality: ClinicalSpecialtyDto): TypeaheadOption<ClinicalSpecialtyDto> {
+		return {
+			compareValue: speciality.name,
+			value: speciality
+		};
+	}
+
+	getCtrl(key: string, form: FormGroup): any {
+		return form.get(key);
+	}
+
+	initFormSpecialties(elem?: HealthcareProfessionalSpecialtyDto): FormGroup {
+		const arraySpecialties = this.form.get('specialties') as FormArray;
+		let length = arraySpecialties?.length;
+
+		return new FormGroup({
+			id: new FormControl(elem ? elem.id : null, []),
+			professionalProfessionId: new FormControl(elem ? elem.professionalProfessionId : null, []),
+			healthcareProfessionalId: new FormControl(elem ? elem.healthcareProfessionalId : null, []),
+			clinicalSpecialty: new FormGroup({
+				id: new FormControl(elem ? elem.clinicalSpecialty.id : null, length === 0 ? Validators.required : null),
+				specialty: new FormControl(elem ? elem.clinicalSpecialty.name : null)
+			})
+		});
+	}
+
+	addSpecialties(elem?: HealthcareProfessionalSpecialtyDto): void {
+		const arraySpecialties = this.form.get('specialties') as FormArray;
+		arraySpecialties.push(this.initFormSpecialties(elem));
+	}
+
+
+	hasError(): boolean {
+		const arraySpecialties = this.form.get('specialties') as FormArray;
+		let length = arraySpecialties?.length;
+		return (this.confirmationValidation && (length > 0 ? (arraySpecialties.at(length - 1).value.clinicalSpecialty?.id === null) : false) && (this.form.value.profession !== null));
+	}
+
+	isDisableAddSpecialties(): boolean {
+		const arraySpecialties = this.form.get('specialties') as FormArray;
+		let length = arraySpecialties?.length;
+		return (!this.form.value?.profession?.id || (length > 0 ? (arraySpecialties.at(arraySpecialties.length - 1).value.clinicalSpecialty.id === null) : false));
+	}
+
+	writeValue(obj: any): void {
+		if (obj)
+			this.form.setValue(obj);
+	}
+
+	registerOnChange(fn: any): void {
+		this.onChangeSub = this.form.valueChanges
+			.subscribe(value => {
+				const toEmit = this.form.valid ? value : null;
+				fn(toEmit);
+			})
+	}
+
+	registerOnTouched(fn: any): void {
+		this.onTouched = fn;
+	}
+
+	setDisabledState?(isDisabled: boolean): void {
+		isDisabled ? this.form.disable() : this.form.enable();
+	}
+
+	ngOnDestroy(): void {
+		this.onChangeSub.unsubscribe();
+	}
 }
