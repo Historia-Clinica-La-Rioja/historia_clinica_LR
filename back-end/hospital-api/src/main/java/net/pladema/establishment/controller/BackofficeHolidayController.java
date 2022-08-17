2b57587cfbbad3a747f702abacf194b6e4786d36
fsimaro <fsimaro@pladema.net>
2022-08-17 09:53:42 -0300

    tg-8337 [BE] Crear controlador en backoffice para manejo de feríados.

---
+++ back-end/hospital-api/src/main/java/net/pladema/establishment/controller/BackofficeHolidayController.java
@@ -0,0 +1,18 @@
+package net.pladema.establishment.controller;
+
+import net.pladema.establishment.repository.HolidayRepository;
+import net.pladema.establishment.repository.entity.Holiday;
+import net.pladema.sgx.backoffice.rest.AbstractBackofficeController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("backoffice/holidays")
+public class BackofficeHolidayController extends AbstractBackofficeController<Holiday, Integer> {
+
+	public BackofficeHolidayController(HolidayRepository repository) {
+		super(repository);
+	}
+	
+}
