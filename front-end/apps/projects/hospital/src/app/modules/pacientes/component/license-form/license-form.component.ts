f32fb88cc98425fbfbbd92fa0ae2430ebe092543
FacundoBarreiros <facundobarreiros@gmail.com>
2022-08-26 15:07:09 -0300

    tg-8438 [Mejora] Cuando la matricula es muy extensa, se solapa con la especialidad

--- front-end/apps/projects/hospital/src/app/modules/pacientes/component/license-form/license-form.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/pacientes/component/license-form/license-form.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component, forwardRef, Input, OnChanges, OnDestroy } from '@angular/core';
 import { ControlValueAccessor, FormBuilder, FormControl, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
 import { ClinicalSpecialtyDto } from '@api-rest/api-model';
+import { hasError } from '@core/utils/form.utils';
 import { ProfessionalSpecialties } from '@pacientes/routes/profile/profile.component';
 import { Subscription } from 'rxjs';
 
+const MAX_LENGTH = 15;
 
 @Component({
 	selector: 'app-license-form',
@@ -21,6 +23,7 @@ import { Subscription } from 'rxjs';
 
 export class LicenseFormComponent implements ControlValueAccessor, OnDestroy, OnChanges {
 
+	hasError = hasError;
 	onTouched = () => { };
 	RADIO_OPTION_NATIONAL = 1;
 	RADIO_OPTION_PROVINCE = 2;
@@ -32,7 +35,8 @@ export class LicenseFormComponent implements ControlValueAccessor, OnDestroy, On
 
 	form = this.formBuilder.group({
 		id: null,
-		licenseNumber: new FormControl(null, [Validators.required]),
+		licenseNumber: new FormControl(null, [Validators.required, Validators.pattern(/^([a-zA-Z1-9])+$/), Validators.maxLength(MAX_LENGTH)]),
+
 		typeId: this.RADIO_OPTION_NATIONAL,
 		professionalProfessionId: new FormControl(null, [Validators.required]),
 		healthcareProfessionalSpecialtyId: new FormControl(null, []),
@@ -65,7 +69,8 @@ export class LicenseFormComponent implements ControlValueAccessor, OnDestroy, On
 				this.form.controls.healthcareProfessionalSpecialtyId.removeValidators([Validators.required]);
 				this.form.controls.healthcareProfessionalSpecialtyId.updateValueAndValidity();
 				this.form.controls.healthcareProfessionalSpecialtyId.setValue(null);
-		}});
+			}
+		});
 
 	}
 
