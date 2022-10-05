193d8bbd9ff89acb9e4ba97052c45651f31b1e96
FacundoBarreiros <facundobarreiros@gmail.com>
2022-10-04 22:24:39 -0300

    tg-8798 Agregar paginacion a las card de pacientes

--- front-end/apps/projects/hospital/src/app/modules/pacientes/component/card-patient/card-patient.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/pacientes/component/card-patient/card-patient.component.ts
@@ -6,13 +6,19 @@ import { PatientNameService } from "@core/services/patient-name.service";
 import { DatePipeFormat } from '@core/utils/date.utils';
 import { CardModel } from '@presentation/components/card/card.component';
 
+const PAGE_SIZE_OPTIONS = [5, 10, 25];
+const PAGE_MIN_SIZE = 5;
+
 @Component({
 	selector: 'app-card-patient',
 	templateUrl: './card-patient.component.html',
 	styleUrls: ['./card-patient.component.scss']
 })
 export class CardPatientComponent {
-	patientContent: CardModel[] = [];
+	pageSizeOptions = PAGE_SIZE_OPTIONS;
+	patientContent = [];
+	pageSlice = [];
+	numberOfPatients = 0;
 	@Input() patientData: PatientSearchDto[] = [];
 	@Input() genderTableView: string[] = [];
 
@@ -30,14 +36,16 @@ export class CardPatientComponent {
 
 	ngOnChanges() {
 		this.patientContent = this.mapToPatientContent();
+		this.numberOfPatients = this.patientContent?.length;
+		this.pageSlice = this.patientContent.slice(0, PAGE_MIN_SIZE);
 	}
 
-	mapToPatientContent(): CardModel[] {
+	private mapToPatientContent(): CardModel[] {
 		return this.patientData?.map((patient: PatientSearchDto) => {
 			return {
-				header: [{ title: " ", value: this.patientNameService.getFullName(patient.person.firstName, patient.person.nameSelfDetermination, patient.person?.middleNames) + ' ' +this.getLastNames(patient)}],
+				header: [{ title: " ", value: this.patientNameService.getFullName(patient.person.firstName, patient.person.nameSelfDetermination, patient.person?.middleNames) + ' ' + this.getLastNames(patient) }],
 				id: patient.idPatient,
-				dni: patient.person.identificationNumber,
+				dni: patient.person.identificationNumber || "-",
 				gender: this.genderTableView[patient.person.genderId],
 				date: patient.person.birthDate ? this.datePipe.transform(patient.person.birthDate, DatePipeFormat.SHORT_DATE) : '',
 				ranking: patient?.ranking,
@@ -50,6 +58,13 @@ export class CardPatientComponent {
 	}
 
 	private getLastNames(patient: PatientSearchDto): string {
-		return patient.person?.otherLastNames  ?  patient.person?.lastName + ' ' + patient.person?.otherLastNames : patient.person?.lastName;
+		return patient.person?.otherLastNames ? patient.person?.lastName + ' ' + patient.person?.otherLastNames : patient.person?.lastName;
 	}
+
+	onPageChange($event: any): void {
+		const page = $event;
+		const startPage = page.pageIndex * page.pageSize;
+		this.pageSlice = this.patientContent.slice(startPage, $event.pageSize + startPage);
+	}
+
 }
