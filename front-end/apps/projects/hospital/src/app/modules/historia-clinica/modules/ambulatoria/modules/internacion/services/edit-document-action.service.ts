6cf9b932ae6d1cb22e4f3a9ac73d4d5a564ab045
jvaldez <joselinavaldez99@gmail.com>
2022-10-13 14:08:58 -0300

    tg-8930 [FIX-P2]Error al editar documentos de internacion

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/modules/internacion/services/edit-document-action.service.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/modules/internacion/services/edit-document-action.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { DocumentSearchDto } from '@api-rest/api-model';
 import { InternmentActionsService } from './internment-actions.service';
 import { MatDialog } from "@angular/material/dialog";
+import { InternmentFields, InternmentSummaryFacadeService } from './internment-summary-facade.service';
 
 @Injectable()
 export class EditDocumentActionService {
@@ -10,6 +11,7 @@ export class EditDocumentActionService {
 
 	constructor(
 		private readonly internmentActions: InternmentActionsService,
+		private readonly internmentSummaryFacadeService: InternmentSummaryFacadeService,
 		private readonly dialog: MatDialog
 	) { }
 
@@ -18,12 +20,18 @@ export class EditDocumentActionService {
 	}
 
 	editDocument(document: DocumentSearchDto) {
-		if (document.documentType === "Anamnesis")
-			this.internmentActions.openAnamnesis(document.id)
-		if (document.documentType === "Nota de evolución" || document.documentType === "Nota de evolución de enfermería")
-			this.internmentActions.openEvolutionNote(document.id, document.documentType)
-		if (document.documentType === "Epicrisis")
-			this.internmentActions.openEpicrisis(document.id)
+		if (document.documentType === "Anamnesis") {
+			this.internmentActions.anamnesis$.subscribe(fieldsToUpdate => this.updateInternment(fieldsToUpdate));
+			this.internmentActions.openAnamnesis(document.id);
+		}
+		if (document.documentType === "Nota de evolución" || document.documentType === "Nota de evolución de enfermería") {
+			this.internmentActions.evolutionNote$.subscribe(fieldsToUpdate => this.updateInternment(fieldsToUpdate));
+			this.internmentActions.openEvolutionNote(document.id, document.documentType);
+		}
+		if (document.documentType === "Epicrisis") {
+			this.internmentActions.epicrisis$.subscribe(fieldsToUpdate => this.updateInternment(fieldsToUpdate));
+			this.internmentActions.openEpicrisis(document.id);
+		}
 	}
 
 	editDraftEpicrisis(document: DocumentSearchDto, canConfirmedDocument: boolean) {
@@ -31,4 +39,8 @@ export class EditDocumentActionService {
 		this.internmentActions.openEpicrisis(document.id, true);
 	}
 
+	private updateInternment(fieldsToUpdate: InternmentFields) {
+		if (fieldsToUpdate)
+			this.internmentSummaryFacadeService.setFieldsToUpdate(fieldsToUpdate);
+	}
 }
