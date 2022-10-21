b7ec83a0e02633b497b22a713333d3e49ac41d90
Cristian Quevedo <cristian.quevedox>
2022-10-21 10:14:33 -0300

    se modificaron los queryFactory agregando el rango de fechas

--- back-end/hospital-api/src/main/java/net/pladema/generalreports/repository/QueryFactoryGR.java
+++ back-end/hospital-api/src/main/java/net/pladema/generalreports/repository/QueryFactoryGR.java
@@ -6,6 +6,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
@@ -27,17 +30,29 @@ public class QueryFactoryGR {
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<ConsultationDetailDiabeticosHipertensos> queryDiabeticos(Integer institutionId){
+	public List<ConsultationDetailDiabeticosHipertensos> queryDiabeticos(Integer institutionId, LocalDate start, LocalDate end){
+
+		var startDate = LocalDateTime.of(start.getYear(), start.getMonth(), start.getDayOfMonth(), 0,0);
+		var endDate = LocalDateTime.of(end.getYear(), end.getMonth(), end.getDayOfMonth(), 23,59,59, LocalTime.MAX.getNano());
+
 		Query query = entityManager.createNamedQuery("GeneralReports.ConsultationDetailDiabeticos");
 		query.setParameter("institutionId", institutionId);
+		query.setParameter("startDate", startDate);
+		query.setParameter("endDate", endDate);
 		List<ConsultationDetailDiabeticosHipertensos> data = query.getResultList();
 		return data;
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<ConsultationDetailDiabeticosHipertensos> queryHipertensos(Integer institutionId){
+	public List<ConsultationDetailDiabeticosHipertensos> queryHipertensos(Integer institutionId, LocalDate start, LocalDate end){
+
+		var startDate = LocalDateTime.of(start.getYear(), start.getMonth(), start.getDayOfMonth(), 0,0);
+		var endDate = LocalDateTime.of(end.getYear(), end.getMonth(), end.getDayOfMonth(), 23,59,59, LocalTime.MAX.getNano());
+
 		Query query = entityManager.createNamedQuery("GeneralReports.ConsultationDetailHipertensos");
 		query.setParameter("institutionId", institutionId);
+		query.setParameter("startDate", startDate);
+		query.setParameter("endDate", endDate);
 		List<ConsultationDetailDiabeticosHipertensos> data = query.getResultList();
 		return data;
 	}
