12393b8db59c63da0cb273d786a65b33e2f93b87
tcepeda <tcepeda@pladema.net>
2022-11-25 13:26:43 -0300

    tg-9331 [BE] Crear service para el cifrado bidireccional de contraseñas

--- back-end/hospital-api/src/main/java/net/pladema/establishment/controller/BackofficePacServerController.java
+++ back-end/hospital-api/src/main/java/net/pladema/establishment/controller/BackofficePacServerController.java
@@ -1,18 +1,19 @@
 package net.pladema.establishment.controller;
 
-import java.util.Objects;
 import java.util.regex.Pattern;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ar.lamansys.sgx.auth.twoWayEncryption.infrastructure.output.TwoWayEncryptionService;
 import ar.lamansys.sgx.auth.user.application.registeruser.exceptions.RegisterUserEnumException;
 import ar.lamansys.sgx.auth.user.application.registeruser.exceptions.RegisterUserException;
-import ar.lamansys.sgx.auth.user.domain.userpassword.PasswordEncryptor;
 import net.pladema.establishment.repository.PacServerRepository;
 import net.pladema.establishment.repository.entity.PacServer;
 import net.pladema.sgx.backoffice.rest.AbstractBackofficeController;
@@ -20,41 +21,66 @@ import net.pladema.sgx.exceptions.BackofficeValidationException;
 
 @RestController
 @RequestMapping("backoffice/pacservers")
+@PreAuthorize("hasAnyAuthority('ROOT')")
 public class BackofficePacServerController extends AbstractBackofficeController<PacServer, Integer> {
-
-	private final PasswordEncryptor passwordEncryptor;
 
 	private final Pattern usernamePattern;
 
-	public BackofficePacServerController(PacServerRepository repository, PasswordEncryptor passwordEncryptor,
-										 @Value("${authentication.username.pattern:.+}") String pattern) {
+	private final TwoWayEncryptionService twoWayEncryptionService;
+
+	public BackofficePacServerController(PacServerRepository repository, @Value("${authentication.username.pattern:.+}") String pattern, TwoWayEncryptionService twoWayEncryptionService) {
 		super(repository);
-		this.passwordEncryptor = passwordEncryptor;
 		this.usernamePattern = Pattern.compile(pattern);
+		this.twoWayEncryptionService = twoWayEncryptionService;
+	}
+
+	@Override
+	public PacServer getOne(@PathVariable("id") Integer id) {
+		var entity = super.getOne(id);
+		try {
+			entity.setPassword(twoWayEncryptionService.decrypt(entity.getPassword()));
+		} catch (Exception e) {
+			entity.setPassword(null);
+		}
+		return entity;
+	}
+
+	@Override
+	public PacServer update(@PathVariable("id") Integer id, @RequestBody PacServer entity) {
+		validations(entity);
+		try {
+			entity.setPassword(twoWayEncryptionService.encrypt(entity.getPassword()));
+		} catch (Exception e) {
+			throw new BackofficeValidationException("Contraseña invalida");
+		}
+		return super.update(id, entity);
 	}
 
 	@Override
 	public PacServer create(@Valid @RequestBody PacServer entity) {
 		validations(entity);
-		var salt = "salt";
-		var hashAlgorithm = "hashAlgorithm";
-		var encodedPassword = passwordEncryptor.encode(entity.getPassword(), salt, hashAlgorithm);
-		entity.setPassword(encodedPassword);
+		try {
+			entity.setPassword(twoWayEncryptionService.encrypt(entity.getPassword()));
+		} catch (Exception e) {
+			throw new BackofficeValidationException("Contraseña invalida");
+		}
 		return super.create(entity);
 	}
 
 	private void validations(PacServer entity) {
-		var username = entity.getUsername();
-		Objects.requireNonNull(username, () -> {
-			throw new RegisterUserException(RegisterUserEnumException.NULL_USERNAME, "El username es obligatorio");
-		});
-		if (!usernamePattern.matcher(username).matches()){
-			throw new RegisterUserException(RegisterUserEnumException.INVALID_USERNAME_PATTERN,
-					String.format("El username %s no cumple con el patrón %s obligatorio", username, usernamePattern.pattern()));
-		}
 		if(entity.getPacServerType() == null)
 			throw new BackofficeValidationException("No se definió el tipo de servidor");
 		if(entity.getPacServerProtocol() == null)
 			throw new BackofficeValidationException("No se definió el protocolo de imagen");
+		var username = entity.getUsername();
+		if(username == null)
+			throw new RegisterUserException(RegisterUserEnumException.NULL_USERNAME, "El nombre de usuario es obligatorio");
+		if (!usernamePattern.matcher(username).matches()){
+			throw new RegisterUserException(RegisterUserEnumException.INVALID_USERNAME_PATTERN,
+					String.format("El username %s no cumple con el patrón %s obligatorio", username, usernamePattern.pattern()));
+		}
+		var password = entity.getPassword();
+		if(password == null || password.isBlank())
+			throw new BackofficeValidationException("Contraseña invalida");
 	}
 }
