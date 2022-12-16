b405c8e0463353861e2465b65ce35da91aabc7ef
Tano <lucas.bergottini@gmail.com>
2022-12-16 11:24:25 -0300

    tg-9458 Agregar datos de consultas odontologicas y de enfermería a gráficos de consultas

--- cubejs/schema/CantidadConsultasAmbulatorias.js
+++ cubejs/schema/CantidadConsultasAmbulatorias.js
@@ -1,14 +1,36 @@
 cube(`CantidadConsultasAmbulatorias`, {
   sql: `SELECT 
-          oc.id, oc.start_date as fecha_consulta, g.description as gender, pe.birth_date, cs.name as especialidad, doc.last_name as profesional
+            oc.id, oc.start_date as fecha_consulta, g.description as gender, pe.birth_date, cs.name as especialidad, doc.last_name as profesional
         FROM 
-          outpatient_consultation oc
-          JOIN clinical_specialty cs ON (oc.clinical_specialty_id = cs.id)
-          JOIN healthcare_professional hp ON (oc.doctor_id = hp.id)
-          JOIN person doc ON (hp.person_id = doc.id)
-          JOIN patient pa ON (oc.patient_id = pa.id)
-          JOIN person pe ON (pa.person_id = pe.id)
-          JOIN gender g ON (pe.gender_id = g.id)`,
+            outpatient_consultation oc
+            JOIN clinical_specialty cs ON (oc.clinical_specialty_id = cs.id)
+            JOIN healthcare_professional hp ON (oc.doctor_id = hp.id)
+            JOIN person doc ON (hp.person_id = doc.id)
+            JOIN patient pa ON (oc.patient_id = pa.id)
+            JOIN person pe ON (pa.person_id = pe.id)
+            JOIN gender g ON (pe.gender_id = g.id)
+      UNION ALL
+        SELECT 
+            oc.id, oc.performed_date as fecha_consulta, g.description as gender, pe.birth_date, cs.name as especialidad, doc.last_name as profesional
+        FROM 
+            odontology_consultation oc
+            JOIN clinical_specialty cs ON (oc.clinical_specialty_id = cs.id)
+            JOIN healthcare_professional hp ON (oc.doctor_id = hp.id)
+            JOIN person doc ON (hp.person_id = doc.id)
+            JOIN patient pa ON (oc.patient_id = pa.id)
+            JOIN person pe ON (pa.person_id = pe.id)
+            JOIN gender g ON (pe.gender_id = g.id)
+      UNION ALL
+        SELECT 
+            nc.id, nc.performed_date as fecha_consulta, g.description as gender, pe.birth_date, cs.name as especialidad, doc.last_name as profesional
+        FROM 
+            nursing_consultation nc
+            JOIN clinical_specialty cs ON (nc.clinical_specialty_id = cs.id)
+            JOIN healthcare_professional hp ON (nc.doctor_id = hp.id)
+            JOIN person doc ON (hp.person_id = doc.id)
+            JOIN patient pa ON (nc.patient_id = pa.id)
+            JOIN person pe ON (pa.person_id = pe.id)
+            JOIN gender g ON (pe.gender_id = g.id)`,
   
   measures: {
     cantidad_turnos_estado: {
