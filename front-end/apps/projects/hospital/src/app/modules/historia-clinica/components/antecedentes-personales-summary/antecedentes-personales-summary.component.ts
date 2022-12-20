04f1cb8c9bf44e794537733f29600eabe749af19
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-12-20 15:37:30 -0300

    tg-9495 [Mejora] [FE] Sacar la tabla de la card de problemas activos en resumen y ajustar diseño para coincidir con figma

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/antecedentes-personales-summary/antecedentes-personales-summary.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/antecedentes-personales-summary/antecedentes-personales-summary.component.ts
@@ -1,62 +1,60 @@
-import {Component, Input, OnChanges, OnInit} from '@angular/core';
-import {TableModel} from '@presentation/components/table/table.component';
-import {HCEPersonalHistoryDto, HealthConditionDto} from '@api-rest/api-model';
+import {Component, Input} from '@angular/core';
+import {HCEPersonalHistoryDto} from '@api-rest/api-model';
 import {SummaryHeader} from '@presentation/components/summary-card/summary-card.component';
 import {InternacionMasterDataService} from '@api-rest/services/internacion-master-data.service';
-import {DateFormat, momentFormat, momentParseDate} from '@core/utils/moment.utils';
 
 @Component({
 	selector: 'app-antecedentes-personales-summary',
 	templateUrl: './antecedentes-personales-summary.component.html',
 	styleUrls: ['./antecedentes-personales-summary.component.scss']
 })
-export class AntecedentesPersonalesSummaryComponent implements OnInit, OnChanges {
-
-	@Input() personalHistory: HCEPersonalHistoryDto[];
+export class AntecedentesPersonalesSummaryComponent {
+	problems: Problem[] = [];
+	private severityTypesMasterData: any[] = [];
 	@Input() personalHistoriesHeader: SummaryHeader;
-
-	tableModel: TableModel<HealthConditionDto>;
-	severityTypesMasterData: any[];
+	@Input() 
+	set personalHistory(personalHistory: HCEPersonalHistoryDto[]){
+		if (personalHistory.length){
+			this.setSeverityMasterData(personalHistory);
+		}
+	};
 
 	constructor(
 		private readonly internacionMasterDataService: InternacionMasterDataService
 	) {}
 
-	ngOnInit(): void {
-		this.internacionMasterDataService.getHealthSeverity().subscribe(
-			severityTypes => this.severityTypesMasterData = severityTypes
-		);
-	}
-
-	ngOnChanges(): void {
-		this.tableModel = this.buildTable(this.personalHistory);
-	}
-
-	private buildTable(data: HCEPersonalHistoryDto[]): TableModel<HCEPersonalHistoryDto> {
-		return {
-			columns: [
-				{
-					columnDef: 'tipo',
-					header: 'Problema',
-					text: (row) => row.snomed.pt
-				},
-				{
-					columnDef: 'severidad',
-					header: 'Severidad',
-					text: (row) => row.severity ? this.getSeverityDisplayName(row.severity) : ''
-				},
-				{
-					columnDef: 'fechaInicio',
-					header: 'Desde',
-					text: (row) => row.startDate ? momentFormat(momentParseDate(row.startDate), DateFormat.VIEW_DATE) : ''
-				}
-				],
-			data
-		};
-	}
-
-	getSeverityDisplayName(severityCode): string {
+	getSeverityTypeDisplayByCode(severityCode): string {
 		return (severityCode && this.severityTypesMasterData) ?
-			this.severityTypesMasterData.find(severityType => severityType.code === severityCode).display : '';
+			this.severityTypesMasterData.find(severityType => severityType.code === severityCode).display
+			: '';
 	}
+
+	private setProblems(problems: HCEPersonalHistoryDto[]){
+		this.problems = problems.map(prob => {
+			return this.mapToProblem(prob);
+		});
+	}
+
+	private setSeverityMasterData(problems: HCEPersonalHistoryDto[]){
+		if (!this.severityTypesMasterData.length){
+			this.internacionMasterDataService.getHealthSeverity().subscribe(
+				severityTypes => {
+					this.severityTypesMasterData = severityTypes;
+					this.setProblems(problems);
+				}
+			);
+		}
+	}
+
+	private mapToProblem(problem: HCEPersonalHistoryDto): Problem {
+		return {
+			data: problem,
+			severityName: this.getSeverityTypeDisplayByCode(problem.severity)
+		}
+	}
+}
+
+interface Problem {
+	data: HCEPersonalHistoryDto;
+	severityName: string;
 }
