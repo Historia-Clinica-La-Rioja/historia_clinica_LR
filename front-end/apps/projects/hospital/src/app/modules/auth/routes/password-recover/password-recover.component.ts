3b76ff793e8bf0e9ec8604948b4b3d3ae454d30b
FacundoBarreiros <facundobarreiros@gmail.com>
2022-12-21 09:44:19 -0300

    tg-9439 [FE] Crear pantalla de validación de usuario

---
+++ front-end/apps/projects/hospital/src/app/modules/auth/routes/password-recover/password-recover.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-recover',
+  templateUrl: './password-recover.component.html',
+  styleUrls: ['./password-recover.component.scss']
+})
+export class PasswordRecoverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
