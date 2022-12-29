1ca6176f8bd7e7536aa5f0a90a4515e5373f3dc1
tcepeda <tcepeda@pladema.net>
2022-12-29 10:52:01 -0300

    tg-9553 [P2] Boton volver no redirige correctamente

--- front-end/backoffice/src/modules/imagelvlpacservers/ImageLvlPacShow.js
+++ front-end/backoffice/src/modules/imagelvlpacservers/ImageLvlPacShow.js
@@ -1,61 +1,21 @@
 import React from 'react';
 import {
-    Button,
-    EditButton,
+    ReferenceField,
     Show,
     SimpleShowLayout,
-    TextField,
-    TopToolbar,
-    useRedirect
+    TextField
 } from 'react-admin';
-import {makeStyles} from "@material-ui/core/styles";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-
-const PacShowActions = ({ data }) => {
-    const useStyles = makeStyles({
-        button: {
-            marginRight: '1%',
-        },
-        deleteButton: {
-            marginLeft: 'auto',
-        }
-    });
-    const classes = useStyles();
-    const redirect = useRedirect();
-    const goBack = () => {
-        redirect("/sectors/" + data.id + "/show");
-    }
-    return (!data || !data.id) ? <TopToolbar/> :
-        (
-            <TopToolbar>
-                <Button
-                    variant="outlined"
-                    color="primary"
-                    size="medium"
-                    className={classes.button}
-                    label="sgh.components.customtoolbar.backButton"
-                    onClick={goBack}>
-                    <ArrowBackIcon />
-                </Button>
-                <EditButton
-                    basePath="/pacserversimagelvl"
-                    record={{ id: data.id }}
-                    variant="outlined"
-                    color="primary"
-                    size="medium"
-                    className={classes.button}
-                />
-            </TopToolbar>
-        )
-};
 
 const ImageLvlPacShow = props => (
-    <Show actions={<PacShowActions />} {...props}>
+    <Show {...props}>
         <SimpleShowLayout>
             <TextField source="name" />
             <TextField source="aetitle" />
             <TextField source="domain" />
             <TextField source="port" />
+            <ReferenceField source="sectorId" reference="sectors">
+                <TextField source="description"/>
+            </ReferenceField>
         </SimpleShowLayout>
     </Show>
 );
