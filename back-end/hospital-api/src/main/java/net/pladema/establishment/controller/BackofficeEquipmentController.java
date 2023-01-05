a735a02b933f9b42a1bb152c4a1d6bab8eb2c845
amurrie <amurrie@lamansys.com.ar>
2023-01-05 14:44:30 -0300

    tg-9708.[BE] Crear controller para los equipos

---
+++ back-end/hospital-api/src/main/java/net/pladema/establishment/controller/BackofficeEquipmentController.java
@@ -0,0 +1,32 @@
+package net.pladema.establishment.controller;
+
+import net.pladema.establishment.repository.EquipmentRepository;
+import net.pladema.establishment.repository.OrchestratorRepository;
+import net.pladema.establishment.repository.entity.Equipment;
+import net.pladema.establishment.repository.entity.Orchestrator;
+import net.pladema.sgx.backoffice.rest.AbstractBackofficeController;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("backoffice/equipment")
+public class BackofficeEquipmentController extends AbstractBackofficeController<Equipment, Integer> {
+
+	private final EquipmentRepository repository;
+
+	public BackofficeEquipmentController(EquipmentRepository repository) {
+		super(repository);
+		this.repository = repository;
+	}
+
+	@Override
+	public Equipment create(@Valid @RequestBody Equipment entity) {
+		return super.create(entity);
+
+	}
+}
+
