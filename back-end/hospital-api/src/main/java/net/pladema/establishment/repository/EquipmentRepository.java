75b526b0da2a5c885286f841edea7288b158c8d0
amurrie <amurrie@lamansys.com.ar>
2023-01-05 14:42:08 -0300

    tg-9707.[BE] Crear entity y repository para los equipos

---
+++ back-end/hospital-api/src/main/java/net/pladema/establishment/repository/EquipmentRepository.java
@@ -0,0 +1,10 @@
+package net.pladema.establishment.repository;
+
+import net.pladema.establishment.repository.entity.Equipment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipmentRepository extends JpaRepository<Equipment, Integer> {
+
+}
+
