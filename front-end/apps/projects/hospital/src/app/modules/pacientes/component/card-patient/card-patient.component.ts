bdcfd8a332646cb5d942cb57177f76e28b606eea
jvaldez <joselinavaldez99@gmail.com>
2023-01-12 16:38:51 +0000

    tg-9782 [FIX] Búsqueda de paciente temporales - Redirige al inicio
    (cherry picked from commit 72a2addb972bd8d2a939c5ca9b10133cea4ccc30)

--- front-end/apps/projects/hospital/src/app/modules/pacientes/component/card-patient/card-patient.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/pacientes/component/card-patient/card-patient.component.ts
@@ -1,11 +1,13 @@
 import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { PatientSearchDto } from '@api-rest/api-model';
+import { ERole } from '@api-rest/api-model';
 import { ContextService } from '@core/services/context.service';
 import { PatientNameService } from "@core/services/patient-name.service";
+import { PermissionsService } from '@core/services/permissions.service';
+import { anyMatch } from '@core/utils/array.utils';
 import { DatePipeFormat } from '@core/utils/date.utils';
-import { CardModel } from '@presentation/components/card/card.component';
-
+import { CardModel, ValueAction } from '@presentation/components/card/card.component';
 const PAGE_SIZE_OPTIONS = [5, 10, 25];
 const PAGE_MIN_SIZE = 5;
 
@@ -28,8 +30,7 @@ export class CardPatientComponent {
 		private readonly patientNameService: PatientNameService,
 		private readonly datePipe: DatePipe,
 		private readonly contextService: ContextService,
-
-
+		private readonly permissionsService: PermissionsService,
 	) {
 		this.routePrefix = `institucion/${this.contextService.institutionId}/`;
 	}
@@ -39,8 +40,10 @@ export class CardPatientComponent {
 		this.numberOfPatients = this.patientContent?.length;
 		this.pageSlice = this.patientContent.slice(0, PAGE_MIN_SIZE);
 	}
-
 	private mapToPatientContent(): CardModel[] {
+		let isAdministrative = false;
+		this.permissionsService.contextAssignments$().subscribe((userRoles: ERole[]) => isAdministrative = anyMatch<ERole>(userRoles, [ERole.ADMINISTRATIVO]));
+		
 		return this.patientData?.map((patient: PatientSearchDto) => {
 			return {
 				header: [{ title: " ", value: this.patientNameService.getFullName(patient.person.firstName, patient.person.nameSelfDetermination, patient.person?.middleNames) + ' ' + this.getLastNames(patient) }],
@@ -49,22 +52,29 @@ export class CardPatientComponent {
 				gender: this.genderTableView[patient.person.genderId],
 				date: patient.person.birthDate ? this.datePipe.transform(patient.person.birthDate, DatePipeFormat.SHORT_DATE) : '',
 				ranking: patient?.ranking,
-				action: {
-					display: 'ambulatoria.card-patient.BUTTON',
-					do: `${this.routePrefix}ambulatoria/paciente/${patient.idPatient}`
-				}
+				action: this.setActionByRole(isAdministrative, patient.idPatient)
 			}
 		});
 	}
-
 	private getLastNames(patient: PatientSearchDto): string {
 		return patient.person?.otherLastNames ? patient.person?.lastName + ' ' + patient.person?.otherLastNames : patient.person?.lastName;
 	}
-
 	onPageChange($event: any): void {
 		const page = $event;
 		const startPage = page.pageIndex * page.pageSize;
 		this.pageSlice = this.patientContent.slice(startPage, $event.pageSize + startPage);
 	}
 
+	private setActionByRole(isAdministrative: boolean, idPatient: number): ValueAction {
+		if (!isAdministrative)
+            return {
+                display: 'ambulatoria.card-patient.BUTTON',
+                do: `${this.routePrefix}ambulatoria/paciente/${idPatient}`
+            }
+        else
+            return {
+                display: 'ambulatoria.card-patient.BUTTON',
+                do: `${this.routePrefix}pacientes/profile/${idPatient}`
+            }
+    }
 }
