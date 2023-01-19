7f61ea64c55b6878473a46303322cd7f3077f9dc
FacundoBarreiros <facundobarreiros@gmail.com>
2023-01-19 18:47:13 -0300

    tg-9666 [FE] Conectar con el BE

---
+++ front-end/apps/projects/hospital/src/app/modules/api-rest/services/indication-by-professional.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { PharmacoSummaryDto } from '@api-rest/api-model';
+import { ContextService } from '@core/services/context.service';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
+
+const BASIC_URL_PREFIX = '/institutions';
+const BASIC_URL_SUFIX = '/indication';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class IndicationByProfessionalService {
+
+	constructor(
+		private readonly http: HttpClient,
+		private readonly contextService: ContextService,
+	) { }
+	getMostFrequentPharmacos(): Observable<PharmacoSummaryDto[]> {
+		const url = `${environment.apiBase}` + BASIC_URL_PREFIX + `/${this.contextService.institutionId}` + BASIC_URL_SUFIX + `/pharmacos/most-frequent`;
+		return this.http.get<PharmacoSummaryDto[]>(url);
+	}
+}
