35a7a7c94f45d17c2aeb86294b4dfea059d26127
FacundoBarreiros <facundobarreiros@gmail.com>
2023-01-19 18:16:40 -0300

    tg-9663 [FE] Crear tabla para mostrar farmacos

---
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/modules/indicacion/components/card-pharmaco/card-pharmaco.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Pharmaco } from '../../dialogs/pharmaco/pharmaco.component';
+
+@Component({
+	selector: 'app-card-pharmaco',
+	templateUrl: './card-pharmaco.component.html',
+	styleUrls: ['./card-pharmaco.component.scss']
+})
+export class CardPharmacoComponent {
+	@Input() pharmacos: [];
+	@Output() selectionChange = new EventEmitter();
+
+	emit(pharmaco: Pharmaco) {
+		return this.selectionChange.emit(pharmaco);
+	}
+}
