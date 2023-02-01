c88b67d84965062e1546f3b0833b9ed6fec8e461
jirimoldi <juanirimoldi@gmail.com>
2023-02-01 08:42:33 -0300

    tg-9960 [FE] Agregar titulos a cada card

--- front-end/apps/projects/hospital/src/app/modules/extensions/components/query-renderer/query-renderer.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/extensions/components/query-renderer/query-renderer.component.ts
@@ -413,12 +413,41 @@ export class QueryRendererComponent {
 			.seriesNames()
 			.map((s) => resultSet.totalRow()[s.key]);
 
-		this.numericTitle = resultSet.totalRow()?.x.toUpperCase();
+		switch (resultSet.loadResponse.results[0].query.measures[0]){
+			case 'CantidadTurnos.cantidad_turnos': {
+				this.numericTitle = 'TURNOS';
 
-		if (this.numericTitle === "")
-			this.numericTitle = "CONSULTAS";
+				switch (resultSet.loadResponse.results[0].query.filters[0]?.values[0]) {
+					case '1': {
+						this.numericTitle = "ASIGNADOS";
+						break;
+					}
+					case '3': {
+						this.numericTitle = "AUSENTES";
+						break;
+					}
+					case '4': {
+						this.numericTitle = "CANCELADOS";
+						break;
+					}
+					case '5': {
+						this.numericTitle = "ATENDIDOS";
+						break;
+					}
+				}
+				break;
+			}
 
-		if (this.numericTitle === "MASCULINO" || this.numericTitle === "FEMENINO")
-			this.numericTitle += 'S';
+			case 'CantidadConsultasAmbulatorias.cantidad_turnos_estado': {
+				this.numericTitle = resultSet.totalRow()?.x.toUpperCase();
+
+				if (this.numericTitle === "")
+					this.numericTitle = "CONSULTAS";
+
+				if (this.numericTitle === "MASCULINO" || this.numericTitle === "FEMENINO")
+					this.numericTitle += 'S';
+				break;
+			}
+		}
 	}
 }
