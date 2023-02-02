6a0c3d9ed8524efda0cab7f6ca4ecf7edd51a433
jvaldez <joselinavaldez99@gmail.com>
2023-02-02 13:08:58 -0300

    tg-9941 [FE] Agrega flujo para el FF de red de imágenes en false

--- front-end/apps/projects/hospital/src/app/modules/turnos/routes/home/home.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/turnos/routes/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTabChangeEvent } from '@angular/material/tabs';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AppFeature } from '@api-rest/api-model';
+import { AppFeature, ERole } from '@api-rest/api-model';
 import { ContextService } from '@core/services/context.service';
 import { FeatureFlagService } from '@core/services/feature-flag.service';
+import { PermissionsService } from '@core/services/permissions.service';
 
 
 @Component({
@@ -18,18 +19,28 @@ export class HomeComponent implements OnInit {
 	tabActiveIndex = 0;
 
 	ffIsOn = false;
+	noPermission = false;
+
+	readonly mssg = 'image-network.home.NO_PERMISSION';
 
 	constructor(
 		private readonly router: Router,
 		public readonly route: ActivatedRoute,
 		private readonly contextService: ContextService,
-		private readonly featureFlagService: FeatureFlagService
+		private readonly featureFlagService: FeatureFlagService,
+		private readonly permissionsService: PermissionsService,
 	) {
 		this.routePrefix = `institucion/${this.contextService.institutionId}/turnos`;
+
 	}
 
 	ngOnInit() {
-		this.featureFlagService.isActive(AppFeature.HABILITAR_DESARROLLO_RED_IMAGENES).subscribe(isOn => this.ffIsOn =  isOn);
+		this.permissionsService.hasContextAssignments$([ERole.ADMINISTRATIVO_RED_DE_IMAGENES]).subscribe(hasRole => {
+			this.featureFlagService.isActive(AppFeature.HABILITAR_DESARROLLO_RED_IMAGENES).subscribe(ffIsOn => {
+				this.ffIsOn = ffIsOn;
+				this.noPermission = (hasRole && !ffIsOn);
+			})
+		})
 	}
 
 	goToNewProfessionalDiary(): void {
