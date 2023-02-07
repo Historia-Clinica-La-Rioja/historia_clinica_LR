87819630f7c0aecfa7c42cf538cd426737b8c698
FacundoBarreiros <facundobarreiros@gmail.com>
2023-02-07 14:21:30 -0300

    tg-10080 [FE] Concectar con el BE

--- front-end/apps/projects/hospital/src/app/modules/api-rest/services/indication-by-professional.service.ts
+++ front-end/apps/projects/hospital/src/app/modules/api-rest/services/indication-by-professional.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { PharmacoSummaryDto } from '@api-rest/api-model';
+import { ParenteralPlanDto, PharmacoSummaryDto } from '@api-rest/api-model';
 import { ContextService } from '@core/services/context.service';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
@@ -20,5 +20,10 @@ export class IndicationByProfessionalService {
 	getMostFrequentPharmacos(): Observable<PharmacoSummaryDto[]> {
 		const url = `${environment.apiBase}` + BASIC_URL_PREFIX + `/${this.contextService.institutionId}` + BASIC_URL_SUFIX + `/pharmacos/most-frequent`;
 		return this.http.get<PharmacoSummaryDto[]>(url);
+	}
+
+	getMostFrequentParenteralPlan(): Observable<ParenteralPlanDto[]> {
+		const url = `${environment.apiBase}` + BASIC_URL_PREFIX + `/${this.contextService.institutionId}` + BASIC_URL_SUFIX + `/parenteralplans/most-frequent`;
+		return this.http.get<ParenteralPlanDto[]>(url);
 	}
 }
