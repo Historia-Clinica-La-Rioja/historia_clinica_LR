785e099a7e9d4f95768763bcaf5974425206ab9d
Lucas Benjamin Obyszuk <lucasobyszuk@gmail.com>
2023-02-24 13:18:31 -0300

    tg-10293. [FE] Los botones de acciones relacionadas al episodio de guardia deben aparecer ahora dentro del resumen

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/guardia/guardia.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/guardia/guardia.component.ts
@@ -1,14 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { Router, ActivatedRoute } from '@angular/router';
-import { EmergencyCareEpisodeInProgressDto, ResponseEmergencyCareDto } from '@api-rest/api-model';
+import { EmergencyCareEpisodeInProgressDto } from '@api-rest/api-model';
 import { EmergencyCareEpisodeStateService } from '@api-rest/services/emergency-care-episode-state.service';
 import { ContextService } from '@core/services/context.service';
 import { FACTORES_DE_RIESGO } from '@historia-clinica/constants/summaries';
 import { RiskFactorFull } from '@historia-clinica/modules/guardia/components/triage-details/triage-details.component';
 import { Triages, EstadosEpisodio } from '@historia-clinica/modules/guardia/constants/masterdata';
-import { SelectConsultorioComponent } from '@historia-clinica/modules/guardia/dialogs/select-consultorio/select-consultorio.component';
-import { EpisodeStateService } from '@historia-clinica/modules/guardia/services/episode-state.service';
 import { SummaryHeader } from '@presentation/components/summary-card/summary-card.component';
 import { SnackBarService } from '@presentation/services/snack-bar.service';
 
@@ -27,8 +24,6 @@ export class GuardiaComponent implements OnInit {
     private readonly routePrefix;
 
     episodeId: number;
-    responseEmergencyCare: ResponseEmergencyCareDto;
-
     episodeState: EstadosEpisodio;
 
     riskFactors: RiskFactorFull[];
@@ -39,9 +34,7 @@ export class GuardiaComponent implements OnInit {
     constructor(
         private readonly router: Router,
         private readonly route: ActivatedRoute,
-        private readonly dialog: MatDialog,
         private snackBarService: SnackBarService,
-        private readonly episodeStateService: EpisodeStateService,
         private readonly emergencyCareEpisodeStateService: EmergencyCareEpisodeStateService,
         private readonly contextService: ContextService,
     ) {
@@ -62,43 +55,6 @@ export class GuardiaComponent implements OnInit {
             }
         );
 
-    }
-
-    cancelAttention() {
-        const dialogRef = this.dialog.open(SelectConsultorioComponent, {
-            width: '25%',
-            data: { title: 'ambulatoria.paciente.guardia.CANCEL_BUTTON' }
-        });
-
-        dialogRef.afterClosed().subscribe(consultorio => {
-            if (consultorio) {
-                this.episodeStateService.cancelar(this.episodeId, consultorio.id).subscribe(changed => {
-                    if (changed) {
-                        this.snackBarService.showSuccess('ambulatoria.paciente.guardia.CANCEL_ATTENTION_SUCCESS');
-                        this.episodeState = EstadosEpisodio.EN_ESPERA;
-                    } else {
-                        this.snackBarService.showError('ambulatoria.paciente.guardia.CANCEL_ATTENTION_ERROR');
-                    }
-                }, _ => this.snackBarService.showError('ambulatoria.paciente.guardia.CANCEL_ATTENTION_ERROR')
-                );
-            }
-        });
-    }
-
-    goToMedicalDischarge() {
-        if (!this.responseEmergencyCare?.patient) {
-            this.snackBarService.showError('ambulatoria.paciente.guardia.PATIENT_REQUIRED');
-        } else {
-            this.router.navigate([`/institucion/${this.contextService.institutionId}/guardia/episodio/${this.episodeId}/alta-medica`]);
-        }
-    }
-
-    goToAdministrativeDischarge() {
-        this.router.navigate([`/institucion/${this.contextService.institutionId}/guardia/episodio/${this.episodeId}/alta-administrativa`]);
-    }
-
-    goToEditEpisode() {
-        this.router.navigate([`/institucion/${this.contextService.institutionId}/guardia/episodio/${this.episodeId}/edit`]);
     }
 
     updateRiskFactors(triageRiskFactors: RiskFactorFull[]): void {
--- front-end/apps/projects/hospital/src/app/modules/pacientes/component/resumen-de-guardia/resumen-de-guardia.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/pacientes/component/resumen-de-guardia/resumen-de-guardia.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ERole } from '@api-rest/api-model';
 import { ResponseEmergencyCareDto, TriageListDto } from '@api-rest/api-model';
 import { EmergencyCareEpisodeService } from '@api-rest/services/emergency-care-episode.service';
 import { TriageService } from '@api-rest/services/triage.service';
@@ -9,8 +10,20 @@ import { TriageDefinitionsService } from '@historia-clinica/modules/guardia/serv
 import { GUARDIA } from '@historia-clinica/constants/summaries';
 import { SummaryHeader } from '@presentation/components/summary-card/summary-card.component';
 import { RiskFactorFull, Triage } from '@historia-clinica/modules/guardia/components/triage-details/triage-details.component';
-import { EmergencyCareTypes } from '@historia-clinica/modules/guardia/constants/masterdata';
+import { EmergencyCareTypes, EstadosEpisodio } from '@historia-clinica/modules/guardia/constants/masterdata';
 import { MatDialog } from '@angular/material/dialog';
+import { EpisodeStateService } from '@historia-clinica/modules/guardia/services/episode-state.service';
+import { SelectConsultorioComponent } from '@historia-clinica/modules/guardia/dialogs/select-consultorio/select-consultorio.component';
+import { SnackBarService } from '@presentation/services/snack-bar.service';
+import { ContextService } from '@core/services/context.service';
+import { Router } from '@angular/router';
+import { EmergencyCareEpisodeStateService } from '@api-rest/services/emergency-care-episode-state.service';
+import { ConfirmDialogComponent } from '@presentation/dialogs/confirm-dialog/confirm-dialog.component';
+import { EmergencyCareEpisodeAdministrativeDischargeService } from '@api-rest/services/emergency-care-episode-administrative-service.service';
+import { PermissionsService } from '@core/services/permissions.service';
+import { anyMatch } from '@core/utils/array.utils';
+
+const TRANSLATE_KEY_PREFIX = 'guardia.home.episodes.episode.actions';
 
 @Component({
   selector: 'app-resumen-de-guardia',
@@ -21,18 +34,24 @@ export class ResumenDeGuardiaComponent implements OnInit {
 
   @Input() episodeId: number;
   @Input() withoutMedicalDischarge: boolean;
-
   @Output() triageRiskFactors = new EventEmitter<RiskFactorFull[]>();
+
   guardiaSummary: SummaryHeader = GUARDIA;
+  readonly STATES = EstadosEpisodio;
+  episodeState: EstadosEpisodio;
 
   responseEmergencyCare: ResponseEmergencyCareDto;
+  emergencyCareType: EmergencyCareTypes;
+  doctorsOfficeDescription: string;
 
   triagesHistory: TriageReduced[];
   fullNamesHistoryTriage: string[];
   lastTriage: Triage;
 
-  emergencyCareType: EmergencyCareTypes;
-  doctorsOfficeDescription: string;
+  hasEmergencyCareRelatedRole: boolean;
+  hasRoleAdministrative: boolean;
+
+  avaibleActions: ActionInfo[] = [];
 
   constructor(
     private readonly emergencyCareEpisodeService: EmergencyCareEpisodeService,
@@ -41,11 +60,33 @@ export class ResumenDeGuardiaComponent implements OnInit {
     private readonly patientNameService: PatientNameService,
     private readonly triageDefinitionsService: TriageDefinitionsService,
     private readonly dialog: MatDialog,
+    private readonly episodeStateService: EpisodeStateService,
+    private snackBarService: SnackBarService,
+    private readonly contextService: ContextService,
+    private readonly router: Router,
+    private readonly emergencyCareEpisodeStateService: EmergencyCareEpisodeStateService,
+    private readonly emergencyCareEpisodeAdministrativeDischargeService: EmergencyCareEpisodeAdministrativeDischargeService,
+    private readonly permissionsService: PermissionsService,
   ) {
 
   }
 
   ngOnInit(): void {
+    this.permissionsService.contextAssignments$().subscribe(
+      (userRoles: ERole[]) => {
+        this.hasEmergencyCareRelatedRole = anyMatch<ERole>(userRoles, [ERole.ESPECIALISTA_MEDICO, ERole.ENFERMERO, ERole.PROFESIONAL_DE_SALUD]);
+        this.hasRoleAdministrative = anyMatch<ERole>(userRoles, [ERole.ADMINISTRATIVO]);
+
+        this.emergencyCareEpisodeStateService.getState(this.episodeId).subscribe(
+          state => {
+            this.episodeState = state.id;
+            this.calculateAvaibleActions();
+          }
+        );
+
+      }
+    );
+
     this.loadEpisode();
   }
 
@@ -59,6 +100,154 @@ export class ResumenDeGuardiaComponent implements OnInit {
           }
         });
       });
+  }
+
+  cancelAttention() {
+    const dialogRef = this.dialog.open(SelectConsultorioComponent, {
+      width: '25%',
+      data: { title: 'ambulatoria.paciente.guardia.CANCEL_BUTTON' }
+    });
+
+    dialogRef.afterClosed().subscribe(consultorio => {
+      if (consultorio) {
+        this.episodeStateService.cancelar(this.episodeId, consultorio.id).subscribe(changed => {
+          if (changed) {
+            this.snackBarService.showSuccess('ambulatoria.paciente.guardia.CANCEL_ATTENTION_SUCCESS');
+            this.episodeState = EstadosEpisodio.EN_ESPERA;
+            this.calculateAvaibleActions();
+          } else {
+            this.snackBarService.showError('ambulatoria.paciente.guardia.CANCEL_ATTENTION_ERROR');
+          }
+        }, _ => this.snackBarService.showError('ambulatoria.paciente.guardia.CANCEL_ATTENTION_ERROR')
+        );
+      }
+    });
+  }
+
+  goToMedicalDischarge() {
+    if (!this.responseEmergencyCare?.patient) {
+      this.snackBarService.showError('ambulatoria.paciente.guardia.PATIENT_REQUIRED');
+    } else {
+      this.router.navigate([`/institucion/${this.contextService.institutionId}/guardia/episodio/${this.episodeId}/alta-medica`]);
+    }
+  }
+
+  goToAdministrativeDischarge() {
+    this.router.navigate([`/institucion/${this.contextService.institutionId}/guardia/episodio/${this.episodeId}/alta-administrativa`]);
+  }
+
+  goToEditEpisode() {
+    this.router.navigate([`/institucion/${this.contextService.institutionId}/guardia/episodio/${this.episodeId}/edit`]);
+  }
+
+  atender(): void {
+
+    const dialogRef = this.dialog.open(SelectConsultorioComponent, {
+      width: '25%',
+      data: { title: 'guardia.select_consultorio.ATENDER' }
+    });
+
+    dialogRef.afterClosed().subscribe(consultorio => {
+      if (consultorio) {
+        this.episodeStateService.atender(this.episodeId, consultorio.id).subscribe(changed => {
+          if (changed) {
+            this.snackBarService.showSuccess(`${TRANSLATE_KEY_PREFIX}.atender.SUCCESS`);
+            this.episodeState = EstadosEpisodio.EN_ATENCION;
+            this.calculateAvaibleActions();
+          }
+          else {
+            this.snackBarService.showError(`${TRANSLATE_KEY_PREFIX}.atender.ERROR`);
+          }
+        }, _ => this.snackBarService.showError(`${TRANSLATE_KEY_PREFIX}.atender.ERROR`)
+        );
+      }
+    });
+  }
+
+  finalizar(): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: 'guardia.home.episodes.episode.actions.finalizar_ausencia.TITLE',
+        content: 'guardia.home.episodes.episode.actions.finalizar_ausencia.CONFIRM'
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(confirmed => {
+      if (confirmed) {
+        this.emergencyCareEpisodeAdministrativeDischargeService.newAdministrativeDischargeByAbsence(this.episodeId).subscribe(
+          changed => {
+            if (changed) {
+              this.snackBarService.showSuccess(`${TRANSLATE_KEY_PREFIX}.finalizar_ausencia.SUCCESS`);
+              window.location.reload();
+            }
+            else {
+              this.snackBarService.showError(`${TRANSLATE_KEY_PREFIX}.finalizar_ausencia.ERROR`);
+            }
+          },
+          _ => this.snackBarService.showError(`${TRANSLATE_KEY_PREFIX}.finalizar_ausencia.ERROR`)
+        );
+      }
+    });
+  }
+
+  private calculateAvaibleActions() {
+    this.avaibleActions = [];
+
+    // Following code within this function must be in this order
+
+    if (this.hasEmergencyCareRelatedRole && this.episodeState === this.STATES.EN_ATENCION) {
+      let action: ActionInfo = {
+        label: 'ambulatoria.paciente.guardia.MEDICAL_DISCHARGE_BUTTON',
+        id: 'alta-medica',
+        callback: this.goToMedicalDischarge.bind(this)
+      }
+      this.avaibleActions.push(action);
+    }
+
+    if (this.hasRoleAdministrative && this.episodeState === this.STATES.CON_ALTA_MEDICA) {
+      let action: ActionInfo = {
+        label: 'ambulatoria.paciente.guardia.ADMINISTRATIVE_DISCHARGE_BUTTON',
+        id: 'administrative-discharge',
+        callback: this.goToAdministrativeDischarge.bind(this)
+      }
+      this.avaibleActions.push(action);
+    }
+
+    if (this.episodeState === this.STATES.EN_ATENCION || this.episodeState === this.STATES.EN_ESPERA) {
+      let action: ActionInfo = {
+        label: 'ambulatoria.paciente.guardia.EDIT_BUTTON',
+        id: 'edit-episode',
+        callback: this.goToEditEpisode.bind(this).bind(this)
+      }
+      this.avaibleActions.push(action);
+    }
+
+    if (this.episodeState === this.STATES.EN_ESPERA) {
+      let action: ActionInfo = {
+        label: 'guardia.home.episodes.episode.actions.atender.TITLE',
+        id: 'atender',
+        callback: this.atender.bind(this)
+      }
+      this.avaibleActions.push(action);
+    }
+
+    if (this.episodeState === this.STATES.EN_ATENCION) {
+      let action: ActionInfo = {
+        label: 'ambulatoria.paciente.guardia.CANCEL_BUTTON',
+        id: 'cancel-attention',
+        callback: this.cancelAttention.bind(this)
+      }
+      this.avaibleActions.push(action);
+    }
+
+    if (this.hasRoleAdministrative && this.episodeState === this.STATES.EN_ESPERA) {
+      let action: ActionInfo = {
+        label: 'guardia.home.episodes.episode.actions.finalizar_ausencia.TITLE',
+        id: 'finalizar-ausencia',
+        callback: this.finalizar.bind(this)
+      }
+      this.avaibleActions.push(action);
+    }
   }
 
   private loadFullNames() {
@@ -107,12 +296,18 @@ export class ResumenDeGuardiaComponent implements OnInit {
 }
 
 export interface TriageReduced {
-  creationDate: Date;
-  category: TriageCategory;
+  creationDate: Date,
+  category: TriageCategory,
   createdBy: {
     firstName: string,
     lastName: string,
     nameSelfDetermination: string
-  };
-  doctorsOfficeDescription: string;
+  },
+  doctorsOfficeDescription: string
+}
+
+interface ActionInfo {
+  label: string,
+  id: string,
+  callback: Function
 }
