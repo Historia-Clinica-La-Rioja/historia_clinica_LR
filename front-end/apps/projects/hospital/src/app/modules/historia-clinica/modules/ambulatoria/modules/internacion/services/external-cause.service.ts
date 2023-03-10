01161ac2fad02df114f76c4b124c25bc4c33ad56
FacundoBarreiros <facundobarreiros@gmail.com>
2023-03-10 17:48:58 -0300

    tg-10538 [FE] Crear servicio de causa externa

---
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/modules/internacion/services/external-cause.service.ts
@@ -0,0 +1,108 @@
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { pushTo, removeFrom } from '@core/utils/array.utils';
+import { SnomedECL } from '@api-rest/api-model';
+import { SnomedDto } from '@api-rest/api-model';
+import { ExternalCauseDto } from '@api-rest/api-model';
+import { Injectable } from '@angular/core';
+import { BasicTable } from '@material/model/table.model';
+import { BehaviorSubject, of, Subject } from 'rxjs';
+import { SnomedService } from '@historia-clinica/services/snomed.service';
+import { SnomedSemanticSearch } from '@historia-clinica/services/snomed.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root'
+})
+
+export class ExternalCauseService {
+
+	snomedConceptEvent: SnomedDto;
+	snomedConceptEvent$: Subject<SnomedDto> = new Subject<SnomedDto>();
+	externalCause: Subject<ExternalCauseDto> = new Subject<ExternalCauseDto>();
+
+	formEvent = new FormGroup({
+		snomedEvent: new FormControl('', Validators.required)
+	});
+
+	ecl = SnomedECL.EVENT;
+
+	table: BasicTable<SnomedDto> = {
+		data: [],
+		columns: [
+			{
+				def: 'eventType',
+				header: 'internaciones.anamnesis.externalCause.EVENT',
+				display: ap => ap.pt
+			}
+		],
+		displayedColumns: ['pt', 'remove'],
+	};
+
+	private tableSubject = new BehaviorSubject<BasicTable<SnomedDto>>(null);
+	table$ = this.tableSubject.asObservable();
+
+	constructor(
+		private readonly snomedService: SnomedService,
+	) { }
+
+	setValue(externalCause: ExternalCauseDto) {
+		this.externalCause.next(externalCause);
+	}
+
+	getValue(): Observable<ExternalCauseDto> {
+		return this.externalCause.asObservable();
+	}
+
+	remove(index: number) {
+		this.table.data = removeFrom<SnomedDto>(this.table.data, index);
+		this.formEvent.reset();
+		this.snomedConceptEvent = null;
+		this.snomedConceptEvent$.next(null);
+		this.getTableInit();
+	}
+
+	openSearchDialogEvent(searchValue: string) {
+		if (searchValue) {
+			const search: SnomedSemanticSearch = {
+				searchValue,
+				eclFilter: SnomedECL.EVENT
+			};
+			this.snomedService.openConceptsSearchDialog(search)
+				.subscribe((selectedConcept: SnomedDto) => this.setConceptEventSnomed(selectedConcept));
+		}
+	}
+
+	setConceptEventSnomed(selectedConcept: SnomedDto) {
+		this.snomedConceptEvent = selectedConcept;
+		this.snomedConceptEvent$.next(selectedConcept);
+		const pt = selectedConcept ? selectedConcept.pt : '';
+		this.formEvent.controls.snomedEvent.setValue(pt);
+	}
+
+	addEvent() {
+		this.table.data = pushTo<SnomedDto>(this.table?.data, this.snomedConceptEvent);
+		if (this.formEvent.valid) {
+			this.formEvent.reset();
+		}
+		this.getTableInit();
+	}
+
+	resetForm() {
+		this.formEvent.reset();
+		this.snomedConceptEvent = null;
+	}
+
+	getTableInit() {
+		const table = {
+			data: this.table.data,
+			columns: this.table.columns,
+			displayedColumns: this.table.displayedColumns
+		};
+		this.tableSubject.next(table);
+	}
+
+	getSnomedConceptEvent() {
+		return this.snomedConceptEvent$.asObservable();
+	}
+}
