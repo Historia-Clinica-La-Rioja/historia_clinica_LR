957f92bc6a902f043900091b9e6074683c562c5e
Matias Valin <matiasvalin98@gmail.com>
2023-03-13 13:40:23 -0300

    tg-10623 Agregar Rol Auditor

--- back-end/hospital-api/src/main/java/net/pladema/authorization/infrastructure/input/rest/mapper/impl/RoleNameMapperImpl.java
+++ back-end/hospital-api/src/main/java/net/pladema/authorization/infrastructure/input/rest/mapper/impl/RoleNameMapperImpl.java
@@ -46,6 +46,7 @@ public class RoleNameMapperImpl implements RoleNameMapper {
 			case ADMINISTRATIVO_RED_DE_IMAGENES: return "Administrativo red de imágenes";
 			case PRESCRIPTOR: return "Prescriptor";
 			case ADMINISTRADOR_INSTITUCIONAL_PRESCRIPTOR: return "Administrador Institucional Prescriptor";
+			case AUDITOR_MPI: return "Auditor MPI";
 		}
 		throw new NotFoundException("role-not-exists", String.format("El rol %s no existe", eRole));
 	}
