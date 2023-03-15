e7883d495b022b35193f99400c7ebfed86c63239
Manuel Gil <manuelgilmoltrasio@gmail.com>
2023-03-15 16:43:30 -0300

    tg-10654 [FE] Agregar dropdown de equipos

--- front-end/apps/projects/hospital/src/app/modules/turnos/components/worklist/worklist.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/turnos/components/worklist/worklist.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { EquipmentDto } from '@api-rest/api-model';
+import { EquipmentService } from '@api-rest/services/equipment.service';
 
 @Component({
-  selector: 'app-worklist',
-  templateUrl: './worklist.component.html',
-  styleUrls: ['./worklist.component.scss']
+    selector: 'app-worklist',
+    templateUrl: './worklist.component.html',
+    styleUrls: ['./worklist.component.scss']
 })
 export class WorklistComponent implements OnInit {
+    equipments: EquipmentDto[] = [];
 
-  constructor() { }
+    constructor(private readonly equipmentService: EquipmentService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.equipmentService.getAll().subscribe((equipments: EquipmentDto[]) => {
+            this.equipments = equipments;
+        });
+    }
 
 }
