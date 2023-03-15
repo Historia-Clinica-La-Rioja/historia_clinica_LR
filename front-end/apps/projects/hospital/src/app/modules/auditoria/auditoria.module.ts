cef50fc5224b5bcecf5113c2e7eb289b09aefba0
Brenda Flamminio <bflamminio.blitzit.com.ar>
2023-03-15 14:13:02 -0300

    tg-10613 [FE] Agregar card de fusion de pacientes

--- front-end/apps/projects/hospital/src/app/modules/auditoria/auditoria.module.ts
+++ front-end/apps/projects/hospital/src/app/modules/auditoria/auditoria.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { AuditoriaRoutingModule } from './auditoria-routing.module';
 import { HomeComponent } from './routes/home/home.component';
+import { PresentationModule } from '@presentation/presentation.module';
+import { LazyMaterialModule } from '../lazy-material/lazy-material.module';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { HomeComponent } from './routes/home/home.component';
   ],
   imports: [
     CommonModule,
-    AuditoriaRoutingModule
+    AuditoriaRoutingModule,
+	LazyMaterialModule,
+	PresentationModule,
   ]
 })
 export class AuditoriaModule { }
