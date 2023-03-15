7d70ad99e5c31c74917a9cf9bf3e651e73ace9df
Brenda Flamminio <bflamminio.blitzit.com.ar>
2023-03-15 14:46:36 -0300

    tg-10614 [FE] Crear pantalla para el listado de pacientes

--- front-end/apps/projects/hospital/src/app/modules/auditoria/auditoria.module.ts
+++ front-end/apps/projects/hospital/src/app/modules/auditoria/auditoria.module.ts
@@ -5,11 +5,13 @@ import { AuditoriaRoutingModule } from './auditoria-routing.module';
 import { HomeComponent } from './routes/home/home.component';
 import { PresentationModule } from '@presentation/presentation.module';
 import { LazyMaterialModule } from '../lazy-material/lazy-material.module';
+import { PatientsFusionComponent } from './routes/patients-fusion/patients-fusion.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    PatientsFusionComponent
   ],
   imports: [
     CommonModule,
---
+++ front-end/apps/projects/hospital/src/app/modules/auditoria/routes/patients-fusion/patients-fusion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patients-fusion',
+  templateUrl: './patients-fusion.component.html',
+  styleUrls: ['./patients-fusion.component.scss']
+})
+export class PatientsFusionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
