b953e1d2ae5d2b423bc72948d1d547cbda9c546d
Brenda Flamminio <bflamminio.blitzit.com.ar>
2023-03-31 12:47:21 -0300

    tg-10835 [FE] Agrega lista de cards para visualizar los datos de los pacientes

--- front-end/apps/projects/hospital/src/app/modules/auditoria/routes/patient-fusion/patient-fusion.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/auditoria/routes/patient-fusion/patient-fusion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { IdentificationTypeDto } from '@api-rest/api-model';
+import { PersonMasterDataService } from '@api-rest/services/person-master-data.service';
 import { ContextService } from '@core/services/context.service';
 
 const ROUTE_CONTROL_PATIENT_DUPLICATE = "auditoria/control-pacientes-duplicados"
@@ -10,7 +12,15 @@ const ROUTE_CONTROL_PATIENT_DUPLICATE = "auditoria/control-pacientes-duplicados"
 })
 export class PatientFusionComponent implements OnInit {
 	private readonly routePrefix;
-	constructor(private router: Router, private contextService: ContextService) { this.routePrefix = 'institucion/' + this.contextService.institutionId + '/' }
+	listPatientData: any[];
+	identificationTypeList: IdentificationTypeDto[];
+	constructor(private router: Router, private contextService: ContextService, private personMasterDataService: PersonMasterDataService) {
+		this.routePrefix = 'institucion/' + this.contextService.institutionId + '/';
+		this.personMasterDataService.getIdentificationTypes()
+			.subscribe(identificationTypes => {
+				this.identificationTypeList = identificationTypes;
+			});
+	}
 
 	ngOnInit(): void {
 	}
@@ -19,4 +29,7 @@ export class PatientFusionComponent implements OnInit {
 		this.router.navigate([this.routePrefix + ROUTE_CONTROL_PATIENT_DUPLICATE])
 	}
 
+	getIdentificationType(value: number) {
+		return this.identificationTypeList?.find(type => type.id === value).description
+	}
 }
