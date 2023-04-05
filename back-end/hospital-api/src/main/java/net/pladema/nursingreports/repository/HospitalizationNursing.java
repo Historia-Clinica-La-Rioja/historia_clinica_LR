f869458f3f5a42da9ddd3abd9ae3d79235422329
Cristian Quevedo <cristianquevedocqr@gmail.com>
2023-04-05 09:51:55 -0300

    created HospitalizationNursing, NursingEmergencies, OutpatientNursing, and TotalNursingRecovery classes in new 'nursingreports' package

---
+++ back-end/hospital-api/src/main/java/net/pladema/nursingreports/repository/HospitalizationNursing.java
@@ -0,0 +1,46 @@
+package net.pladema.nursingreports.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class HospitalizationNursing {
+
+	private String institution;
+
+	private String lastName;
+
+	private String completeName;
+
+	private String gender;
+
+	private String identification;
+
+	private String professional;
+
+	private String licenseNumber;
+
+	private String entryDate;
+
+	private String probableDischargeDate;
+
+	private String bed;
+
+	private String categoryBed;
+
+	private String roomName;
+
+	private String sector;
+
+	private String dischargeDate;
+
+	private String procedures;
+
+	private String vitalSign;
+
+}
---
+++ back-end/hospital-api/src/main/java/net/pladema/nursingreports/repository/OutpatientNursing.java
@@ -0,0 +1,62 @@
+package net.pladema.nursingreports.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class OutpatientNursing {
+
+	private String institution;
+
+	private String operativeUnit;
+
+	private String patientProvider;
+
+	private String providerDni;
+
+	private String attentionDate;
+
+	private String hour;
+
+	private String consultationNumber;
+
+	private String patientDni;
+
+	private String patientName;
+
+	private String gender;
+
+	private String selfPerceivedGender;
+
+	private String selfPerceivedName;
+
+	private String birthDate;
+
+	private String ageTurn;
+
+	private String ageToday;
+
+	private String ethnicity;
+
+	private String medicalCoverage;
+
+	private String patientAddress;
+
+	private String patientLocation;
+
+	private String educationLevel;
+
+	private String occupation;
+
+	private String vitalSign;
+
+	private String procedures;
+
+	private String evolution;
+
+}
