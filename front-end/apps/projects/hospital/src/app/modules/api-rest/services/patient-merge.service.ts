7d24619045d65d40e077ed7e09a13aa2647d919f
Brenda Flamminio <bflamminio.blitzit.com.ar>
2023-04-06 11:11:22 -0300

    tg-10837 [FE] Agrega boton y funcionalidad  fusionar

---
+++ front-end/apps/projects/hospital/src/app/modules/api-rest/services/patient-merge.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { PatientPersonalInfoDto, PatientToMergeDto } from '@api-rest/api-model';
+import { ContextService } from '@core/services/context.service';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientMergeService {
+
+  constructor(private http: HttpClient,
+	private readonly contextService: ContextService) { }
+
+	merge(patientToMerge:PatientToMergeDto):Observable<number>{
+		const url = `${environment.apiBase}/patient-merge/institution/${this.contextService.institutionId}/merge`;
+		return this.http.post<number>(url,patientToMerge);
+	}
+}
--- front-end/apps/projects/hospital/src/app/modules/auditoria/routes/patient-fusion/patient-fusion.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/auditoria/routes/patient-fusion/patient-fusion.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { DuplicatePatientDto, IdentificationTypeDto, PatientPersonalInfoDto, PatientType } from '@api-rest/api-model';
+import { DuplicatePatientDto, IdentificationTypeDto, PatientPersonalInfoDto, PatientToMergeDto, PatientType } from '@api-rest/api-model';
 import { PersonMasterDataService } from '@api-rest/services/person-master-data.service';
 import { ContextService } from '@core/services/context.service';
 import { PatientAuditService } from '../../services/patient-audit.service';
 import { AuditPatientService } from '@api-rest/services/audit-patient.service';
 import { Observable, of } from 'rxjs';
 import { PatientMasterDataService } from '@api-rest/services/patient-master-data.service';
+import { PatientMergeService } from '@api-rest/services/patient-merge.service';
 
 const ROUTE_CONTROL_PATIENT_DUPLICATE = "auditoria/control-pacientes-duplicados"
 @Component({
@@ -21,17 +22,33 @@ export class PatientFusionComponent implements OnInit {
 	patientToAudit: DuplicatePatientDto;
 	patientsTypes: PatientType[];
 	keyAttributes = KeyAttributes;
-	patientFusion: PatientPersonalInfoDto;
+	oldPatientsIds: number[]=[];
+	patientToMerge: PatientToMergeDto = {
+		activePatientId: null,
+		oldPatientsIds:null,
+		registrationDataPerson:  {
+			genderId: null,
+			nameSelfDetermination: null,
+			phonePrefix: null,
+			birthDate: null,
+			firstName: null,
+			identificationNumber: null,
+			identificationTypeId:null,
+			lastName: null,
+			middleNames: null,
+			otherLastNames: null,
+			phoneNumber: null,
+		},
+	}
 
 	constructor(private router: Router, private contextService: ContextService, private personMasterDataService: PersonMasterDataService,
 		private patientAuditService: PatientAuditService, private auditPatientService: AuditPatientService,
-		private patientMasterDataService: PatientMasterDataService) {
+		private patientMasterDataService: PatientMasterDataService, private patientMergeService: PatientMergeService) {
 		this.routePrefix = `institucion/${this.contextService.institutionId}/`;
 		this.personMasterDataService.getIdentificationTypes()
 			.subscribe(identificationTypes => {
 				this.identificationTypeList = identificationTypes;
 			});
-
 	}
 
 	ngOnInit(): void {
@@ -66,27 +83,58 @@ export class PatientFusionComponent implements OnInit {
 		return this.patientsTypes?.find(type => type.id === value).description
 	}
 
+	setSelectedPatient(patient: any) {
+		patient.selected = !patient.selected;
+		let index = this.oldPatientsIds?.indexOf(patient.patientId)
+		if (index !== -1) {
+			this.oldPatientsIds.splice(index, 1);
+		} else {
+			this.oldPatientsIds.push(patient.patientId);
+		}
+	}
+
 	setValuesPatientFusion(key: any, value1: any, value2?: any) {
 		switch (key) {
 			case this.keyAttributes.PATIENT_ID:
-				this.patientFusion.patientId = value1;
+				this.patientToMerge.activePatientId = value1;
 				break;
 			case this.keyAttributes.BIRTHDATE:
-				this.patientFusion.birthdate = value1;
+				this.patientToMerge.registrationDataPerson.birthDate = value1;
 				break;
 			case this.keyAttributes.NAMES:
-				this.patientFusion.firstName = value1;
-				this.patientFusion.middleNames = value2;
+				this.patientToMerge.registrationDataPerson.firstName = value1;
+				this.patientToMerge.registrationDataPerson.middleNames = value2;
 				break;
 			case this.keyAttributes.LASTNAMES:
-				this.patientFusion.lastName = value1;
-				this.patientFusion.otherLastNames = value2;
+				this.patientToMerge.registrationDataPerson.lastName = value1;
+				this.patientToMerge.registrationDataPerson.otherLastNames = value2;
 				break;
 			case this.keyAttributes.IDENTIFICATION:
-				this.patientFusion.identificationTypeId = value1;
-				this.patientFusion.identificationNumber = value2;
+				this.patientToMerge.registrationDataPerson.identificationTypeId = value1;
+				this.patientToMerge.registrationDataPerson.identificationNumber = value2;
 				break;
 		}
+	}
+
+	merge() {
+		this.completePatientDataToMerge();
+		this.patientMergeService.merge(this.patientToMerge).subscribe(res => {
+
+		})
+	}
+
+	completePatientDataToMerge() {
+		let auxiliaryPatientList: PatientPersonalInfoDto[];
+		this.listPatientData$.subscribe(list => {
+			auxiliaryPatientList = list;
+		})
+		this.patientToMerge.registrationDataPerson.genderId = auxiliaryPatientList.find(patient => patient.patientId === this.patientToMerge.activePatientId).genderId;
+		this.patientToMerge.registrationDataPerson.phonePrefix = auxiliaryPatientList.find(patient => patient.patientId === this.patientToMerge.activePatientId).phonePrefix;
+		this.patientToMerge.registrationDataPerson.phoneNumber = auxiliaryPatientList.find(patient => patient.patientId === this.patientToMerge.activePatientId).phoneNumber;
+		this.patientToMerge.registrationDataPerson.nameSelfDetermination = auxiliaryPatientList.find(patient => patient.patientId === this.patientToMerge.activePatientId).nameSelfDetermination;
+		this.patientToMerge.oldPatientsIds = this.oldPatientsIds;
+
+		this.oldPatientsIds.splice( this.oldPatientsIds?.indexOf(this.patientToMerge.activePatientId), 1);
 	}
 
 }
