2b937b9623fac56045af44265e09bb26344d977a
mmalyvarni <mateomv.mmv@gmail.com>
2023-04-14 16:11:17 -0300

    tg-11219 [BE] Creacion de tabla y entidad para almacenar notas de evolucion de guardia

---
+++ back-end/hospital-api/src/main/java/net/pladema/emergencycare/repository/entity/EmergencyCareEvolutionNote.java
@@ -0,0 +1,58 @@
+package net.pladema.emergencycare.repository.entity;
+
+import ar.lamansys.sgx.shared.auditable.entity.SGXAuditableEntity;
+import ar.lamansys.sgx.shared.auditable.listener.SGXAuditListener;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "emergency_care_evolution_note")
+@EntityListeners(SGXAuditListener.class)
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class EmergencyCareEvolutionNote extends SGXAuditableEntity<Integer> {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "patient_id", nullable = false)
+	private Integer patientId;
+
+	@Column(name = "clinical_specialty_id")
+	private Integer clinicalSpecialtyId;
+
+	@Column(name = "institution_id", nullable = false)
+	private Integer institutionId;
+
+	@Column(name = "start_date", nullable = false)
+	private LocalDate startDate;
+
+	@Column(name = "document_id", nullable = false)
+	private Long documentId;
+
+	@Column(name = "doctor_id")
+	private Integer doctorId;
+
+	@Column(name = "billable", nullable = false)
+	private Boolean billable;
+
+	@Column(name = "patient_medical_coverage_id")
+	private Integer patientMedicalCoverageId;
+
+}
