89896f3021978bcd9420eac1c5d51123772ce96f
Maximiliano Romero Piaggio <mromeropiaggio@alumnos.exa.unicen.edu.ar>
2023-04-14 13:21:52 -0300

    tg-11210 Ajustar el reverse proxy exception handler para manejar errores

--- back-end/sgx-reverseproxy/src/main/java/ar/lamansys/base/infrastructure/input/rest/exceptions/handler/RestExceptionHandler.java
+++ back-end/sgx-reverseproxy/src/main/java/ar/lamansys/base/infrastructure/input/rest/exceptions/handler/RestExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.ResourceAccessException;
 
 import ar.lamansys.base.infrastructure.input.rest.exceptions.dto.ApiErrorMessageDto;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +17,16 @@ import lombok.extern.slf4j.Slf4j;
 public class RestExceptionHandler {
 
 	@ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
-	@ExceptionHandler({ RuntimeException.class })
-	protected ApiErrorMessageDto handleDashboardInfoException(RuntimeException ex) {
+	@ExceptionHandler({ ResourceAccessException.class })
+	protected ApiErrorMessageDto handleUnavailableException(ResourceAccessException ex) {
 		log.debug("RuntimeException message -> {}", ex.getMessage(), ex);
-		return new ApiErrorMessageDto(null, ex.getMessage());
+		return new ApiErrorMessageDto(HttpStatus.SERVICE_UNAVAILABLE.toString(), "Timed out. The resource is not available.");
+	}
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler({ RuntimeException.class })
+	protected ApiErrorMessageDto handleGenericException(RuntimeException ex) {
+		log.debug("RuntimeException message -> {}", ex.getMessage(), ex);
+		return new ApiErrorMessageDto(HttpStatus.INTERNAL_SERVER_ERROR.toString(), ex.getCause().getLocalizedMessage());
 	}
 }
