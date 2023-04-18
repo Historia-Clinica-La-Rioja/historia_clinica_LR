c727d55f3f174b53c6ee5640f833613a6c199d52
Maximiliano Romero Piaggio <mromeropiaggio@alumnos.exa.unicen.edu.ar>
2023-04-18 10:43:52 -0300

    tg-11256 Agregar error handler con apimessagedto para obtener url pac global

---
+++ back-end/hospital-api/src/main/java/net/pladema/imagenetwork/infrastructure/input/rest/exception/ImageNetworkExceptionHandler.java
@@ -0,0 +1,34 @@
+package net.pladema.imagenetwork.infrastructure.input.rest.exception;
+
+import java.net.URISyntaxException;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import ar.lamansys.sgx.shared.exceptions.dto.ApiErrorMessageDto;
+import lombok.extern.slf4j.Slf4j;
+import net.pladema.imagenetwork.application.exception.StudyException;
+
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice(basePackages = "net.pladema.imagenetwork")
+public class ImageNetworkExceptionHandler {
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler({StudyException.class})
+	protected ApiErrorMessageDto handleImageNetworkException(StudyException ex) {
+		log.debug("StudyException message -> {}", ex.getMessage(), ex);
+		return new ApiErrorMessageDto(ex.getCode().toString(), ex.getMessage());
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler({URISyntaxException.class})
+	protected ApiErrorMessageDto handleImageNetworkException(URISyntaxException ex) {
+		log.debug("URISyntaxException message -> {}", ex.getMessage(), ex);
+		return new ApiErrorMessageDto(HttpStatus.BAD_REQUEST.toString(), ex.getMessage());
+	}
+}
