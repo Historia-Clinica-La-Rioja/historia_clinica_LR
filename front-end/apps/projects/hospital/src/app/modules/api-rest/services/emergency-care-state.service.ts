fcbc7f04d41e5a6bbedc309a8aa14e22988bc65d
JLardapide <joacolardapide@gmail.com>
2023-04-26 10:54:37 -0300

    tg-11456 [FE] Nuevo servicio para obtener diagnosticos de un episodio de guardia

---
+++ front-end/apps/projects/hospital/src/app/modules/api-rest/services/emergency-care-state.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { DiagnosesGeneralStateDto } from '@api-rest/api-model';
+import { ContextService } from '@core/services/context.service';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class EmergencyCareStateService {
+
+	private URL_PREFIX = `${environment.apiBase}` + '/institution/' + `${this.contextService.institutionId}` + `/emergency-care/episode/`;
+
+	constructor(
+		private http: HttpClient,
+		private contextService: ContextService,
+	) { }
+
+
+	getEmergencyCareEpisodeDiagnoses(episodeId: number): Observable<DiagnosesGeneralStateDto[]> {
+		const url = this.URL_PREFIX + episodeId + `/diagnoses`;
+		console.log(episodeId);
+
+		return this.http.get<DiagnosesGeneralStateDto[]>(url);
+
+	}
+
+}
