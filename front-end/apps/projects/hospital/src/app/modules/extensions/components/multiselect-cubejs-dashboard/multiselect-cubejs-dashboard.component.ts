de1b231b23d6a0482c8936671de62ec7ee261d9e
jirimoldi <juanirimoldi@gmail.com>
2023-05-10 11:20:38 -0300

    jira-149 Crear componente multiselect y manejo de chips

--- front-end/apps/projects/hospital/src/app/modules/extensions/components/multiselect-cubejs-dashboard/multiselect-cubejs-dashboard.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/extensions/components/multiselect-cubejs-dashboard/multiselect-cubejs-dashboard.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
-import {UIComponentDto} from "@extensions/extensions-model";
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { UIComponentDto } from "@extensions/extensions-model";
 
 
 @Component({
@@ -7,15 +8,35 @@ import {UIComponentDto} from "@extensions/extensions-model";
 	templateUrl: './multiselect-cubejs-dashboard.component.html',
 	styleUrls: ['./multiselect-cubejs-dashboard.component.scss']
 })
-export class MultiselectCubejsDashboardComponent {
+export class MultiselectCubejsDashboardComponent implements OnInit{
 
 	@Input() content: UIComponentDto[];
 	public selectedDashboards: UIComponentDto[] = [];
+	options: UIComponentDto[];
+
 	constructor() {
 	}
 
+	ngOnInit(): void {
+		this.options = [...this.content];
+	}
+
 	onChange(category: UIComponentDto) {
-		this.selectedDashboards = [category];
+		this.selectedDashboards.push(category);
+
+		const index = this.options.findIndex( elem => elem.args.label === category.args.label)
+		if (index >= 0) {
+			this.options.splice(index, 1);
+		}
+	}
+
+	removeDashboard(dashboard) {
+		const index = this.selectedDashboards.findIndex( elem => elem.args.label === dashboard.args.label)
+		if (index >= 0) {
+			this.selectedDashboards.splice(index, 1);
+		}
+		this.options.push(dashboard);
+		this.options.sort((a, b) => a.args.label.localeCompare(b.args.label));
 	}
 
 }
