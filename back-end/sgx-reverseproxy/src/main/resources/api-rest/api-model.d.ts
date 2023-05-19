fc96949475a11b09002fa81fe0db87d6c2de533b
Maximiliano Romero Piaggio <mromeropiaggio@alumnos.exa.unicen.edu.ar>
2023-05-19 14:50:19 -0300

    tg-11840 Fix ts generator dtos en pom.xml sgx-reverseproxy

---
+++ back-end/sgx-reverseproxy/src/main/resources/api-rest/api-model.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export interface ApiErrorMessageDto {
+    args: { [index: string]: any };
+    code: string;
+    text: string;
+}
+
+export interface TokenDto {
+    token: string;
+}
