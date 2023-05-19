3ebbc3480c9f1e6b32257925ad2be5b9e4e66b34
Maximiliano Romero Piaggio <mromeropiaggio@alumnos.exa.unicen.edu.ar>
2023-05-19 13:11:51 -0300

    tg-11828 Borrar endpoint de salvar una asociación de pac-study id

--- back-end/hospital-api/src/main/java/net/pladema/imagenetwork/infrastructure/input/rest/StudyPacAssociationController.java
+++ back-end/hospital-api/src/main/java/net/pladema/imagenetwork/infrastructure/input/rest/StudyPacAssociationController.java
@@ -2,15 +2,12 @@ package net.pladema.imagenetwork.infrastructure.input.rest;
 
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +15,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.pladema.imagenetwork.application.getpacwherestudyishosted.GetPacWhereStudyIsHosted;
-import net.pladema.imagenetwork.application.savepacsherestudyishosted.SavePacWhereStudyIsHosted;
 import net.pladema.imagenetwork.domain.PacsListBo;
-import net.pladema.imagenetwork.domain.StudyPacBo;
 import net.pladema.imagenetwork.infrastructure.input.rest.dto.PacsUrlDTO;
-import net.pladema.imagenetwork.infrastructure.input.rest.dto.StudyPacAssociationDTO;
 
 @RequestMapping("/institutions/{institutionId}/imagenetwork/pacs")
 @Tag(name = "Image Network Pacs", description = "Image Network Pacs")
@@ -32,7 +26,6 @@ import net.pladema.imagenetwork.infrastructure.input.rest.dto.StudyPacAssociatio
 public class StudyPacAssociationController {
 
 	private final GetPacWhereStudyIsHosted getPacWhereStudyIsHosted;
-	private final SavePacWhereStudyIsHosted savePacWhereStudyIsHosted;
 
 	@GetMapping(value = "/{studyInstanceUID}")
 	@PreAuthorize("hasPermission(#institutionId, 'ESPECIALISTA_MEDICO, INFORMADOR')")
@@ -43,14 +36,6 @@ public class StudyPacAssociationController {
 		return ResponseEntity.ok().body(url);
 	}
 
-	@PostMapping(value = "/")
-	public ResponseEntity<String> saveStudyPacGlobalAssociation(@RequestBody StudyPacAssociationDTO studyPacAssociationDTO) throws URISyntaxException {
-		log.debug("Input -> studyPacAssociationDTO {}", studyPacAssociationDTO);
-		String study = savePacWhereStudyIsHosted.run(mapToBo(studyPacAssociationDTO));
-		log.debug("Output -> {}", study);
-		return ResponseEntity.ok().body(study);
-	}
-
 	private static PacsUrlDTO mapToDto(PacsListBo pacsListBo) {
 		return PacsUrlDTO.builder().pacs(pacsListBo
 						.getPacs().stream()
@@ -58,9 +43,4 @@ public class StudyPacAssociationController {
 						.collect(Collectors.toList()))
 				.build();
 	}
-
-	private static StudyPacBo mapToBo(StudyPacAssociationDTO dto) throws URISyntaxException {
-		return new StudyPacBo(dto.getStudyInstanceUID(), dto.getPacGlobalURL());
-	}
-
 }
