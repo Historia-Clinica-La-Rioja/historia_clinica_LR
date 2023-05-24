5cd98380a650cc8f17bab7bb92d05f441d676830
Luis Manterola <manterolaluis@gmail.com>
2023-05-24 11:10:26 -0300

    tg-11906 [P1] Error a generar la receta no deja descargarla con rol prescriptor

--- back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/infrastructure/input/rest/document/DocumentFileDownloadController.java
+++ back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/infrastructure/input/rest/document/DocumentFileDownloadController.java
@@ -24,7 +24,7 @@ public class DocumentFileDownloadController {
 
 
     @GetMapping(value = "/{id}/downloadFile")
-    @PreAuthorize("hasPermission(#institutionId, 'ESPECIALISTA_MEDICO, PROFESIONAL_DE_SALUD, ESPECIALISTA_EN_ODONTOLOGIA, ENFERMERO')")
+    @PreAuthorize("hasPermission(#institutionId, 'ESPECIALISTA_MEDICO, PROFESIONAL_DE_SALUD, ESPECIALISTA_EN_ODONTOLOGIA, ENFERMERO, PRESCRIPTOR')")
     public ResponseEntity<Resource> downloadPdf(
 			@PathVariable(name = "institutionId") Integer institutionId,
 			@PathVariable(name = "id") Long id)
