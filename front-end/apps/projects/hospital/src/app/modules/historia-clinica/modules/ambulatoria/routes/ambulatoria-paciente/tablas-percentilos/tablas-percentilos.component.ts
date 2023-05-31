e94bc5331edc40b69cbbf4d6697bd089204bdbf9
Rodrigo Cordoba <rodrigocordoba2296@gmail.com>
2023-05-31 12:58:09 -0300

    added advances table sap

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/routes/ambulatoria-paciente/tablas-percentilos/tablas-percentilos.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/routes/ambulatoria-paciente/tablas-percentilos/tablas-percentilos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-const IMAGE_ITEM = '../../../../../assets/images/'; 
+const IMAGE_ITEM = '../../../../../assets/tableSAP/'; 
 console.log (IMAGE_ITEM);
 
 export interface PeriodicElement {
@@ -11,7 +11,12 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { Grafic: IMAGE_ITEM + 'empty-profile.png', Orden: 1, Nombre: 'Peso, longitud y perimetro cefalico', Sexo: 'NIÑAS' },
+  { Grafic: IMAGE_ITEM + '2.jpeg', Orden: 1, Nombre: 'Peso, longitud y perimetro cefalico', Sexo: 'NIÑAS' },
+  { Grafic: IMAGE_ITEM + '26.jpeg', Orden: 2, Nombre: 'Peso, longitud y perimetro cefalico', Sexo: 'NIÑOS' },
+  { Grafic: IMAGE_ITEM + '5.jpeg', Orden: 3, Nombre: 'Peso 0 a 6 años', Sexo: 'NIÑAS' },
+  { Grafic: IMAGE_ITEM + '29.jpeg', Orden: 4, Nombre: 'Peso 0 a 6 años', Sexo: 'NIÑOS' },
+
+
 ];
 
 @Component({
