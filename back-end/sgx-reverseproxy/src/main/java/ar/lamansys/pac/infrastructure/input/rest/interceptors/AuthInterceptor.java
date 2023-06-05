a608c0c59ce3edf9e672e3d27e5a8bba27ed11a6
Maximiliano Romero Piaggio <mromeropiaggio@alumnos.exa.unicen.edu.ar>
2023-06-05 14:45:40 -0300

    tg-12043 Mover el módulo de sgx-reverseproxy a nuevo repositorio

--- back-end/sgx-reverseproxy/src/main/java/ar/lamansys/pac/infrastructure/input/rest/interceptors/AuthInterceptor.java
+++
@@ -1,8 +0,0 @@
-package ar.lamansys.pac.infrastructure.input.rest.interceptors;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface AuthInterceptor {
-    boolean process(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
