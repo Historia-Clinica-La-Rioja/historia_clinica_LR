ec69f31f134336e62017729efa2a0da641d7de90
Rodrigo Cordoba <rodrigocordoba2296@gmail.com>
2023-06-05 10:38:17 -0300

    update table sap

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/routes/ambulatoria-paciente/tablas-percentilos/tablas-percentilos.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/routes/ambulatoria-paciente/tablas-percentilos/tablas-percentilos.component.ts
@@ -15,7 +15,14 @@ const ELEMENT_DATA: PeriodicElement[] = [
   { Grafic: IMAGE_ITEM + '26.jpeg', Orden: 2, Nombre: 'Peso, longitud y perimetro cefalico', Sexo: 'NIÑOS' },
   { Grafic: IMAGE_ITEM + '5.jpeg', Orden: 3, Nombre: 'Peso 0 a 6 años', Sexo: 'NIÑAS' },
   { Grafic: IMAGE_ITEM + '29.jpeg', Orden: 4, Nombre: 'Peso 0 a 6 años', Sexo: 'NIÑOS' },
-
+  { Grafic: IMAGE_ITEM + '9.jpeg', Orden: 5, Nombre: 'Longitud Corporal- Talla 0 a 6 años', Sexo: 'NIÑAS' },
+  { Grafic: IMAGE_ITEM + '33.jpeg', Orden: 6, Nombre: 'Longitud Corporal- Talla 0 a 6 años', Sexo: 'NIÑOS' },
+  { Grafic: IMAGE_ITEM + '7.jpeg', Orden: 7, Nombre: 'Peso nacimiento - 19 años', Sexo: 'NIÑAS' },
+  { Grafic: IMAGE_ITEM + '31.jpeg', Orden: 8, Nombre: 'Peso nacimiento - 19 años', Sexo: 'NIÑOS' },
+  { Grafic: IMAGE_ITEM + '11.jpeg', Orden: 9, Nombre: 'Estatura nacimiento - 19 años', Sexo: 'NIÑAS' },
+  { Grafic: IMAGE_ITEM + '35.jpeg', Orden: 10, Nombre: 'Estatura nacimiento - 19 años', Sexo: 'NIÑOS' },
+  { Grafic: IMAGE_ITEM + '13.jpeg', Orden: 11, Nombre: 'Perimetro cefálico nacimiento - 5 años', Sexo: 'NIÑAS' },
+  { Grafic: IMAGE_ITEM + '37.jpeg', Orden: 12, Nombre: 'Perimetro cefálico nacimiento - 5 años', Sexo: 'NIÑOS' },
 
 ];
 
