b508e3e8bcbc56309f1883dd45912e84c82486be
mmalyvarni <mateomv.mmv@gmail.com>
2023-07-12 15:18:28 -0300

    hsi-858 Internación - No refresca correctamente las posibles acciones sobre los documentos

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/clinical-history-actions/clinical-history-actions.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/clinical-history-actions/clinical-history-actions.component.ts
@@ -135,17 +135,18 @@ export class ClinicalHistoryActionsComponent implements OnInit {
 
 	setInternmentInformation(internmentId: number) {
 		this.internmentSummaryFacadeService.setInternmentEpisodeInformation(internmentId, false, true);
+		this.internmentSummaryFacadeService.clearInternment();
 		const anamnesis$ = this.internmentSummaryFacadeService.anamnesis$;
 		const evolutionNote$ = this.internmentSummaryFacadeService.evolutionNote$;
 		const epicrisis$ = this.internmentSummaryFacadeService.epicrisis$;
 		const hasMedicalDischarge$ = this.internmentSummaryFacadeService.hasMedicalDischarge$;
 
 		combineLatest([anamnesis$, evolutionNote$, epicrisis$, hasMedicalDischarge$]).subscribe(
-			documentInformation => {
-				this.anamnesisDoc = documentInformation[0];
-				this.lastEvolutionNoteDoc = documentInformation[1];
-				this.epicrisisDoc = documentInformation[2];
-				this.hasMedicalDischarge = documentInformation[3];
+			([anamnesis, evolutionNote, epicrisis, medicalDischarge]) => {
+				this.anamnesisDoc = anamnesis;
+				this.lastEvolutionNoteDoc = evolutionNote;
+				this.epicrisisDoc = epicrisis;
+				this.hasMedicalDischarge = medicalDischarge;
 				this.hasToDoInternmentAction();
 				if (this.epicrisisDoc?.confirmed === false)
 					this.getEpicrisisDraft();
