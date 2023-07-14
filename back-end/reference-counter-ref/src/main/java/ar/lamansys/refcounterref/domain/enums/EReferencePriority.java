f2c45f212741ce0825ed85b9e1de00700c228574
joselinavaldez <joselinavaldez99@gmail.com>
2023-07-14 13:01:55 -0300

    hsi-927 [BE] Agrega endpoint para obtener las prioridades

--- back-end/reference-counter-ref/src/main/java/ar/lamansys/refcounterref/domain/enums/EReferencePriority.java
+++ back-end/reference-counter-ref/src/main/java/ar/lamansys/refcounterref/domain/enums/EReferencePriority.java
@@ -1,8 +1,18 @@
 package ar.lamansys.refcounterref.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Getter;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum EReferencePriority {
 
 	HIGH(1, "Alta prioridad"),
@@ -17,6 +27,13 @@ public enum EReferencePriority {
 	EReferencePriority(Number id, String description) {
 		this.id = id.shortValue();
 		this.description = description;
+	}
+
+	@JsonCreator
+	public static List<EReferencePriority> getAll(){
+		List <EReferencePriority> result = Stream.of(EReferencePriority.values()).collect(Collectors.toList());
+		result.sort(Comparator.comparing(EReferencePriority::getId).reversed());
+		return result;
 	}
 
 }
