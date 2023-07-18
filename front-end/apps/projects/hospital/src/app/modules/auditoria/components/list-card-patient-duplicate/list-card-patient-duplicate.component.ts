f4a485c3a1238028a399f019f7cf78a52646dee4
Brenda Flamminio <bflamminio.blitzit.com.ar>
2023-07-17 23:44:09 -0300

    hsi-893 [FE] Agregar filtrado en lista de pacientes duplicados

--- front-end/apps/projects/hospital/src/app/modules/auditoria/components/list-card-patient-duplicate/list-card-patient-duplicate.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/auditoria/components/list-card-patient-duplicate/list-card-patient-duplicate.component.ts
@@ -5,11 +5,13 @@ import { PersonMasterDataService } from '@api-rest/services/person-master-data.s
 import { ContextService } from '@core/services/context.service';
 import { Observable, of } from 'rxjs';
 import {capitalize} from "@core/utils/core.utils";
-
+import { REMOVE_SUBSTRING_DNI } from '@core/constants/validation-constants';
+import { UntypedFormGroup } from '@angular/forms';
 
 const ROUTE_PATIENTS_FUSION = "auditoria/fusionar-pacientes"
 const PAGE_SIZE_OPTIONS = [5, 10, 25];
 const PAGE_MIN_SIZE = 5;
+const PAGE_SIZE_MIN = [5];
 
 @Component({
 	selector: 'app-list-card-patient-duplicate',
@@ -17,7 +19,7 @@ const PAGE_MIN_SIZE = 5;
 	styleUrls: ['./list-card-patient-duplicate.component.scss']
 })
 export class ListCardPatientDuplicateComponent implements OnInit {
-	readonly pageSizeOptions: number[] = PAGE_SIZE_OPTIONS;
+	pageSizeOptions: number[] = PAGE_SIZE_OPTIONS;
 	listPatientDuplicate: DuplicatePatientDto[]
 	pageSliceObs$: Observable<DuplicatePatientDto[]>;
 	numberOfPatients = 0;
@@ -25,6 +27,10 @@ export class ListCardPatientDuplicateComponent implements OnInit {
 	identificationTypeList: IdentificationTypeDto[];
 	initialSize:Observable<any>;
 	routePrefix:string;
+	formFilter: UntypedFormGroup;
+	panelOpenState = false;
+	private applySearchFilter = '';
+	private sizePageSelect = PAGE_MIN_SIZE;
 	@Input() set setPatientDuplicate(listPatientDuplicate: DuplicatePatientDto[]) {
 		this.listPatientDuplicate = listPatientDuplicate;
 		this.pageSlice = this.listPatientDuplicate?.slice(0, PAGE_MIN_SIZE);
@@ -67,6 +73,36 @@ export class ListCardPatientDuplicateComponent implements OnInit {
 
 		const capitalizedNames = names.map(name => capitalize(name));
 		return capitalizedNames.join(' ');
+	}
+
+	applyFilter($event: any): void {
+		this.applySearchFilter = ($event.target as HTMLInputElement).value?.replace(REMOVE_SUBSTRING_DNI, '');
+		this.upDateFilters();
+	}
+
+	upDateFilters(): void {
+		this.applyFiltes();
+		this.pageSlice = this.pageSlice.slice(0, this.sizePageSelect);
+	}
+
+	private applyFiltes(): void {
+		this.pageSlice = this.filter();
+		this.setPageSizeOptions();
+	}
+
+	private setPageSizeOptions(): void {
+		this.pageSizeOptions = (this.pageSlice.length < PAGE_MIN_SIZE) ? PAGE_SIZE_MIN : PAGE_SIZE_OPTIONS;
+		this.numberOfPatients = this.pageSlice.length;
+		this.pageSlice.length = this.numberOfPatients;
+	}
+
+	private filter(): DuplicatePatientDto[] {
+		let listFilter = this.listPatientDuplicate;
+		if (this.applySearchFilter) {
+			listFilter = listFilter.filter((e: DuplicatePatientDto) => this.getFullName(e).toLowerCase().includes(this.applySearchFilter.toLowerCase())
+			|| e?.identificationNumber.toString().includes(this.applySearchFilter))
+		}
+		return listFilter;
 	}
 
 }
