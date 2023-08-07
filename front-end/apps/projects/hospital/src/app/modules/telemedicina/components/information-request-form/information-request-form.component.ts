cdaa55a63d5c6f7d8354cef54197849a77a1d0c1
Brenda Flamminio <bflamminio.blitzit.com.ar>
2023-08-07 11:20:13 -0300

    hsi-1311 [FE] Agrega input de lineas de cuidado al formulario de información de la solicitud

--- front-end/apps/projects/hospital/src/app/modules/telemedicina/components/information-request-form/information-request-form.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/telemedicina/components/information-request-form/information-request-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { CareLineDto } from '@api-rest/api-model';
+import { CareLineService } from '@api-rest/services/care-line.service';
+import { ContextService } from '@core/services/context.service';
+import { Patient } from '@pacientes/component/search-patient/search-patient.component';
+import { TypeaheadOption } from '@presentation/components/typeahead/typeahead.component';
 
 @Component({
 	selector: 'app-information-request-form',
@@ -6,10 +12,39 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./information-request-form.component.scss']
 })
 export class InformationRequestFormComponent implements OnInit {
-
-	constructor() { }
+	patient: Patient;
+	informationForm: UntypedFormGroup;
+	careLines: CareLineDto[];
+	careLinesTypeahead: TypeaheadOption<CareLineDto>[] = [];
+	showCareLineError = false;
+	constructor(private carelineService: CareLineService, private contextService: ContextService,
+		private readonly formBuilder: UntypedFormBuilder) { }
 
 	ngOnInit(): void {
+		this.initForm();
+		this.carelineService.getCareLinesAttachedToInstitution(this.contextService.institutionId).subscribe(data => {
+			this.careLines = data;
+			this.careLinesTypeahead = this.careLines.map(c => this.toCareLinesDtoTypeahead(c));
+		})
+	}
+
+	private initForm(): void {
+		this.informationForm = this.formBuilder.group({
+			careLine: [null, Validators.required],
+		});
+	}
+
+	setCareLine(careLine: CareLineDto) {
+		this.informationForm.controls.careLine.setValue(careLine);
+		this.showCareLineError = false;
+	}
+
+	private toCareLinesDtoTypeahead(careLine: CareLineDto): TypeaheadOption<CareLineDto> {
+		return {
+			compareValue: careLine.description,
+			value: careLine,
+			viewValue: careLine.description
+		};
 	}
 
 }
