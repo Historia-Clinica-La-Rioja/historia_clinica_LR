1320543874893d05d59b8772b5fef0cbe4efb8c1
JLardapide <joacolardapide@gmail.com>
2023-08-16 16:53:45 -0300

    hsi-1494 Fix errores por consola al conectar con ws

--- front-end/apps/projects/hospital/src/app/stomp.config.ts
+++ front-end/apps/projects/hospital/src/app/stomp.config.ts
@@ -1,9 +1,8 @@
+import { environment } from "@environments/environment";
 import { RxStompConfig } from "@stomp/rx-stomp";
 
-export const stompConfiguration: RxStompConfig = {
-	brokerURL: 'ws://localhost:8080/api/ws',
+const deployedLocally = window.location.hostname.startsWith('localhost') || window.location.hostname.startsWith('127.0.0.1');
 
-	debug: (msg: string): void => {
-		console.log(new Date(), msg);
-	},
+export const stompConfiguration: RxStompConfig = {
+	brokerURL: deployedLocally || !environment.production ? `ws://localhost:8080${environment.apiBase}/ws` : `wss://${window.location.hostname}${environment.apiBase}/ws`,
 }
