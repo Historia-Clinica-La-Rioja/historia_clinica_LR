e93f041be01fc69cfcef9b01bffb568ce9fdecaa
joselinavaldez <joselinavaldez99@gmail.com>
2023-09-05 12:27:41 -0300

    HSI-1824 [FE] Agrega componentes a storybook

---
+++ front-end/apps/projects/hospital/src/app/modules/presentation/components/typeahead/typeahead.stories.ts
@@ -0,0 +1,52 @@
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TypeaheadComponent } from './typeahead.component';
+
+const meta: Meta<TypeaheadComponent> = {
+    title: 'Example/TypeaheadComponent',
+    component: TypeaheadComponent,
+    tags: ['autodocs'],
+    decorators: [
+        moduleMetadata({
+            imports: [ReactiveFormsModule],
+        }),
+    ],
+};
+
+export default meta;
+
+type Story = StoryObj<TypeaheadComponent>;
+
+export const full: Story = {
+    args: {
+        options: [
+            {
+                value: "ejemplos",
+                compareValue: "ejemplos",
+            },
+            {
+                value: "para",
+                compareValue: "para",
+            },
+            {
+                value: "mostrar",
+                compareValue: "mostrar",
+            },
+            {
+                value: "el",
+                compareValue: "el",
+            },
+            {
+                value: "typeahead",
+                compareValue: "typeahead",
+            }
+        ],
+        placeholder: "Ejemplo de placeholder...",
+        externalSetValue: {
+            value: "ejemplos",
+            compareValue: "ejemplos",
+        },
+        titleInput: "Titulo de typeahead"
+    }
+};
+
