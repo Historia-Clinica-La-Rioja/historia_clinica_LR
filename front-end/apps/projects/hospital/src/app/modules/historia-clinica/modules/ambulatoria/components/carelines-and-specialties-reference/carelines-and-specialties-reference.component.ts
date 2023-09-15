60d13e37c7012485f2b2b849590271e9ae0da82f
FacundoBarreiros <facundobarreiros@gmail.com>
2023-09-15 09:03:51 -0300

    hsi-1862 [FE] Agregar campos de Practicas y procedimientos en referencia

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/carelines-and-specialties-reference/carelines-and-specialties-reference.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/carelines-and-specialties-reference/carelines-and-specialties-reference.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { UntypedFormGroup, Validators } from '@angular/forms';
-import { AddressDto, CareLineDto, ClinicalSpecialtyDto, ReferenceProblemDto } from '@api-rest/api-model';
+import { AddressDto, CareLineDto, ClinicalSpecialtyDto, PracticeDto, ReferenceProblemDto } from '@api-rest/api-model';
 import { CareLineService } from '@api-rest/services/care-line.service';
 import { ClinicalSpecialtyService } from '@api-rest/services/clinical-specialty.service';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { ReferenceProblemsService } from '../../services/reference-problems.service';
+import { RequestMasterDataService } from '@api-rest/services/request-masterdata.service';
+import { PracticesService } from '@api-rest/services/practices.service';
+import { CareLineInstitutionPracticeService } from '@api-rest/services/care-line-institution-practice.service';
+import { TypeaheadOption } from '@presentation/components/typeahead/typeahead.component';
 
 @Component({
 	selector: 'app-carelines-and-specialties-reference',
@@ -13,6 +17,18 @@ import { ReferenceProblemsService } from '../../services/reference-problems.serv
 })
 
 export class CarelinesAndSpecialtiesReferenceComponent implements OnInit {
+	studyCategories$: Observable<any[]>;
+	practices$: Observable<any[]> = null;
+	practices = [];
+	originalPractices = [];
+	submit = false;
+	@Input() set submitForm(submit: boolean) {
+		this.submit = submit;
+		if (submit && !this.formReference.value.consultation) {
+			this.formReference.controls.studyCategory.markAllAsTouched();
+			this.formReference.controls.practiceOrProcedure.markAllAsTouched();
+		}
+	};
 
 	@Input() formReference: UntypedFormGroup;
 	@Input() set setProvinceId(provinceId: number) {
@@ -38,9 +54,36 @@ export class CarelinesAndSpecialtiesReferenceComponent implements OnInit {
 		private readonly careLineService: CareLineService,
 		private readonly clinicalSpecialty: ClinicalSpecialtyService,
 		private readonly referenceProblemsService: ReferenceProblemsService,
+		private readonly requestMasterDataService: RequestMasterDataService,
+		private readonly practicesService: PracticesService,
+		private readonly careLineInstitutionPracticeService: CareLineInstitutionPracticeService,
+
 	) { }
 
 	ngOnInit(): void {
+		this.formReference.controls.searchByCareLine.valueChanges.subscribe((changes) => {
+			if (!changes) {
+				this.formReference.controls.practiceOrProcedure.setValue(null);
+				this.formReference.controls.practiceOrProcedure.reset();
+				this.practicesService.getPracticesFromInstitutions().subscribe((practices) => {
+					this.originalPractices = practices;
+					this.practices = this.toTypeaheadOptions(practices, 'pt');
+				});
+			}
+		});
+
+		this.formReference.controls.careLine.valueChanges.subscribe((changes) => {
+			if (changes) {
+				this.formReference.controls.practiceOrProcedure.setValue(null);
+				this.formReference.controls.practiceOrProcedure.reset();
+				this.careLineInstitutionPracticeService.getPracticesByCareLine(changes.id).subscribe((practices) => {
+					this.originalPractices = practices;
+					this.practices = this.toTypeaheadOptions(practices, 'pt');
+				});
+			}
+		})
+
+		this.studyCategories$ = this.requestMasterDataService.categories();
 		this.subscribesToChangesInForm();
 	}
 
@@ -54,7 +97,7 @@ export class CarelinesAndSpecialtiesReferenceComponent implements OnInit {
 			this.specialtiesSubject$.next(this.formReference.value.careLine.clinicalSpecialties);
 		}
 	}
-	setSpecialty(){
+	setSpecialty() {
 		this.formReference.controls.clinicalSpecialtyId.setValue(null);
 		this.formReference.controls.clinicalSpecialtyId.reset();
 	}
@@ -113,6 +156,16 @@ export class CarelinesAndSpecialtiesReferenceComponent implements OnInit {
 		this.formReference.controls.careLine.updateValueAndValidity();
 	}
 
+	private toTypeaheadOptions(response: any[], attribute: string): TypeaheadOption<any>[] {
+		return response.map(r => {
+			return {
+				value: r.id,
+				compareValue: r[attribute],
+				viewValue: r[attribute]
+			}
+		})
+	}
+
 	clearFormFields() {
 
 		this.specialtiesSubject$.next(null);
@@ -140,6 +193,32 @@ export class CarelinesAndSpecialtiesReferenceComponent implements OnInit {
 
 		}
 
+	}
+
+	selectedOption($event: any) {
+		this.formReference.controls.consultation.setValue(!!!$event);
+		if ($event) {
+			this.formReference.controls.clinicalSpecialtyId.removeValidators([Validators.required]);
+			this.formReference.controls.clinicalSpecialtyId.setValue(null);
+			this.formReference.controls.practiceOrProcedure.setValidators([Validators.required]);
+			this.formReference.controls.studyCategory.setValidators([Validators.required]);
+		}
+		else {
+			this.formReference.controls.practiceOrProcedure.removeValidators([Validators.required]);
+			this.formReference.controls.practiceOrProcedure.setValue(null);
+			this.formReference.controls.studyCategory.removeValidators([Validators.required]);
+			this.formReference.controls.studyCategory.setValue(null);
+			this.formReference.controls.clinicalSpecialtyId.setValidators([Validators.required]);
+		}
+
+		this.formReference.controls.practiceOrProcedure.updateValueAndValidity();
+		this.formReference.controls.clinicalSpecialtyId.updateValueAndValidity();
+		this.formReference.controls.studyCategory.updateValueAndValidity();
+	}
+
+	onPracticeSelectionChange($event: any) {
+		const practice = this.originalPractices.find((p: PracticeDto) => p.id === $event);
+		this.formReference.controls.practiceOrProcedure.setValue(practice);
 	}
 
 	private setAllSpecialties() {
