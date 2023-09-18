2fb67b43a76caf368afa40663fdf00947358972a
mmalyvarni <mateomv.mmv@gmail.com>
2023-09-18 14:08:44 -0300

    hsi-2156 No se envia mail de confirmacion de turno

--- back-end/hospital-api/src/main/java/net/pladema/medicalconsultation/appointment/service/impl/SendVirtualAppointmentEmailServiceImpl.java
+++ back-end/hospital-api/src/main/java/net/pladema/medicalconsultation/appointment/service/impl/SendVirtualAppointmentEmailServiceImpl.java
@@ -1,9 +1,6 @@
 package net.pladema.medicalconsultation.appointment.service.impl;
 
 import ar.lamansys.sgh.shared.HospitalSharedAutoConfiguration;
-import ar.lamansys.sgx.shared.emails.application.EmailNotificationChannel;
-import ar.lamansys.sgx.shared.emails.domain.MailMessageBo;
-import ar.lamansys.sgx.shared.notifications.domain.RecipientBo;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,6 +9,12 @@ import net.pladema.medicalconsultation.appointment.domain.enums.EAppointmentModa
 import net.pladema.medicalconsultation.appointment.service.SendVirtualAppointmentEmailService;
 import net.pladema.medicalconsultation.appointment.service.domain.AppointmentBo;
 
+import net.pladema.medicalconsultation.virtualConsultation.infrastructure.output.notification.VirtualConsultationAppointmentTemplateArgs;
+import net.pladema.medicalconsultation.virtualConsultation.infrastructure.output.notification.VirtualConsultationAppointmentTemplateInput;
+import net.pladema.patient.infrastructure.output.notification.PatientNotificationSender;
+import net.pladema.patient.infrastructure.output.notification.PatientRecipient;
+import net.pladema.person.infraestructure.output.notification.PersonNotificationSender;
+import net.pladema.person.infraestructure.output.notification.PersonRecipient;
 import net.pladema.person.repository.domain.CompletePersonNameBo;
 import net.pladema.person.repository.entity.Person;
 import net.pladema.person.service.PersonService;
@@ -20,65 +23,42 @@ import net.pladema.staff.service.ClinicalSpecialtyService;
 
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @AllArgsConstructor
 @Slf4j
 @Service
 public class SendVirtualAppointmentEmailServiceImpl implements SendVirtualAppointmentEmailService {
 
-	private static final String SUBJECT = "Turno de teleconsulta asignado exitosamente";
-
-	public final TemplateEngine templateEngine;
-
-	private final EmailNotificationChannel emailSender;
+	private final PatientNotificationSender patientNotificationSender;
 
 	private final ClinicalSpecialtyService clinicalSpecialtyService;
 
 	private final PersonService personService;
 
+	private final PersonNotificationSender personNotificationSender;
+
 	@Override
 	public void run(AppointmentBo appointment) {
 		log.debug("Input parameters -> appointment {}", appointment);
-		Assert.notNull(HospitalSharedAutoConfiguration.JITSI_DOMAIN_URL, "No se encuentra configurada ninguna instancia de JITSI");
-		String template = "/mails/digital-consultation-email";
-		Person patient = personService.findByPatientId(appointment.getPatientId()).orElseThrow(() -> new IllegalStateException("No se encontró el paciente solicitado"));
+		Assert.isTrue(HospitalSharedAutoConfiguration.JITSI_DOMAIN_URL != null && !HospitalSharedAutoConfiguration.JITSI_DOMAIN_URL.isEmpty(), "No se encuentra configurada ninguna instancia de JITSI");
 		CompletePersonNameBo healthcareProfessional = personService.findByHealthcareProfessionalPersonDataByDiaryId(appointment.getDiaryId()).orElseThrow(() -> new IllegalStateException("No se encontró el profesional solicitado"));
-
-		RecipientBo recipient;
+		VirtualConsultationAppointmentTemplateArgs args = generateArgs(appointment, healthcareProfessional.getPersonFullName());
 		if (appointment.getModalityId().equals(EAppointmentModality.PATIENT_VIRTUAL_ATTENTION.getId()))
-			recipient = new RecipientBo(patient.getFirstName(), patient.getLastName(), appointment.getPatientEmail(), appointment.getPhoneNumber());
+			patientNotificationSender.send(new PatientRecipient(appointment.getPatientId(), appointment.getPatientEmail()), new VirtualConsultationAppointmentTemplateInput(args));
 		else
-			recipient = new RecipientBo(healthcareProfessional.getPerson().getFirstName(), healthcareProfessional.getPerson().getLastName(), appointment.getApplicantHealthcareProfessionalEmail(), null);
-		Map<String, Object> emailData = loadEmailData(appointment, patient, healthcareProfessional.getPersonFullName());
-		sendTemplatedEmail(recipient, template, emailData);
+			personNotificationSender.send(new PersonRecipient(healthcareProfessional.getPerson().getId(), appointment.getApplicantHealthcareProfessionalEmail()), new VirtualConsultationAppointmentTemplateInput(args));
 	}
 
-	private Map<String, Object> loadEmailData(AppointmentBo appointment, Person patient, String healthcareProfessionalName) {
-		Map<String, Object> model = new HashMap<>();
+	private VirtualConsultationAppointmentTemplateArgs generateArgs(AppointmentBo appointment, String healthcareProfessionalName) {
+		Person patient = personService.findByPatientId(appointment.getPatientId()).orElseThrow(() -> new IllegalStateException("No se encontró el paciente solicitado"));
 		String clinicalSpecialtyDescription = clinicalSpecialtyService.getClinicalSpecialtyNameByDiaryId(appointment.getDiaryId());
-
-		model.put("patientName", patient.getLastName() + " " + patient.getFirstName());
-		model.put("date", appointment.getDate().toString() + " " + appointment.getHour().toString() + ".hs");
-		model.put("professionalName", healthcareProfessionalName);
-		model.put("clinicalSpecialty", clinicalSpecialtyDescription);
-		model.put("callLink", HospitalSharedAutoConfiguration.JITSI_DOMAIN_URL + "/" + appointment.getCallId());
-		return model;
-	}
-
-	private void sendTemplatedEmail(RecipientBo recipient, String template, Map<String,Object> variables) {
-		try {
-			Context context = new Context();
-			context.setVariables(variables);
-			String html = templateEngine.process(template, context);
-			emailSender.send(recipient, new MailMessageBo(SUBJECT, html));
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-		}
+		VirtualConsultationAppointmentTemplateArgs args = new VirtualConsultationAppointmentTemplateArgs();
+		args.setPatientName(patient.getLastName() + " " + patient.getFirstName());
+		args.setDate(appointment.getDate().toString() + " " + appointment.getHour().toString() + ".hs");
+		args.setProfessionalName(healthcareProfessionalName);
+		args.setClinicalSpecialty(clinicalSpecialtyDescription);
+		args.setCallLink(HospitalSharedAutoConfiguration.JITSI_DOMAIN_URL + "/" + appointment.getCallId());
+		return args;
 	}
 
 }
---
+++ back-end/hospital-api/src/main/java/net/pladema/medicalconsultation/virtualConsultation/infrastructure/output/notification/VirtualConsultationAppointmentTemplateInput.java
@@ -0,0 +1,14 @@
+package net.pladema.medicalconsultation.virtualConsultation.infrastructure.output.notification;
+
+import ar.lamansys.sgx.shared.featureflags.AppFeature;
+import ar.lamansys.sgx.shared.templating.NotificationTemplateInput;
+
+public class VirtualConsultationAppointmentTemplateInput extends NotificationTemplateInput<VirtualConsultationAppointmentTemplateArgs> {
+
+	public final static String TEMPLATE_ID = "digital-consultation-email";
+
+	public VirtualConsultationAppointmentTemplateInput(VirtualConsultationAppointmentTemplateArgs args) {
+		super(TEMPLATE_ID, args, AppFeature.HABILITAR_TELEMEDICINA);
+	}
+
+}
