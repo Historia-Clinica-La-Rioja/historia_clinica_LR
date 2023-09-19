853c0a5bef22c9e71c10713a5d3b622d0d7c87be
jirimoldi <juanirimoldi@gmail.com>
2023-09-19 14:07:48 -0300

    Formateo de codigo

--- front-end/apps/projects/hospital/src/app/modules/api-rest/services/internment-episode-document.service.ts
+++ front-end/apps/projects/hospital/src/app/modules/api-rest/services/internment-episode-document.service.ts
@@ -7,43 +7,43 @@ import { ViewPdfService } from '@presentation/dialogs/view-pdf/view-pdf.service'
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class InternmentEpisodeDocumentService {
 
-  url: string = `${environment.apiBase}/institutions/${this.contextService.institutionId}/internments`;
+	url: string = `${environment.apiBase}/institutions/${this.contextService.institutionId}/internments`;
 
-  constructor(
-    private http: HttpClient,
-    private contextService: ContextService,
-    private viewPdfService: ViewPdfService,
-  ) { }
+	constructor(
+		private http: HttpClient,
+		private contextService: ContextService,
+		private viewPdfService: ViewPdfService,
+	) { }
 
-  saveInternmentEpisodeDocument(file, internmentEpisodeId: number, episodeDocumentTypeId: number): Observable<number> {
-    const url = `${this.url}/${internmentEpisodeId}/episodedocuments/${episodeDocumentTypeId}`;
-    return this.http.post<number>(url, file);
-  }
+	saveInternmentEpisodeDocument(file, internmentEpisodeId: number, episodeDocumentTypeId: number): Observable<number> {
+		const url = `${this.url}/${internmentEpisodeId}/episodedocuments/${episodeDocumentTypeId}`;
+		return this.http.post<number>(url, file);
+	}
 
-  getInternmentEpisodeDocuments(internmentEpisodeId: number): Observable<EpisodeDocumentResponseDto[]> {
-    const url = `${this.url}/${internmentEpisodeId}/episodedocuments`;
-    return this.http.get<EpisodeDocumentResponseDto[]>(url);
-  }
+	getInternmentEpisodeDocuments(internmentEpisodeId: number): Observable<EpisodeDocumentResponseDto[]> {
+		const url = `${this.url}/${internmentEpisodeId}/episodedocuments`;
+		return this.http.get<EpisodeDocumentResponseDto[]>(url);
+	}
 
-  getDocumentTypes(): Observable<DocumentTypeDto[]> {
-    const url = `${this.url}/documentstypes`;
-    return this.http.get<DocumentTypeDto[]>(url);
-  }
+	getDocumentTypes(): Observable<DocumentTypeDto[]> {
+		const url = `${this.url}/documentstypes`;
+		return this.http.get<DocumentTypeDto[]>(url);
+	}
 
-  deleteDocument(episodeDocumentId: number): Observable<boolean> {
-    const url = `${this.url}/episodedocuments/${episodeDocumentId}`;
-    return this.http.delete<boolean>(url);
-  }
+	deleteDocument(episodeDocumentId: number): Observable<boolean> {
+		const url = `${this.url}/episodedocuments/${episodeDocumentId}`;
+		return this.http.delete<boolean>(url);
+	}
 
-  download(episodeDocumentId: number, fileName: string) {
-    const url = `${this.url}/episodedocuments/download/${episodeDocumentId}`;
-    this.viewPdfService.showDialog(
-      url,
-      fileName,
-    );
-  }
+	download(episodeDocumentId: number, fileName: string) {
+		const url = `${this.url}/episodedocuments/download/${episodeDocumentId}`;
+		this.viewPdfService.showDialog(
+			url,
+			fileName,
+		);
+	}
 }
--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/modules/internacion/dialogs/attach-document-popup/attach-document-popup.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/modules/internacion/dialogs/attach-document-popup/attach-document-popup.component.ts
@@ -12,100 +12,100 @@ const ALLOWED_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.pdf'];
 const INFORMED_CONSENT = "Consentimiento informado de ingreso";
 
 @Component({
-  selector: 'app-attach-document-popup',
-  templateUrl: './attach-document-popup.component.html',
-  styleUrls: ['./attach-document-popup.component.scss']
+	selector: 'app-attach-document-popup',
+	templateUrl: './attach-document-popup.component.html',
+	styleUrls: ['./attach-document-popup.component.scss']
 })
 
 export class AttachDocumentPopupComponent implements OnInit {
 
-  hasError = hasError;
-  form: UntypedFormGroup;
-  documentTypes: TypeaheadOption<any>[];
-  required: boolean = true;
-  file: File = null;
-  showGenerateDocument = false;
+	hasError = hasError;
+	form: UntypedFormGroup;
+	documentTypes: TypeaheadOption<any>[];
+	required: boolean = true;
+	file: File = null;
+	showGenerateDocument = false;
 
-  constructor(private fb: UntypedFormBuilder,
-              private internmentEpisodeDocument: InternmentEpisodeDocumentService,
-              public dialogRef: MatDialogRef<AttachDocumentPopupComponent>,
-			  private readonly snackBarService: SnackBarService,
-              @Inject(MAT_DIALOG_DATA) public data) { }
+	constructor(private fb: UntypedFormBuilder,
+		private internmentEpisodeDocument: InternmentEpisodeDocumentService,
+		public dialogRef: MatDialogRef<AttachDocumentPopupComponent>,
+		private readonly snackBarService: SnackBarService,
+		@Inject(MAT_DIALOG_DATA) public data) { }
 
-  ngOnInit(): void {
-    this.form = this.fb.group({
-      fileName: new UntypedFormControl({value: null, disabled: true}),
-      file: new UntypedFormControl(null, requiredFileType(ExtesionFile.PDF) && Validators.required),
-      type: new UntypedFormControl(null, Validators.required)
-    });
-    this.setDocumentTypesFilter();
-  }
-
-  setDocumentTypesFilter() {
-    this.internmentEpisodeDocument.getDocumentTypes().subscribe(response => {
-      if (response.length > 0) {
-        const options: TypeaheadOption<any>[] = this.setFilterValues(response);
-        this.documentTypes = options;
-      }
-    })
-  }
-
-  setFilterValues(response) {
-    const opt: TypeaheadOption<any>[] = [];
-    response.map(value => {
-      opt.push({
-        value: value.id,
-        compareValue: value.description,
-        viewValue: value.description,
-      });
-    })
-    return opt;
-  }
-
-  save() {
-    if ( ! this.form.valid) return;
-
-    const formDataFile: FormData = new FormData();
-    formDataFile.append('file', this.file);
-    this.internmentEpisodeDocument.saveInternmentEpisodeDocument(formDataFile, this.data.internmentEpisodeId, this.form.get('type').value)
-      .subscribe(resp => {
-        if (resp)
-          this.dialogRef.close()
-      });
-  }
-
-  setDocumentType(type: DocumentTypeDto) {
-	this.showGenerateDocument = !!(this.documentTypes.find( elem => elem.value === type)?.viewValue === INFORMED_CONSENT);
-	this.form.get('type').setValue(type);
-  }
-
-  onFileSelected(event) {
-	const file: File = event.target.files[0];
-
-	if (file) {
-	  const fileExtension = file.name.substring(file.name.lastIndexOf('.')).toLowerCase();
-
-	  if (ALLOWED_EXTENSIONS.includes(fileExtension)) {
-		this.file = file;
-		this.form.get('file').setValue(file);
-	  } else {
-			this.snackBarService.showError('Solo se permiten archivos de imagen y PDF.');
-			event.target.value = null;
-	  }
+	ngOnInit(): void {
+		this.form = this.fb.group({
+			fileName: new UntypedFormControl({ value: null, disabled: true }),
+			file: new UntypedFormControl(null, requiredFileType(ExtesionFile.PDF) && Validators.required),
+			type: new UntypedFormControl(null, Validators.required)
+		});
+		this.setDocumentTypesFilter();
 	}
-  }
+
+	setDocumentTypesFilter() {
+		this.internmentEpisodeDocument.getDocumentTypes().subscribe(response => {
+			if (response.length > 0) {
+				const options: TypeaheadOption<any>[] = this.setFilterValues(response);
+				this.documentTypes = options;
+			}
+		})
+	}
+
+	setFilterValues(response) {
+		const opt: TypeaheadOption<any>[] = [];
+		response.map(value => {
+			opt.push({
+				value: value.id,
+				compareValue: value.description,
+				viewValue: value.description,
+			});
+		})
+		return opt;
+	}
+
+	save() {
+		if (!this.form.valid) return;
+
+		const formDataFile: FormData = new FormData();
+		formDataFile.append('file', this.file);
+		this.internmentEpisodeDocument.saveInternmentEpisodeDocument(formDataFile, this.data.internmentEpisodeId, this.form.get('type').value)
+			.subscribe(resp => {
+				if (resp)
+					this.dialogRef.close()
+			});
+	}
+
+	setDocumentType(type: DocumentTypeDto) {
+		this.showGenerateDocument = !!(this.documentTypes.find(elem => elem.value === type)?.viewValue === INFORMED_CONSENT);
+		this.form.get('type').setValue(type);
+	}
+
+	onFileSelected(event) {
+		const file: File = event.target.files[0];
+
+		if (file) {
+			const fileExtension = file.name.substring(file.name.lastIndexOf('.')).toLowerCase();
+
+			if (ALLOWED_EXTENSIONS.includes(fileExtension)) {
+				this.file = file;
+				this.form.get('file').setValue(file);
+			} else {
+				this.snackBarService.showError('Solo se permiten archivos de imagen y PDF.');
+				event.target.value = null;
+			}
+		}
+	}
 
 
-  deleteFile() {
-	this.file = null;
-	this.form.get('file').setValue(null);
-  }
+	deleteFile() {
+		this.file = null;
+		this.form.get('file').setValue(null);
+	}
 
-  openFile(file) {
-	const fileUrl = URL.createObjectURL(file);
-    window.open(fileUrl, '_blank');
-  }
+	openFile(file) {
+		const fileUrl = URL.createObjectURL(file);
+		window.open(fileUrl, '_blank');
+	}
 
-  generateDocument() { }
+	generateDocument() { }
 
 }
