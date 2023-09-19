ee758f3bee626e9ac6af0748c163927a0a403345
mmalyvarni <mateomv.mmv@gmail.com>
2023-09-19 10:14:04 -0300

    hsi-2194 TeleSalud - Espontánea - Profesional de teleconsultas NO visualiza estado de llamada en curso

--- back-end/hospital-api/src/main/java/net/pladema/medicalconsultation/virtualConsultation/application/notifyVirtualConsultationAcceptedCall/NotifyVirtualConsultationAcceptedCallServiceImpl.java
+++ back-end/hospital-api/src/main/java/net/pladema/medicalconsultation/virtualConsultation/application/notifyVirtualConsultationAcceptedCall/NotifyVirtualConsultationAcceptedCallServiceImpl.java
@@ -1,5 +1,6 @@
 package net.pladema.medicalconsultation.virtualConsultation.application.notifyVirtualConsultationAcceptedCall;
 
+import net.pladema.medicalconsultation.virtualConsultation.application.changeVirtualConsultationStatus.ChangeVirtualConsultationStatusService;
 import net.pladema.medicalconsultation.virtualConsultation.domain.VirtualConsultationEventBo;
 import net.pladema.medicalconsultation.virtualConsultation.domain.enums.EVirtualConsultationEvent;
 import net.pladema.medicalconsultation.virtualConsultation.domain.enums.EVirtualConsultationStatus;
@@ -26,6 +27,10 @@ public class NotifyVirtualConsultationAcceptedCallServiceImpl implements NotifyV
 
 	private final ObjectMapper objectMapper;
 
+	private final ChangeVirtualConsultationStatusService changeVirtualConsultationStatusService;
+
+	private final EVirtualConsultationStatus IN_PROGRESS_STATUS = EVirtualConsultationStatus.IN_PROGRESS;
+
 	@Override
 	public void run(Integer virtualConsultationId) {
 		log.debug("Input parameters -> virtualConsultationId {}", virtualConsultationId);
@@ -34,7 +39,8 @@ public class NotifyVirtualConsultationAcceptedCallServiceImpl implements NotifyV
 			VirtualConsultationEventBo userMessage = new VirtualConsultationEventBo(virtualConsultationId, professionalUserId, EVirtualConsultationEvent.CALL_ACCEPTED);
 			virtualConsultationPublisher.publish("NOTIFY", objectMapper.writeValueAsString(userMessage));
 			//Mensaje cambiando el estado de la consulta a en progreso
-			VirtualConsultationStatusDataDto generalMessage = new VirtualConsultationStatusDataDto(virtualConsultationId, EVirtualConsultationStatus.IN_PROGRESS);
+			changeVirtualConsultationStatusService.run(virtualConsultationId, IN_PROGRESS_STATUS);
+			VirtualConsultationStatusDataDto generalMessage = new VirtualConsultationStatusDataDto(virtualConsultationId, IN_PROGRESS_STATUS);
 			virtualConsultationPublisher.publish("CHANGE-VIRTUAL-CONSULTATION-STATE", objectMapper.writeValueAsString(generalMessage));
 		}
 		catch (JsonProcessingException e) {
