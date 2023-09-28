7f5358110913ee0de55c0273d199d1f59191c293
FacundoBarreiros <facundobarreiros@gmail.com>
2023-09-28 20:05:32 -0300

    hsi-2475 [FE] Agregar constantes

--- front-end/apps/projects/hospital/src/app/modules/turnos/utils/reference.utils.ts
+++ front-end/apps/projects/hospital/src/app/modules/turnos/utils/reference.utils.ts
@@ -2,6 +2,7 @@ import { EReferenceClosureType } from "@api-rest/api-model";
 import { PRIORITY } from "@historia-clinica/modules/ambulatoria/constants/reference-masterdata";
 import { Color } from "@presentation/colored-label/colored-label.component";
 import { ColoredIconText } from "@presentation/components/colored-icon-text/colored-icon-text.component";
+import { DescriptionPriority } from "@presentation/components/priority-select/priority-select.component";
 import { Priority } from "@presentation/components/priority/priority.component";
 import { ReferenceState } from "@turnos/components/report-information/report-information.component";
 import { APPOINTMENT_STATES_ID } from "@turnos/constants/appointment";
@@ -26,6 +27,8 @@ export enum REFERENCE_STATES {
 	ABSENT = 'AUSENTE',
 	SERVED = 'ATENDIDO',
 }
+
+
 
 export function getReferenceState(appointmentStateId: number): ReferenceState {
 
@@ -52,3 +55,48 @@ export function getReferenceState(appointmentStateId: number): ReferenceState {
 		color: Color.YELLOW
 	}
 }
+
+export const APPOINTMENT_STATE = [{
+	id: APPOINTMENT_STATES_ID.ASSIGNED,
+	description: REFERENCE_STATES.ASSIGNED
+}, {
+	id: APPOINTMENT_STATES_ID.ABSENT,
+	description: REFERENCE_STATES.ABSENT
+}, {
+	id: APPOINTMENT_STATES_ID.SERVED,
+	description: REFERENCE_STATES.SERVED
+}, {
+	id: -1,
+	description: REFERENCE_STATES.PENDING
+}]
+
+
+export const PRIORITY_OPTIONS = [{
+	id: 1,
+	description: DescriptionPriority.HIGH
+}, {
+	id: 2,
+	description: DescriptionPriority.MEIDUM
+}, {
+	id: 3,
+	description: DescriptionPriority.LOW
+}]
+
+export const CLOSURE_OPTIONS = [
+	{
+		id: -1,
+		description: "Estado de solicitud"
+	}, {
+		id: 1,
+		description: "Continúa en observación"
+	}, {
+		id: 2,
+		description: "Inicia tratamiento en centro de referencia"
+	}, {
+		id: 3,
+		description: "Requiere estudios complementarios"
+	}, {
+		id: 4,
+		description: "Contrarreferencia"
+	}
+];
