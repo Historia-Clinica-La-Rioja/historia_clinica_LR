d7da57ec6b49044f6d6138b1cf1bc6d99a89f601
Maximiliano Romero Piaggio <mromeropiaggio@alumnos.exa.unicen.edu.ar>
2023-10-06 13:14:53 -0300

    hsi-2604 Error al crear un nuevo grupo de prácticas en Backoffice
    (cherry picked from commit f5a305f34b0b5c8fc8b6001ad0e29591655a8827)

--- back-end/hospital-api/src/main/java/net/pladema/establishment/controller/BackofficeSnomedGroupController.java
+++ back-end/hospital-api/src/main/java/net/pladema/establishment/controller/BackofficeSnomedGroupController.java
@@ -1,13 +1,5 @@
 package net.pladema.establishment.controller;
 
-import javax.validation.Valid;
-
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import ar.lamansys.sgx.shared.dates.configuration.DateTimeProvider;
 import net.pladema.establishment.controller.constraints.validator.permissions.BackofficeSnomedGroupValidator;
 import net.pladema.sgx.backoffice.repository.BackofficeRepository;
@@ -16,6 +8,13 @@ import net.pladema.sgx.backoffice.rest.BackofficeQueryAdapter;
 import net.pladema.sgx.exceptions.BackofficeValidationException;
 import net.pladema.snowstorm.repository.SnomedGroupRepository;
 import net.pladema.snowstorm.repository.entity.SnomedGroup;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("backoffice/snomedgroups")
@@ -31,13 +30,12 @@ public class BackofficeSnomedGroupController extends AbstractBackofficeControlle
 										   BackofficeSnomedGroupValidator backofficeSnomedGroupValidator) {
         super(new BackofficeRepository<>(
 				repository,
-				new BackofficeQueryAdapter<SnomedGroup>() {
+				new BackofficeQueryAdapter<>() {
 					@Override
 					public Example<SnomedGroup> buildExample(SnomedGroup entity) {
 						ExampleMatcher matcher = ExampleMatcher
 								.matching()
-								.withMatcher("description", x -> x.ignoreCase().contains())
-								;
+								.withMatcher("description", ExampleMatcher.GenericPropertyMatchers.exact());
 						return Example.of(entity, matcher);
 					}
 				}
