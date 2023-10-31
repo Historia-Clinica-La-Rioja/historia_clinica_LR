5c33679e4550ee1dbc84911244cea193ad0e84ee
Joselina <jvaldez@lamansys.com.ar>
2023-10-31 11:18:32 -0300

    hsi-2983 [FE] Modifica permisos para la botonera

--- front-end/apps/projects/hospital/src/app/modules/turnos/components/reference-report/reference-report.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/turnos/components/reference-report/reference-report.component.ts
@@ -1,13 +1,9 @@
 import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
-import { ERole, ReferenceReportDto } from '@api-rest/api-model';
-import { PermissionsService } from '@core/services/permissions.service';
 import { DateRange } from '@presentation/components/date-range-picker/date-range-picker.component';
 import { ReferenceReportFacadeService } from '@turnos/services/reference-report-facade.service';
 import differenceInDays from 'date-fns/differenceInDays';
-import { Observable } from 'rxjs';
 
 const MAX_DAYS = 90;
-const ADMINISTRATIVE = [ERole.ADMINISTRATIVO];
 
 @Component({
 	selector: 'app-reference-report',
@@ -18,20 +14,13 @@ export class ReferenceReportComponent implements OnInit, OnDestroy {
 
 	referenceView = ReferenceView;
 	showValidation = false;
-	reports$: Observable<ReferenceReportDto[]>;
 
 	constructor(
-		private readonly permissionService: PermissionsService,
 		private readonly changeDetectorRef: ChangeDetectorRef,
 		readonly referenceReportFacade: ReferenceReportFacadeService,
 	) { }
 
 	ngOnInit() {
-
-		this.permissionService.hasContextAssignments$(ADMINISTRATIVE).subscribe(hasRole => {
-			this.referenceReportFacade.dashboardView = !hasRole ? ReferenceView.REQUESTED : ReferenceView.RECEIVED;
-		});
-
 		this.referenceReportFacade.updateReports();
 	}
 
