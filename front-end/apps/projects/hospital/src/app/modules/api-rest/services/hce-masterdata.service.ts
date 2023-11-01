8a2cb807084a0c6ec4e6c9698bc8fd312ada46c6
Manuel Gil <manuelgilmoltrasio@gmail.com>
2023-11-01 16:25:06 -0300

    hsi-3124 [FE] Obtener desde el BE los datos maestros para los motivos

---
+++ front-end/apps/projects/hospital/src/app/modules/api-rest/services/hce-masterdata.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MasterDataDto } from '@api-rest/api-model';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HceMasterdataService {
+
+    private readonly URL_SUFFIX = `${environment.apiBase}/hce/masterdata/`;
+
+    constructor(
+        private http: HttpClient,
+    ) { }
+
+    getMotives(): Observable<MasterDataDto[]> {
+		const url = this.URL_SUFFIX + `health/error-reasons`;
+		return this.http.get<MasterDataDto[]>(url);
+	}
+}
--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/dialogs/amend-problem/amend-problem.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/dialogs/amend-problem/amend-problem.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
+import {  MatDialogRef } from '@angular/material/dialog';
+import { MasterDataDto } from '@api-rest/api-model';
+import { HceMasterdataService } from '@api-rest/services/hce-masterdata.service';
+import { TypeaheadOption } from '@presentation/components/typeahead/typeahead.component';
 
 @Component({
     selector: 'app-amend-problem',
@@ -10,34 +13,46 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class AmendProblemComponent implements OnInit {
 
     form: UntypedFormGroup;
-    motive: string = '';
+    motives: TypeaheadOption<MasterDataDto>[] = [];
+    motive: MasterDataDto;
     submit = false;
     
     constructor(
         public dialogRef: MatDialogRef<AmendProblemComponent>,
 		private readonly formBuilder: UntypedFormBuilder,
-    ) { }
+        private readonly HCEMasterdataService: HceMasterdataService,
+    ) { 
+    }
 
     ngOnInit(): void {
         this.form = this.formBuilder.group({
 			observations: [null, [Validators.required]],
 		});
+
+        this.HCEMasterdataService.getMotives().subscribe(motives => {
+            motives.forEach(motive => {
+                this.motives.push(this.masterDataDtoToTypeahead(motive))
+            })
+        });
     }
 
-    onMotiveSelectionChange(motive: string) {
+    onMotiveSelectionChange(motive: MasterDataDto) {
         this.motive = motive;
     }
 
     save() {
         this.markAsSaved();
-        if (this.motive.length && this.form.get("observations").value){
-            let error: ErrorMotive = {
-                errorReasonId: 1,
-                errorObservations: 'Problema erroneo'
-            }
-            this.dialogRef.close(error);
+        if (this.motive && this.form.get("observations").value){
+            this.dialogRef.close(this.motive);
         }
     }
+
+    private masterDataDtoToTypeahead(motive: MasterDataDto): TypeaheadOption<MasterDataDto> {
+		return {
+			compareValue: motive.description,
+			value: motive
+		};
+	}
 
     private markAsSaved() {
         this.submit = true;
