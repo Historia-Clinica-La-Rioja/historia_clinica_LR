bfa9f9665b4ed1f0e2544ddaaa17095ffb57d342
RodrigoCba96 <rodrigocordoba2296@gmail.com>
2023-11-08 11:24:38 -0300

    changes in button latest-studies

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/emergente/inicio-estudio/inicio-estudio.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/emergente/inicio-estudio/inicio-estudio.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { EstudiosPopupComponent } from '../pop-up/estudios-popup.component';
 import { MatDialog } from '@angular/material/dialog';
+import { BasicPatientDto } from '@api-rest/api-model';
+import { ContextService } from '@core/services/context.service';
 import { ActivatedRoute } from '@angular/router';
-  
+import { LatestStudiesComponent } from '../pop-up/latest-studies/latest-studies.component';  
 @Component({
   selector: 'app-adulto-mayor',
   templateUrl: './inicio-estudio.component.html',
@@ -10,16 +12,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AdultoMayorComponent implements OnInit {
   patientId: number;
+  private readonly routePrefix;
+  patientData: BasicPatientDto | undefined;
 
   constructor(
+    private dialog: MatDialog,
+    private readonly contextService: ContextService,
     private readonly route: ActivatedRoute,
-    private dialog: MatDialog  
+
   ) { 
-    this.route.paramMap.subscribe(
-      (params) => {
-				this.patientId = Number(params.get('idPaciente'));
-      }
-    )
+    
+      
+    this.routePrefix = `${this.contextService.institutionId}`;
+    this.route.paramMap.subscribe(params => {
+      this.patientId = Number(params.get('idPaciente'));
+      });
   }
 
   ngOnInit(): void {
@@ -36,4 +43,17 @@ export class AdultoMayorComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
      });
   }
+  
+  mostrarPopupVerEstudios(): void {
+    const dialogRef = this.dialog.open(LatestStudiesComponent, {
+      width: '800px',
+      data: {
+        patientId: this.patientId,
+        institutionId: this.routePrefix,
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+    });
+}
+
 }
