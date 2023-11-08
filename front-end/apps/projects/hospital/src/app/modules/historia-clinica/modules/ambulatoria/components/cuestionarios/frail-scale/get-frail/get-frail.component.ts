9de2ef8053e473263b1385f8b6a26d75d3755795
RodrigoCba96 <rodrigocordoba2296@gmail.com>
2023-11-08 11:26:41 -0300

    new component get-frail

---
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/frail-scale/get-frail/get-frail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-frail',
+  templateUrl: './get-frail.component.html',
+  styleUrls: ['./get-frail.component.scss']
+})
+export class GetFrailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
