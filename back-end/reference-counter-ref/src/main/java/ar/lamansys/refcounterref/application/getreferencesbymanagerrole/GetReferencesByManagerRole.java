859331736a9a77704ce2c7a7c310c4a9d6ff6f3d
Bernardita Chacon <bchacon@lamansys.com.ar>
2023-11-09 13:55:24 -0300

    hsi-3252 [BE] Crea controller y endpoint para obtener las referencias del dominio

---
+++ back-end/reference-counter-ref/src/main/java/ar/lamansys/refcounterref/application/getreferencesbymanagerrole/GetReferencesByManagerRole.java
@@ -0,0 +1,22 @@
+package ar.lamansys.refcounterref.application.getreferencesbymanagerrole;
+
+import ar.lamansys.refcounterref.application.port.ReferenceReportStorage;
+import ar.lamansys.refcounterref.domain.report.ReferenceReportBo;
+import ar.lamansys.refcounterref.domain.report.ReferenceReportFilterBo;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class GetReferencesByManagerRole {
+
+	private final ReferenceReportStorage referenceReportStorage;
+
+	public Page<ReferenceReportBo> run(ReferenceReportFilterBo filter, Pageable pageable) {
+		return referenceReportStorage.fetchReferencesReport(filter, pageable);
+	}
+
+}
---
+++ back-end/reference-counter-ref/src/main/java/ar/lamansys/refcounterref/infraestructure/input/rest/InstitutionalNetworkReferenceReportController.java
@@ -0,0 +1,50 @@
+package ar.lamansys.refcounterref.infraestructure.input.rest;
+
+import ar.lamansys.refcounterref.application.getreferencesbymanagerrole.GetReferencesByManagerRole;
+import ar.lamansys.refcounterref.infraestructure.input.ReferenceReportFilterUtils;
+import ar.lamansys.refcounterref.infraestructure.input.rest.dto.ReferenceReportDto;
+import ar.lamansys.refcounterref.infraestructure.input.rest.mapper.GetReferenceMapper;
+import ar.lamansys.sgh.shared.infrastructure.input.service.datastructures.PageDto;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@Validated
+@RequestMapping("/references-report")
+@Tag(name = "Institutional Network Reference Report", description = "Institutional Network Reference Report")
+@RestController
+public class InstitutionalNetworkReferenceReportController {
+
+	private final GetReferencesByManagerRole getReferencesByManagerRole;
+
+	private final GetReferenceMapper getReferenceMapper;
+
+	private final ObjectMapper objectMapper;
+
+	@GetMapping("/manager")
+	@PreAuthorize("hasAnyAuthority('GESTOR_DE_ACCESO_DE_DOMINIO')")
+	public ResponseEntity<PageDto<ReferenceReportDto>> getReferencesByManagerRole(@RequestParam(name = "filter") String filter,
+																				  @RequestParam(name = "pageNumber") Integer pageNumber,
+																				  @RequestParam(name = "pageSize") Integer pageSize) {
+		log.debug("Input parameters -> filter {}, pageNumber {}, pageSize {}", filter, pageNumber, pageSize);
+		var reportFilter = ReferenceReportFilterUtils.parseFilter(filter, objectMapper);
+		var result = getReferencesByManagerRole.run(reportFilter, PageRequest.of(pageNumber, pageSize))
+				.map(getReferenceMapper::toReferenceReportDto);
+		return ResponseEntity.ok(PageDto.fromPage(result));
+	}
+
+}
