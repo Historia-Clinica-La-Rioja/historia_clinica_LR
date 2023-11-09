7ac96dfb07f05158bc188f470017091a4d1ce33a
Manuel Gil <manuelgilmoltrasio@gmail.com>
2023-11-09 12:31:52 -0300

    hsi-3265 [FE] Agregar mappeo para calcular el nombre de la severidad del problema y las fechas

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/amended-problems/amended-problems.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/amended-problems/amended-problems.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, Injector, OnInit, Output } from '@angular/core';
 import { HCEPersonalHistoryDto } from '@api-rest/api-model';
 import { PROBLEMAS_POR_ERROR } from '@historia-clinica/constants/summaries';
 import { Subject } from 'rxjs';
-import { Observable } from 'rxjs/internal/Observable';
+import { AmbulatoriaSummaryFacadeService } from '../../services/ambulatoria-summary-facade.service';
+import { DateFormat, momentFormat, momentParseDate } from '@core/utils/moment.utils';
+import { InternacionMasterDataService } from '@api-rest/services/internacion-master-data.service';
 
 @Component({
     selector: 'app-amended-problems',
@@ -12,15 +14,50 @@ import { Observable } from 'rxjs/internal/Observable';
 export class AmendedProblemsComponent implements OnInit {
     
     @Output() setProblemOnHistoric = new Subject<HCEPersonalHistoryDto>();
-    public amendedProblems$: Observable<HCEPersonalHistoryDto[]>;
+    public amendedProblems: ProblemsData[];
 	public readonly problemasPorError = PROBLEMAS_POR_ERROR;
-    isFilterExpanded = false;
+    public isFilterExpanded = false;
+    public severityTypeMasterData: any[];
 
+    // Injected dependencies
+    private readonly internacionMasterDataService: InternacionMasterDataService;
+    
     constructor(
-    ) { }
+		private ambulatoriaSummaryFacadeService: AmbulatoriaSummaryFacadeService,
+        private injector: Injector,
+    ) { 
+		this.internacionMasterDataService = this.injector.get<InternacionMasterDataService>(InternacionMasterDataService);
+    }
 
     ngOnInit(): void {
+        this.internacionMasterDataService.getHealthSeverity().subscribe(healthConditionSeverities => {
+			this.severityTypeMasterData = healthConditionSeverities;
+            this.setAmendedProblems();
+		});
     }
+
+    setAmendedProblems() {
+		this.ambulatoriaSummaryFacadeService.solvedProblems$.subscribe(amendedProblems => {
+            this.amendedProblems = this.mapToProblemsData(amendedProblems)
+        })
+	}
+
+    getSeverityTypeDisplayByCode(severityCode: string): string {
+        return (severityCode && this.severityTypeMasterData) ?
+            this.severityTypeMasterData.find(severityType => severityType.code === severityCode).display
+            : '';
+    }
+
+    mapToProblemsData(problems: HCEPersonalHistoryDto[]) {
+		return problems.map((problem: HCEPersonalHistoryDto) => {
+			return {
+				data: problem,
+				startDate: problem.startDate ? momentFormat(momentParseDate(problem.startDate), DateFormat.VIEW_DATE) : undefined,
+				endDate: problem.inactivationDate ? momentFormat(momentParseDate(problem.inactivationDate), DateFormat.VIEW_DATE) : undefined,
+                severity: problem.severity ? this.getSeverityTypeDisplayByCode(problem.severity) : '',
+			};
+		});
+	}    
 
     toggleFilter() {
         this.isFilterExpanded = !this.isFilterExpanded;
@@ -29,4 +66,12 @@ export class AmendedProblemsComponent implements OnInit {
     viewProblemDetails(problem: HCEPersonalHistoryDto) {
         this.setProblemOnHistoric.next(problem);
     }
+}
+
+
+export interface ProblemsData {
+    data: HCEPersonalHistoryDto,
+    startDate: string,
+    endDate: string,
+    severity: string,
 }
