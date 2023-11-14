176c73afcc0c3cdddd965020f803d5b55344f7df
Bernardita Chacon <bchacon@lamansys.com.ar>
2023-11-14 11:21:52 -0300

    hsi-3394 [BE] Agrega endpoint para poder visualizar el detalle de la referencia con rol gestor

--- back-end/reference-counter-ref/src/main/java/ar/lamansys/refcounterref/infraestructure/input/rest/InstitutionalNetworkReferenceReportController.java
+++ back-end/reference-counter-ref/src/main/java/ar/lamansys/refcounterref/infraestructure/input/rest/InstitutionalNetworkReferenceReportController.java
@@ -1,8 +1,11 @@
 package ar.lamansys.refcounterref.infraestructure.input.rest;
 
+import ar.lamansys.refcounterref.application.getreferencecompletedata.GetReferenceCompleteData;
 import ar.lamansys.refcounterref.application.getreferencesbymanagerrole.GetReferencesByManagerRole;
+import ar.lamansys.refcounterref.domain.reference.ReferenceCompleteDataBo;
 import ar.lamansys.refcounterref.infraestructure.input.ReferenceReportFilterUtils;
 import ar.lamansys.refcounterref.infraestructure.input.rest.dto.ReferenceReportDto;
+import ar.lamansys.refcounterref.infraestructure.input.rest.dto.reference.ReferenceCompleteDataDto;
 import ar.lamansys.refcounterref.infraestructure.input.rest.mapper.GetReferenceMapper;
 import ar.lamansys.sgh.shared.infrastructure.input.service.datastructures.PageDto;
 
@@ -17,6 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +32,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Institutional Network Reference Report", description = "Institutional Network Reference Report")
 @RestController
 public class InstitutionalNetworkReferenceReportController {
+
+	private final GetReferenceCompleteData getReferenceCompleteData;
 
 	private final GetReferencesByManagerRole getReferencesByManagerRole;
 
@@ -45,6 +51,16 @@ public class InstitutionalNetworkReferenceReportController {
 		var result = getReferencesByManagerRole.run(reportFilter, PageRequest.of(pageNumber, pageSize))
 				.map(getReferenceMapper::toReferenceReportDto);
 		return ResponseEntity.ok(PageDto.fromPage(result));
+	}
+
+	@GetMapping("/reference-detail/{referenceId}")
+	@PreAuthorize("hasAnyAuthority('GESTOR_DE_ACCESO_DE_DOMINIO', 'GESTOR_DE_ACCESO_REGIONAL', 'GESTOR_DE_ACCESO_LOCAL')")
+	public ResponseEntity<ReferenceCompleteDataDto> getReferenceDetail(@PathVariable(name = "referenceId") Integer referenceId) {
+		log.debug("Input parameters -> referenceId {} ", referenceId);
+		ReferenceCompleteDataBo reference = getReferenceCompleteData.run(referenceId);
+		ReferenceCompleteDataDto result = getReferenceMapper.toReferenceCompleteDataDto(reference);
+		log.debug("Output -> result {}", result);
+		return ResponseEntity.ok(result);
 	}
 
 }
