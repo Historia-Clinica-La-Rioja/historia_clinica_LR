569919b11cb8348a22d18e1bf673eeff1a374b5d
Cristian Quevedo <cristian.quevedox@gmail.com>
2023-11-17 09:14:36 -0300

    added pregnant people attentions report

---
+++ back-end/hospital-api/src/main/java/net/pladema/provincialreports/pregnantpeoplereports/service/PregnantPeopleReportExcelService.java
@@ -0,0 +1,12 @@
+package net.pladema.provincialreports.pregnantpeoplereports.service;
+
+import ar.lamansys.sgx.shared.reports.util.struct.IWorkbook;
+import net.pladema.provincialreports.pregnantpeoplereports.repository.PregnantAttentionsConsultationDetail;
+
+import java.util.List;
+
+public interface PregnantPeopleReportExcelService {
+
+	IWorkbook buildExcelPregnantAttentions(String title, String[] headers, List<PregnantAttentionsConsultationDetail> query, String startDate, String endDate, String institutionName);
+
+}
