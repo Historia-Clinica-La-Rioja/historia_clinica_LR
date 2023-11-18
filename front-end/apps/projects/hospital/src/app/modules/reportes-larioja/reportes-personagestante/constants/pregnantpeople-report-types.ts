e6455b4cd6a0a61196ef1411589e04b0575b3a09
Cristian Quevedo <cristian.quevedox@gmail.com>
2023-11-18 19:19:27 -0300

    feat(component): added constants file for pregnant person reports

---
+++ front-end/apps/projects/hospital/src/app/modules/reportes-larioja/reportes-personagestante/constants/pregnantpeople-report-types.ts
@@ -0,0 +1,11 @@
+
+export const PREGNANTPEOPLE_REPORT_TYPES = [
+    {
+        description: 'Atenciones a personas gestantes',
+        id: 1,
+    },
+    {
+        description: 'Control de personas gestantes',
+        id: 2,
+    }
+]
