957575d38231c41e7a5f74b3036cbf0872c18f16
jirimoldi <juanirimoldi@gmail.com>
2023-11-21 11:37:49 -0300

    hsi-2790 Se visualiza botón +Agregar documentos habilitado en card de internación de episodios cerrados

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/modules/internacion/components/interment-document-episode/interment-document-episode.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/modules/internacion/components/interment-document-episode/interment-document-episode.component.ts
@@ -14,6 +14,7 @@ export class IntermentDocumentEpisodeComponent {
 
 	@Input() documents: EpisodeDocumentResponseDto[];
 	@Input() internmentEpisodeId: number;
+	@Input() disabled: boolean;
 	@Output() updateDocuments: EventEmitter<any> = new EventEmitter();
 
 	constructor(
