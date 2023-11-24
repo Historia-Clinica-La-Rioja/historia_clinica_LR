9ea0bc8eae844ae7c8ffff32faed087d63f79019
RodrigoCba96 <rodrigocordoba2296@gmail.com>
2023-11-24 13:09:38 -0300

    pdf download is true

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/frail-scale/get-frail/get-frail.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/frail-scale/get-frail/get-frail.component.ts
@@ -6,34 +6,54 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   selector: 'app-get-frail',
   templateUrl: './get-frail.component.html',
   styleUrls: ['./get-frail.component.scss'],
-  
+
 })
 
 export class GetFrailComponent implements OnInit {
   submitted: boolean;
   patientId: any;
   consultation: number;
- 
+
   constructor(
     private frailService: FrailService,
     @Inject(MAT_DIALOG_DATA) public data: any,
-  ) { 
+  ) {
 
     this.patientId = data.patientId
 
   }
 
   ngOnInit(): void {
-   }
+  }
 
-   generatePdf() {
+  generatePdf() {
     this.submitted = true;
 
-        if (this.patientId) {
+    if (this.patientId) {
       this.frailService.getPdf(this.patientId).subscribe(
-      )
+        (response) => {
+          const blob = new Blob([response], { type: 'application/pdf' });
 
-      console.log(this.generatePdf, "descarga exitosa")
+          this.patientId = this.data.patientId
+          const fileName = `EscaladeFrail_${this.patientId}.pdf`;
+
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = fileName;
+
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+
+          window.URL.revokeObjectURL(url);
+
+          // console.log("Descarga exitosa");
+        },
+        (error) => {
+          //  console.error("Error en la descarga", error);
         }
-      }
-}
+      );
+    }
+  }
+}  
