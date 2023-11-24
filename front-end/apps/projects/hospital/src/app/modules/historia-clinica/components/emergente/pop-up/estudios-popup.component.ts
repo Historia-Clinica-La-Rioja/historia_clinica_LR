801654cf62059043640d207f818d95dc5972d036
Cristian Quevedo <cristian.quevedox@gmail.com>
2023-11-24 12:22:05 -0300

    component 'estudios-popup' code format updated

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/emergente/pop-up/estudios-popup.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/emergente/pop-up/estudios-popup.component.ts
@@ -1,24 +1,21 @@
 import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';  
-
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-pop-up',
-  templateUrl: './estudios-popup.component.html',
-  styleUrls: ['./estudios-popup.component.scss']
+	selector: 'app-pop-up',
+	templateUrl: './estudios-popup.component.html',
+	styleUrls: ['./estudios-popup.component.scss'],
 })
 export class EstudiosPopupComponent {
-  selectedOption: string = '';
-  patientId: number;
+	selectedOption: string = '';
+	patientId: number;
 
-  constructor(
-    private dialogRef: MatDialogRef<EstudiosPopupComponent>
-  ) { }
+	constructor(private dialogRef: MatDialogRef<EstudiosPopupComponent>) {}
 
-  closeModal(simpleClose: boolean, completed?: boolean): void {
-    if (simpleClose) {
-      this.dialogRef.close();  
-    } else {
-     }
-  }
+	closeModal(simpleClose: boolean, completed?: boolean): void {
+		if (simpleClose) {
+			this.dialogRef.close();
+		} else {
+		}
+	}
 }
