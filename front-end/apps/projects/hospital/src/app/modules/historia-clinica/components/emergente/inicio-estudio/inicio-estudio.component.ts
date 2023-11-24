2e31c99e6cdb9327812ab3be47243f28feec425c
Cristian Quevedo <cristian.quevedox@gmail.com>
2023-11-24 13:18:39 -0300

    component 'inicio-estudio' code format updated

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/emergente/inicio-estudio/inicio-estudio.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/emergente/inicio-estudio/inicio-estudio.component.ts
@@ -2,38 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { EstudiosPopupComponent } from '../pop-up/estudios-popup.component';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
-  
+
 @Component({
-  selector: 'app-adulto-mayor',
-  templateUrl: './inicio-estudio.component.html',
-  styleUrls: ['./inicio-estudio.component.scss']
+	selector: 'app-adulto-mayor',
+	templateUrl: './inicio-estudio.component.html',
+	styleUrls: ['./inicio-estudio.component.scss'],
 })
 export class AdultoMayorComponent implements OnInit {
-  patientId: number;
+	patientId: number;
 
-  constructor(
-    private readonly route: ActivatedRoute,
-    private dialog: MatDialog  
-  ) { 
-    this.route.paramMap.subscribe(
-      (params) => {
-				this.patientId = Number(params.get('idPaciente'));
-      }
-    )
-  }
+	constructor(
+		private readonly route: ActivatedRoute,
+		private dialog: MatDialog
+	) {
+		this.route.paramMap.subscribe((params) => {
+			this.patientId = Number(params.get('idPaciente'));
+		});
+	}
 
-  ngOnInit(): void {
-   }
+	ngOnInit(): void {}
 
-  mostrarPopup(): void {
-    const dialogRef = this.dialog.open(EstudiosPopupComponent, {
-      width: '800px',
-      data: {
-        patientId: this.patientId
-      }
-    });
+	mostrarPopup(): void {
+		const dialogRef = this.dialog.open(EstudiosPopupComponent, {
+			width: '800px',
+			data: {
+				patientId: this.patientId,
+			},
+		});
 
-    dialogRef.afterClosed().subscribe(result => {
-     });
-  }
+		dialogRef.afterClosed().subscribe((result) => {});
+	}
 }
