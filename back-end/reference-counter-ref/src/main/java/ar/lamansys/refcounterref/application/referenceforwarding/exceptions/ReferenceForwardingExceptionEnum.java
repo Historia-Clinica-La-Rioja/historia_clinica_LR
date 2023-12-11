e6c99acbad903127f5894cc471ea6995c2bef1f8
Bernardita Chacon <bchacon@lamansys.com.ar>
2023-12-11 16:41:58 -0300

    hsi-4038 [BE] Agrega excepciones al caso de uso editar derivaración

--- back-end/reference-counter-ref/src/main/java/ar/lamansys/refcounterref/application/referenceforwarding/exceptions/ReferenceForwardingExceptionEnum.java
+++ back-end/reference-counter-ref/src/main/java/ar/lamansys/refcounterref/application/referenceforwarding/exceptions/ReferenceForwardingExceptionEnum.java
@@ -3,6 +3,7 @@ package ar.lamansys.refcounterref.application.referenceforwarding.exceptions;
 public enum ReferenceForwardingExceptionEnum {
 
 	NULL_REFRENCE_ID,
+	NULL_FORWARDING_ID,
 	NULL_OBSERVATION,
 	INVALID_REFERENCE_STATE,
 	INVALID_ROLE,
--- back-end/reference-counter-ref/src/main/java/ar/lamansys/refcounterref/application/updatereferenceforwarding/UpdateReferenceForwarding.java
+++ back-end/reference-counter-ref/src/main/java/ar/lamansys/refcounterref/application/updatereferenceforwarding/UpdateReferenceForwarding.java
@@ -1,6 +1,10 @@
 package ar.lamansys.refcounterref.application.updatereferenceforwarding;
 
 import ar.lamansys.refcounterref.application.port.ReferenceForwardingStorage;
+import ar.lamansys.refcounterref.application.referenceforwarding.exceptions.ReferenceForwardingException;
+import ar.lamansys.refcounterref.application.referenceforwarding.exceptions.ReferenceForwardingExceptionEnum;
+import ar.lamansys.refcounterref.domain.reference.ReferenceForwardingBo;
+import ar.lamansys.sgx.shared.security.UserInfo;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,8 +19,20 @@ public class UpdateReferenceForwarding {
 	private final ReferenceForwardingStorage referenceForwardingStorage;
 
 	public void run(Integer forwardingId, String observation) {
+		if (forwardingId == null)
+			throw new ReferenceForwardingException(ReferenceForwardingExceptionEnum.NULL_FORWARDING_ID, "El identificador de la derivación es obligatorio");
+
 		var forwarding = referenceForwardingStorage.getForwarding(forwardingId);
+		assertContextValid(forwarding, observation);
+
 		referenceForwardingStorage.save(forwarding.getReferenceId(), observation, forwarding.getType().getId());
+	}
+
+	private void assertContextValid(ReferenceForwardingBo forwarding, String observation) {
+		if (!forwarding.getUserId().equals(UserInfo.getCurrentAuditor()))
+			throw new ReferenceForwardingException(ReferenceForwardingExceptionEnum.INVALID_USER, "La derivación solo puede ser editada por quien la haya iniciado");
+		if (observation == null || observation.isBlank())
+			throw new ReferenceForwardingException(ReferenceForwardingExceptionEnum.NULL_OBSERVATION, "El texto de observación es obligatorio");
 	}
 
 }
