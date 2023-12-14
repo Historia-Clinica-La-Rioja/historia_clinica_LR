049a298145d8aff719cd80a282284f9d0fad18dc
Ezequiel FV <zikofv@gmail.com>
2023-12-14 09:59:49 -0300

    hsi-4086 Agrega endpoints para backoffice de unit of measure

---
+++ back-end/hospital-api/src/main/java/net/pladema/unitofmeasure/insfrastructure/input/rest/BackofficeUnitOfMeasureController.java
@@ -0,0 +1,85 @@
+package net.pladema.unitofmeasure.insfrastructure.input.rest;
+
+
+import net.pladema.sgx.backoffice.permissions.BackofficePermissionValidator;
+import net.pladema.sgx.backoffice.rest.AbstractBackofficeController;
+
+import net.pladema.sgx.backoffice.rest.ItemsAllowed;
+import net.pladema.sgx.backoffice.validation.BackofficeEntityValidator;
+import net.pladema.sgx.exceptions.BackofficeValidationException;
+import net.pladema.unitofmeasure.insfrastructure.output.entity.UnitOfMeasure;
+
+import net.pladema.unitofmeasure.insfrastructure.output.repository.UnitOfMeasureRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("backoffice/units-of-measure")
+public class BackofficeUnitOfMeasureController extends AbstractBackofficeController<UnitOfMeasure, Short> {
+
+	public BackofficeUnitOfMeasureController(UnitOfMeasureRepository repository) {
+		super(repository, new BackofficePermissionValidator<UnitOfMeasure, Short>() {
+			@Override
+			@PreAuthorize("hasAnyAuthority('ROOT', 'ADMINISTRADOR')")
+			public void assertGetList(UnitOfMeasure entity) {}
+
+			@Override
+			@PreAuthorize("hasAnyAuthority('ROOT', 'ADMINISTRADOR')")
+			public List<Short> filterIdsByPermission(List<Short> ids) {
+				return ids;
+			}
+
+			@Override
+			@PreAuthorize("hasAnyAuthority('ROOT', 'ADMINISTRADOR')")
+			public void assertGetOne(Short id) {}
+
+			@Override
+			@PreAuthorize("hasAnyAuthority('ROOT', 'ADMINISTRADOR')")
+			public void assertCreate(UnitOfMeasure entity) {}
+
+			@Override
+			@PreAuthorize("hasAnyAuthority('ROOT', 'ADMINISTRADOR')")
+			public void assertUpdate(Short id, UnitOfMeasure entity) {}
+
+			@Override
+			@PreAuthorize("hasAnyAuthority('ROOT', 'ADMINISTRADOR')")
+			public void assertDelete(Short id) {}
+
+			@Override
+			@PreAuthorize("hasAnyAuthority('ROOT', 'ADMINISTRADOR')")
+			public ItemsAllowed<Short> itemsAllowedToList(UnitOfMeasure entity) {
+				return new ItemsAllowed<>();
+			}
+
+			@Override
+			@PreAuthorize("hasAnyAuthority('ROOT', 'ADMINISTRADOR')")
+			public ItemsAllowed<Short> itemsAllowedToList() {
+				return new ItemsAllowed<>();
+			}
+		}, new BackofficeEntityValidator<UnitOfMeasure, Short>() {
+			@Override
+			public void assertCreate(UnitOfMeasure entity) {
+
+			}
+
+			@Override
+			public void assertUpdate(Short id, UnitOfMeasure entity) {
+				var onlyAllowedFiledsChanged = repository
+				.findById(id)
+				.map(original -> entity.updateAllowed(original))
+				.orElse(true);
+
+				if (!onlyAllowedFiledsChanged)
+					throw new BackofficeValidationException("unit-of-measure.editable-fields.disallowed");
+			}
+
+			@Override
+			public void assertDelete(Short id) {}
+		});
+	}
+}
---
+++ back-end/hospital-api/src/main/java/net/pladema/unitofmeasure/insfrastructure/output/repository/UnitOfMeasureRepository.java
@@ -0,0 +1,11 @@
+package net.pladema.unitofmeasure.insfrastructure.output.repository;
+
+import ar.lamansys.sgx.shared.auditable.repository.SGXAuditableEntityJPARepository;
+import net.pladema.unitofmeasure.insfrastructure.output.entity.UnitOfMeasure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UnitOfMeasureRepository extends SGXAuditableEntityJPARepository<UnitOfMeasure, Short> {
+}
