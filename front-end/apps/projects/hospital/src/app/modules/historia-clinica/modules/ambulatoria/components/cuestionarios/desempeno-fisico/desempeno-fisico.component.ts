d7c9fb0ce38ff4a4708cdbe421a02dc08e456ac1
RodrigoCba96 <rodrigocordoba2296@gmail.com>
2023-12-18 13:08:19 -0300

    advanced in new function

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/desempeno-fisico/desempeno-fisico.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/desempeno-fisico/desempeno-fisico.component.ts
@@ -8,33 +8,66 @@ import { Component, OnInit } from '@angular/core';
 export class DesempenoFisicoComponent implements OnInit {
 
   selectedoptionA: string = '';
-
+  selectedoptionB: string = '';
+  imageInactive1: boolean;
+  iconActive: boolean;
   constructor() { }
 
   ngOnInit(): void {
-    
+
   }
 
 
-isSubmitDisabled(): boolean {
-  return! (
-this.selectedoptionA === '1A' || this.selectedoptionA === '2A'
-
-  )
-}
+  isSubmitDisabled(): boolean {
+    return !(
+      this.selectedoptionA === '1A'
 
 
+    )
+  }
+
+  isCounterDisabled(): boolean {
+    return !(
+
+      this.selectedoptionA === '1A' || this.selectedoptionA === '2A'
+
+    )
+  }
+  isCounterDisabled2(): boolean {
+    return !(
+
+      this.selectedoptionB === '1B' || this.selectedoptionB === '2B'
+
+    )
+
+  }
+
+  isSubmitDisabled3(): boolean {
+    return !(
+
+      this.selectedoptionB === '1B'
+
+    )
+
+  }
+
+  isCounterDisabled3(): boolean {
+    return !(
+      this.selectedoptionB === '1B'
+    )
+  }
 
 
+//   imgActive(value: string) {
+//     this.iconActive = false;
+//     this.imageInactive1 = false;
 
+//     if (value === '1A') {
+//         this.iconActive = true;
+//     } else if (value === '2A' || value === '3A') {
+//         this.imageInactive1 = true;
+//     }
+// }
 
+   }
 
-
-
-
-
-
-
-
-  
-}
