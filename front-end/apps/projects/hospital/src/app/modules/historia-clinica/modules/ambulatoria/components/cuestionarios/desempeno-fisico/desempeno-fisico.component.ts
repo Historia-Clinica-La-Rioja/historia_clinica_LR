feacdb9239ab4ba6275c6a6085364be0aef1f035
RodrigoCba96 <rodrigocordoba2296@gmail.com>
2024-01-03 13:01:05 -0300

    added function poinst in form

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/desempeno-fisico/desempeno-fisico.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/desempeno-fisico/desempeno-fisico.component.ts
@@ -11,9 +11,17 @@ export class DesempenoFisicoComponent implements OnInit {
   selectedoptionB: string = '';
   selectedoptionC: string = '';
   selectedoptionD: string = '';
-
-  imageInactive1: boolean;
+  selectedoptionE: string = '';
+  selectedoptionB2: string = '';
+  selectedoptionE2: string = '';
+  counterB2: number = 0;
+  counterC2: number = 0;
+  counterC3: number = 0;
+  totalScore: number = 0;
+  calculatePoints: number;
+  ticketNum1: number = 0;
   iconActive: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -82,20 +90,75 @@ export class DesempenoFisicoComponent implements OnInit {
     )
   }
 
+  isSubmitDisabled5(): boolean {
+    return !(
+      this.selectedoptionE === '1D'
+
+    )
+  }
+
+  calculePoints(): number {
+    let balance1 = (this.selectedoptionA === '1A') ? 1 : 0;
+    let balance2 = (this.selectedoptionB === '1B') ? 1 : 0;
+    let sillaTest1 = (this.selectedoptionE === '1D') ? 1 : 0;
+    let sillaTest2 = (this.selectedoptionE2 === '1E') ? 1 : 0;
+
+    let counterB2Points = 0;
+    if (this.counterB2 < 3.0) {
+      counterB2Points = 0;
+    } else if (this.counterB2 >= 3.0 && this.counterB2 < 10) {
+      counterB2Points = 1;
+    } else if (this.counterB2 >= 10 && this.counterB2 <= 15) {
+      counterB2Points = 2;
+    }
+
+
+    let counterC2Points = 0;
+    if (this.counterC2 < 3.0) {
+      counterC2Points = 0;
+
+    } else if (this.counterC2 >= 3.0 && this.counterC2 < 10) {
+      counterC2Points = 1;
+    } else if (this.counterC2 >= 10 && this.counterC2 <= 15) {
+      counterC2Points = 2;
+    }
+
+    let counterC3Points = 0;
+
+    if (this.counterC3 < 4.82) {
+      counterC3Points = 4; 
+
+    } else if (this.counterC3 >= 4.82 && this.counterC3 < 6.2){
+      counterC3Points = 3;
+
+    } else if (this.counterC3 >= 6.2 && this.counterC3 < 8.7) {
+      counterC3Points = 2;
+ 
+    } else if (this.counterC3 >= 8.7) {
+      counterC3Points = 1;
+    }
+
+
+    let totalScore = balance1 + balance2 + 
+    sillaTest1 + sillaTest2 + counterB2Points + counterC2Points 
+    + counterC3Points;
+
+    return totalScore
+
+  }
 
 
 
 
-  //   imgActive(value: string) {
-  //     this.iconActive = false;
-  //     this.imageInactive1 = false;
 
-  //     if (value === '1A') {
-  //         this.iconActive = true;
-  //     } else if (value === '2A' || value === '3A') {
-  //         this.imageInactive1 = true;
-  //     }
-  // }
+
+
+
+
+
+
+
+
 
 }
 
