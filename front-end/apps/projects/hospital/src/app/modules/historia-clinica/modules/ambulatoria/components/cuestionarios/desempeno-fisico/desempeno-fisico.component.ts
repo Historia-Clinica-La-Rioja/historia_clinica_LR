29ca0465b4d9cbfa1eb97fd57154d73d20a241ab
RodrigoCba96 <rodrigocordoba2296@gmail.com>
2024-01-08 13:11:43 -0300

    update typescript new functions

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/desempeno-fisico/desempeno-fisico.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/desempeno-fisico/desempeno-fisico.component.ts
@@ -16,16 +16,24 @@ export class DesempenoFisicoComponent implements OnInit {
   selectedoptionE2: string = '';
   counterB2: number = 0;
   counterC2: number = 0;
-  counterC3: number = 0;
-  counterE1: number = 0;
+  counterC3: number = undefined;
+  counterE1: number = undefined;
   totalScore: number = 0;
+  Calculefinal: any;
+  finalScore: number = 0;
   calculatePoints: number;
   ticketNum1: number = 0;
   iconActive: boolean;
+  iconImageSource: string = 'assets/icons/icon-07.png';
+  customIconSource: string = 'assets/icons/icon-10.png';
+  iconImageSource2: string = 'assets/icons/icon-08.png';
+  customIconSource2: string = 'assets/icons/icon-09.png';
+
 
   constructor() { }
 
   ngOnInit(): void {
+ 
 
   }
 
@@ -68,6 +76,12 @@ export class DesempenoFisicoComponent implements OnInit {
     )
   }
 
+  isCounterDisabledCounterC() : boolean {
+    return! (
+    this.selectedoptionB2 !== '1C'
+    )
+  }
+
   isCounterDisabled4(): boolean {
     return !(
       this.selectedoptionC !== '2C'
@@ -106,7 +120,7 @@ export class DesempenoFisicoComponent implements OnInit {
   }
 
   isCounterDisabled7(): boolean {
-    return ! (
+    return !(
       this.selectedoptionE2 === '1E'
 
     )
@@ -116,6 +130,19 @@ export class DesempenoFisicoComponent implements OnInit {
     let balance2 = (this.selectedoptionB === '1B') ? 1 : 0;
     let sillaTest1 = (this.selectedoptionE === '1D') ? 1 : 0;
     let sillaTest2 = (this.selectedoptionE2 === '1E') ? 1 : 0;
+
+    
+    if (this.selectedoptionA === '2A' || this.selectedoptionA === '3A') {
+       this.iconImageSource = this.customIconSource;
+    } else {
+       this.iconImageSource = 'assets/icons/icon-07.png';
+    }
+
+    if (this.selectedoptionB === '2B' || this.selectedoptionB === '3B') {
+      this.iconImageSource2 = this.customIconSource2;
+   } else {
+      this.iconImageSource2 = 'assets/icons/icon-08.png';
+   }
 
     let counterB2Points = 0;
     if (this.counterB2 < 3.0) {
@@ -158,9 +185,9 @@ export class DesempenoFisicoComponent implements OnInit {
       counterE1Points = 4
     } else if (this.counterE1 >= 11.2 && this.counterE1 <= 13.69) {
       counterE1Points = 3;
-    } else if (this.counterE1 >= 13.69 && this.counterE1 <= 16.69) {
+    } else if (this.counterE1 > 13.69 && this.counterE1 <= 16.69) {
       counterE1Points = 2;
-    } else if (this.counterE1 >= 16.69 && this.counterE1 <= 60) {
+    } else if (this.counterE1 > 16.69 && this.counterE1 <= 60) {
       counterE1Points = 1;
     } else if (this.counterE1 > 60.00) {
       counterE1Points = 0;
@@ -169,23 +196,35 @@ export class DesempenoFisicoComponent implements OnInit {
     let totalScore = balance1 + balance2 +
       sillaTest1 + sillaTest2 + counterB2Points + counterC2Points
       + counterC3Points + counterE1Points;
+    
+    console.log("balance1: ", balance1)
+    console.log("balance2: ", balance2)
+    console.log("silla1: ", sillaTest1)
+    console.log("silla2: ", sillaTest2)
+    console.log("counterB2Points: ", counterB2Points)
+    console.log("counterC2Points: ", counterC2Points)
+    console.log("counterC3Points: ", counterC3Points)
+    console.log("counterE1Points: ", counterE1Points)
+    console.log("Trae: ", totalScore)
+
+      this.finalScore = totalScore;
+
+      this.Calculefinal = (this.finalScore >= 8) ? '1F' : '2F';
 
     return totalScore;
 
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
 
