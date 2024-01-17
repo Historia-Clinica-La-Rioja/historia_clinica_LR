f94d43955c502c074fe19e5b05df558879e745f1
RodrigoCba96 <rodrigocordoba2296@gmail.com>
2024-01-17 13:07:22 -0300

    config logic with download last form in type pdf blod

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/frail-scale/get-frail/get-frail.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/frail-scale/get-frail/get-frail.component.ts
@@ -11,50 +11,84 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export class GetFrailComponent implements OnInit {
   submitted: boolean;
-  patientId: any;
+  patientId: number;
+  patientName: string;
   consultation: number;
-  questionnaireId: number;
+  questionnaireResponseId: number;
 
   constructor(
     private frailService: FrailService,
     @Inject(MAT_DIALOG_DATA) public data: any,
   ) {
-
-    this.questionnaireId = data.patientId
-
-  }
+    console.log('Data recibida:', data);
+    this.patientId = data.patientId
+}
 
   ngOnInit(): void {
   }
 
-  generatePdf() {
-    this.submitted = true;
 
-    if (this.questionnaireId) {
-      this.frailService.getPdf(this.questionnaireId).subscribe(
-        (response) => {
-          const blob = new Blob([response], { type: 'application/pdf' });
-          console.log(this.questionnaireId, "cuestionario")
+  //   getQuestionnaires(): void {
+//   this.frailService.getAllByPatientId(this.patientId).subscribe(
+//     (allQuestionnaires) => {
+//       console.log('Respuesta: ', allQuestionnaires);
 
+//        if (allQuestionnaires && allQuestionnaires.length > 0) {
+//         this.questionnaireResponseId = allQuestionnaires[0];
+
+//         this.generatePdf();
+//       } else {
+//         console.error('La respuesta está vacía o no contiene el formato esperado.');
+//       }
+//     },
+//     (error) => {
+//       console.error('Error al obtener data:', error);
+//     }
+//   );
+// }
+
+  downloadPdf(): void {
+    this.frailService.getAllByPatientId(this.patientId).subscribe(
+      (allQuestionnaires) => {
+        if (allQuestionnaires) {
+          const firstQuestionnaireId = allQuestionnaires[allQuestionnaires.length - 1].id;
+
+          for (let i = 0; i < allQuestionnaires.length; i++) {
+            const fecha = allQuestionnaires[i].createdOn;
+            console.log(fecha);
+          }
+          this.generatePdf(firstQuestionnaireId);
+        }
+      },
+      (error) => {
+        console.error("Error: ", error)
+      }
+    );
+  }
+
+  generatePdf(frailQuestionnaireId: number): void {
+    
+      this.frailService.getPdf(frailQuestionnaireId).subscribe(
+        (pdfBlob) => {
+          const blob = new Blob([pdfBlob], { type: 'application/pdf' });
+
+          console.log("Trae okk: ", frailQuestionnaireId);
           const fileName = `EscaladeFrail_${this.patientId}.pdf`;
-
           const url = window.URL.createObjectURL(blob);
           const a = document.createElement('a');
           a.href = url;
           a.download = fileName;
-
+  
           document.body.appendChild(a);
           a.click();
           document.body.removeChild(a);
-
+  
           window.URL.revokeObjectURL(url);
-
-          
         },
         (error) => {
-          
+          console.error('Error al descargar PDF:', error);
         }
       );
-    }
   }
+
 }  
