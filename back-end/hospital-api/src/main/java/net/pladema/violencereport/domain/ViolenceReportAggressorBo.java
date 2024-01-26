068cb6774932038766d9e6c4b6bf6c676332a49c
mmalyvarni <mateomv.mmv@gmail.com>
2024-01-26 14:52:40 -0300

    hsi-5183 [BE] Error al chequear agresores anteriores

--- back-end/hospital-api/src/main/java/net/pladema/violencereport/domain/ViolenceReportAggressorBo.java
+++ back-end/hospital-api/src/main/java/net/pladema/violencereport/domain/ViolenceReportAggressorBo.java
@@ -1,5 +1,6 @@
 package net.pladema.violencereport.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode
 public class ViolenceReportAggressorBo {
 
 	private ViolenceReportActorBo aggressorData;
