a6fc1357689996f21970b3bd054ad705bfb0d02e
Cristian Quevedo <cristian.quevedox@gmail.com>
2024-02-01 11:11:11 -0300

    refactor: remove unused getHeaderData method

--- back-end/hospital-api/src/main/java/net/pladema/provincialreports/odontologicalreports/service/impl/OdontologicalReportExcelServiceImpl.java
+++ back-end/hospital-api/src/main/java/net/pladema/provincialreports/odontologicalreports/service/impl/OdontologicalReportExcelServiceImpl.java
@@ -119,48 +119,6 @@ public class OdontologicalReportExcelServiceImpl implements OdontologicalReportE
 		subTitleStyle.setVAlign(ICellStyle.VALIGNMENT.BOTTOM);
 	}
 
-	private List<CellContent> getHeaderData(String[] subtitles, String title, String institutionName){
-		List<CellContent> data = new ArrayList<>();
-
-		int nRow = 0;
-
-		data.add(new CellContent(nRow, 0, 1, 2, "", basicStyle));
-		data.add(new CellContent(nRow, 2, 2, 1, "1", titleStyle));
-		data.add(new CellContent(nRow, 3, 2, 16, title, titleStyle));
-		data.add(new CellContent(nRow, 19, 1, 3, "1. Hoja N°", fieldStyle));
-		data.add(new CellContent(nRow, 22, 1, 1, "", basicStyle));
-		data.add(new CellContent(nRow, 23, 1, 1, "", basicStyle));
-
-		nRow++;
-		data.add(new CellContent(nRow, 0, 1, 2, "", basicStyle));
-		data.add(new CellContent(nRow, 19, 1, 5, "", basicStyle));
-
-		nRow++;
-		data.add(new CellContent(nRow, 0, 1, 2, "2. ESTABLECIMIENTO: " + institutionName, fieldStyle));
-		data.add(new CellContent(nRow, 2, 1, 14, "", basicStyle));
-		data.add(new CellContent(nRow, 16, 1, 1, "3. MES", fieldStyle));
-		data.add(new CellContent(nRow, 17, 1, 1, "", basicStyle));
-		data.add(new CellContent(nRow, 18, 1, 1, "4. AÑO", fieldStyle));
-		data.add(new CellContent(nRow, 19, 1, 5, "", basicStyle));
-
-		nRow++;
-		data.add(new CellContent(nRow, 0, 1, 2, "5. PARTIDO", fieldStyle));
-		data.add(new CellContent(nRow, 2, 1, 8, "", basicStyle));
-		data.add(new CellContent(nRow, 10, 1, 1, "6. REGIÓN SANITARIA", fieldStyle));
-		data.add(new CellContent(nRow, 11, 1, 5, "", basicStyle));
-		data.add(new CellContent(nRow, 16, 1, 3, "7. SERVICIO", fieldStyle));
-		data.add(new CellContent(nRow, 19, 1, 3, "", basicStyle));
-		data.add(new CellContent(nRow, 22, 1, 1, "", basicStyle));
-		data.add(new CellContent(nRow, 23, 1, 1, "", basicStyle));
-
-		nRow++;
-		int column = 0;
-		for (String subtitle : subtitles)
-			data.add(new CellContent(nRow, column++, 1, 1, subtitle, subTitleStyle));
-
-		return data;
-	}
-
 	private List<CellContent> getHeaderData(String[] subtitles, String title, String startDate, String endDate, String institutionName) {
 		List<CellContent> data = new ArrayList<>();
 
