7b39a8c1779f1b3ea4776331c97ef48105331148
RodrigoCba96 <rodrigocordoba2296@gmail.com>
2024-02-06 09:45:45 -0300

    added physical-service

---
+++ front-end/apps/projects/hospital/src/app/modules/api-rest/services/physical-performance.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FrailAnswers, FrailSummary } from '@api-rest/api-model'; 
+import { environment } from '@environments/environment';
+import { ContextService } from '@core/services/context.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhysicalPerformanceService {  
+
+  constructor(
+    private contextService: ContextService,
+    private readonly http: HttpClient  ) {}
+
+  createPhysical(patientId: number, physicalData: any): Observable<boolean> {
+    const url = `${environment.apiBase}/institution/${this.contextService.institutionId}/patient/${patientId}/questionnaire/create`; 
+    return this.http.post<boolean>(url, physicalData);
+  }
+
+  getFrailSummary(institutionId: number, patientId: number, questionnaireId: number): Observable<FrailSummary> {
+    const url = `${environment.apiBase}/institution/${institutionId}/patient/${patientId}/hce/general-state/summary/frail/${questionnaireId}`;
+    return this.http.get<FrailSummary>(url);
+  }
+
+  getAllByPatientId(institutionId: number, patientId: number): Observable<FrailAnswers[]> {
+    const url = `${environment.apiBase}/institution/${institutionId}/patient/${patientId}/hce/general-state/frail`; 
+    return this.http.get<FrailAnswers[]>(url);
+  }
+
+  getPdf(questionnaireId: number): Observable<Blob> {
+    const url = `${environment.apiBase}/institution/patient/outpatient/consultation/frail/${questionnaireId}/pdf-download`; 
+    return this.http.get(url, { responseType: 'blob' });
+    
+  }
+
+}
