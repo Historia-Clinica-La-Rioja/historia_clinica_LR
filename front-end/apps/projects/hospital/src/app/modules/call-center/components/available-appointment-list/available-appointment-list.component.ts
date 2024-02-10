77e2083efeb91b51f8441ad4e922854c275efee6
jvaldez <jvaldez@lamansys.com.ar>
2024-02-10 13:28:20 -0300

    hsi-5471 [FE] Agrega advertencia de feriado

--- front-end/apps/projects/hospital/src/app/modules/call-center/components/available-appointment-list/available-appointment-list.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/call-center/components/available-appointment-list/available-appointment-list.component.ts
@@ -1,7 +1,15 @@
+import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { PageEvent } from '@angular/material/paginator';
 import { DiaryAvailableAppointmentsDto } from '@api-rest/api-model';
+import { dateDtoToDate } from '@api-rest/mapper/date-dto.mapper';
+import { HolidaysService } from '@api-rest/services/holidays.service';
+import { DatePipeFormat } from '@core/utils/date.utils';
+import { DiscardWarningComponent } from '@presentation/dialogs/discard-warning/discard-warning.component';
+import { format } from 'date-fns';
 import { Observable, of } from 'rxjs';
+import { DateFormat } from '@core/utils/date.utils';
 
 const PAGE_SIZE_OPTIONS = [5, 10, 25];
 @Component({
@@ -19,14 +27,56 @@ export class AvailableAppointmentListComponent {
 	@Input()
 	set availableAppointments(availableAppointments: DiaryAvailableAppointmentsDto[]) {
 		this.appointments = availableAppointments;
-		this.setPageSizeAndLoadFirstPage();		
+		this.setPageSizeAndLoadFirstPage();
 	}
 
-	constructor() {	}
+	constructor(
+		private readonly dialog: MatDialog,
+		private readonly holidayService: HolidaysService,
+		private readonly datePipe: DatePipe,
+	) { }
 
 	onPageChange($event: PageEvent) {
 		const startPage = $event.pageIndex * $event.pageSize;
 		this.appointmentsCurrentPage = this.appointments.slice(startPage, $event.pageSize + startPage);
+	}
+
+	checkHolidayAndAssign(indexOfAppointment: number) {
+
+		const date = dateDtoToDate(this.appointments[indexOfAppointment].date);
+		const stringDate = format(date, DateFormat.API_DATE);
+
+		this.holidayService.getHolidays(stringDate, stringDate).subscribe(holidays => {
+			if (!holidays.length) {
+				this.assignAppointment();
+				return;
+			}
+			this.openHolidayWarning(date);
+		});
+	}
+
+	private openHolidayWarning(date: Date) {
+		const holidayText = 'corresponde a un día feriado.';
+		const holidayDate = this.datePipe.transform(date, DatePipeFormat.FULL_DATE);
+		const dialogRef = this.dialog.open(DiscardWarningComponent, {
+			data: {
+				title: 'access-management.holiday_warning.TITLE',
+				content: `${holidayDate} ${holidayText}`,
+				contentBold: `access-management.holiday_warning.HOLIDAY_DISCLAIMER`,
+				okButtonLabel: 'access-management.holiday_warning.OK_BUTTON',
+				cancelButtonLabel: 'access-management.holiday_warning.CANCEL_BUTTON',
+			},
+			disableClose: true,
+		});
+
+		dialogRef.afterClosed().subscribe(assignAnotherAppointment => {
+			if (!assignAnotherAppointment)
+				this.assignAppointment();
+		});
+	}
+
+	private assignAppointment() {
+
 	}
 
 	private setPageSizeAndLoadFirstPage() {
