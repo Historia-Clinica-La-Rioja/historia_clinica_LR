9e27af59920ee5e933a005135efeef1e8de4affb
Maximiliano Romero Piaggio <mromeropiaggio@lamansys.com.ar>
2024-02-20 13:43:44 -0300

    hsi-5609 PBA - Problemas marcados como incorrectos que previamente figuraban en la HC del paciente como problemas activos o crónicos desaparecen de la HC

--- back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/infrastructure/output/repository/hce/HCEHealthConditionRepository.java
+++ back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/infrastructure/output/repository/hce/HCEHealthConditionRepository.java
@@ -5,12 +5,16 @@ import ar.lamansys.sgh.clinichistory.infrastructure.output.repository.hce.entity
 
 import ar.lamansys.sgh.clinichistory.infrastructure.output.repository.hce.entity.HCEPersonalHistoryVo;
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface HCEHealthConditionRepository {
 
     List<HCEHealthConditionVo> getSummaryProblems(Integer patientId);
 
 	List<HCEHealthConditionVo> getSummaryProblemsByUser(Integer patientId, Integer userId);
+
+    @Transactional(readOnly = true)
+    List<HCEHealthConditionVo> getProblemsMarkedAsError(Integer patientId);
 
     List<HCEPersonalHistoryVo> getPersonalHistories(Integer patientId);
 
