5c9fbae7339ed6a474d251bb5e89bab351784b44
Manuel Gil <manuelgilmoltrasio@gmail.com>
2024-03-04 15:25:22 -0300

    hsi-5893 [FE] Agregar paginado para card de puntos de medición registrados

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/modules/internacion/components/measuring-point-background-list/measuring-point-background-list.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/modules/internacion/components/measuring-point-background-list/measuring-point-background-list.component.ts
@@ -1,7 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { AnestheticReportVitalSignsService, MeasuringPointData } from '../../services/anesthetic-report-vital-signs.service';
 import { MatDialog } from '@angular/material/dialog';
 import { EditMeasuringPointComponent } from '../../dialogs/edit-measuring-point/edit-measuring-point.component';
+import { MatPaginator } from '@angular/material/paginator';
+
+const PAGE_SIZE_OPTIONS = [5];
+const PAGE_SIZE = 5;
 
 @Component({
     selector: 'app-measuring-point-background-list',
@@ -10,8 +14,16 @@ import { EditMeasuringPointComponent } from '../../dialogs/edit-measuring-point/
 })
 export class MeasuringPointBackgroundListComponent implements OnInit {
 
+    @ViewChild('paginator') paginator: MatPaginator;
     @Input() service: AnestheticReportVitalSignsService;
     measuringPoints: MeasuringPointData[];
+
+    pageSlice: MeasuringPointData[];
+    pageSizeOptions = PAGE_SIZE_OPTIONS;
+    pageSize = PAGE_SIZE;
+    startPage = 0;
+    endPage = PAGE_SIZE;
+    page = 0;
 
     constructor(
 		public dialog: MatDialog,
@@ -20,7 +32,19 @@ export class MeasuringPointBackgroundListComponent implements OnInit {
     ngOnInit(): void {
         this.service.measuringPoints$.subscribe((measuringPoints) => {
             this.measuringPoints = measuringPoints;
+            this.pageSlice = this.measuringPoints.slice(this.startPage, this.endPage);
+            this.checkIfPageEmpty();
         })
+    }
+
+    private checkIfPageEmpty() {
+        if(this.pageSlice.length == 0 && this.startPage != 0){
+            this.startPage = this.startPage - PAGE_SIZE;
+            this.endPage = this.endPage - PAGE_SIZE;
+            this.page = this.page - 1;
+            this.pageSlice = this.measuringPoints.slice(this.startPage, this.endPage);
+            this.paginator.previousPage();
+        }
     }
 
     openEditDialog(index: number) {
@@ -37,4 +61,12 @@ export class MeasuringPointBackgroundListComponent implements OnInit {
             }
 		});
     }
+
+    onPageChange($event: any) {
+        const page = $event;
+        this.page = page.pageIndex;
+		this.startPage = this.page * page.pageSize;
+        this.endPage = $event.pageSize + this.startPage;
+		this.pageSlice = this.measuringPoints.slice(this.startPage, this.endPage);
+	}
 }
