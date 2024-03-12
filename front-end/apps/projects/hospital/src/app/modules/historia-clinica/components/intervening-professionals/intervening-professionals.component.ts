3f4ff6b149d964a8e3a91df8f686b8d8fca2fc69
Brenda Flamminio <brendaflamminio@gmail.com>
2024-03-12 14:52:18 -0300

    hsi-6043 [FE] Nuevo servicio y endpoint para traer y mostrar los profesionales

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/intervening-professionals/intervening-professionals.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/intervening-professionals/intervening-professionals.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ELicenseNumberType, ElectronicJointSignatureInstitutionProfessionalDto } from '@api-rest/api-model';
+import { ElectronicJointSignatureInstitutionalProfessionalLicenseService } from '@api-rest/services/electronic-joint-signature-institutional-professional-license.service';
+import { TypeaheadOption } from '@presentation/components/typeahead/typeahead.component';
 
 @Component({
   selector: 'app-intervening-professionals',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterveningProfessionalsComponent implements OnInit {
 
-  constructor() { }
+  professionals: TypeaheadOption<any>[];
+  constructor(private readonly electronicJointSignatureInstitutionalProfessionalLicenseService: ElectronicJointSignatureInstitutionalProfessionalLicenseService) { }
 
   ngOnInit(): void {
+    this.electronicJointSignatureInstitutionalProfessionalLicenseService.getInstitutionalProfessionalsLicense().subscribe(professionals => {
+      this.professionals = professionals.map(d => this.toProfessionalTypeahead(d));;
+    })
+  }
+
+  setProfessional(event) {
+  }
+
+  private toProfessionalTypeahead(professionalDto: ElectronicJointSignatureInstitutionProfessionalDto): TypeaheadOption<any> {
+    const licence = professionalDto.licenses[0].type === ELicenseNumberType.NATIONAL ? ', MN' : ', MP';
+    const mas = professionalDto.licenses.length >= 2 ? ' (+' + (professionalDto.licenses.length - 1) + ' más)' : "";
+    return {
+      compareValue: professionalDto.completeName + licence + ' ' + professionalDto.licenses[0].number + ' ' + professionalDto.licenses[0].clinicalSpecialtyName + mas,
+      value: professionalDto.healthcareProfessionalId,
+    };
   }
 
 }
