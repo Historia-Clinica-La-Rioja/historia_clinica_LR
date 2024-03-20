1d105a5f5ec69f1a6e581f72e61ed898158ff5f1
Manuel Gil <manuelgilmoltrasio@gmail.com>
2024-03-20 16:38:16 -0300

    hsi-6270 Modificar archivo de storybook para el componente time-picker

--- front-end/apps/projects/hospital/src/app/modules/presentation/components/time-picker/time-picker.stories.ts
+++ front-end/apps/projects/hospital/src/app/modules/presentation/components/time-picker/time-picker.stories.ts
@@ -7,11 +7,21 @@ const meta: Meta<TimePickerComponent> = {
     tags: ['autodocs'],
     argTypes: {
         timePickerData: {
-            description: '<strong>defaultTime:</strong> objeto para settear hora por defecto en el componente <br> <strong>hoursMinValue y hoursMaxValue</strong> son inputs de tipo number que permiten limitar las opciones de las horas <br><strong>minuteStep</strong> es un input de tipo number que permite cambiar el step de los minutos <br> <strong>hideLabel</strong> es un input de tipo boolean que permite ocultar el label de Hora',
+            description: '<strong>defaultTime:</strong> objeto para settear hora por defecto en el componente <br> <strong>hoursMinValue y hoursMaxValue</strong> son inputs de tipo number que permiten limitar las opciones de las horas <br><strong>minuteStep</strong> es un input de tipo number que permite cambiar el step de los minutos <br> <strong>hideLabel</strong> es un input de tipo boolean que permite ocultar el label de Hora <br> <strong>isRequired</strong> es un input de tipo boolean que indica si pertenece a un formulario cuyo campo es requerido',
             table: { 
                 defaultValue: { summary: 'Para utilizar el componente con valores por defecto no es necesario mandar el timePickerData como input' } 
             }
         },
+        submitted: {
+            description: 'Input de tipo boolean que le indica al componente que el formulario que lo utiliza se intentó enviar y que debe revisar la validez de sus campos',
+            table: { 
+                defaultValue: { summary: 'false' } 
+            },
+            options: [true, false],
+            control: {
+				type: 'select',
+			}
+        }
 	},
     parameters: {
         docs: {
@@ -33,6 +43,7 @@ export const defaultValue: Story = {
             hoursMaxValue: 23,
             minuteStep: 5,
             hideLabel: false,
+            isRequired: false,
         }
     }
 };
