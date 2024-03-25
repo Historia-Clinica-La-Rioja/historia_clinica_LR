743b86460c00c0d2e8d80067afbd3dbc65340962
Cristian Quevedo <cristian.quevedox@gmail.com>
2024-03-25 13:14:38 -0300

    Revert "hsi-2724 [BE] Adición de controles al reservar turnos online"
    This reverts commit baef23e11e19c5765e6dfefb669a617475258fa4.

--- back-end/booking/src/main/java/ar/lamansys/online/application/diary/IsAppointmentAlreadyAssignedService.java
+++
@@ -1,7 +0,0 @@
-package ar.lamansys.online.application.diary;
-
-public interface IsAppointmentAlreadyAssignedService {
-
-	boolean run(Integer diaryId, Integer openingHoursId, String date, String time);
-
-}
