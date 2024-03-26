b44a0e6b14e9de7da02a49921edb1d76c79ca08a
FacundoBarreiros <facundobarreiros@gmail.com>
2024-03-26 15:07:22 -0300

    hsi-6133 [FE] Crear componente CODIGO LOINC INPUT SNOMD (ECL)

---
+++ front-end/apps/projects/hospital/src/app/modules/hsi-components/loinc-form/components/loinc-input-snomed/loinc-input-snomed.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SnomedDto, SnomedECL } from '@api-rest/api-model';
+
+
+const snomedEclMap: Record<string, SnomedECL> = {
+	ALLERGY: SnomedECL.ALLERGY,
+	ANESTHESIA: SnomedECL.ANESTHESIA,
+	BLOOD_TYPE: SnomedECL.BLOOD_TYPE,
+	CARDIOVASCULAR_DISORDER: SnomedECL.CARDIOVASCULAR_DISORDER,
+	CONSULTATION_REASON: SnomedECL.CONSULTATION_REASON,
+	DIABETES: SnomedECL.DIABETES,
+	DIAGNOSIS: SnomedECL.DIAGNOSIS,
+	ELECTROCARDIOGRAPHIC_PROCEDURE: SnomedECL.ELECTROCARDIOGRAPHIC_PROCEDURE,
+	EVENT: SnomedECL.EVENT,
+	FAMILY_RECORD: SnomedECL.FAMILY_RECORD,
+	HOSPITAL_REASON: SnomedECL.HOSPITAL_REASON,
+	HYPERTENSION: SnomedECL.HYPERTENSION,
+	MEDICINE_WITH_UNIT_OF_PRESENTATION: SnomedECL.MEDICINE_WITH_UNIT_OF_PRESENTATION,
+	MEDICINE: SnomedECL.MEDICINE,
+	PERSONAL_RECORD: SnomedECL.PERSONAL_RECORD,
+	PROCEDURE: SnomedECL.PROCEDURE,
+	VACCINE: SnomedECL.VACCINE,
+	VIOLENCE_MODALITY: SnomedECL.VIOLENCE_MODALITY,
+	VIOLENCE_PROBLEM: SnomedECL.VIOLENCE_PROBLEM,
+	VIOLENCE_TYPE: SnomedECL.VIOLENCE_TYPE,
+};
+
+@Component({
+	selector: 'app-loinc-input-snomed',
+	templateUrl: './loinc-input-snomed.component.html',
+	styleUrls: ['./loinc-input-snomed.component.scss']
+})
+export class LoincInputSnomedComponent {
+
+	eclSelected: SnomedECL;
+	@Input() set ecl(snomedGroupId: string) {
+		this.eclSelected = convertStringToEnum(snomedGroupId);
+	};
+	@Input() title: string;
+	@Output() valueSelected: EventEmitter<SnomedDto> = new EventEmitter<SnomedDto>();
+
+}
+
+function convertStringToEnum(value: string): SnomedECL | undefined {
+	return snomedEclMap[value as keyof typeof snomedEclMap];
+}
