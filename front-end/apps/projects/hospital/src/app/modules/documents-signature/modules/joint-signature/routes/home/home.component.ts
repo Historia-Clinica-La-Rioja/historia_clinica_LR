87044504d5dc2876142238a17b40ab8a705c0db9
jirimoldi <juanirimoldi@gmail.com>
2024-04-05 14:14:17 -0300

    hsi-6636 [FE] Formateo de codigo

--- front-end/apps/projects/hospital/src/app/modules/documents-signature/modules/joint-signature/routes/home/home.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/documents-signature/modules/joint-signature/routes/home/home.component.ts
@@ -7,28 +7,28 @@ import { FeatureFlagService } from '@core/services/feature-flag.service';
 import { Detail } from '@presentation/components/details-section-custom/details-section-custom.component';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
-export class HomeComponent{
-  buttonBack = false;
-  routePrefix: string;
-  data: Detail[] = [];
-  constructor(private readonly router: Router, private readonly contextService: ContextService, 
-    private readonly featureFlagService: FeatureFlagService) { 
-    this.routePrefix = 'institucion/' + this.contextService.institutionId + '/'
-    this.featureFlagService.isActive(AppFeature.HABILITAR_FIRMA_DIGITAL).subscribe(isEnabledDigital =>{
-      this.featureFlagService.isActive(AppFeature.HABILITAR_FIRMA_CONJUNTA).subscribe(isEnabledConjunta =>{
-        if(isEnabledConjunta && isEnabledDigital){
-          this.buttonBack = true;
-        }
-      }) 
-    })
-  }
+export class HomeComponent {
+	buttonBack = false;
+	routePrefix: string;
+	data: Detail[] = [];
+	constructor(private readonly router: Router, private readonly contextService: ContextService,
+		private readonly featureFlagService: FeatureFlagService) {
+		this.routePrefix = 'institucion/' + this.contextService.institutionId + '/'
+		this.featureFlagService.isActive(AppFeature.HABILITAR_FIRMA_DIGITAL).subscribe(isEnabledDigital => {
+			this.featureFlagService.isActive(AppFeature.HABILITAR_FIRMA_CONJUNTA).subscribe(isEnabledConjunta => {
+				if (isEnabledConjunta && isEnabledDigital) {
+					this.buttonBack = true;
+				}
+			})
+		})
+	}
 
-  goToBackDocumentsSignature(){
-    this.router.navigate([`${this.routePrefix}${URL_DOCUMENTS_SIGNATURE}`]);
-  }
+	goToBackDocumentsSignature() {
+		this.router.navigate([`${this.routePrefix}${URL_DOCUMENTS_SIGNATURE}`]);
+	}
 
 }
