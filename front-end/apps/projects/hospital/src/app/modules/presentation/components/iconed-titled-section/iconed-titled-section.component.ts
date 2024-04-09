fad27e48bd608db2825c7285ca052ccd8ef39a14
Manuel Gil <manuelgilmoltrasio@gmail.com>
2024-04-09 11:34:45 -0300

    hsi-6553 [FE] Agregar componente iconedTitledSection a storybook

--- front-end/apps/projects/hospital/src/app/modules/presentation/components/iconed-titled-section/iconed-titled-section.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/presentation/components/iconed-titled-section/iconed-titled-section.component.ts
@@ -1,17 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-iconed-titled-section',
     templateUrl: './iconed-titled-section.component.html',
     styleUrls: ['./iconed-titled-section.component.scss']
 })
-export class IconedTitledSectionComponent implements OnInit {
+export class IconedTitledSectionComponent {
     @Input() icon: string;
     @Input() title: string;
-
-    constructor() { }
-
-    ngOnInit(): void {
-    }
-
 }
