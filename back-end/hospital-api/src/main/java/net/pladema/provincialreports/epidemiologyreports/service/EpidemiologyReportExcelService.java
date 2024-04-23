50bc3a5075bfa33eddc566d31faa191d240546bf
Cristian Quevedo <cristian.quevedox@gmail.com>
2024-04-22 21:39:54 -0300

    feat(epidemiology-report-service): add buildExcelCompleteDengue method to EpidemiologyReportExcelService interface

--- back-end/hospital-api/src/main/java/net/pladema/provincialreports/epidemiologyreports/service/EpidemiologyReportExcelService.java
+++ back-end/hospital-api/src/main/java/net/pladema/provincialreports/epidemiologyreports/service/EpidemiologyReportExcelService.java
@@ -1,6 +1,7 @@
 package net.pladema.provincialreports.epidemiologyreports.service;
 
 import ar.lamansys.sgx.shared.reports.util.struct.IWorkbook;
+import net.pladema.provincialreports.epidemiologyreports.repository.CompleteDengueConsultationDetail;
 import net.pladema.provincialreports.epidemiologyreports.repository.DenguePatientControlConsultationDetail;
 
 import java.util.List;
@@ -8,5 +9,7 @@ import java.util.List;
 public interface EpidemiologyReportExcelService {
 
 	IWorkbook buildExcelDenguePatientControl(String title, String[] headers, List<DenguePatientControlConsultationDetail> query, String institutionName, String observations);
+
+	IWorkbook buildExcelCompleteDengue(String title, String[] headers, List<CompleteDengueConsultationDetail> query, String institutionName, String observations);
 
 }
