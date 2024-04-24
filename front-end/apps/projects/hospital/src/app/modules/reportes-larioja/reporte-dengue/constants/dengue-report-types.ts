ff16c32b91abb40b1aa9df464d279d043d94b767
Macarena Martín <macarenamartin1111@gmail.com>
2024-04-24 10:02:38 -0300

    added dengue report module with components

---
+++ front-end/apps/projects/hospital/src/app/modules/reportes-larioja/reporte-dengue/constants/dengue-report-types.ts
@@ -0,0 +1,10 @@
+export const DENGUE_REPORT_TYPES = [
+    {
+        description: 'Atenciones relacionadas al Dengue - Control de pacientes',
+        id: 2,
+    },
+    {
+        description: 'Atenciones relacionadas al Dengue - Consultas completas',
+        id: 1,
+    }
+]
---
+++ front-end/apps/projects/hospital/src/app/modules/reportes-larioja/reporte-dengue/reporte-dengue.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { hasError } from '@core/utils/form.utils';
+import { DENGUE_REPORT_TYPES } from './constants/dengue-report-types';
+import { DengueReportsService } from '@api-rest/services/dengue-reports.service';
+
+@Component({
+  selector: 'app-reporte-dengue',
+  templateUrl: './reporte-dengue.component.html',
+  styleUrls: ['./reporte-dengue.component.scss']
+})
+export class ReporteDengueComponent implements OnInit {
+
+    form: UntypedFormGroup;
+    public submitted = false;
+  
+    public hasError = hasError;
+  
+    REPORT_TYPES = DENGUE_REPORT_TYPES;
+    
+    constructor(
+      private readonly formBuilder: UntypedFormBuilder,
+      private readonly dengueReportsService: DengueReportsService,
+    ) { }
+  
+    ngOnInit(): void {
+      this.form = this.formBuilder.group({
+        reportType: [null, Validators.required],
+      });
+    }
+      generateDengueReport() {
+      this.submitted = true;
+      if(this.form.valid) {
+        const params = {
+        }
+        const dengueReportId = this.form.controls.reportType.value;
+        switch (dengueReportId) {
+          case 1:
+            this.dengueReportsService.getDengueAttentionsReport(params, `${this.REPORT_TYPES[0].description}.xls`).subscribe();
+            console.log('reporte consultas 1')
+            break;
+          case 2:
+            this.dengueReportsService.getDengueControlsReport(params, `${this.REPORT_TYPES[1].description}.xls`).subscribe();
+            console.log('reporte control 2')
+            break; 
+          default:
+        }
+      }
+    }
+  
+  }
+  
