ded9661a70b14335762b1d2f5492d257f1e45dcd
Cristian Quevedo <cristian.quevedox@gmail.com>
2024-04-26 11:39:21 -0300

    style(reportes-larioja): format code and remove unnecessary console.log()

--- front-end/apps/projects/hospital/src/app/modules/reportes-larioja/reporte-dengue/reporte-dengue.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/reportes-larioja/reporte-dengue/reporte-dengue.component.ts
@@ -9,45 +9,43 @@ import { DengueReportsService } from '@api-rest/services/dengue-reports.service'
   templateUrl: './reporte-dengue.component.html',
   styleUrls: ['./reporte-dengue.component.scss']
 })
-
 export class ReporteDengueComponent implements OnInit {
 
-    form: UntypedFormGroup;
-    public submitted = false;
+  form: UntypedFormGroup;
+  public submitted = false;
   
-    public hasError = hasError;
+  public hasError = hasError;
   
-    REPORT_TYPES = DENGUE_REPORT_TYPES;
+  REPORT_TYPES = DENGUE_REPORT_TYPES;
     
-    constructor(
-      private readonly formBuilder: UntypedFormBuilder,
-      private readonly dengueReportsService: DengueReportsService,
-    ) { }
+  constructor(
+    private readonly formBuilder: UntypedFormBuilder,
+    private readonly dengueReportsService: DengueReportsService,
+  ) { }
   
-    ngOnInit(): void {
-      this.form = this.formBuilder.group({
-        reportType: [null, Validators.required],
-      });
-    }
-      generateDengueReport() {
-      this.submitted = true;
-      if(this.form.valid) {
-        const params = {
-        }
-        const dengueReportId = this.form.controls.reportType.value;
-        switch (dengueReportId) {
-          case 1:
-            this.dengueReportsService.getDengueAttentionsReport(params, `${this.REPORT_TYPES[0].description}.xls`).subscribe();
-            console.log('reporte consultas 1')
-            break;
-          case 2:
-            this.dengueReportsService.getDengueControlsReport(params, `${this.REPORT_TYPES[1].description}.xls`).subscribe();
-            console.log('reporte control 2')
-            break; 
-          default:
-        }
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      reportType: [null, Validators.required],
+    });
+  }
+
+  generateDengueReport() {
+    this.submitted = true;
+    if(this.form.valid) {
+      const params = {
+
+      }
+      const dengueReportId = this.form.controls.reportType.value;
+      switch (dengueReportId) {
+        case 1:
+          this.dengueReportsService.getDengueAttentionsReport(params, `${this.REPORT_TYPES[0].description}.xls`).subscribe();
+          break;
+        case 2:
+          this.dengueReportsService.getDengueControlsReport(params, `${this.REPORT_TYPES[1].description}.xls`).subscribe();
+          break;
+        default:
       }
     }
-  
   }
-  
+
+}
