55c6f7e1246a3502cd3b51e4229dbf5f2f356567
Federico de Muguruza <fedemuguruza@gmail.com>
2024-05-07 11:31:18 -0300

    hsi-7020 [FE] Agregar componente contenedor de las acciones, el listado y el título a Storybook

---
+++ front-end/apps/projects/hospital/src/app/modules/hsi-components/concepts-list/concepts-list.stories.ts
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { ConceptsListComponent } from './concepts-list.component';
+
+const meta: Meta<ConceptsListComponent> = {
+	title: 'HSI/ConceptsListComponent',
+	component: ConceptsListComponent,
+	tags: ['autodocs'],
+	argTypes: {
+		content: {
+			description: 'Objeto con todo el texto para mostrar',
+		},
+		hasConcepts: {
+			description: 'Indica si tiene conceptos',
+			options: [true, false],
+		},
+	}
+};
+
+export default meta;
+type Story = StoryObj<ConceptsListComponent>;
+
+export const Minimun: Story = {
+	args: {
+		content: {
+            header: {
+                text: 'Alergias',
+                icon: 'cancel'
+            },
+            titleList: '',
+            actions: {
+                button: 'Agregar alergia',
+            }
+        },
+	}
+}
+
+export const WithNoReferCheckbox: Story = {
+	args: {
+		content: {
+            header: {
+                text: 'Alergias',
+                icon: 'cancel'
+            },
+            titleList: '',
+            actions: {
+                button: 'Agregar alergia',
+                checkbox: 'No refiere'
+            }
+        },
+	},
+}
+
+export const WithConceptListAndNoReferCheckbox: Story = {
+	args: {
+		content: {
+            header: {
+                text: 'Alergias',
+                icon: 'cancel'
+            },
+            titleList: 'Alergias registradas',
+            actions: {
+                button: 'Agregar alergia',
+                checkbox: 'No refiere'
+            }
+        },
+        hasConcepts: true
+	},
+}
