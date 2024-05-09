d7994cfa854904f2c47f84e81e4fcdbece485b29
jirimoldi <juanirimoldi@gmail.com>
2024-05-09 09:51:32 -0300

    hsi-7425 [FE] Refactor 8: Desplegable “Equipo de quirófano”

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/surgical-report-professional-team/surgical-report-professional-team.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/surgical-report-professional-team/surgical-report-professional-team.component.ts
@@ -11,89 +11,87 @@ export class SurgicalReportProfessionalTeamComponent implements OnInit {
 
 	@Input() professionals: ProfessionalDto[];
 	@Input() surgicalReport: SurgicalReportDto;
-	ayudanteCount: number = 1;
 
 	healthcareProfessionals: DocumentHealthcareProfessionalDto[] = [];
-
-	SURGEON = EProfessionType.SURGEON;
-	SURGEON_ASSISTANT = EProfessionType.SURGEON_ASSISTANT;
-	ANESTHESIOLOGIST = EProfessionType.ANESTHESIOLOGIST;
-	CARDIOLOGIST = EProfessionType.CARDIOLOGIST;
-	SURGICAL_INSTRUMENT_TECHNICIAN = EProfessionType.SURGICAL_INSTRUMENT_TECHNICIAN;
-	OBSTETRICIAN = EProfessionType.OBSTETRICIAN;
-	PEDIATRICIAN = EProfessionType.PEDIATRICIAN;
-
 	surgeon: DocumentHealthcareProfessionalDto;
-	surgeonAssistant: DocumentHealthcareProfessionalDto;
-	anesthesiologist: DocumentHealthcareProfessionalDto;
-	cardiologist: DocumentHealthcareProfessionalDto;
-	surgicalInstrumentTechnician: DocumentHealthcareProfessionalDto;
-	obstetrician: DocumentHealthcareProfessionalDto;
-	pediatrician: DocumentHealthcareProfessionalDto;
+	OTHER = EProfessionType.OTHER;
 
-	constructor() {	}
+	mockedProfessional: DocumentHealthcareProfessionalDto = {
+		healthcareProfessional: {
+			id: 1,
+			licenseNumber: "1111111",
+			person: {
+				birthDate: "",
+				fullName: "Juan Carlos Rodriguez",
+				id: 1,
+				identificationNumber: "37123456",
+			}
+		},
+		profession: {
+			type: EProfessionType.ANESTHESIOLOGIST,
+			typeDescription: null
+		}
+	};
+
+	mockedProfessional2: DocumentHealthcareProfessionalDto = {
+		healthcareProfessional: {
+			id: 2,
+			licenseNumber: "2222222",
+			person: {
+				birthDate: "",
+				fullName: "Mariano Gutierrez",
+				id: 2,
+				identificationNumber: "18123456",
+			}
+		},
+		profession: {
+			type: EProfessionType.OTHER,
+			typeDescription: "AYUDANTE"
+		}
+	};
+
+	constructor() { }
 
 	ngOnInit(): void {
-		this.surgicalReport?.healthcareProfessionals.forEach(p => {
-			switch (p.type) {
-				case EProfessionType.SURGEON:
-					this.surgeon = p;
-					break;
-				case EProfessionType.SURGEON_ASSISTANT:
-					this.surgeonAssistant = p;
-					break;
-				case EProfessionType.ANESTHESIOLOGIST:
-					this.anesthesiologist = p;
-					break;
-				case EProfessionType.CARDIOLOGIST:
-					this.cardiologist = p;
-					break;
-				case EProfessionType.SURGICAL_INSTRUMENT_TECHNICIAN:
-					this.surgicalInstrumentTechnician = p;
-					break;
-				case EProfessionType.OBSTETRICIAN:
-					this.obstetrician = p;
-					break;
-				case EProfessionType.PEDIATRICIAN:
-					this.pediatrician = p;
-					break;
-			}
-		})
+		this.surgeon = this.surgicalReport.healthcareProfessionals.find(p => p.profession.type === EProfessionType.SURGEON)
 	}
 
-	addAyudante() {
-		this.ayudanteCount++;
+	addProfessional(): void {
+		this.healthcareProfessionals.push(this.mockedProfessional);
+		this.surgicalReport.healthcareProfessionals.push(this.mockedProfessional);
+		this.healthcareProfessionals.push(this.mockedProfessional2);
+		this.surgicalReport.healthcareProfessionals.push(this.mockedProfessional2);
 	}
 
-	professionalChange(professional: HCEHealthcareProfessionalDto, type: EProfessionType): void {
-		const index = this.healthcareProfessionals.findIndex(p => p.type === type);
+	deleteProfessional(index: number): void {
+		this.healthcareProfessionals.splice(index, 1);
+		this.surgicalReport.healthcareProfessionals.splice(index, 1);
+	}
+
+	selectSurgeon(professional: HCEHealthcareProfessionalDto): void {
+		const index = this.surgicalReport.healthcareProfessionals.findIndex(p => p.profession.type === EProfessionType.SURGEON);
+
 		if (professional && index == -1)
-			this.healthcareProfessionals.push(this.mapToDocumentHealthcareProfessionalDto(professional, type));
+			this.surgicalReport.healthcareProfessionals.push(this.mapToDocumentHealthcareProfessionalDto(professional, EProfessionType.SURGEON));
 
 		if (professional && index != -1)
-			this.healthcareProfessionals.splice(index, 1, this.mapToDocumentHealthcareProfessionalDto(professional, type));
+			this.surgicalReport.healthcareProfessionals.splice(index, 1, this.mapToDocumentHealthcareProfessionalDto(professional, EProfessionType.SURGEON));
 
 		if (!professional && index != -1)
-			this.healthcareProfessionals.splice(index, 1);
-		this.surgicalReport.healthcareProfessionals = this.healthcareProfessionals;
+			this.surgicalReport.healthcareProfessionals.splice(index, 1);
 	}
 
 	isEmpty(): boolean {
-		return !this.surgicalReport.healthcareProfessionals.find(p =>
-			p.type === EProfessionType.SURGEON ||
-			p.type === EProfessionType.SURGEON_ASSISTANT ||
-			p.type === EProfessionType.ANESTHESIOLOGIST ||
-			p.type === EProfessionType.CARDIOLOGIST ||
-			p.type === EProfessionType.SURGICAL_INSTRUMENT_TECHNICIAN ||
-			p.type === EProfessionType.OBSTETRICIAN ||
-			p.type === EProfessionType.PEDIATRICIAN
-		);
+		return !this.surgicalReport.healthcareProfessionals.length;
 	}
 
-	private mapToDocumentHealthcareProfessionalDto(professional: HCEHealthcareProfessionalDto, type: EProfessionType): DocumentHealthcareProfessionalDto {
+	private mapToDocumentHealthcareProfessionalDto(professional: HCEHealthcareProfessionalDto, type: EProfessionType, description?: string): DocumentHealthcareProfessionalDto {
 		return {
 			healthcareProfessional: professional,
-			type: type
+			profession: {
+				type: type,
+				typeDescription: description
+			}
 		}
 	}
 }
