c4073f3d9ae9734199d766c09598eaa3a25bce43
Manuel Gil <manuelgilmoltrasio@gmail.com>
2024-05-17 14:30:57 -0300

    hsi-7855 [FE] Agregar seccion evolucion

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/services/evolution-note-document-summary.service.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/services/evolution-note-document-summary.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ResponseEvolutionNoteDto } from '@api-rest/api-model';
 import { DescriptionItemData } from '@presentation/components/description-item/description-item.component';
 import { DocumentsSummaryMapperService } from './documents-summary-mapper.service';
+import { ClinicalEvaluationData } from '@historia-clinica/utils/document-summary.model';
 
 @Injectable({
     providedIn: 'root'
@@ -16,6 +17,7 @@ export class EvolutionNoteDocumentSummaryService {
         return {
             ...(evolutionNote.mainDiagnosis && { mainDiagnosis: [{ description: this.documentsSummaryService.mapDescriptionAndStatusToString(evolutionNote.mainDiagnosis) }] }),
             ...(evolutionNote.diagnosis.length && { diagnosis: this.documentsSummaryService.mapDiagnosisToDescriptionItemData(evolutionNote.diagnosis)} ),
+            ...(this.documentsSummaryService.hasClinicalEvaluations(evolutionNote.notes) && { clinicalEvaluation: this.documentsSummaryService.mapClinicalEvaluationToDescriptionItemData(evolutionNote.notes)} )
         }
     }
 }
@@ -23,4 +25,5 @@ export class EvolutionNoteDocumentSummaryService {
 export interface EvolutionNoteAsViewFormat {
     mainDiagnosis: DescriptionItemData[],
     diagnosis: DescriptionItemData[],
+    clinicalEvaluation: ClinicalEvaluationData,
 }
