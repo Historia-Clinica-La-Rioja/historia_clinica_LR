9e2aab5118ae65e50d7b43076df2c1ba62de93b9
Manuel Gil <manuelgilmoltrasio@gmail.com>
2024-05-17 14:24:23 -0300

    hsi-7854 [FE] Agregar sección diagnosticos

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/services/evolution-note-document-summary.service.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/services/evolution-note-document-summary.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { ResponseEvolutionNoteDto } from '@api-rest/api-model';
+import { DescriptionItemData } from '@presentation/components/description-item/description-item.component';
+import { DocumentsSummaryMapperService } from './documents-summary-mapper.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class EvolutionNoteDocumentSummaryService {
 
-    constructor() { }
+    constructor(
+        private readonly documentsSummaryService: DocumentsSummaryMapperService,
+    ) { }
 
     mapEvolutionNoteAsViewFormat(evolutionNote: ResponseEvolutionNoteDto): EvolutionNoteAsViewFormat {
         return {
-            
+            ...(evolutionNote.mainDiagnosis && { mainDiagnosis: [{ description: this.documentsSummaryService.mapDescriptionAndStatusToString(evolutionNote.mainDiagnosis) }] }),
+            ...(evolutionNote.diagnosis.length && { diagnosis: this.documentsSummaryService.mapDiagnosisToDescriptionItemData(evolutionNote.diagnosis)} ),
         }
     }
 }
 
 export interface EvolutionNoteAsViewFormat {
-    
+    mainDiagnosis: DescriptionItemData[],
+    diagnosis: DescriptionItemData[],
 }
