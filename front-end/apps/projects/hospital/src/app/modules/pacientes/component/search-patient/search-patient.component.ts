7a9e67bc88dbb3f6f8afcef5e432f13557690e39
nencinosa <ninaencinosacastro@gmail.com>
2024-05-20 13:02:13 -0600

    hsi-7938 Añadir boton "crear nuevo paciente"

--- front-end/apps/projects/hospital/src/app/modules/pacientes/component/search-patient/search-patient.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/pacientes/component/search-patient/search-patient.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-import { BasicPatientDto, GenderDto, IdentificationTypeDto, PersonPhotoDto } from '@api-rest/api-model';
+import { BasicPatientDto, ERole, GenderDto, IdentificationTypeDto, PersonPhotoDto } from '@api-rest/api-model';
 import { PatientService } from '@api-rest/services/patient.service';
 import { PersonMasterDataService } from '@api-rest/services/person-master-data.service';
 import { REMOVE_SUBSTRING_DNI } from '@core/constants/validation-constants';
@@ -9,8 +9,12 @@ import { IDENTIFICATION_TYPE_IDS } from '@core/utils/patient.utils';
 import { PatientBasicData } from '@presentation/utils/patient.utils';
 import { MapperService } from '@presentation/services/mapper.service';
 import { SnackBarService } from '@presentation/services/snack-bar.service';
-import { forkJoin, Observable } from 'rxjs';
+import { catchError, forkJoin, Observable, of, take } from 'rxjs';
+import { Router } from '@angular/router';
+import { ContextService } from '@core/services/context.service';
+import { PermissionsService } from '@core/services/permissions.service';
 
+const ROUTE_SEARCH = 'pacientes/search';
 
 @Component({
 	selector: 'app-search-patient',
@@ -33,13 +37,20 @@ export class SearchPatientComponent implements OnInit {
 	hasError = hasError;
 	getError = getError;
 	isFormSubmitted = false;
+	showAddPatient = false;
+
+	private readonly routePrefix = `institucion/${this.contextService.institutionId}/`;
+
 	constructor(
+		private readonly permissionService: PermissionsService,
 		private readonly personMasterDataService: PersonMasterDataService,
 		private readonly formBuilder: UntypedFormBuilder,
 		private readonly patientService: PatientService,
 		private readonly snackBarService: SnackBarService,
-		private readonly mapperService: MapperService
-	) { }
+		private readonly mapperService: MapperService,
+		private readonly router: Router,
+		private readonly contextService: ContextService
+	) {	}
 
 	ngOnInit(): void {
 		this.genders$ = this.personMasterDataService.getGenders();
@@ -59,6 +70,7 @@ export class SearchPatientComponent implements OnInit {
 
 	search(): void {
 		this.isFormSubmitted = true;
+		this.showAddPatient = false;
 		if (this.formSearch.valid) {
 			this.cardPatient = null;
 			const formSearchValue = this.formSearch.value;
@@ -73,6 +85,7 @@ export class SearchPatientComponent implements OnInit {
 						this.patientSearch(data[0]);
 					} else {
 						this.snackBarService.showError('pacientes.search_patient.PATIENT_NOT_FOUND');
+						this.patientNotFound();
 					}
 				}
 			);
@@ -80,15 +93,35 @@ export class SearchPatientComponent implements OnInit {
 	}
 
 	searchById(): void {
+		this.isFormSubmitted = true;
+		this.showAddPatient = false;
 		if (this.formSearchById.valid) {
 			this.cardPatient = null;
-			this.patientSearch(this.formSearchById.value.id);
+			this.patientService.getPatientBasicData(this.formSearchById.value.id).pipe(
+				catchError(error => {
+					if (error.status === 400) {
+						this.snackBarService.showError('pacientes.search_patient.PATIENT_NOT_FOUND');
+						this.patientNotFound();
+					}
+					return of(null);
+				})
+			).subscribe(
+				(data: BasicPatientDto) => {
+					if (data) {
+						this.patientSearch(this.formSearchById.value.id);
+					} else {
+						this.snackBarService.showError('pacientes.search_patient.PATIENT_NOT_FOUND');
+						this.patientNotFound();
+					}
+				}
+			);
 		}
 	}
 
 	clearResults(): void {
 		this.cardPatient = null;
 		this.isFormSubmitted = false;
+		this.showAddPatient = false;
 		this.onSelectedPatient.emit(null);
 	}
 
@@ -97,7 +130,7 @@ export class SearchPatientComponent implements OnInit {
 	}
 
 	private patientSearch(patientId: number) {
-
+		this.showAddPatient = false;
 		const basicPatientDto$: Observable<BasicPatientDto> = this.patientService.getPatientBasicData(patientId);
 		const photo$: Observable<PersonPhotoDto> = this.patientService.getPatientPhoto(patientId);
 
@@ -114,6 +147,26 @@ export class SearchPatientComponent implements OnInit {
 				};
 			}, _ => {
 				this.snackBarService.showError('pacientes.search_patient.PATIENT_NOT_FOUND');
+				this.patientNotFound();
+			});
+	}
+
+	private patientNotFound(){
+		this.permissionService.hasContextAssignments$([ERole.ADMINISTRATIVO])
+			.pipe(take(1)).subscribe(hasRole => {
+				this.showAddPatient = hasRole;
+			});
+	}
+
+	goCreatePatient() {
+		this.router.navigate([this.routePrefix + ROUTE_SEARCH],
+			{
+				queryParams: {
+					fromGuardModule: true,
+					identificationTypeId: this.formSearch.value.identificationType,
+					identificationNumber: this.formSearch.value.identificationNumber,
+					genderId: this.formSearch.value.gender
+				}
 			});
 	}
 }
