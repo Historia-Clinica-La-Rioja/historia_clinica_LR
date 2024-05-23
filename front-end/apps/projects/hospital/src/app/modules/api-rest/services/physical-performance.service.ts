d19fa5fbcff6bbf652d0a227a6db563cd73b44af
RodrigoCba96 <rodrigocordoba2296@gmail.com>
2024-05-22 22:35:41 -0300

    added new .service for desempeno fisico and aded new summaryPhysical in api model

--- front-end/apps/projects/hospital/src/app/modules/api-rest/services/physical-performance.service.ts
+++ front-end/apps/projects/hospital/src/app/modules/api-rest/services/physical-performance.service.ts
@@ -1,38 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { FrailAnswers, FrailSummary } from '@api-rest/api-model'; 
+import {PhysicalSummary, QuestionnairesResponses } from '@api-rest/api-model';
 import { environment } from '@environments/environment';
 import { ContextService } from '@core/services/context.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PhysicalPerformanceService {  
+export class PhysicalPerformanceService {
 
   constructor(
     private contextService: ContextService,
-    private readonly http: HttpClient  ) {}
+    private readonly http: HttpClient) { }
+
+
+  physicalPerformancePdf(questionnaireId: number): Observable<Blob> {
+    const url = `${environment.apiBase}/institution/${this.contextService.institutionId}/questionnaire/${questionnaireId}/get-pdf`;
+    return this.http.get(url, { responseType: 'blob' });
+
+  }
+  
+  getPhysicalSummary( questionnaireResponseId: any): Observable<PhysicalSummary> {
+    const url = `${environment.apiBase}/institution/${this.contextService.institutionId}/questionnaire/${questionnaireResponseId}/answers `;
+    return this.http.get<PhysicalSummary>(url);
+  }
+
+  getAllByPatientId(patientId: number): Observable<QuestionnairesResponses[]> {
+    const url = `${environment.apiBase}/institution/${this.contextService.institutionId}/patient/${patientId}/all-questionnaire-responses`;
+    return this.http.get<QuestionnairesResponses[]>(url);
+  }
 
   createPhysical(patientId: number, physicalData: any): Observable<boolean> {
-    const url = `${environment.apiBase}/institution/${this.contextService.institutionId}/patient/${patientId}/questionnaire/create`; 
+    const url = `${environment.apiBase}/institution/${this.contextService.institutionId}/patient/${patientId}/questionnaire/create`;
     return this.http.post<boolean>(url, physicalData);
-  }
 
-  getFrailSummary(institutionId: number, patientId: number, questionnaireId: number): Observable<FrailSummary> {
-    const url = `${environment.apiBase}/institution/${institutionId}/patient/${patientId}/hce/general-state/summary/frail/${questionnaireId}`;
-    return this.http.get<FrailSummary>(url);
   }
-
-  getAllByPatientId(institutionId: number, patientId: number): Observable<FrailAnswers[]> {
-    const url = `${environment.apiBase}/institution/${institutionId}/patient/${patientId}/hce/general-state/frail`; 
-    return this.http.get<FrailAnswers[]>(url);
-  }
-
-  getPdf(questionnaireId: number): Observable<Blob> {
-    const url = `${environment.apiBase}/institution/patient/outpatient/consultation/frail/${questionnaireId}/pdf-download`; 
-    return this.http.get(url, { responseType: 'blob' });
-    
-  }
-
 }
