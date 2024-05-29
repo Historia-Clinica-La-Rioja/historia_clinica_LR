caec8c8418e410ce8a67c117ad594b57849d07af
Cristian Quevedo <cristian.quevedox@gmail.com>
2024-05-29 01:00:42 -0300

    feat(get-frail): add alert component in get-frail component

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/frail-scale/get-frail/get-frail.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/frail-scale/get-frail/get-frail.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FrailService } from '@api-rest/services/fragility-test.service';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { QuestionnairesResponses } from '@api-rest/api-model';
+import { AlertDialogComponent } from '../../alert-dialog/alert-dialog.component';
 
 @Component({
   selector: 'app-get-frail',
@@ -19,6 +20,7 @@ export class GetFrailComponent implements OnInit {
   constructor(
     private frailService: FrailService,
     @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialog: MatDialog
   ) {
 
     this.patientId = data.patientId
@@ -53,6 +55,7 @@ export class GetFrailComponent implements OnInit {
       this.lastFrailQuestionnaireId = frailQuestionnaires[0].id;
     } else {
       console.warn('No frail questionnaires found for this patient')
+      this.showAlert('No hay cuestionarios disponibles para este paciente.')
     }
   }
 
@@ -67,10 +70,19 @@ export class GetFrailComponent implements OnInit {
         },
         (error) => {
           console.error('Error downloading the questionnaire PDF:', error);
+          this.showAlert('Error al descargar el PDF del cuestionario.');
         }
       );
     } else {
       console.warn('No Frail questionnaires available to download PDF.')
+      this.showAlert('No hay cuestionarios disponibles para descargar.')
     }
   }
+
+  showAlert(message: string): void {
+    this.dialog.open(AlertDialogComponent, {
+      data: { message: message },
+    });
+  }
+
 }
