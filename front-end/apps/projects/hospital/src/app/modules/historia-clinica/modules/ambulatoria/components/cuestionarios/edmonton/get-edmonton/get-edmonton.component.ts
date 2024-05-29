cd42d9f3d32a5b51a27d2e5cd6d0c83d193da484
Cristian Quevedo <cristian.quevedox@gmail.com>
2024-05-29 01:22:17 -0300

    feat(get-edmonton): add alert component in get-edmonton component

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/edmonton/get-edmonton/get-edmonton.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/ambulatoria/components/cuestionarios/edmonton/get-edmonton/get-edmonton.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { EdmontonService } from '@api-rest/services/edmonton.service';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { QuestionnairesResponses } from '@api-rest/api-model';
+import { AlertDialogComponent } from '../../alert-dialog/alert-dialog.component';
 
 @Component({
   selector: 'app-get-edmonton',
@@ -18,6 +19,7 @@ export class GetEdmontonComponent implements OnInit {
   constructor(
     private EdmontonService: EdmontonService,
     @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialog: MatDialog
   ) {
 
     this.patientId = data.patientId
@@ -52,6 +54,7 @@ export class GetEdmontonComponent implements OnInit {
       this.lastEdmontonQuestionnaireId = edmontonQuestionnaires[0].id;
     } else {
       console.warn('No frail questionnaires found for this patient')
+      this.showAlert('No hay cuestionarios disponibles para este paciente.')
     }
   }
 
@@ -66,10 +69,19 @@ export class GetEdmontonComponent implements OnInit {
         },
         (error) => {
           console.error('Error downloading the questionnaire PDF:', error);
+          this.showAlert('Error al descargar el PDF del cuestionario.');
         }
       );
     } else {
       console.warn('No Frail questionnaires available to download PDF.')
+      this.showAlert('No hay cuestionarios disponibles para descargar.')
     }
   }
+
+  showAlert(message: string): void {
+    this.dialog.open(AlertDialogComponent, {
+      data: { message: message },
+    });
+  }
+
 }
