082c0211d05f1c55893315821794016603171d50
Federico Pinel <federicopinel12@gmail.com>
2024-05-30 17:01:06 -0300

    hsi-7415 [FE] Boton seguir editando en historico evoluciones

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/anesthetic-report-document-summary/anesthetic-report-document-summary.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/anesthetic-report-document-summary/anesthetic-report-document-summary.component.ts
@@ -1,35 +1,41 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { DocumentSearchDto } from '@api-rest/api-model';
 import { AnesthethicReportService } from '@api-rest/services/anesthethic-report.service';
 import { DocumentsSummaryService } from '@api-rest/services/documents-summary.service';
 import { DocumentSearch } from '@historia-clinica/modules/ambulatoria/modules/internacion/services/document-actions.service';
+import { DocumentActionsService } from '@historia-clinica/modules/ambulatoria/modules/internacion/services/document-actions.service';
 import { AnestheticReportDocumentSummaryService, AnestheticReportViewFormat } from '@historia-clinica/services/anesthetic-report-document-summary.service';
 import { DocumentsSummaryMapperService } from '@historia-clinica/services/documents-summary-mapper.service';
 import { HeaderDescription } from '@historia-clinica/utils/document-summary.model';
 import { Observable, forkJoin, map } from 'rxjs';
 
 @Component({
-    selector: 'app-anesthetic-report-document-summary',
-    templateUrl: './anesthetic-report-document-summary.component.html',
-    styleUrls: ['./anesthetic-report-document-summary.component.scss']
+	selector: 'app-anesthetic-report-document-summary',
+	templateUrl: './anesthetic-report-document-summary.component.html',
+	styleUrls: ['./anesthetic-report-document-summary.component.scss']
 })
 export class AnestheticReportDocumentSummaryComponent implements OnInit {
     @Input() isPopUpOpen: boolean;
     @Input() internmentEpisodeId: number;
     @Input() set activeDocument (activeDocument: DocumentSearch) {
         this._activeDocument = activeDocument;
+		this.isDraft = !this._activeDocument.document.confirmed
         this.fetchSummaryInfo();
     };
     @Output() resetActiveDocument = new EventEmitter<boolean>();
 
     _activeDocument: DocumentSearch;
     documentSummary$: Observable<{headerDescription: HeaderDescription, anestheticReport: AnestheticReportViewFormat}>;
+	isDraft = false
+	activeDocumentDraft: DocumentSearchDto;
 
-    constructor( 
-        private readonly anestheticReportService: AnesthethicReportService,
-        private readonly anestheticReportDocumentSummaryService: AnestheticReportDocumentSummaryService,
+	constructor(
+		private readonly anestheticReportService: AnesthethicReportService,
+		private readonly anestheticReportDocumentSummaryService: AnestheticReportDocumentSummaryService,
+		private readonly documentActions: DocumentActionsService,
         private readonly documentSummaryService: DocumentsSummaryService,
         private readonly documentSummaryMapperService: DocumentsSummaryMapperService,
-     ) { }
+	) { }
 
     ngOnInit(): void {
         this.fetchSummaryInfo();
@@ -47,4 +53,8 @@ export class AnestheticReportDocumentSummaryComponent implements OnInit {
             }}));
         }
     }
+
+	openEditDraft() {
+		this.documentActions.editAnestheticPartDraft(this._activeDocument.document)
+	}
 }
