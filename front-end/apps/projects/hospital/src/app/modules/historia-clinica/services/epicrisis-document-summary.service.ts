3c7561a2d35a8ed096b8ebeeed0b4fea8b92464e
Manuel Gil <manuelgilmoltrasio@gmail.com>
2024-05-30 12:03:35 -0300

    hsi-8284 [FE] Agregar seccion diagnostico de epicrisis

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/services/epicrisis-document-summary.service.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/services/epicrisis-document-summary.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ResponseEpicrisisDto } from '@api-rest/api-model';
+import { DescriptionItemData } from '@presentation/components/description-item/description-item.component';
+import { DocumentsSummaryMapperService } from './documents-summary-mapper.service';
 
 @Injectable({
     providedIn: 'root'
@@ -7,16 +9,18 @@ import { ResponseEpicrisisDto } from '@api-rest/api-model';
 export class EpicrisisDocumentSummaryService {
 
     constructor(
-        
+        private readonly documentsSummaryService: DocumentsSummaryMapperService,
     ) { }
 
     mapEpicrisisAsViewFormat(epicrisis: ResponseEpicrisisDto): EpicrisisViewFormat {
         return {
-            
+            ...(epicrisis.mainDiagnosis && { mainDiagnosis: [{ description: this.documentsSummaryService.mapDescriptionAndStatusToString(epicrisis.mainDiagnosis) }] }),
+            ...(epicrisis.diagnosis.length && { diagnosis: this.documentsSummaryService.mapDiagnosisToDescriptionItemData(epicrisis.diagnosis)} ),
         }
 }
 }
 
 export interface EpicrisisViewFormat {
-
+    mainDiagnosis: DescriptionItemData[],
+    diagnosis: DescriptionItemData[],
 }
