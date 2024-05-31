2bfed6466813c63a3439427d3f3694323e133cc0
Federico de Muguruza <fedemuguruza@gmail.com>
2024-05-31 16:47:50 -0300

    hsi-8365 [FE] Cambiar estilo del punto en el mapa

--- front-end/apps/projects/hospital/src/app/modules/gis/services/gis-layers.service.ts
+++ front-end/apps/projects/hospital/src/app/modules/gis/services/gis-layers.service.ts
@@ -8,6 +8,10 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import { GlobalCoordinatesDto } from '@api-rest/api-model';
 import { fromLonLat } from 'ol/proj';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+
+const LOCATION_POINT = '../../../../assets/icons/gis_location_point.svg';
 
 @Injectable({
   	providedIn: 'root'
@@ -38,14 +42,23 @@ export class GisLayersService {
 		this.map.addLayer(this.vectorLayer);
 	}
 
-	createPoint = (coords: number[]): Feature => {
-		return new Feature({
+	createLocationPoint = (coords: number[]): Feature => {
+		let feature = new Feature({
 			geometry: new Point(coords)
 		});
+		const markerStyle = new Style({
+			image: new Icon({
+				anchor: [0.5, 1],
+				src: LOCATION_POINT,
+				scale: 0.5
+			})
+		});
+		feature.setStyle(markerStyle);
+		return feature;
 	}
 
 	addPoint = (position: number[]) => {
-		this.locationPoint = this.createPoint(position);
+		this.locationPoint = this.createLocationPoint(position);
 		this.vectorLayer?.getSource().addFeature(this.locationPoint);
 	}
 
