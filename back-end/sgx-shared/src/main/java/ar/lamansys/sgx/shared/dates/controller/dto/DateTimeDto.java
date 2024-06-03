a97d208b3ae533e44d20870ce35a67cdbed0a95a
larav <lvenere@lamansys.com.ar>
2024-06-03 14:17:00 -0300

    HSI-7485 Ordenar problemas de guardia e internación por fecha en historia clinica

--- back-end/sgx-shared/src/main/java/ar/lamansys/sgx/shared/dates/controller/dto/DateTimeDto.java
+++ back-end/sgx-shared/src/main/java/ar/lamansys/sgx/shared/dates/controller/dto/DateTimeDto.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 public class DateTimeDto implements Comparable<DateTimeDto>{
 
@@ -20,32 +22,31 @@ public class DateTimeDto implements Comparable<DateTimeDto>{
 
 	@Override
 	public int compareTo(DateTimeDto dateTimeDto) {
-		Integer dateComparisonResult = dateComparator(dateTimeDto.date);
-		Integer timeComparisonResult = timeComparator(dateTimeDto.time);
-		if (dateComparisonResult.equals(1) || dateComparisonResult.equals(-1))
+		int dateComparisonResult = dateComparator(dateTimeDto.date);
+		if (dateComparisonResult != 0) {
 			return dateComparisonResult;
-		if (timeComparisonResult.equals(1) || timeComparisonResult.equals(-1))
-			return timeComparisonResult;
-		return dateComparisonResult;
+		}
+		return timeComparator(dateTimeDto.time);
 	}
 
-	private Integer dateComparator(DateDto date) {
-		if (this.date.getYear() < date.getYear()) return -1;
-		if (this.date.getMonth() < date.getMonth()) return -1;
-		if (this.date.getDay() < date.getDay()) return -1;
-		if (this.date.getYear() > date.getYear()) return 1;
-		if (this.date.getMonth() > date.getMonth()) return 1;
-		if (this.date.getDay() > date.getDay()) return 1;
-		return 0;
+	private int dateComparator(DateDto date) {
+		if (!Objects.equals(this.date.getYear(), date.getYear())) {
+			return Integer.compare(this.date.getYear(), date.getYear());
+		} else if (!Objects.equals(this.date.getMonth(), date.getMonth())) {
+			return Integer.compare(this.date.getMonth(), date.getMonth());
+		} else {
+			return Integer.compare(this.date.getDay(), date.getDay());
+		}
 	}
 
-	private Integer timeComparator(TimeDto time) {
-		if (this.time.getHours() < time.getHours()) return -1;
-		if (this.time.getMinutes() < time.getMinutes()) return -1;
-		if (this.time.getSeconds() < time.getSeconds()) return -1;
-		if (this.time.getHours() > time.getHours()) return 1;
-		if (this.time.getMinutes() > time.getMinutes()) return 1;
-		if (this.time.getSeconds() > time.getSeconds()) return 1;
+	private int timeComparator(TimeDto time) {
+		if (!Objects.equals(this.time.getHours(), time.getHours())) {
+			return Integer.compare(this.time.getHours(), time.getHours());
+		} else if (!Objects.equals(this.time.getMinutes(), time.getMinutes())) {
+			return Integer.compare(this.time.getMinutes(), time.getMinutes());
+		} else if (this.time.getSeconds() != null) {
+			return Integer.compare(this.time.getSeconds(), time.getSeconds());
+		}
 		return 0;
 	}
 
