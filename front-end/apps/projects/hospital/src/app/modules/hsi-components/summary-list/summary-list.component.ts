d7c62695fe23a4e73d3f7cad3f445326b1045aa5
jirimoldi <juanirimoldi@gmail.com>
2024-06-05 13:40:42 -0300

    hsi-8191 [FE] Crear componente SummaryList

---
+++ front-end/apps/projects/hospital/src/app/modules/hsi-components/summary-list/summary-list.component.ts
@@ -0,0 +1,66 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PresentationModule } from '@presentation/presentation.module';
+import { SummaryMultipleSignComponent, SummaryMultipleSignData } from '../summary-multiple-sign/summary-multiple-sign.component';
+
+const INPUT_NODE_NAME: string = "INPUT";
+
+@Component({
+	selector: 'app-summary-list',
+	templateUrl: './summary-list.component.html',
+	styleUrls: ['./summary-list.component.scss'],
+	standalone: true,
+	imports: [PresentationModule, SummaryMultipleSignComponent]
+})
+
+export class SummaryListComponent {
+
+	@Input() dataList: SummaryItem[];
+	@Input() noDataMessage: string = 'presentation.selectable-card.NO_DATA'
+	@Input() enableCheckboxes: boolean = true;
+
+	@Output() checkedIds = new EventEmitter<number[]>();
+	@Output() selectedId = new EventEmitter<number>();
+
+	selectedIds: number[] = [];
+	isAllSelected: boolean = false;
+
+	constructor() {	}
+
+	selectAll(): void {
+		if (this.isAllSelected) {
+			this.selectedIds = [];
+			this.toggleAllCheckboxes(false);
+		} else {
+			this.selectedIds = this.dataList.map((data) => data.id);
+			this.toggleAllCheckboxes(true);
+		}
+		this.isAllSelected = !this.isAllSelected;
+		this.checkedIds.emit(this.selectedIds);
+	}
+
+	private toggleAllCheckboxes(value: boolean) {
+		this.dataList.forEach((item: SummaryItem) => item.checked = value);
+	}
+
+	checkboxSelect(id: number): void {
+		(!this.selectedIds.includes(id))
+			? this.selectedIds.push(id)
+			: this.selectedIds.splice(this.selectedIds.indexOf(id), 1);
+
+		const item: SummaryItem = this.dataList.find(item => item.id === id);
+		item.checked = !item.checked;
+		this.checkedIds.emit(this.selectedIds);
+	}
+
+	seeDetails(id: number, event: any): void {
+		(event.target.nodeName === INPUT_NODE_NAME)
+			? event.stopPropagation()
+			: this.selectedId.emit(id);
+	}
+}
+
+export interface SummaryItem {
+	id: number,
+	checked?: boolean
+	data?: SummaryMultipleSignData;
+}
