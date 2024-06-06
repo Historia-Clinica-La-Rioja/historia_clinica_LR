7ac7695a8ec03af1e91fd326fdbf6472f990e9fc
mmalyvarni <mateomv.mmv@gmail.com>
2024-06-06 10:18:28 -0300

    hsi-8587 [BE] Tabla y entidad para guardado de polígonos de área de responsabilidad sanitaria

---
+++ back-end/hospital-api/src/main/java/net/pladema/establishment/sanitaryresponsibilityarea/institutionresponsibilityarea/infrastructure/output/repository/InstitutionResponsibilityAreaRepository.java
@@ -0,0 +1,12 @@
+package net.pladema.establishment.sanitaryresponsibilityarea.institutionresponsibilityarea.infrastructure.output.repository;
+
+import net.pladema.establishment.sanitaryresponsibilityarea.institutionresponsibilityarea.infrastructure.output.repository.entity.InstitutionResponsibilityArea;
+
+import net.pladema.establishment.sanitaryresponsibilityarea.institutionresponsibilityarea.infrastructure.output.repository.entity.InstitutionResponsibilityAreaPK;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InstitutionResponsibilityAreaRepository extends JpaRepository<InstitutionResponsibilityArea, InstitutionResponsibilityAreaPK> {
+}
