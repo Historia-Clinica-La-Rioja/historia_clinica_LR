6cbdbd08bf84528ee79f247d9e7421ab79812fbd
Sebastian Perruolo <sebastianperruolo@gmail.com>
2024-06-12 10:59:50 -0300

    hsi-8745 Rol API_IMAGENES accede a backoffice de Red de Imágenes

--- front-end/backoffice/src/modules/imageNetwork/allmovestudies/index.ts
+++ front-end/backoffice/src/modules/imageNetwork/allmovestudies/index.ts
@@ -1,16 +1,17 @@
 import SGXPermissions from '../../../libs/sgx/auth/SGXPermissions';
+
 import {
-    ROOT,
-    ADMINISTRADOR,
-} from '../../roles';
+    BASIC_RDI_ROLES,
+} from '../roles';
+
 import AllMoveStudiesShow from './AllMoveStudiesShow';
 import AllMoveStudiesList from './AllMoveStudiesList';
 import AllMoveStudiesEdit from './AllMoveStudiesEdit';
 
 const allMoveStudies = (permissions: SGXPermissions) => ({
-    show: permissions.hasAnyAssignment(ROOT,ADMINISTRADOR) ?AllMoveStudiesShow: undefined,
-    list: permissions.hasAnyAssignment(ROOT,ADMINISTRADOR) ? AllMoveStudiesList: undefined,
-    edit: permissions.hasAnyAssignment(ROOT,ADMINISTRADOR) ?AllMoveStudiesEdit: undefined,
+    show: permissions.hasAnyAssignment(...BASIC_RDI_ROLES) ?AllMoveStudiesShow: undefined,
+    list: permissions.hasAnyAssignment(...BASIC_RDI_ROLES) ? AllMoveStudiesList: undefined,
+    edit: permissions.hasAnyAssignment(...BASIC_RDI_ROLES) ?AllMoveStudiesEdit: undefined,
     options: {
         submenu: 'imageNetwork'
     }
--- front-end/backoffice/src/modules/imageNetwork/globalpacservers/index.ts
+++ front-end/backoffice/src/modules/imageNetwork/globalpacservers/index.ts
@@ -1,20 +1,19 @@
 import RouterIcon from '@material-ui/icons/Router';
 import SGXPermissions from '../../../libs/sgx/auth/SGXPermissions';
 import {
-    ROOT,
-} from '../../roles';
+    BASIC_RDI_ROLES,
+} from '../roles';
 import GlobalPacsList from './GlobalPacsList';
 import GlobalPacsCreate from './GlobalPacsCreate';
 import GlobalPacsShow from './GlobalPacsShow';
 import GlobalPacsEdit from './GlobalPacsEdit';
 
-
 const globalpacs = (permissions: SGXPermissions) => ({
     icon: RouterIcon,
-    list: permissions.hasAnyAssignment(ROOT) ? GlobalPacsList : undefined,
-    show: permissions.hasAnyAssignment(ROOT) ? GlobalPacsShow : undefined,
-    create: permissions.hasAnyAssignment(ROOT) ? GlobalPacsCreate : undefined,
-    edit: permissions.hasAnyAssignment(ROOT) ? GlobalPacsEdit : undefined,
+    list: permissions.hasAnyAssignment(...BASIC_RDI_ROLES) ? GlobalPacsList : undefined,
+    show: permissions.hasAnyAssignment(...BASIC_RDI_ROLES) ? GlobalPacsShow : undefined,
+    create: permissions.hasAnyAssignment(...BASIC_RDI_ROLES) ? GlobalPacsCreate : undefined,
+    edit: permissions.hasAnyAssignment(...BASIC_RDI_ROLES) ? GlobalPacsEdit : undefined,
     options: {
         submenu: 'imageNetwork'
     }
