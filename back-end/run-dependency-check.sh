a4e7391219f6783092fd93bea09b73f919f3577b
gcapozzo <gianlucagabrielcapozzo@gmail.com>
2024-07-08 17:38:46 -0300

    DEVOPS-226 dependency-check

---
+++ back-end/run-dependency-check.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -o errexit
+
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR"
+
+# MVN_CACHE can be set as Environment Variable
+# Defaults to back-end/.maven (added to gitignore)
+[ -z "$MVN_CACHE" ] && MVN_CACHE=$(pwd)/.maven
+
+echo "MVN_CACHE set as ${MVN_CACHE}"
+
+export MAVEN_OPTS="$MAVEN_OPTS -Dmaven.repo.local=${MVN_CACHE}"
+
+echo "MAVEN_OPTS set as '${MAVEN_OPTS}'"
+
+start_all=$(date +%s)
+
+echo "Dependency check start"
+
+mvn org.owasp:dependency-check-maven:10.0.2:aggregate -Dformat=GITLAB -DnvdApiKey="$NVD_API_KEY"
+end_all=$(date +%s)
+
+echo "Elapsed Time for dependency check: ($end_all-$start_all) seconds"
+
+cp target/dependency-check-gitlab.json target/dependency-check-report.json
