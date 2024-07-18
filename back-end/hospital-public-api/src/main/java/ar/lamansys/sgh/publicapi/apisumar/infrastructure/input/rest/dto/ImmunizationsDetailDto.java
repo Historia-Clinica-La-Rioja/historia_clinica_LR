2de6c6df37bd2f05257ec8248cba4a01c0c50e77
Cristian Quevedo <cristian.quevedox@gmail.com>
2024-07-18 09:30:50 -0300

    feat(sumar-api): create ImmunizationsDetailBo and ImmunizationsDetailDto classes

---
+++ back-end/hospital-public-api/src/main/java/ar/lamansys/sgh/publicapi/apisumar/infrastructure/input/rest/dto/ImmunizationsDetailDto.java
@@ -0,0 +1,41 @@
+package ar.lamansys.sgh.publicapi.apisumar.infrastructure.input.rest.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@ToString
+@Builder
+@Getter
+public class ImmunizationsDetailDto {
+	String institution;
+	String operativeUnit;
+	String lender;
+	String lenderIdentificationNumber;
+	Timestamp attentionDate;
+	String patientIdentificationNumber;
+	String patientName;
+	String patientSex;
+	String patientGender;
+	String patientSelfPerceivedName;
+	Date patientBirthDate;
+	String patientAgeTurn;
+	String patientAge;
+	String ethnicity;
+	String medicalCoverage;
+	String address;
+	String location;
+	String instructionLevel;
+	String workSituation;
+	String vaccine;
+	String dosage;
+	String lotNumber;
+	String note;
+	String scheme;
+	String vaccineScheme;
+	String applicationCondition;
+	String evolution;
+}
