fda62313f280afcb2e8c70299801ffd1290ef8be
Cristian Quevedo <cristian.quevedox@gmail.com>
2024-07-18 09:44:57 -0300

    feat(api-sumar): add getImmunizations method in ConsultationDetailDataStorage interface

--- back-end/hospital-public-api/src/main/java/ar/lamansys/sgh/publicapi/apisumar/application/port/out/ConsultationDetailDataStorage.java
+++ back-end/hospital-public-api/src/main/java/ar/lamansys/sgh/publicapi/apisumar/application/port/out/ConsultationDetailDataStorage.java
@@ -1,6 +1,7 @@
 package ar.lamansys.sgh.publicapi.apisumar.application.port.out;
 
 import ar.lamansys.sgh.publicapi.apisumar.domain.ConsultationDetailDataBo;
+import ar.lamansys.sgh.publicapi.apisumar.domain.ImmunizationsDetailBo;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -8,5 +9,7 @@ import java.util.List;
 public interface ConsultationDetailDataStorage {
 
 	List<ConsultationDetailDataBo> getConsultationsData(String sisaCode, LocalDateTime startDate, LocalDateTime endDate);
+
+	List<ImmunizationsDetailBo> getImmunizations(String sisaCode, LocalDateTime startDate, LocalDateTime endDate);
 
 }
