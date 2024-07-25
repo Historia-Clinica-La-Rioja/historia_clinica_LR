a02bdd61ed2d70d413fe688595065283acdd388a
FacundoBarreiros <facundobarreiros@gmail.com>
2024-07-25 08:29:37 -0300

    hsi-9450 [FE] Cambios en el componente de agregar procedimientos para soportar la opcion de completar un template asociado a un estudio

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/dialogs/add-procedure/add-procedure.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/dialogs/add-procedure/add-procedure.component.ts
@@ -1,10 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatRadioChange } from '@angular/material/radio';
 import { ServiceRequestCategoryDto, SnomedDto } from '@api-rest/api-model';
 import { RequestMasterDataService } from '@api-rest/services/request-masterdata.service';
-import { STUDY_STATUS } from '@historia-clinica/modules/ambulatoria/constants/prescripciones-masterdata';
-import { CreateOrderService } from '@historia-clinica/services/create-order.service';
+import { STUDY_STATUS_ENUM } from '@historia-clinica/modules/ambulatoria/constants/prescripciones-masterdata';
+import { COMLETE_NOW, CreateOrderService } from '@historia-clinica/services/create-order.service';
 
 @Component({
 	selector: 'app-add-procedure',
@@ -14,19 +15,34 @@ import { CreateOrderService } from '@historia-clinica/services/create-order.serv
 
 export class AddProcedureComponent implements OnInit {
 	studyCategoryOptions = [];
-	study_status = STUDY_STATUS;
+	study_status = STUDY_STATUS_ENUM;
+	completeNow = COMLETE_NOW;
+	radioControl : FormGroup<RadioControl>;
+	isLinear = false;
 
 	constructor(
 		public dialogRef: MatDialogRef<AddProcedureComponent>,
 		@Inject(MAT_DIALOG_DATA) public readonly data: CreateOrder,
 		private requestMasterDataService: RequestMasterDataService,
 
-	) { }
+	) {
+		this.radioControl = new FormGroup<RadioControl>({
+			controlButton: new FormControl<STUDY_STATUS_ENUM | null>(null)});
+
+	 }
 
 	ngOnInit() {
 		this.requestMasterDataService.categoriesWithoutDiagnosticImaging().subscribe((categories: ServiceRequestCategoryDto[]) => {
 			this.studyCategoryOptions = categories;
 		});
+
+		this.radioControl.controls.controlButton.setValue(this.study_status.FINAL);
+		this.data.createOrderService.setCreationStatus(this.study_status.FINAL);
+
+		this.data.createOrderService.hasTemplate$.subscribe(_ => {
+			this.radioControl.controls.controlButton.setValue(this.study_status.FINAL);
+			this.data.createOrderService.setCreationStatus(this.study_status.FINAL);
+		})
 	}
 
 	close() {
@@ -49,4 +65,8 @@ export interface CreateOrder {
 	patientId: number,
 	createOrderService: CreateOrderService,
 	problems: SnomedDto[]
+}
+
+interface RadioControl {
+	controlButton: FormControl<STUDY_STATUS_ENUM | null>;
 }
