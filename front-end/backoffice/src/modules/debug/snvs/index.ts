36eb0141bf9ea06f24eb0dd72f23442b030195ae
Federico Simaro <fede.simaro@gmail.com>
2024-07-30 13:56:43 -0300

    hsi-10055 [BE] Incluir listado de snvs para admin institucional

--- front-end/backoffice/src/modules/debug/snvs/index.ts
+++ front-end/backoffice/src/modules/debug/snvs/index.ts
@@ -1,6 +1,6 @@
 import SGXPermissions from '../../../libs/sgx/auth/SGXPermissions';
 import {
-    BASIC_BO_ROLES,
+    ADMIN_ROLES,
 } from '../../roles-set';
 
 import SnvsShow from './SnvsShow';
@@ -8,7 +8,7 @@ import SnvsList from './SnvsList';
 
 const snvs = (permissions: SGXPermissions) => ({
     show: SnvsShow,
-    list: permissions.isOn('HABILITAR_REPORTE_EPIDEMIOLOGICO') && permissions.hasAnyAssignment(...BASIC_BO_ROLES) ? SnvsList : undefined,
+    list: permissions.isOn('HABILITAR_REPORTE_EPIDEMIOLOGICO') && permissions.hasAnyAssignment(...ADMIN_ROLES) ? SnvsList : undefined,
     options: {
          submenu: 'debug'
     }
