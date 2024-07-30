f417e2a3b835fd7dbbf491a65df03d1836a44c2c
Manuel Gil <manuelgilmoltrasio@gmail.com>
2024-07-30 12:46:15 -0300

    hsi-10050 Refactor en dialogo study-status-popup

--- front-end/apps/projects/hospital/src/app/modules/image-network/dialogs/study-status-popup/study-status-popup.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/image-network/dialogs/study-status-popup/study-status-popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -6,25 +6,18 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 	templateUrl: './study-status-popup.component.html',
 	styleUrls: ['./study-status-popup.component.scss']
 })
-export class StudyStatusPopupComponent implements OnInit {
+export class StudyStatusPopupComponent {
 
 	constructor(
 		@Inject(MAT_DIALOG_DATA) public readonly data: {
-			icon: string, //material icon name
-			iconColor: string, //icon color - defined on clases on scss file
-			popUpMessage?: string, //A complementary message - not translate route
-			popUpMessageTranslate: string, //The translate route for the message
-			acceptBtn?: boolean  //If you want the 'Accept' button to be visible
-			iconCircle?: boolean  //If the material icon doesnt have a circle around and you need one
+			icon: string,
+			iconColor: string,
+			popUpMessage?: string,
+			popUpMessageTranslate: string,
+			acceptBtn?: boolean
+			iconCircle?: boolean
 		},
 		public dialogRef: MatDialogRef<StudyStatusPopupComponent>
 	) {
-	}
-
-	ngOnInit(): void {
-	}
-
-	closeDialog() {
-		this.dialogRef.close()
 	}
 }
