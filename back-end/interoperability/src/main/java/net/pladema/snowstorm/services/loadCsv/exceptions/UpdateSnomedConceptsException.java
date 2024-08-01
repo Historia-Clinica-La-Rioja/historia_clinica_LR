320b14a4b7cfba90bd97a0aa9c153dc418ce38ad
Maximiliano Romero Piaggio <mromeropiaggio@lamansys.com.ar>
2024-08-01 18:01:24 -0300

    hsi-9893 [FIX] Hospital Bonaparte - No se visualizan ciertos conceptos snomed (sinónimos)

--- back-end/interoperability/src/main/java/net/pladema/snowstorm/services/loadCsv/exceptions/UpdateSnomedConceptsException.java
+++ back-end/interoperability/src/main/java/net/pladema/snowstorm/services/loadCsv/exceptions/UpdateSnomedConceptsException.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 @Getter
 public class UpdateSnomedConceptsException extends RuntimeException {
 
-	private final UpdateSnomedConceptsExceptionEnum code;
+	private final EUpdateSnomedConceptsException code;
 
-	public UpdateSnomedConceptsException(UpdateSnomedConceptsExceptionEnum code, String message) {
+	public UpdateSnomedConceptsException(EUpdateSnomedConceptsException code, String message) {
 		super(message);
 		this.code = code;
 	}
