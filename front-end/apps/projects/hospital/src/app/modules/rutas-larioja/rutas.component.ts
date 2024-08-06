09d509257088c9fcee0d083ecfd15b7ef806c241
Macarena Martín <macarenamartin1111@gmail.com>
2024-08-05 23:22:57 -0300

    added 'Odontología' item to the list of assistencial routes

--- front-end/apps/projects/hospital/src/app/modules/rutas-larioja/rutas.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/rutas-larioja/rutas.component.ts
@@ -119,6 +119,17 @@ const ELEMENT_DATA: RutasPeriodic[] = [
     routerLink: '../../../assets/rutas-lr/', 
     pdfDT: null,
   },
+  {
+    Miniature1: IMAGE_ITEM + "DR-O.png",
+    Miniature2: IMAGE_ITEM + "DT-O.png",
+    IconDR: ICON + "DR-active-icon.png",
+    IconDT: ICON + "DT-active-icon.png",
+    Orden: 10,
+    Nombre: " Odontología",
+    pdfDR: PDF_BASE_PATH + "diagramas-de-ruta-pdf/Odontologia-RutaAsistencial.pdf",
+    routerLink: '../../../assets/rutas-lr/', 
+    pdfDT: PDF_BASE_PATH + "documentos-tecnicos-pdf/Odontología-RutaAsistencial-DocumentoTecnico.pdf",
+  }
 
 ];
 
