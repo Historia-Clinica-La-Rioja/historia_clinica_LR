2ee2f2455f5944cd15664e910c27d2a93af41381
Federico Simaro <fede.simaro@gmail.com>
2024-08-08 10:49:35 -0300

    hsi-9838 DTOs mal documentados en swagger

--- back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/infrastructure/input/service/dto/ReferableItemDto.java
+++ back-end/clinic-history/src/main/java/ar/lamansys/sgh/clinichistory/infrastructure/input/service/dto/ReferableItemDto.java
@@ -1,6 +1,7 @@
 package ar.lamansys.sgh.clinichistory.infrastructure.input.service.dto;
 
 import ar.lamansys.sgx.shared.validation.CustomContainer;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @NoArgsConstructor
+@Schema(name = "ar.lamansys.sgh.clinichistory.infrastructure.input.service.dto.ReferableItemDto", implementation = ReferableItemDto.class)
 public class ReferableItemDto<T> extends CustomContainer<T> {
 
 	private Boolean isReferred;
