a96da0338b7800bc1179e623eb1aa5875388ef6b
jvaldez <jvaldez@lamansys.com.ar>
2024-08-15 09:58:44 -0300

    hsi-10384 [FE] Adapta visual de columna triage

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/guardia/components/emergency-care-triage-summary-item/emergency-care-triage-summary-item.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/modules/guardia/components/emergency-care-triage-summary-item/emergency-care-triage-summary-item.component.ts
@@ -1,17 +1,35 @@
 import { Component, Input } from '@angular/core';
 import { EpisodeListTriage } from '../emergency-care-patients-summary/emergency-care-patients-summary.component';
-import { MasterDataDto } from '@api-rest/api-model';
+import { RegisterEditor } from '@presentation/components/register-editor-info/register-editor-info.component';
+import { IDENTIFIER_CASES } from '@hsi-components/identifier-cases/identifier-cases.component';
 
 @Component({
-  selector: 'app-emergency-care-triage-summary-item',
-  templateUrl: './emergency-care-triage-summary-item.component.html',
-  styleUrls: ['./emergency-care-triage-summary-item.component.scss']
+	selector: 'app-emergency-care-triage-summary-item',
+	templateUrl: './emergency-care-triage-summary-item.component.html',
+	styleUrls: ['./emergency-care-triage-summary-item.component.scss']
 })
 export class EmergencyCareTriageSummaryItemComponent {
 
-  @Input() triage: EpisodeListTriage;
-  @Input() episodeType: MasterDataDto;
+	readonly REASON = IDENTIFIER_CASES.REASON;
+	triageCreator: RegisterEditor;
+	triage: EpisodeListTriage;
 
-  constructor() { }
+	@Input() set episodeListTriage(episodeListTriage: EpisodeListTriage) {
+		this.triage = episodeListTriage;
+		const hasTriage = !!episodeListTriage.emergencyCareEpisodeListTriageDto.creator;
+
+		if (hasTriage) {
+			const { firstName, lastName } = this.triage.emergencyCareEpisodeListTriageDto.creator;
+			this.setTriageCreatorData(firstName, lastName);
+		}
+	};
+
+	constructor() { }
+
+	private setTriageCreatorData(firstName: string, lastName: string) {
+		this.triageCreator = {
+			createdBy: `${firstName} ${lastName}`
+		}
+	}
 
 }
