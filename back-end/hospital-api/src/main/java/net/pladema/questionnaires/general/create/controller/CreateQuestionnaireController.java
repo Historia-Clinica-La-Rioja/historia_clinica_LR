1246f2c5e0059ee565d108fa0f1c91e1862262a6
RLRama <rl.ramiro11@gmail.com>
2024-08-19 11:04:44 -0300

    fix: add logging for 'create questionnaire' controller and service layers

--- back-end/hospital-api/src/main/java/net/pladema/questionnaires/general/create/controller/CreateQuestionnaireController.java
+++ back-end/hospital-api/src/main/java/net/pladema/questionnaires/general/create/controller/CreateQuestionnaireController.java
@@ -1,5 +1,7 @@
 package net.pladema.questionnaires.general.create.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import net.pladema.questionnaires.common.repository.entity.QuestionnaireResponse;
 import net.pladema.questionnaires.common.dto.QuestionnaireResponseDTO;
+import net.pladema.questionnaires.common.repository.entity.QuestionnaireResponse;
 import net.pladema.questionnaires.general.create.domain.service.CreateQuestionnaireService;
 
 @RestController
 @RequestMapping
 @Tag(name = "Patient questionnaires and assessments", description = "Patient questionnaires and assessments")
 public class CreateQuestionnaireController {
+
+	private static final Logger logger = LoggerFactory.getLogger(CreateQuestionnaireController.class);
 
 	@Autowired
 	private CreateQuestionnaireService questionnaireService;
@@ -28,7 +32,15 @@ public class CreateQuestionnaireController {
 	public ResponseEntity<QuestionnaireResponse> createQuestionnaireResponse(
 			@RequestBody QuestionnaireResponseDTO responseDTO, @PathVariable Integer institutionId, @PathVariable Integer patientId
 			) {
-		QuestionnaireResponse createdResponse = questionnaireService.createQuestionnaireResponse(responseDTO, patientId);
+		logger.info("Received request to create questionnaire response for patientId: {} in institutionId: {}", patientId, institutionId);
+		logger.debug("QuestionnaireResponseDTO: {}", responseDTO);
+		QuestionnaireResponse createdResponse;
+		try {
+			createdResponse = questionnaireService.createQuestionnaireResponse(responseDTO, patientId);
+		} catch (Exception e) {
+			logger.error("Error creating questionnaire response for patientId: {}", patientId, e);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return new ResponseEntity<>(createdResponse, HttpStatus.CREATED);
 	}
 }
