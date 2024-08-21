fa071e81598f2d8c919fd8794f713a1d3d95791f
larav <lvenere@lamansys.com.ar>
2024-08-21 12:58:45 -0300

    HSI-10822 [BE] Implementar lógica para obtener el usuario actual

--- back-end/hospital-api/src/main/java/net/pladema/clinichistory/requests/servicerequests/service/impl/GetMostFrequentConceptsServiceImpl.java
+++ back-end/hospital-api/src/main/java/net/pladema/clinichistory/requests/servicerequests/service/impl/GetMostFrequentConceptsServiceImpl.java
@@ -1,9 +1,12 @@
 package net.pladema.clinichistory.requests.servicerequests.service.impl;
 
 import ar.lamansys.sgh.shared.infrastructure.input.service.SharedSnomedDto;
+import ar.lamansys.sgx.shared.security.UserInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.pladema.clinichistory.requests.servicerequests.service.GetMostFrequentConceptsService;
+
+import net.pladema.staff.controller.service.HealthcareProfessionalExternalService;
 
 import org.springframework.stereotype.Service;
 
@@ -15,10 +18,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GetMostFrequentConceptsServiceImpl implements GetMostFrequentConceptsService {
 
+	private final HealthcareProfessionalExternalService healthcareProfessionalExternalService;
+
 	@Override
 	public List<SharedSnomedDto> getMostFrequentStudies(Integer institutionId){
 		log.debug("Input parameter -> institutionId {}", institutionId);
 
+		Integer professionalId = healthcareProfessionalExternalService.getProfessionalId(UserInfo.getCurrentAuditor());
 		List<SharedSnomedDto> result = new ArrayList<>();
 
 		log.debug("Output -> {}", result);
