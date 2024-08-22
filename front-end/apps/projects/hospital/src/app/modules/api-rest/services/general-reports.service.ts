b14357c61f3c43cf3e78488e5d939effeb611f30
Macarena Martín <macarenamartin1111@gmail.com>
2024-08-22 12:24:30 -0300

    adde 'Prescripciones de medicamentos' to general reports list

--- front-end/apps/projects/hospital/src/app/modules/api-rest/services/general-reports.service.ts
+++ front-end/apps/projects/hospital/src/app/modules/api-rest/services/general-reports.service.ts
@@ -43,5 +43,9 @@ export class GeneralReportsService {
         const url = `${environment.apiBase}/generalreports/${this.contextService.institutionId}/complementary-studies`;
         return this.getGeneralReport(params, fileName, url);
     }
+    getMedicationPrescriptionReport(params: any, fileName: string): Observable<any> {
+        const url = `${environment.apiBase}/generalreports/${this.contextService.institutionId}/medicines-prescription`;
+        return this.getGeneralReport(params, fileName, url);
+    }
     
 }
