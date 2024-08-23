2382eebda0e20e82ef97aff434f8c0c55119defd
Federico Pinel <federicopinel12@gmail.com>
2024-08-23 15:17:48 -0300

    hsi-10781 [FE] Deshabilitar campo descripcion cuando no haya conceptos

--- front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/procedure-and-description/procedure-and-description.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/historia-clinica/components/procedure-and-description/procedure-and-description.component.ts
@@ -19,117 +19,124 @@ const PROCEDURE_NOTE_POSITION = 0;
 export class ProcedureAndDescriptionComponent implements OnInit {
 
 	@Input() title: string;
-    @Input() tableTitle: string;
-    @Input() buttonTitle: string;
-    @Input() icon: string;
-    @Input() surgicalReport: SurgicalReportDto;
-    @Input() type: ProcedureTypeEnum;
+	@Input() tableTitle: string;
+	@Input() buttonTitle: string;
+	@Input() icon: string;
+	@Input() surgicalReport: SurgicalReportDto;
+	@Input() type: ProcedureTypeEnum;
 
-    procedureService = new ProcedimientosService(this.formBuilder, this.snomedService, this.snackBarService, this.dateFormatPipe);
-    searchConceptsLocallyFF = false;
-    procedures: HospitalizationProcedureDto[] = [];
-    description: string;
+	procedureService = new ProcedimientosService(this.formBuilder, this.snomedService, this.snackBarService, this.dateFormatPipe);
+	searchConceptsLocallyFF = false;
+	procedures: HospitalizationProcedureDto[] = [];
+	isEmptyConcepts: boolean = true;
+	description: string;
 
-    constructor(
-        private readonly snackBarService: SnackBarService,
-        private readonly snomedService: SnomedService,
-        private readonly formBuilder: UntypedFormBuilder,
-        private readonly dialog: MatDialog,
-        private readonly featureFlagService: FeatureFlagService,
-        private readonly dateFormatPipe: DateFormatPipe
-    ) {
-        this.setFeatureFlags();
-        this.procedureService.procedimientos$.subscribe(procedures => this.changeProcedure(procedures));
-    }
+	constructor(
+		private readonly snackBarService: SnackBarService,
+		private readonly snomedService: SnomedService,
+		private readonly formBuilder: UntypedFormBuilder,
+		private readonly dialog: MatDialog,
+		private readonly featureFlagService: FeatureFlagService,
+		private readonly dateFormatPipe: DateFormatPipe
+	) {
+		this.setFeatureFlags();
+		this.procedureService.procedimientos$.subscribe(procedures => this.changeProcedure(procedures));
+	}
 
-    ngOnInit(): void {
-        switch (this.type) {
-            case ProcedureTypeEnum.DRAINAGE:
-                this.procedures = this.surgicalReport.drainages;
-                break;
-            case ProcedureTypeEnum.CULTURE:
-                this.procedures = this.surgicalReport.cultures;
-                break;
-            case ProcedureTypeEnum.FROZEN_SECTION_BIOPSY:
-                this.procedures = this.surgicalReport.frozenSectionBiopsies;
-                break;
-        }
+	ngOnInit(): void {
+		switch (this.type) {
+			case ProcedureTypeEnum.DRAINAGE:
+				this.procedures = this.surgicalReport.drainages;
+				break;
+			case ProcedureTypeEnum.CULTURE:
+				this.procedures = this.surgicalReport.cultures;
+				break;
+			case ProcedureTypeEnum.FROZEN_SECTION_BIOPSY:
+				this.procedures = this.surgicalReport.frozenSectionBiopsies;
+				break;
+		}
 
-        if (this.procedures.length > 0) {
-            this.description = this.procedures[PROCEDURE_NOTE_POSITION].note;
-        }
-    }
+		if (this.procedures.length > 0) {
+			this.description = this.procedures[PROCEDURE_NOTE_POSITION].note;
+		}
+		this.checkEmptyConcepts();
+	}
 
-    addProcedure() {
-        this.dialog.open(NewConsultationProcedureFormComponent, {
-            data: {
-                procedureService: this.procedureService,
-                searchConceptsLocallyFF: this.searchConceptsLocallyFF,
-                hideDate: true
-            },
-            autoFocus: false,
-            width: '35%',
-            disableClose: true,
-        });
-    }
+	addProcedure() {
+		this.dialog.open(NewConsultationProcedureFormComponent, {
+			data: {
+				procedureService: this.procedureService,
+				searchConceptsLocallyFF: this.searchConceptsLocallyFF,
+				hideDate: true
+			},
+			autoFocus: false,
+			width: '35%',
+			disableClose: true,
+		});
+	}
 
-    private changeProcedure(procedures) {
-        procedures.forEach(procedure => {
-            this.procedures = pushIfNotExists(this.procedures, this.mapToHospitalizationProcedure(procedure, this.type), this.compare);
-        });
-        this.updateSurgicalReportProcedures();
+	deleteProcedure(index: number) {
+		this.procedures = removeFrom(this.procedures, index);
+		this.procedureService.remove(index);
+		this.updateSurgicalReportProcedures();
+		this.checkEmptyConcepts();
+	}
 
-        if (this.procedures.length > 0) {
-            this.description = this.procedures[PROCEDURE_NOTE_POSITION].note;
-        }
-    }
+	onDescriptionChange() {
+		if (this.procedures.length > 0) {
+			this.procedures[PROCEDURE_NOTE_POSITION].note = this.description;
+			this.updateSurgicalReportProcedures();
+		}
+	}
 
-    private updateSurgicalReportProcedures() {
-        switch (this.type) {
-            case ProcedureTypeEnum.DRAINAGE:
-                this.surgicalReport.drainages = this.procedures;
-                break;
-            case ProcedureTypeEnum.CULTURE:
-                this.surgicalReport.cultures = this.procedures;
-                break;
-            case ProcedureTypeEnum.FROZEN_SECTION_BIOPSY:
-                this.surgicalReport.frozenSectionBiopsies = this.procedures;
-                break;
-        }
-    }
+	private checkEmptyConcepts() {
+		this.isEmptyConcepts = this.procedures.length === 0;
+		if (this.isEmptyConcepts) {
+			this.description = '';
+		}
+	}
 
-    private compare(first, second): boolean {
-        return first.snomed.sctid === second.snomed.sctid;
-    }
+	private changeProcedure(procedures) {
+		procedures.forEach(procedure => {
+			this.procedures = pushIfNotExists(this.procedures, this.mapToHospitalizationProcedure(procedure, this.type), this.compare);
+		});
+		this.updateSurgicalReportProcedures();
 
-    deleteProcedure(index: number) {
-        this.procedures = removeFrom(this.procedures, index);
-        this.procedureService.remove(index);
-        this.updateSurgicalReportProcedures();
-    }
+		if (this.procedures.length > 0) {
+			this.description = this.procedures[PROCEDURE_NOTE_POSITION].note;
+		}
+		this.checkEmptyConcepts();
+	}
 
-    private mapToHospitalizationProcedure(procedure, type: ProcedureTypeEnum): HospitalizationProcedureDto {
-        return {
-            snomed: procedure.snomed,
-            type: type,
-            note: this.description || procedure.note || '',
-        }
-    }
+	private updateSurgicalReportProcedures() {
+		switch (this.type) {
+			case ProcedureTypeEnum.DRAINAGE:
+				this.surgicalReport.drainages = this.procedures;
+				break;
+			case ProcedureTypeEnum.CULTURE:
+				this.surgicalReport.cultures = this.procedures;
+				break;
+			case ProcedureTypeEnum.FROZEN_SECTION_BIOPSY:
+				this.surgicalReport.frozenSectionBiopsies = this.procedures;
+				break;
+		}
+	}
 
-    isEmpty(): boolean {
-        return !this.procedures.length && !this.description;
-    }
-
-    onDescriptionChange() {
-        if (this.procedures.length > 0) {
-            this.procedures[PROCEDURE_NOTE_POSITION].note = this.description;
-            this.updateSurgicalReportProcedures();
-        }
-    }
+	private mapToHospitalizationProcedure(procedure, type: ProcedureTypeEnum): HospitalizationProcedureDto {
+		return {
+			snomed: procedure.snomed,
+			type: type,
+			note: this.description || procedure.note || '',
+		}
+	}
 
 	private setFeatureFlags() {
 		this.featureFlagService.isActive(AppFeature.HABILITAR_BUSQUEDA_LOCAL_CONCEPTOS).subscribe(isOn => {
-            this.searchConceptsLocallyFF = isOn;
-        })
+			this.searchConceptsLocallyFF = isOn;
+		})
+	}
+
+	private compare(first, second): boolean {
+		return first.snomed.sctid === second.snomed.sctid;
 	}
 }
