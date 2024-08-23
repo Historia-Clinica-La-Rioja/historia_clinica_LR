ff73272699c1ac8c2902e011a2990f4f1bd3b438
mmalyvarni <mateomv.mmv@gmail.com>
2024-08-23 19:08:54 -0300

    hsi-10909 [BE] Mejora en consultas de búsqueda de pacientes en historia clínica

--- back-end/hospital-api/src/main/java/net/pladema/patient/repository/PatientRepositoryImpl.java
+++ back-end/hospital-api/src/main/java/net/pladema/patient/repository/PatientRepositoryImpl.java
@@ -1,6 +1,7 @@
 package net.pladema.patient.repository;
 
 import ar.lamansys.sgh.shared.infrastructure.input.service.patient.enums.EPatientType;
+import lombok.AllArgsConstructor;
 import net.pladema.clinichistory.hospitalization.repository.domain.InternmentEpisodeStatus;
 import net.pladema.emergencycare.repository.entity.EmergencyCareState;
 import net.pladema.medicalconsultation.appointment.repository.entity.AppointmentState;
@@ -8,7 +9,8 @@ import net.pladema.patient.controller.dto.PatientSearchFilter;
 import net.pladema.patient.repository.entity.Patient;
 import net.pladema.patient.repository.entity.PatientType;
 import net.pladema.patient.service.domain.PatientSearch;
-import ar.lamansys.sgx.shared.repositories.QueryPart;
+import net.pladema.person.repository.entity.Person;
+
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -16,75 +18,83 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import java.math.BigInteger;
+import java.sql.Date;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static net.pladema.patient.repository.PatientSearchQuery.AND_JOINING_OPERATOR;
-import static net.pladema.patient.repository.PatientSearchQuery.LIKE_COMPARATOR;
-
+@AllArgsConstructor
 @Repository
 public class PatientRepositoryImpl implements PatientRepositoryCustom {
 
     @PersistenceContext
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
-    public PatientRepositoryImpl(EntityManager entityManager){
-        super();
-        this.entityManager = entityManager;
-    }
-
-    @SuppressWarnings("unchecked")
     @Override
     public List<PatientSearch> getAllByOptionalFilter(PatientSearchFilter searchFilter, Integer resultSize) {
-        PatientSearchQuery patientSearchQuery = new PatientSearchQuery(searchFilter);
-        QueryPart queryPart = new QueryPart(
-                "SELECT ")
-                .concatPart(patientSearchQuery.select())
-                .concat(" FROM ")
-                .concatPart(patientSearchQuery.from())
-                .concat("WHERE ")
-                .concatPart(patientSearchQuery.whereWithAllAttributes(AND_JOINING_OPERATOR, LIKE_COMPARATOR))
-				.concat(" AND patient.deleted = false AND patient.type_id != " + EPatientType.REJECTED.getId() + " ");
+		String queryString = "SELECT p.id as patientId, p2.id as personId, p2.first_name, p2.middle_names, p2.last_name, " +
+				"p2.other_last_names, p2.gender_id, p2.identification_type_id, p2.identification_number, p2.birth_date, pt.active, " +
+				"pe.name_self_determination " +
+				"FROM {h-schema}patient p " +
+				"JOIN {h-schema}person p2 ON (p.person_id = p2.id) " +
+				"JOIN {h-schema}patient_type pt ON (p.type_id = pt.id) " +
+				"LEFT JOIN {h-schema}person_extended pe ON (pe.person_id = p2.id) " +
+				"WHERE p.deleted = FALSE " +
+				"AND p.type_id != " + EPatientType.REJECTED.getId() + " " +
+				(searchFilter.getFirstName() != null && !searchFilter.getFirstName().isBlank() ? (searchFilter.getFilterByNameSelfDetermination() ? "AND ((pe.name_self_determination IS NOT NULL AND UPPER(pe.name_self_determination) LIKE '%" + searchFilter.getFirstName().toUpperCase() + "%') OR (pe.name_self_determination IS NULL AND UPPER(p2.first_name) LIKE '%" + searchFilter.getFirstName().toUpperCase() + "%')) " : "AND UPPER(p2.first_name) LIKE '%" + searchFilter.getFirstName().toUpperCase() + "%' ") : "") +
+				(searchFilter.getMiddleNames() != null && !searchFilter.getMiddleNames().isBlank() ? "AND UPPER(p2.middle_names) LIKE '%" + searchFilter.getMiddleNames().toUpperCase() + "%' " : "") +
+				(searchFilter.getLastName() != null && !searchFilter.getLastName().isBlank() ? "AND UPPER(p2.last_name) LIKE '%" + searchFilter.getLastName().toUpperCase() + "%' " : "") +
+				(searchFilter.getOtherLastNames() != null ? "AND UPPER(p2.other_last_names) LIKE '%" + searchFilter.getOtherLastNames().toUpperCase() +"%' " : "") +
+				(searchFilter.getGenderId() != null ? "AND p2.gender_id = " + searchFilter.getGenderId() + " " : "") +
+				(searchFilter.getIdentificationNumber() != null && !searchFilter.getIdentificationNumber().isBlank() ? "AND p2.identification_number = '" + searchFilter.getIdentificationNumber() + "' " : "") +
+				(searchFilter.getIdentificationTypeId() != null ? "AND p2.identification_type_id = " + searchFilter.getIdentificationTypeId() + " " : "") +
+				(searchFilter.getBirthDate() != null ? "AND p2.birth_date = '" + searchFilter.getBirthDate() + "' " : "");
 
-        if (searchFilter.getFilterByNameSelfDetermination()) {
-			queryPart.concat("UNION ");
-			queryPart.concatPart(patientSearchQuery.addUnion());
-		}
-
-		Query query = entityManager.createNativeQuery(queryPart.toString());
+		Query query = entityManager.createNativeQuery(queryString);
 		query.setMaxResults(resultSize);
-		queryPart.configParams(query);
 
-        return patientSearchQuery.construct(query.getResultList());
+        return parseToPatientSearchList(query.getResultList());
     }
 
-    public Integer getCountByOptionalFilter(PatientSearchFilter searchFilter) {
-        PatientSearchQuery patientSearchQuery = new PatientSearchQuery(searchFilter);
+	private List<PatientSearch> parseToPatientSearchList(List<Object[]> queryResult) {
+		return queryResult.stream().map(this::parseToPatientSearch).collect(Collectors.toList());
+	}
 
-        QueryPart queryPart = new QueryPart(
-                "SELECT COUNT(DISTINCT result.id) \n")
-                .concat("FROM ( \n")
-				.concat("	SELECT patient.id as id \n")
-				.concat("	FROM \n")
-                .concatPart(patientSearchQuery.from())
-                .concat("	WHERE \n")
-                .concatPart(patientSearchQuery.whereWithAllAttributes(AND_JOINING_OPERATOR, LIKE_COMPARATOR))
-				.concat(" AND patient.deleted = false ");
+	private PatientSearch parseToPatientSearch(Object[] data) {
+		return new PatientSearch(mapToPersonEntity(data), (Integer) data[0], (Boolean) data[10], 0, (String) data[11]);
+	}
 
-        if (searchFilter.getFilterByNameSelfDetermination()) {
-			queryPart.concat("UNION \n" +
-					"SELECT patient.id as id \n");
-			queryPart.concat("FROM \n")
-					.concatPart(patientSearchQuery.from())
-					.concat("WHERE \n")
-					.concatPart(patientSearchQuery.whereWithAllAttributesAndNameSelfDetermination(AND_JOINING_OPERATOR, LIKE_COMPARATOR))
-					.concat(" AND patient.deleted = false ");
-		}
+	private Person mapToPersonEntity(Object[] data) {
+		Person result = new Person();
+		result.setId((Integer) data[1]);
+		result.setFirstName((String) data[2]);
+		result.setMiddleNames((String) data[3]);
+		result.setLastName((String) data[4]);
+		result.setOtherLastNames((String) data[5]);
+		result.setGenderId((Short) data[6]);
+		result.setIdentificationTypeId((Short) data[7]);
+		result.setIdentificationNumber((String) data[8]);
+		result.setBirthDate(data[9] != null ? ((Date) data[9]).toLocalDate() : null);
+		return result;
+	}
 
-        queryPart.concat(") as result");
+	public Integer getCountByOptionalFilter(PatientSearchFilter searchFilter) {
+		String queryString = "SELECT COUNT(DISTINCT p.id) " +
+				"FROM {h-schema} patient p " +
+				"JOIN {h-schema}person p2 ON (p.person_id = p2.id) " +
+				"JOIN {h-schema}patient_type pt ON (p.type_id = pt.id) " +
+				"LEFT JOIN {h-schema}person_extended pe ON (pe.person_id = p2.id) " +
+				"WHERE p.deleted = FALSE " +
+				(searchFilter.getFirstName() != null && !searchFilter.getFirstName().isBlank() ? (searchFilter.getFilterByNameSelfDetermination() ? "AND ((pe.name_self_determination IS NOT NULL AND UPPER(pe.name_self_determination) LIKE '%" + searchFilter.getFirstName().toUpperCase() + "%') OR (pe.name_self_determination IS NULL AND UPPER(p2.first_name) LIKE '%" + searchFilter.getFirstName().toUpperCase() + "%')) " : "AND UPPER(p2.first_name) LIKE '%" + searchFilter.getFirstName().toUpperCase() + "%' ") : "") +
+				(searchFilter.getMiddleNames() != null && !searchFilter.getMiddleNames().isBlank() ? "AND UPPER(p2.middle_names) LIKE '%" + searchFilter.getMiddleNames().toUpperCase() + "%' " : "") +
+				(searchFilter.getLastName() != null && !searchFilter.getLastName().isBlank() ? "AND UPPER(p2.last_name) LIKE '%" + searchFilter.getLastName().toUpperCase() + "%' " : "") +
+				(searchFilter.getOtherLastNames() != null ? "AND UPPER(p2.other_last_names) LIKE '%" + searchFilter.getOtherLastNames().toUpperCase() +"%' " : "") +
+				(searchFilter.getGenderId() != null ? "AND p2.gender_id = " + searchFilter.getGenderId() + " " : "") +
+				(searchFilter.getIdentificationNumber() != null && !searchFilter.getIdentificationNumber().isBlank() ? "AND p2.identification_number = '" + searchFilter.getIdentificationNumber() + "' " : "") +
+				(searchFilter.getIdentificationTypeId() != null ? "AND p2.identification_type_id = " + searchFilter.getIdentificationTypeId() + " " : "") +
+				(searchFilter.getBirthDate() != null ? "AND p2.birth_date = '" + searchFilter.getBirthDate() + "' " : "");
 
-        Query query = entityManager.createNativeQuery(queryPart.toString());
-		queryPart.configParams(query);
+        Query query = entityManager.createNativeQuery(queryString);
 
 		BigInteger result = (BigInteger) query.getSingleResult();
 		return result.intValue();
--- back-end/hospital-api/src/main/java/net/pladema/patient/repository/PatientSearchQuery.java
+++ back-end/hospital-api/src/main/java/net/pladema/patient/repository/PatientSearchQuery.java
@@ -1,6 +1,5 @@
 package net.pladema.patient.repository;
 
-import ar.lamansys.sgh.shared.infrastructure.input.service.patient.enums.EPatientType;
 import ar.lamansys.sgx.shared.repositories.QueryStringHelper;
 import lombok.NoArgsConstructor;
 import net.pladema.patient.controller.dto.PatientSearchFilter;
@@ -22,15 +21,10 @@ import static java.util.stream.Collectors.toList;
 public class PatientSearchQuery {
 
     // WHERE options
-    public static final Integer AND_JOINING_OPERATOR = 1;
     public static final Integer OR_JOINING_OPERATOR = 2;
-    public static final Integer LIKE_COMPARATOR = 3;
-    public static final Integer EQUAL_COMPARATOR = 4;
 
     // WHERE clause formats
-    private static final String LIKE_FORMAT = " (UPPER(%s) LIKE '%%%s%%') \n"; //double '%' to escape the character
 	private static final String LIKE_FORMAT_INDEX = " (UPPER(%s) LIKE '%s%%') \n"; //double '%' to escape the character
-	private static final String EQUAL_FORMAT = " (UPPER(%s) = '%s') \n";
 
     String firstName;
     String middleNames;
@@ -41,8 +35,6 @@ public class PatientSearchQuery {
     String identificationNumber;
     LocalDate birthDate;
     Boolean filterByNameSelfDetermination;
-
-    private static final String MESSAGE = "No se han encontrado pacientes";
 
     public PatientSearchQuery(PatientSearchFilter patientSearchFilter){
         this.firstName = patientSearchFilter.getFirstName();
@@ -79,11 +71,6 @@ public class PatientSearchQuery {
 				"	join {h-schema}person as person on (patient.person_id = person.id) \n" +
 				"	join {h-schema}patient_type as type on (patient.type_id = type.id) \n" +
 				" 	left join {h-schema}person_extended as personExtended on ( personExtended.person_id = person.id) \n";
-
-    	/*String fromWithPersonExtended = "join {h-schema}person_extended as personExtended on (person.id = personExtended.person_id) \n";
-		if (this.filterByNameSelfDetermination)
-    		from = from + fromWithPersonExtended;
-*/
 		return new QueryPart(from);
     }
 
@@ -110,102 +97,16 @@ public class PatientSearchQuery {
         }
 
         String joiningOperatorString = getJoiningOperator(joiningOperator);
-		QueryPart result = new QueryPart(" AND ( ")
+		return new QueryPart(" AND ( ")
 				.concatPart(new QueryPart(String.join(joiningOperatorString, whereString)))
 				.concat(" )");
-        return result;
     }
-
-    public QueryPart whereWithAllAttributes(Integer joiningOperator, Integer clauseComparator) {
-        List<String> whereString = new ArrayList<>();
-
-        if (firstName != null) {
-            firstName = (QueryStringHelper.escapeSql(firstName)).toUpperCase();
-            String clause = String.format(LIKE_FORMAT_INDEX, "person.first_name", firstName);
-            whereString.add(clause);
-        }
-
-        return addOtherAttributes(whereString, joiningOperator, clauseComparator);
-    }
-
-	public QueryPart whereWithAllAttributesAndNameSelfDetermination(Integer joiningOperator, Integer clauseComparator) {
-		List<String> whereString = new ArrayList<>();
-
-		if (firstName != null) {
-			firstName = (QueryStringHelper.escapeSql(firstName)).toUpperCase();
-			String clause = String.format(LIKE_FORMAT_INDEX, "personExtended.name_self_determination", firstName);
-			whereString.add(clause);
-		}
-
-		return addOtherAttributes(whereString, joiningOperator, clauseComparator);
-	}
-
-	public QueryPart addOtherAttributes (List<String> whereString, Integer joiningOperator, Integer clauseComparator) {
-
-		String clauseFormat = getClauseFormat(clauseComparator);
-
-		if (middleNames != null) {
-			middleNames = (QueryStringHelper.escapeSql(middleNames)).toUpperCase();
-			String clause = String.format(clauseFormat, "person.middle_names", middleNames);
-			whereString.add(clause);
-		}
-		if (lastName != null) {
-			lastName = (QueryStringHelper.escapeSql(lastName)).toUpperCase();
-			String clause = String.format(LIKE_FORMAT_INDEX, "person.last_name", lastName);
-			whereString.add(clause);
-		}
-		if (otherLastNames != null) {
-			otherLastNames = (QueryStringHelper.escapeSql(otherLastNames)).toUpperCase();
-			String clause = String.format(clauseFormat, "person.other_last_names", otherLastNames);
-			whereString.add(clause);
-		}
-		if (genderId != null) {
-			whereString.add(" (person.gender_id = '" + genderId + "') \n");
-		}
-		if (identificationNumber != null){
-			String clause = " (person.identification_number = '" + identificationNumber+"')";
-			whereString.add(clause);
-		}
-		if (identificationTypeId != null) {
-			whereString.add(" (person.identification_type_id = '"+identificationTypeId+"') \n");
-		}
-		if (birthDate != null){
-			String birthDateString = (QueryStringHelper.escapeSql(birthDate.toString())).toUpperCase();
-			whereString.add(" (person.birth_date = '"+birthDateString+"') \n");
-		}
-
-		String joiningOperatorString = getJoiningOperator(joiningOperator);
-		return new QueryPart(String.join(joiningOperatorString, whereString));
-	}
-
-    private String getClauseFormat(Integer clauseComparator) {
-        String clauseFormat;
-        if (clauseComparator.equals(EQUAL_COMPARATOR))
-            clauseFormat = EQUAL_FORMAT;
-        else
-            clauseFormat = LIKE_FORMAT;
-        return clauseFormat;
-    }
-
-	public QueryPart addUnion() {
-		return new QueryPart("SELECT ")
-				.concatPart(this.select())
-				.concat(" FROM ")
-				.concatPart(this.from())
-				.concat(" WHERE ")
-				.concatPart(whereWithAllAttributesAndNameSelfDetermination(AND_JOINING_OPERATOR, LIKE_COMPARATOR))
-				.concat(" AND patient.deleted = false AND patient.type_id != " + EPatientType.REJECTED.getId() + " ");
-	}
 
     private String getJoiningOperator(Integer joiningOperator) {
         if (joiningOperator.equals(OR_JOINING_OPERATOR))
             return " OR ";
         else
             return " AND ";
-    }
-
-    public String noResultMessage(){
-        return MESSAGE;
     }
 
     public List<PatientSearch> construct(List<Object[]> resultQuery){
