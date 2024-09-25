48392f65215b801e2637c5a47a00cc49a7aa2412
mmalyvarni <mateomv.mmv@gmail.com>
2024-09-25 08:46:00 -0300

    hsi-11742 [DB] Materialización de vistas del esquema snomedct

--- dba/snomed-schema/views.sql
+++ dba/snomed-schema/views.sql
@@ -1,20 +1,19 @@
 -- Fármaco comercial y fármaco genérico
-CREATE OR REPLACE VIEW snomedct.v_commercial_medication AS
-WITH mwuop AS (
-	SELECT DISTINCT s.sctid 
-	FROM public.snomed s 
-	JOIN public.snomed_related_group srg ON (srg.snomed_id = s.id)
-	JOIN public.snomed_group sg ON (sg.id = srg.group_id)
-	WHERE sg.description = 'MEDICINE_WITH_UNIT_OF_PRESENTATION'
-	AND sg.institution_id = -1
-)
+CREATE MATERIALIZED VIEW snomedct.v_commercial_medication AS
 SELECT DISTINCT ct2.id AS commercial_sctid, ct2.term AS commercial_pt, ct3.id AS generic_sctid, ct3.term AS generic_pt
 FROM snomedct.relationship r 
 JOIN snomedct.concept_term ct ON (r.sourceid = ct.id)
 JOIN snomedct.concept_term ct2 ON (r.destinationid  = ct2.id)
 JOIN snomedct.relationship r2 ON (ct2.id = r2.sourceid)
 JOIN snomedct.concept_term ct3 ON (r2.destinationid = ct3.id)
-JOIN mwuop ON (mwuop.sctid = ct3.id)
+JOIN (
+	SELECT DISTINCT s.sctid 
+	FROM public.snomed s 
+	JOIN public.snomed_related_group srg ON (srg.snomed_id = s.id)
+	JOIN public.snomed_group sg ON (sg.id = srg.group_id)
+	WHERE sg.description = 'MEDICINE_WITH_UNIT_OF_PRESENTATION'
+	AND sg.institution_id = -1
+) AS mwuop ON (mwuop.sctid = ct3.id)
 WHERE r.active = '1'
 AND r2.active = '1' 
 AND r.typeid = '774160008'
@@ -25,8 +24,7 @@ AND ct3."type" = 'syn'
 AND ct3.acceptability = 'preferido';
 
 -- Unidades de presentación
-
-CREATE OR REPLACE VIEW snomedct.v_medication_presentation_unit AS
+CREATE MATERIALIZED VIEW snomedct.v_medication_presentation_unit AS
 SELECT r.destinationid AS sctid, rcv.value AS presentation_unit_quantity
 FROM snomedct.relationship r
 JOIN snomedct.concept_term cpt ON (r.sourceid = cpt.id) 
