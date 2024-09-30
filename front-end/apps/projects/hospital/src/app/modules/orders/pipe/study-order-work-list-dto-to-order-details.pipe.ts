d1e453072560dfd0e782676c9ab9315832cd4699
FacundoBarreiros <facundobarreiros@gmail.com>
2024-09-30 15:05:35 -0300

    hsi-12026 [FE] Agregar pipe para convertir StudyOrderWorkListDto en OrderDetails

---
+++ front-end/apps/projects/hospital/src/app/modules/orders/pipe/study-order-work-list-dto-to-order-details.pipe.ts
@@ -0,0 +1,36 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { OrderDetails } from '../components/order-detail/order-detail.component';
+import { ESourceType, EStudyType, StudyOrderWorkListDto } from '@api-rest/api-model';
+
+@Pipe({
+	name: 'studyOrderWorkListDtoToOrderDetails'
+})
+export class StudyOrderWorkListDtoToOrderDetailsPipe implements PipeTransform {
+
+
+	transform(studyOrder: StudyOrderWorkListDto): OrderDetails {
+		const date = studyOrder.deferredDate ? studyOrder.deferredDate : studyOrder.createdDate;
+		const sourceTypeId = (studyOrder.sourceTypeId === ESourceType.HOSPITALIZATION) ? "Internación" : "Guardia";
+		const studyType = (studyOrder.studyTypeId === EStudyType.URGENT) ? "Urgente" : "Rutina";
+		return {
+			status: studyOrder.status.description,
+			sourceTypeId: sourceTypeId,
+			EStudyType: studyType,
+			snomedPt: studyOrder.snomed.pt,
+			requiresTransfer: studyOrder.requiresTransfer,
+			date: {
+				date: {
+					year: date.date.year,
+					month: date.date.month,
+					day: date.date.day,
+				},
+				time: {
+					hours: date.time.hours,
+					minutes: date.time.minutes,
+					seconds: date.time.seconds,
+				}
+			}
+		};
+	}
+
+}
