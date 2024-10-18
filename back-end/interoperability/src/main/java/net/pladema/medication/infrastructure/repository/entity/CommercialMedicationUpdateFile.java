6d066c193f55481dd906a7380330554959b0dd90
mmalyvarni <mateomv.mmv@gmail.com>
2024-10-18 10:04:07 -0300

    hsi-12424 [BE] Crear tablas y entidades para la actualización del esquema de medicación comercial

---
+++ back-end/interoperability/src/main/java/net/pladema/medication/infrastructure/repository/entity/CommercialMedicationUpdateFile.java
@@ -0,0 +1,39 @@
+package net.pladema.medication.infrastructure.repository.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "update_file", schema = "commercial_medication")
+@Entity
+public class CommercialMedicationUpdateFile implements Serializable {
+
+	private static final long serialVersionUID = 5626820266663252786L;
+
+	@Id
+	@Column(name = "log_id")
+	private Long logId;
+
+	@Column(name = "file_name")
+	private String fileName;
+
+	@Column(name = "processed")
+	private Boolean processed;
+
+	public CommercialMedicationUpdateFile(Long logId) {
+		this.logId = logId;
+		this.processed = false;
+	}
+}
