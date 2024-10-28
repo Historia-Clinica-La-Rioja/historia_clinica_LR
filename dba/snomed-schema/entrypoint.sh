b407c012cce7547d15203d80c769f248820e42b8
mmalyvarni <mateomv.mmv@gmail.com>
2024-10-28 09:51:19 -0300

    hsi-12644 [BE] Crear esquema de medicacion comercial

--- dba/snomed-schema/entrypoint.sh
+++ dba/snomed-schema/entrypoint.sh
@@ -13,37 +13,46 @@ if [ -z "$POSTGRES_HOST" ] || [ -z "$POSTGRES_PORT" ] || [ -z "$POSTGRES_USER" ]
   exit 1
 fi
 
-echo "Actualizando esquema"
-PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f "create-schema.sql"
+# Verifica que deba crearse el esquema commercial_medication
+if [ -z "$CREATE_COMMERCIAL_MEDICATION_SCHEMA" ] || [ "$CREATE_COMMERCIAL_MEDICATION_SCHEMA" = "false" ]; then
+  echo "Actualizando esquema"
+  PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f "create-schema.sql"
 
-# Función para ejecutar el comando psql \copy con un archivo TXT
-run_copy() {
-  local file=$1
-  local table=$2
-  echo "Copiando file $file en table $table"
+  # Función para ejecutar el comando psql \copy con un archivo TXT
+  run_copy() {
+    local file=$1
+    local table=$2
+    echo "Copiando file $file en table $table"
 
-  export PGPASSWORD=$POSTGRES_PASSWORD
-  # Salteo primer fila
-  tail -n +2 $file | psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -c "\copy $table FROM STDIN WITH (FORMAT text, DELIMITER E'\t', ENCODING 'UTF8')"
-}
+    export PGPASSWORD=$POSTGRES_PASSWORD
+    # Salteo primer fila
+    tail -n +2 $file | psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -c "\copy $table FROM STDIN WITH (FORMAT text, DELIMITER E'\t', ENCODING 'UTF8')"
+  }
 
-echo "Tabla concept"
-run_copy "Release2405/Snapshot/Terminology/sct2_Concept_Snapshot_ArgentinaEdition_20240531.txt" "snomedct.concept(id, effectivetime, active, moduleid, definitionstatusid)"
-echo "Tabla description"
-run_copy "Release2405/Snapshot/Terminology/sct2_Description_Snapshot_ArgentinaEdition_20240531.txt" "snomedct.description(id, effectivetime, active, moduleid, conceptid, languagecode, typeid, term, casesignificanceid)"
-echo "Tabla relationship"
-run_copy "Release2405/Snapshot/Terminology/sct2_Relationship_Snapshot_ArgentinaEdition_20240531.txt" "snomedct.relationship(id, effectivetime, active, moduleid, sourceid, destinationid, relationshipgroup, typeid,characteristictypeid, modifierid)"
-echo "Tabla langrefset"
-run_copy "Release2405/Snapshot/Refset/Language/der2_cRefset_LanguageSnapshot_ArgentinaEdition_20240531.txt" "snomedct.langrefset(id, effectivetime, active, moduleid, refsetid, referencedcomponentid, acceptabilityid)"
-echo "Tabla relationship_concrete_value"
-run_copy "Release2405/Snapshot/Terminology/sct2_RelationshipConcreteValues_Snapshot_ArgentinaEdition_20240531.txt" "snomedct.relationship_concrete_value(id, effectivetime, active, moduleid, sourceid, value, relationshipgroup, typeid, characteristictypeid, modifierid)"
+  echo "Tabla concept"
+  run_copy "Release2405/Snapshot/Terminology/sct2_Concept_Snapshot_ArgentinaEdition_20240531.txt" "snomedct.concept(id, effectivetime, active, moduleid, definitionstatusid)"
+  echo "Tabla description"
+  run_copy "Release2405/Snapshot/Terminology/sct2_Description_Snapshot_ArgentinaEdition_20240531.txt" "snomedct.description(id, effectivetime, active, moduleid, conceptid, languagecode, typeid, term, casesignificanceid)"
+  echo "Tabla relationship"
+  run_copy "Release2405/Snapshot/Terminology/sct2_Relationship_Snapshot_ArgentinaEdition_20240531.txt" "snomedct.relationship(id, effectivetime, active, moduleid, sourceid, destinationid, relationshipgroup, typeid,characteristictypeid, modifierid)"
+  echo "Tabla langrefset"
+  run_copy "Release2405/Snapshot/Refset/Language/der2_cRefset_LanguageSnapshot_ArgentinaEdition_20240531.txt" "snomedct.langrefset(id, effectivetime, active, moduleid, refsetid, referencedcomponentid, acceptabilityid)"
+  echo "Tabla relationship_concrete_value"
+  run_copy "Release2405/Snapshot/Terminology/sct2_RelationshipConcreteValues_Snapshot_ArgentinaEdition_20240531.txt" "snomedct.relationship_concrete_value(id, effectivetime, active, moduleid, sourceid, value, relationshipgroup, typeid, characteristictypeid, modifierid)"
 
-echo "Poblar tabla concept_term"
-PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f "concept_term.sql"
+  echo "Poblar tabla concept_term"
+  PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f "concept_term.sql"
 
-echo "Redefiniendo las vistas..."
-# En este paso pisaríamos las vistas por defecto de HSI
-PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f "views.sql"
+  echo "Redefiniendo las vistas..."
+  # En este paso pisaríamos las vistas por defecto de HSI
+  PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f "views.sql"
+else
+  CREATE_SCHEMA=$(PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -t -A -c "SELECT 1 FROM information_schema.schemata WHERE schema_name = 'commercial_medication';")
+  if [ -z "$CREATE_SCHEMA" ]; then
+    echo "Creating commercial_medication schema..."
+    PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f "create-commercial-medication-schema.sql"
+  fi
+fi
 
 # Comandos psql adicionales (modifica o añade lo que necesites aquí)
 # Ejemplo:
