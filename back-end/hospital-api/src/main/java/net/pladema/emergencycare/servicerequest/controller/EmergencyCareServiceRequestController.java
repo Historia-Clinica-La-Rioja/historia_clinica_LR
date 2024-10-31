8fd84e5d217fbaba0922f3cc1fbdab18092a8cfc
jirimoldi <juanirimoldi@gmail.com>
2024-10-31 17:17:36 -0300

    hsi-12733 [BE] Guardar templateIds usados en una orden de guardia

--- back-end/hospital-api/src/main/java/net/pladema/emergencycare/servicerequest/controller/EmergencyCareServiceRequestController.java
+++ back-end/hospital-api/src/main/java/net/pladema/emergencycare/servicerequest/controller/EmergencyCareServiceRequestController.java
@@ -76,13 +76,13 @@ public class EmergencyCareServiceRequestController {
 		Integer patientMedicalCoverageId = emergencyCareEpisodeService.getPatientMedicalCoverageIdByEpisode(episodeId);
 		BasicPatientDto patientDto = patientExternalService.getBasicDataFromPatient(patientId);
 		List<Integer> result = new ArrayList<>();
-		srGroupBy.forEach((categoryId, studyListDto) ->	handleStudyList(institutionId, episodeId, doctorId, patientMedicalCoverageId, patientDto, result, categoryId, studyListDto, serviceRequestListDto.getObservations(), serviceRequestListDto.getStudyType().getId(), serviceRequestListDto.getRequiresTransfer(), serviceRequestListDto.getDeferredDate()));
+		srGroupBy.forEach((categoryId, studyListDto) ->	handleStudyList(institutionId, episodeId, doctorId, patientMedicalCoverageId, patientDto, result, categoryId, studyListDto, serviceRequestListDto.getObservations(), serviceRequestListDto.getStudyType().getId(), serviceRequestListDto.getRequiresTransfer(), serviceRequestListDto.getDeferredDate(), serviceRequestListDto.getTemplateIds()));
 		log.debug("Output -> {}", result);
 		return result;
 	}
 
-	private void handleStudyList(Integer institutionId, Integer episodeId, Integer doctorId, Integer medicalCoverageId, BasicPatientDto patientDto, List<Integer> result, String categoryId, List<PrescriptionItemDto> studyListDto, String observations, Short studyTypeId, Boolean requiresTransfer, DateTimeDto deferredDate) {
-		ExtendedServiceRequestBo serviceRequestBo = parseTo(doctorId, patientDto, categoryId, medicalCoverageId, studyListDto, observations, studyTypeId, requiresTransfer, deferredDate);
+	private void handleStudyList(Integer institutionId, Integer episodeId, Integer doctorId, Integer medicalCoverageId, BasicPatientDto patientDto, List<Integer> result, String categoryId, List<PrescriptionItemDto> studyListDto, String observations, Short studyTypeId, Boolean requiresTransfer, DateTimeDto deferredDate, List<Integer> templateIds) {
+		ExtendedServiceRequestBo serviceRequestBo = parseTo(doctorId, patientDto, categoryId, medicalCoverageId, studyListDto, observations, studyTypeId, requiresTransfer, deferredDate, templateIds);
 		serviceRequestBo.setInstitutionId(institutionId);
 		Integer srId = emergencyCareServiceRequestService.execute(serviceRequestBo, episodeId);
 		hospitalApiPublisher.publish(serviceRequestBo.getPatientId(), institutionId, getTopicToPublish(categoryId));
@@ -97,7 +97,7 @@ public class EmergencyCareServiceRequestController {
 		return EHospitalApiTopicDto.CLINIC_HISTORY__HOSPITALIZATION__SERVICE_RESQUEST;
 	}
 
-	public ExtendedServiceRequestBo parseTo(Integer doctorId, BasicPatientDto patientDto, String categoryId, Integer medicalCoverageId, List<PrescriptionItemDto> studies, String observations, Short studyTypeId, Boolean requiresTransfer, DateTimeDto deferredDate){
+	public ExtendedServiceRequestBo parseTo(Integer doctorId, BasicPatientDto patientDto, String categoryId, Integer medicalCoverageId, List<PrescriptionItemDto> studies, String observations, Short studyTypeId, Boolean requiresTransfer, DateTimeDto deferredDate, List<Integer> templateIds){
 		log.debug("parseTo -> doctorId {}, patientDto {}, categoryId {}, medicalCoverageId {}, studies {} ", doctorId, patientDto, categoryId, medicalCoverageId, studies);
 		ExtendedServiceRequestBo result = new ExtendedServiceRequestBo();
 		result.setCategoryId(categoryId);
@@ -116,6 +116,7 @@ public class EmergencyCareServiceRequestController {
 		} else {
 			result.setDeferredDate(null);
 		}
+		result.setTemplateIds(templateIds);
 		log.debug("Output -> {}", result);
 		return result;
 	}
