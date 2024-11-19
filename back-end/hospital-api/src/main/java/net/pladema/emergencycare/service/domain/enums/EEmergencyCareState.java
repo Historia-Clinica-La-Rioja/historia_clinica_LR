79a8a50d6fcce7283aa84c1aadf94ac9c153f2bd
Bernardita Chacon <bchacon@lamansys.com.ar>
2024-11-19 15:07:07 -0300

    hsi-13085 [BE] Aplica ordenamiento en endpoint que retorna estado de atención

--- back-end/hospital-api/src/main/java/net/pladema/emergencycare/service/domain/enums/EEmergencyCareState.java
+++ back-end/hospital-api/src/main/java/net/pladema/emergencycare/service/domain/enums/EEmergencyCareState.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import ar.lamansys.sgx.shared.exceptions.NotFoundException;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -11,15 +12,17 @@ import java.util.stream.Stream;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum EEmergencyCareState {
 
-    ATENCION(1, "En atención"),
-    ESPERA(2, "En espera de atención"),
-    ALTA_ADMINISTRATIVA(3, "Con alta administrativa"),
-    ALTA_PACIENTE(4, "Alta de paciente"),
-	AUSENTE(5, "Ausente"),
-	LLAMADO(6, "Llamado");
+    ATENCION(1, "En atención", (short) 1),
+    ESPERA(2, "En espera de atención", (short) 3),
+    ALTA_ADMINISTRATIVA(3, "Con alta administrativa", (short) 6),
+    ALTA_PACIENTE(4, "Alta de paciente", (short) 5),
+	AUSENTE(5, "Ausente", (short) 4),
+	LLAMADO(6, "Llamado", (short) 2);
 
     private final Short id;
     private final String description;
+
+	private final Short order;
 
 	/**
 	 * Matriz que indica las transiciones de estado válidas.
@@ -37,14 +40,17 @@ public enum EEmergencyCareState {
 			/* 6  */{false, true , true , true , true , true , true }
 	};
 
-    EEmergencyCareState(Number id, String description) {
+    EEmergencyCareState(Number id, String description, Short order) {
         this.id = id.shortValue();
         this.description = description;
+		this.order = order;
     }
 
     @JsonCreator
     public static List<EEmergencyCareState> getAll(){
-        return Stream.of(EEmergencyCareState.values()).collect(Collectors.toList());
+        return Stream.of(EEmergencyCareState.values())
+				.sorted(Comparator.comparing(ecs -> ecs.order))
+				.collect(Collectors.toList());
     }
 
     @JsonCreator
