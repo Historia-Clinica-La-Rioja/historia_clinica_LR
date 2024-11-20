1efcf5aab2c1674d6347d11a677fe0edbca939a3
FacundoBarreiros <facundobarreiros@gmail.com>
2024-11-20 14:53:43 -0300

    hsi-12970 [FE] Creacion del componente PatientLocationComponent

--- front-end/apps/projects/hospital/src/app/modules/orders/orders.module.ts
+++ front-end/apps/projects/hospital/src/app/modules/orders/orders.module.ts
@@ -8,16 +8,15 @@ import { OrderDetailComponent } from './components/order-detail/order-detail.com
 import { PatientSummaryComponent } from '@hsi-components/patient-summary/patient-summary.component';
 import { PresentationModule } from '@presentation/presentation.module';
 import { OrdersRoutingModule } from './orders-routing.module';
-import { PatientLocationComponent } from './components/patient-location/patient-location.component';
 import { StudyOrderWorkListDtoToOrderDetailsPipe } from './pipe/study-order-work-list-dto-to-order-details.pipe';
 //Standalone Component
 import { IdentifierCasesComponent } from '@hsi-components/identifier-cases/identifier-cases.component';
+import { PatientLocationComponent } from '@hsi-components/patient-location/patient-location.component';
 
 @NgModule({
 	declarations: [
 		HomeComponent,
 		OrderDetailComponent,
- 		PatientLocationComponent,
 		StudyOrderWorkListDtoToOrderDetailsPipe,
 		WorkOrderListComponent,
 	],
@@ -29,6 +28,7 @@ import { IdentifierCasesComponent } from '@hsi-components/identifier-cases/ident
 		PatientSummaryComponent,
 		//Standalone Component
 		IdentifierCasesComponent,
+		PatientLocationComponent,
 	]
 })
 export class OrdersModule { }
