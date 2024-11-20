0433a03a977ee144a00ee9b408fa65cc083e4881
Maximiliano Romero Piaggio <mromeropiaggio@lamansys.com.ar>
2024-11-20 15:55:45 -0300

    hsi-1653 Error al resetear password

--- back-end/sgx-auth/src/main/java/ar/lamansys/sgx/auth/user/domain/passwordreset/PasswordResetTokenStorage.java
+++ back-end/sgx-auth/src/main/java/ar/lamansys/sgx/auth/user/domain/passwordreset/PasswordResetTokenStorage.java
@@ -1,8 +1,10 @@
 package ar.lamansys.sgx.auth.user.domain.passwordreset;
 
+import ar.lamansys.sgx.auth.user.domain.passwordreset.exceptions.PasswordResetTokenStorageException;
+
 public interface PasswordResetTokenStorage {
 
-    PasswordResetTokenBo get(String token);
+    PasswordResetTokenBo get(String token) throws PasswordResetTokenStorageException;
 
     void disableTokens(Integer userId);
 
