bf1c897d3eac7082d15afe681c204e783af239de
FacundoBarreiros <facundobarreiros@gmail.com>
2024-12-06 15:02:30 -0300

    hsi-13529 [FE] Cambios en el api module

--- front-end/apps/projects/hospital/src/app/modules/orders/orders.module.ts
+++ front-end/apps/projects/hospital/src/app/modules/orders/orders.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CheckboxGroupComponent } from './components/checkbox-group/checkbox-group.component';
 import { HomeComponent } from './routes/home/home/home.component';
 import { WorkOrderListComponent } from './routes/home/work-order-list/work-order-list.component';
 import { CoreModule } from '@core/core.module';
@@ -9,6 +10,12 @@ import { PatientSummaryComponent } from '@hsi-components/patient-summary/patient
 import { PresentationModule } from '@presentation/presentation.module';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { StudyOrderWorkListDtoToOrderDetailsPipe } from './pipe/study-order-work-list-dto-to-order-details.pipe';
+import { FilterContainerComponent } from './components/filter-combo/filter-combo.component';
+import { FilterByPatientComponent } from './components/filter-by-patient/filter-by-patient.component';
+
+//Services
+import { FilterServiceService } from './services/filter-service.service';
+
 //Standalone Component
 import { IdentifierCasesComponent } from '@hsi-components/identifier-cases/identifier-cases.component';
 import { PatientLocationComponent } from '@hsi-components/patient-location/patient-location.component';
@@ -16,6 +23,9 @@ import { TemporaryPatientComponent } from '@hsi-components/temporary-patient/tem
 
 @NgModule({
 	declarations: [
+		CheckboxGroupComponent,
+		FilterContainerComponent,
+		FilterByPatientComponent,
 		HomeComponent,
 		OrderDetailComponent,
 		StudyOrderWorkListDtoToOrderDetailsPipe,
@@ -31,6 +41,11 @@ import { TemporaryPatientComponent } from '@hsi-components/temporary-patient/tem
 		IdentifierCasesComponent,
 		PatientLocationComponent,
 		TemporaryPatientComponent,
-	]
+	],
+	providers: [
+		FilterServiceService
+	],
+
+
 })
 export class OrdersModule { }
