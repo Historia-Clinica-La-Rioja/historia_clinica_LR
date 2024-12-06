3b52dd774811dd66bb0dee5e29081655a0d633f9
FacundoBarreiros <facundobarreiros@gmail.com>
2024-12-06 14:58:07 -0300

    hsi-13406 [FE] Conectar nueva lógica de filtros al BE

--- front-end/apps/projects/hospital/src/app/modules/api-rest/services/service-request-work-list-controller.service.ts
+++ front-end/apps/projects/hospital/src/app/modules/api-rest/services/service-request-work-list-controller.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { PageDto, StudyOrderWorkListDto } from '@api-rest/api-model';
+import { PageDto, StudyOrderWorkListDto, StudyOrderWorkListFilterDto } from '@api-rest/api-model';
 import { ContextService } from '@core/services/context.service';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
@@ -16,11 +16,12 @@ export class ServiceRequestWorkListControllerService {
 
 	) { }
 
-	getList(categories: string[], pageNumber: number, pageSize: number): Observable<PageDto<StudyOrderWorkListDto[]>> {
+	getList(categories: string[], pageNumber: number, pageSize: number, studyOrderWorkListFilterDto:StudyOrderWorkListFilterDto): Observable<PageDto<StudyOrderWorkListDto[]>> {
 		let params: HttpParams = new HttpParams()
 			.set('pageNumber', pageNumber)
 			.set('pageSize', pageSize)
-			.set('categories', categories.join(','));
+			.set('categories', categories.join(','))
+			.set('filter', JSON.stringify(studyOrderWorkListFilterDto));
 		const url = `${environment.apiBase}/institutions/${this.contextService.institutionId}/service-request-work-list`;
 		return this.http.get<PageDto<StudyOrderWorkListDto[]>>(url, { params });
 	}
