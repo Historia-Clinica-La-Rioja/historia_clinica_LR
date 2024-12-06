55d5f2bda26428798a390340771206157518239a
FacundoBarreiros <facundobarreiros@gmail.com>
2024-12-06 15:53:02 -0300

    hsi-13531 [FE] Cambios en el componente WorkOrderListComponent para soportar filtros

--- front-end/apps/projects/hospital/src/app/modules/orders/routes/home/work-order-list/work-order-list.component.ts
+++ front-end/apps/projects/hospital/src/app/modules/orders/routes/home/work-order-list/work-order-list.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
-import { PageDto, StudyOrderWorkListDto } from '@api-rest/api-model';
+import { PageDto, StudyOrderWorkListDto, StudyOrderWorkListFilterDto } from '@api-rest/api-model';
 import { ServiceRequestWorkListControllerService } from '@api-rest/services/service-request-work-list-controller.service';
 import { PatientType } from '@historia-clinica/constants/summaries';
 import { BehaviorSubject, Observable, } from 'rxjs';
+import { FilterServiceService } from '../../../services/filter-service.service';
 
 
 const PAGE_SIZE_OPTIONS = [5, 10, 25];
@@ -16,30 +17,50 @@ const EMERGENCY_CARE_TEMPORARY = PatientType.EMERGENCY_CARE_TEMPORARY;
 @Component({
 	selector: 'app-work-order-list',
 	templateUrl: './work-order-list.component.html',
-	styleUrls: ['./work-order-list.component.scss']
+	styleUrls: ['./work-order-list.component.scss'],
+
 })
 
-export class WorkOrderListComponent {
+export class WorkOrderListComponent implements OnInit {
 	EMERGENCY_CARE_TEMPORARY = EMERGENCY_CARE_TEMPORARY;
 	pageSizeOptions = PAGE_SIZE_OPTIONS;
 	pageSize: Observable<number>;
 	allOrders = [];
 	totalElementsAmount = 0;
 	groupedStudies: StudyOrderWorkListDto[] = [];
+	pageIndex = PAGE_INIT;
+	pageSizeCurrent = PAGE_MIN_SIZE;
 	private references = new BehaviorSubject<PageDto<StudyOrderWorkListDto[]>>(null);
+	private studyOrderWorkListFilterDto: StudyOrderWorkListFilterDto = {
+		"categories": ["108252007"],
+		"sourceTypeIds": [0, 4],
+		"studyTypeIds": [1, 2],
+		"requiresTransfer": false,
+		"notRequiresTransfer": true,
+		"patientTypeId": [1, 2, 3, 4, 5, 6, 7, 8]
+	};
 	readonly allOrders$ = this.references.asObservable();
 	constructor(
 		private serviceRequestWorkListControllerService: ServiceRequestWorkListControllerService,
+		private readonly filterServiceService: FilterServiceService,
 	) {
-		this.getOrders(PAGE_INIT, PAGE_MIN_SIZE)
+		this.getOrders(PAGE_INIT, PAGE_MIN_SIZE, this.studyOrderWorkListFilterDto);
+	}
+	ngOnInit() {
+		this.filterServiceService.filters$.subscribe((filter: StudyOrderWorkListFilterDto) => {
+			this.studyOrderWorkListFilterDto = filter;
+			this.getOrders(PAGE_INIT, PAGE_MIN_SIZE, this.studyOrderWorkListFilterDto);
+		});
 	}
 
 	onPageChange($event: PageEvent) {
-		this.getOrders($event.pageIndex, $event.pageSize);
+		this.pageIndex = $event.pageIndex;
+		this.pageSizeCurrent = $event.pageSize;
+		this.getOrders($event.pageIndex, $event.pageSize, this.studyOrderWorkListFilterDto);
 	}
 
-	private getOrders(pageNumber: number, pageSize: number) {
-		this.serviceRequestWorkListControllerService.getList([LABORATORIO], pageNumber, pageSize)
+	private getOrders(pageNumber: number, pageSize: number, studyOrderWorkListFilterDto) {
+		this.serviceRequestWorkListControllerService.getList([LABORATORIO], pageNumber, pageSize, this.studyOrderWorkListFilterDto)
 			.subscribe((allOrders: PageDto<StudyOrderWorkListDto[]>) => {
 				this.references.next(allOrders);
 				this.totalElementsAmount = allOrders.totalElementsAmount;
