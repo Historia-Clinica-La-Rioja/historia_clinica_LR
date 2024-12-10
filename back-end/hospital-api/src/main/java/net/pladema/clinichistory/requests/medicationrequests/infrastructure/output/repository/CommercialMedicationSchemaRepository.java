56ac123b3571bd971b446a247de67dcaefd6a46b
mmalyvarni <mateomv.mmv@gmail.com>
2024-12-10 13:24:19 -0300

    hsi-13612 [FIX] No se muestra error cuando el medicamento no tiene código Alfabeta

--- back-end/hospital-api/src/main/java/net/pladema/clinichistory/requests/medicationrequests/infrastructure/output/repository/CommercialMedicationSchemaRepository.java
+++ back-end/hospital-api/src/main/java/net/pladema/clinichistory/requests/medicationrequests/infrastructure/output/repository/CommercialMedicationSchemaRepository.java
@@ -16,9 +16,14 @@ public class CommercialMedicationSchemaRepository {
 		String queryString = "SELECT a.id " +
 				"FROM commercial_medication.article a " +
 				"WHERE a.snomed_id = :snomedSctid";
-		return (Integer) entityManager.createNativeQuery(queryString)
-				.setParameter("snomedSctid", snomedSctid)
-				.getSingleResult();
+		try {
+			return (Integer) entityManager.createNativeQuery(queryString)
+					.setParameter("snomedSctid", snomedSctid)
+					.getSingleResult();
+		}
+		catch (Exception e) {
+			return null;
+		}
 	}
 
 }
