4a158b1da4a82eba139f3b6406bb3f477dec8aba
larav <lvenere@lamansys.com.ar>
2024-12-11 12:01:04 -0300

    HSI-13631 Error en género - Lista de trabajo laboratorio

--- back-end/hospital-api/src/main/java/net/pladema/clinichistory/requests/servicerequests/controller/mapper/StudyOrderWorkListPatientMapper.java
+++ back-end/hospital-api/src/main/java/net/pladema/clinichistory/requests/servicerequests/controller/mapper/StudyOrderWorkListPatientMapper.java
@@ -1,5 +1,6 @@
 package net.pladema.clinichistory.requests.servicerequests.controller.mapper;
 
+import ar.lamansys.sgh.shared.infrastructure.input.service.GenderDto;
 import ar.lamansys.sgx.shared.dates.configuration.LocalDateMapper;
 
 import net.pladema.clinichistory.requests.servicerequests.controller.dto.StudyOrderBasicPatientDto;
@@ -7,11 +8,31 @@ import net.pladema.clinichistory.requests.servicerequests.controller.dto.StudyOr
 import net.pladema.clinichistory.requests.servicerequests.domain.StudyOrderBasicPatientBo;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Named;
+import org.mapstruct.Mapping;
 
-@Mapper(uses = {LocalDateMapper.class})
+@Mapper(uses = {LocalDateMapper.class, GenderDto.class})
 public interface StudyOrderWorkListPatientMapper {
 
-	@Named("toStudyOrderBasicPatientDto")
-	StudyOrderBasicPatientDto toStudyOrderBasicPatientDto(StudyOrderBasicPatientBo studyOrderBasicPatientBo);
+	@Mapping(target = "id", source = "id")
+	@Mapping(target = "firstName", source = "firstName")
+	@Mapping(target = "middleNames", source = "middleNames")
+	@Mapping(target = "lastName", source = "lastName")
+	@Mapping(target = "otherLastNames", source = "otherLastNames")
+	@Mapping(target = "identificationNumber", source = "identificationNumber")
+	@Mapping(target = "identificationTypeId", source = "identificationTypeId")
+	@Mapping(target = "gender", source = ".")
+	@Mapping(target = "birthDate", source = "birthDate")
+	@Mapping(target = "patientTypeId", source = "patientTypeId")
+	StudyOrderBasicPatientDto toPatientDto(StudyOrderBasicPatientBo patientBo);
+
+	@Mapping(target = "gender", source = "patientBo")
+	default GenderDto mapGender(StudyOrderBasicPatientBo patientBo) {
+		if (patientBo.getGenderId() == null && patientBo.getGenderDescription() == null) {
+			return null;
+		}
+		GenderDto genderDto = new GenderDto();
+		genderDto.setId(patientBo.getGenderId());
+		genderDto.setDescription(patientBo.getGenderDescription());
+		return genderDto;
+	}
 }
